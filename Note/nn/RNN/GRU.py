49a283195b0480e7a297b85f2b5a20779dc2d75c
7NoteDancing <63648431+7NoteDancing@users.noreply.github.com>
2020-04-15 13:51:37 +0800

    Update GRU.py

--- Note/nn/RNN/GRU.py
+++ Note/nn/RNN/GRU.py
@@ -104,9 +104,10 @@ class gru:
         self.test_accuracy=None
         self.normalize=None
         self.maximun=False
-        self.test_flag=False
-        self.flag=None
+        self.continue_train=None
         self.continue_flag=None
+        self.flag=None
+        self.test_flag=None
         self.time=None
         self.total_time=None
         self.cpu_gpu='/gpu:0'
