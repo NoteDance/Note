fdf28e7c8f0e21b4c1898b26ad9049e579e0f8fd
7NoteDancing <63648431+7NoteDancing@users.noreply.github.com>
2020-04-16 11:09:53 +0800

    Update GRU.py

--- Note/nn/RNN/GRU.py
+++ Note/nn/RNN/GRU.py
@@ -1078,9 +1078,10 @@ class gru:
         with self.graph.as_default():
             if cpu_gpu!=None:
                 self.use_cpu_gpu=cpu_gpu
+		use_cpu_gpu=self.use_cpu_gpu[-1]
             self.C.clear()
             self.h.clear()
-            with tf.device(self.cla_cpu_gpu):
+            with tf.device(use_cpu_gpu):
                 if self.normalize==True:
                     if self.maximun==True:
                         data/=np.max(data,axis=0)
@@ -1139,9 +1140,10 @@ class gru:
         with self.graph.as_default():
             if cpu_gpu!=None:
                 self.use_cpu_gpu=cpu_gpu
+		use_cpu_gpu=self.use_cpu_gpu[-1]
             self.C.clear()
             self.h.clear()
-            with tf.device(self.pre_cpu_gpu):
+            with tf.device(use_cpu_gpu):
                 if self.normalize==True:
                     if self.maximun==True:
                         data/=np.max(data,axis=0)
