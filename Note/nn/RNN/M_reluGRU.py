432a273f4320d0b6d87ac04f5f821a73c0fa94e5
7NoteDancing <63648431+7NoteDancing@users.noreply.github.com>
2020-04-16 11:10:14 +0800

    Update M_reluGRU.py

--- Note/nn/RNN/M_reluGRU.py
+++ Note/nn/RNN/M_reluGRU.py
@@ -1023,9 +1023,10 @@ class m_relugru:
         with self.graph.as_default():
             if cpu_gpu!=None:
                 self.use_cpu_gpu=cpu_gpu
+		use_cpu_gpu=self.use_cpu_gpu[-1]
             self.C.clear()
             self.h.clear()
-            with tf.device(self.cla_cpu_gpu):
+            with tf.device(use_cpu_gpu):
                 if self.normalize==True:
                     if self.maximun==True:
                         data/=np.max(data,axis=0)
@@ -1084,9 +1085,10 @@ class m_relugru:
         with self.graph.as_default():
             if cpu_gpu!=None:
                 self.use_cpu_gpu=cpu_gpu
+		use_cpu_gpu=self.use_cpu_gpu[-1]
             self.C.clear()
             self.h.clear()
-            with tf.device(self.pre_cpu_gpu):
+            with tf.device(use_cpu_gpu):
                 if self.normalize==True:
                     if self.maximun==True:
                         data/=np.max(data,axis=0)
