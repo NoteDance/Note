60aae99768cd5278b569a0d89782364dbf74c7c2
7NoteDancing <63648431+7NoteDancing@users.noreply.github.com>
2020-04-17 11:51:17 +0800

    Update RNN.py

--- Note/create/RNN.py
+++ Note/create/RNN.py
@@ -18,19 +18,19 @@ def rnn(data,weight,bias,stack=True):
         return h
     
     
-def lstm(data,weight,bias,stack=True):
+def lstm(data,weight_x,weight_h,bias,stack=True):
     C=[]
     h=[]
     if len(data.shape)==3:
-        fx=tf.einsum('ijk,kl->ijl',data,weight[0])
-        ix=tf.einsum('ijk,kl->ijl',data,weight[1])
-        ox=tf.einsum('ijk,kl->ijl',data,weight[2])
-        cx=tf.einsum('ijk,kl->ijl',data,weight[3])
+        fx=tf.einsum('ijk,kl->ijl',data,weight_x[0])
+        ix=tf.einsum('ijk,kl->ijl',data,weight_x[1])
+        ox=tf.einsum('ijk,kl->ijl',data,weight_x[2])
+        cx=tf.einsum('ijk,kl->ijl',data,weight_x[3])
     else:
-        fx=tf.matmul(data,weight[0])
-        ix=tf.matmul(data,weight[1])
-        ox=tf.matmul(data,weight[2])
-        cx=tf.matmul(data,weight[3])
+        fx=tf.matmul(data,weight_x[0])
+        ix=tf.matmul(data,weight_x[1])
+        ox=tf.matmul(data,weight_x[2])
+        cx=tf.matmul(data,weight_x[3])
     for i in range(int(data.shape[1])):
         if i==0:
             f=tf.nn.sigmoid(fx[:][:,i]+bias[0])
@@ -40,10 +40,10 @@ def lstm(data,weight,bias,stack=True):
             C.append(i*c)
             h.append(o*tf.nn.tanh(C[i]))
         else:
-            f=tf.nn.sigmoid(fx[:][:,i]+tf.matmul(h[i-1],weight[0])+bias[0])
-            i=tf.nn.sigmoid(ix[:][:,i]+tf.matmul(h[i-1],weight[1])+bias[1])
-            o=tf.nn.sigmoid(ox[:][:,i]+tf.matmul(h[i-1],weight[2])+bias[2])
-            c=tf.nn.tanh(cx[:][:,i]+tf.matmul(h[i-1],weight[3])+bias[3])
+            f=tf.nn.sigmoid(fx[:][:,i]+tf.matmul(h[i-1],weight_h[0])+bias[0])
+            i=tf.nn.sigmoid(ix[:][:,i]+tf.matmul(h[i-1],weight_h[1])+bias[1])
+            o=tf.nn.sigmoid(ox[:][:,i]+tf.matmul(h[i-1],weight_h[2])+bias[2])
+            c=tf.nn.tanh(cx[:][:,i]+tf.matmul(h[i-1],weight_h[3])+bias[3])
             C.append(f*C[i-1]+i*c)
             h.append(o*tf.nn.tanh(C[i]))
     if stack==True:
@@ -52,16 +52,16 @@ def lstm(data,weight,bias,stack=True):
         return h
 
 
-def gru(data,weight,bias,stack=True):
+def gru(data,weight_x,weight_h,bias,stack=True):
     h=[]
     if len(data.shape)==3:
-        ux=tf.einsum('ijk,kl->ijl',data,weight[0])
-        rx=tf.einsum('ijk,kl->ijl',data,weight[1])
-        cx=tf.einsum('ijk,kl->ijl',data,weight[2])
+        ux=tf.einsum('ijk,kl->ijl',data,weight_x[0])
+        rx=tf.einsum('ijk,kl->ijl',data,weight_x[1])
+        cx=tf.einsum('ijk,kl->ijl',data,weight_x[2])
     else:
-        ux=tf.matmul(data,weight[0])
-        rx=tf.matmul(data,weight[1])
-        cx=tf.matmul(data,weight[2]) 
+        ux=tf.matmul(data,weight_x[0])
+        rx=tf.matmul(data,weight_x[1])
+        cx=tf.matmul(data,weight_x[2]) 
     for i in range(int(data.shape[1])):
         if i==0:
             u=tf.nn.sigmoid(ux[:][:,i]+bias[0])
@@ -69,9 +69,9 @@ def gru(data,weight,bias,stack=True):
             c=tf.nn.tanh(cx[:][:,i]+bias[2])
             h.append((1-u)*c)
         else:
-            u=tf.nn.sigmoid(ux[:][:,i]+tf.matmul(h[i-1],weight[0])+bias[0])
-            r=tf.nn.sigmoid(rx[:][:,i]+tf.matmul(h[i-1],weight[1])+bias[1])
-            c=tf.nn.tanh(cx[:][:,i]+tf.matmul(r*h[i-1],weight[2])+bias[2])
+            u=tf.nn.sigmoid(ux[:][:,i]+tf.matmul(h[i-1],weight_h[0])+bias[0])
+            r=tf.nn.sigmoid(rx[:][:,i]+tf.matmul(h[i-1],weight_h[1])+bias[1])
+            c=tf.nn.tanh(cx[:][:,i]+tf.matmul(r*h[i-1],weight_h[2])+bias[2])
             h.append(u*h[i-1]+(1-u)*c)
     if stack==True:
         return tf.stack(h,axis=1)
@@ -79,14 +79,14 @@ def gru(data,weight,bias,stack=True):
         return h
 
 
-def m_relugru(data,weight,bias,stack=True):
+def m_relugru(data,weight_x,weight_h,bias,stack=True):
     h=[]
     if len(data.shape)==3:
-        ux=tf.einsum('ijk,kl->ijl',data,weight[0])
-        cx=tf.einsum('ijk,kl->ijl',data,weight[1])
+        ux=tf.einsum('ijk,kl->ijl',data,weight_x[0])
+        cx=tf.einsum('ijk,kl->ijl',data,weight_x[1])
     else:
-        ux=tf.matmul(data,weight[0])
-        cx=tf.matmul(data,weight[1])
+        ux=tf.matmul(data,weight_x[0])
+        cx=tf.matmul(data,weight_x[1])
     for i in range(int(data.shape[1])):
         if i==0:
             u=tf.nn.sigmoid(ux[:][:,i]+bias[0])
@@ -95,8 +95,8 @@ def m_relugru(data,weight,bias,stack=True):
             c/=tf.math.reduce_std(c,axis=0)
             h.append((1-u)*c)
         else:
-            u=tf.nn.sigmoid(ux[:][:,i]+tf.matmul(h[i-1],weight[0])+bias[0])
-            c=tf.nn.relu(cx[:][:,i]+tf.matmul(h[i-1],weight[1])+bias[1])
+            u=tf.nn.sigmoid(ux[:][:,i]+tf.matmul(h[i-1],weight_h[0])+bias[0])
+            c=tf.nn.relu(cx[:][:,i]+tf.matmul(h[i-1],weight_h[1])+bias[1])
             c-=tf.reduce_mean(c,axis=0)
             c/=tf.math.reduce_std(c,axis=0)
             h.append(u*h[i-1]+(1-u)*c)
@@ -104,6 +104,128 @@ def m_relugru(data,weight,bias,stack=True):
         return tf.stack(h,axis=1)
     else:
         return h
+
+
+def rnn_weight(shape,mean=0,stddev=0.07,dtype=tf.float32,name=None):
+    return tf.Variable(tf.random.normal(shape=shape,mean=mean,stddev=stddev,dtype=dtype),name=name)
+
+
+def rnn_bias(shape,mean=0,stddev=0.07,dtype=tf.float32,name=None):
+    return tf.Variable(tf.random.normal(shape=shape,mean=mean,stddev=stddev,dtype=dtype),name=name)
+
+
+def lstm_weight_x(shape,mean=0,stddev=0.07,dtype=tf.float32,name=None):
+    if name==None:
+        fg_weight_x=tf.Variable(tf.random.normal(shape=shape,mean=mean,stddev=stddev,dtype=dtype),name=name)
+        ig_weight_x=tf.Variable(tf.random.normal(shape=shape,mean=mean,stddev=stddev,dtype=dtype),name=name)
+        og_weight_x=tf.Variable(tf.random.normal(shape=shape,mean=mean,stddev=stddev,dtype=dtype),name=name)
+        cltm_weight_x=tf.Variable(tf.random.normal(shape=shape,mean=mean,stddev=stddev,dtype=dtype),name=name)
+    else:
+        fg_weight_x=tf.Variable(tf.random.normal(shape=shape,mean=mean,stddev=stddev,dtype=dtype),name=name[0])
+        ig_weight_x=tf.Variable(tf.random.normal(shape=shape,mean=mean,stddev=stddev,dtype=dtype),name=name[1])
+        og_weight_x=tf.Variable(tf.random.normal(shape=shape,mean=mean,stddev=stddev,dtype=dtype),name=name[2])
+        cltm_weight_x=tf.Variable(tf.random.normal(shape=shape,mean=mean,stddev=stddev,dtype=dtype),name=name[3])
+    return fg_weight_x,ig_weight_x,og_weight_x,cltm_weight_x
+
+
+def lstm_weight_h(shape,mean=0,stddev=0.07,dtype=tf.float32,name=None):
+    if name==None:
+        fg_weight_h=tf.Variable(tf.random.normal(shape=shape,mean=mean,stddev=stddev,dtype=dtype),name=name)
+        ig_weight_h=tf.Variable(tf.random.normal(shape=shape,mean=mean,stddev=stddev,dtype=dtype),name=name)
+        og_weight_h=tf.Variable(tf.random.normal(shape=shape,mean=mean,stddev=stddev,dtype=dtype),name=name)
+        cltm_weight_h=tf.Variable(tf.random.normal(shape=shape,mean=mean,stddev=stddev,dtype=dtype),name=name)
+    else:
+        fg_weight_h=tf.Variable(tf.random.normal(shape=shape,mean=mean,stddev=stddev,dtype=dtype),name=name[0])
+        ig_weight_h=tf.Variable(tf.random.normal(shape=shape,mean=mean,stddev=stddev,dtype=dtype),name=name[1])
+        og_weight_h=tf.Variable(tf.random.normal(shape=shape,mean=mean,stddev=stddev,dtype=dtype),name=name[2])
+        cltm_weight_h=tf.Variable(tf.random.normal(shape=shape,mean=mean,stddev=stddev,dtype=dtype),name=name[3])
+    return fg_weight_h,ig_weight_h,og_weight_h,cltm_weight_h
+
+
+def lstm_bias(shape,mean=0,stddev=0.07,dtype=tf.float32,name=None):
+    if name==None:
+        fg_bias=tf.Variable(tf.random.normal(shape=shape,mean=mean,stddev=stddev,dtype=dtype),name=name)
+        ig_bias=tf.Variable(tf.random.normal(shape=shape,mean=mean,stddev=stddev,dtype=dtype),name=name)
+        og_bias=tf.Variable(tf.random.normal(shape=shape,mean=mean,stddev=stddev,dtype=dtype),name=name)
+        cltm_bias=tf.Variable(tf.random.normal(shape=shape,mean=mean,stddev=stddev,dtype=dtype),name=name)
+    else:
+        fg_bias=tf.Variable(tf.random.normal(shape=shape,mean=mean,stddev=stddev,dtype=dtype),name=name[0])
+        ig_bias=tf.Variable(tf.random.normal(shape=shape,mean=mean,stddev=stddev,dtype=dtype),name=name[1])
+        og_bias=tf.Variable(tf.random.normal(shape=shape,mean=mean,stddev=stddev,dtype=dtype),name=name[2])
+        cltm_bias=tf.Variable(tf.random.normal(shape=shape,mean=mean,stddev=stddev,dtype=dtype),name=name[3])
+    return fg_bias,ig_bias,og_bias,cltm_bias
+
+
+def gru_weight_x(shape,mean=0,stddev=0.07,dtype=tf.float32,name=None):
+    if name==None:
+        ug_weight_x=tf.Variable(tf.random.normal(shape=shape,mean=mean,stddev=stddev,dtype=dtype),name=name)
+        rg_weight_x=tf.Variable(tf.random.normal(shape=shape,mean=mean,stddev=stddev,dtype=dtype),name=name)
+        cltm_weight_x=tf.Variable(tf.random.normal(shape=shape,mean=mean,stddev=stddev,dtype=dtype),name=name)
+    else:
+        ug_weight_x=tf.Variable(tf.random.normal(shape=shape,mean=mean,stddev=stddev,dtype=dtype),name=name[0])
+        rg_weight_x=tf.Variable(tf.random.normal(shape=shape,mean=mean,stddev=stddev,dtype=dtype),name=name[1])
+        cltm_weight_x=tf.Variable(tf.random.normal(shape=shape,mean=mean,stddev=stddev,dtype=dtype),name=name[3])
+    return ug_weight_x,rg_weight_x,cltm_weight_x
+
+
+def gru_weight_h(shape,mean=0,stddev=0.07,dtype=tf.float32,name=None):
+    if name==None:
+        ug_weight_h=tf.Variable(tf.random.normal(shape=shape,mean=mean,stddev=stddev,dtype=dtype),name=name)
+        rg_weight_h=tf.Variable(tf.random.normal(shape=shape,mean=mean,stddev=stddev,dtype=dtype),name=name)
+        cltm_weight_h=tf.Variable(tf.random.normal(shape=shape,mean=mean,stddev=stddev,dtype=dtype),name=name)
+    else:
+        ug_weight_h=tf.Variable(tf.random.normal(shape=shape,mean=mean,stddev=stddev,dtype=dtype),name=name[0])
+        rg_weight_h=tf.Variable(tf.random.normal(shape=shape,mean=mean,stddev=stddev,dtype=dtype),name=name[1])
+        cltm_weight_h=tf.Variable(tf.random.normal(shape=shape,mean=mean,stddev=stddev,dtype=dtype),name=name[3])
+    return ug_weight_h,rg_weight_h,cltm_weight_h
+
+
+def gru_bias(shape,mean=0,stddev=0.07,dtype=tf.float32,name=None):
+    if name==None:
+        ug_bias=tf.Variable(tf.random.normal(shape=shape,mean=mean,stddev=stddev,dtype=dtype),name=name)
+        rg_bias=tf.Variable(tf.random.normal(shape=shape,mean=mean,stddev=stddev,dtype=dtype),name=name)
+        cltm_bias=tf.Variable(tf.random.normal(shape=shape,mean=mean,stddev=stddev,dtype=dtype),name=name)
+    else:
+        ug_bias=tf.Variable(tf.random.normal(shape=shape,mean=mean,stddev=stddev,dtype=dtype),name=name[0])
+        rg_bias=tf.Variable(tf.random.normal(shape=shape,mean=mean,stddev=stddev,dtype=dtype),name=name[1])
+        cltm_bias=tf.Variable(tf.random.normal(shape=shape,mean=mean,stddev=stddev,dtype=dtype),name=name[3])
+    return ug_bias,rg_bias,cltm_bias
+
+
+def m_relugru_weight_x(shape,mean=0,stddev=0.07,dtype=tf.float32,name=None):
+    if name==None:
+        ug_weight_x=tf.Variable(tf.random.normal(shape=shape,mean=mean,stddev=stddev,dtype=dtype),name=name)
+        rg_weight_x=tf.Variable(tf.random.normal(shape=shape,mean=mean,stddev=stddev,dtype=dtype),name=name)
+        cltm_weight_x=tf.Variable(tf.random.normal(shape=shape,mean=mean,stddev=stddev,dtype=dtype),name=name)
+    else:
+        ug_weight_x=tf.Variable(tf.random.normal(shape=shape,mean=mean,stddev=stddev,dtype=dtype),name=name[0])
+        rg_weight_x=tf.Variable(tf.random.normal(shape=shape,mean=mean,stddev=stddev,dtype=dtype),name=name[1])
+        cltm_weight_x=tf.Variable(tf.random.normal(shape=shape,mean=mean,stddev=stddev,dtype=dtype),name=name[3])
+    return ug_weight_x,rg_weight_x,cltm_weight_x
+
+
+def m_relugru_weight_h(shape,mean=0,stddev=0.07,dtype=tf.float32,name=None):
+    if name==None:
+        ug_weight_h=tf.Variable(tf.random.normal(shape=shape,mean=mean,stddev=stddev,dtype=dtype),name=name)
+        rg_weight_h=tf.Variable(tf.random.normal(shape=shape,mean=mean,stddev=stddev,dtype=dtype),name=name)
+        cltm_weight_h=tf.Variable(tf.random.normal(shape=shape,mean=mean,stddev=stddev,dtype=dtype),name=name)
+    else:
+        ug_weight_h=tf.Variable(tf.random.normal(shape=shape,mean=mean,stddev=stddev,dtype=dtype),name=name[0])
+        rg_weight_h=tf.Variable(tf.random.normal(shape=shape,mean=mean,stddev=stddev,dtype=dtype),name=name[1])
+        cltm_weight_h=tf.Variable(tf.random.normal(shape=shape,mean=mean,stddev=stddev,dtype=dtype),name=name[3])
+    return ug_weight_h,rg_weight_h,cltm_weight_h
+
+
+def m_relugru_bias(shape,mean=0,stddev=0.07,dtype=tf.float32,name=None):
+    if name==None:
+        ug_bias=tf.Variable(tf.random.normal(shape=shape,mean=mean,stddev=stddev,dtype=dtype),name=name)
+        rg_bias=tf.Variable(tf.random.normal(shape=shape,mean=mean,stddev=stddev,dtype=dtype),name=name)
+        cltm_bias=tf.Variable(tf.random.normal(shape=shape,mean=mean,stddev=stddev,dtype=dtype),name=name)
+    else:
+        ug_bias=tf.Variable(tf.random.normal(shape=shape,mean=mean,stddev=stddev,dtype=dtype),name=name[0])
+        rg_bias=tf.Variable(tf.random.normal(shape=shape,mean=mean,stddev=stddev,dtype=dtype),name=name[1])
+        cltm_bias=tf.Variable(tf.random.normal(shape=shape,mean=mean,stddev=stddev,dtype=dtype),name=name[3])
+    return ug_bias,rg_bias,cltm_bias
 
 
 def embedding(data,embedding_w,embedding_b):
@@ -113,10 +235,7 @@ def embedding(data,embedding_w,embedding_b):
 def attention(en_h,de_h,attention_w1,attention_w2,attention_w3):
     if type(en_h)==list:
         stack_en_h=tf.stack(en_h,axis=1)
-    if type(de_h)==tf.Tensor:
-        length=int(de_h.shape[1])
-    else:
-        length=len(de_h)
+    length=int(de_h.shape[1])
     attention_vector=[]
     context_vector=[]
     for i in range(length):
@@ -125,8 +244,6 @@ def attention(en_h,de_h,attention_w1,attention_w2,attention_w3):
         else:
             de_h=de_h[i]
         score=tf.einsum('ijk,kl->ijl',tf.nn.tanh(tf.expand_dims(tf.matmul(de_h,attention_w1),axis=1)+tf.einsum('ijk,kl->ijl',stack_en_h,attention_w2)),attention_w3)
-        score=tf.reduce_mean(score,axis=2)
-        score=tf.expand_dims(score,axis=2)
         attention_weights=tf.nn.softmax(score,axis=1)
         context_vector.append(tf.reduce_mean(attention_weights*stack_en_h,axis=1))
         attention_vector.append(tf.concat([de_h,context_vector[i]],axis=1))
