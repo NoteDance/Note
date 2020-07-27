dce7eea1c37c88321ac3c09574963ec83c2e7139
7NoteDancing <63648431+7NoteDancing@users.noreply.github.com>
2020-07-27 22:21:21 +0800

    Update Note Architecture.py

--- Note/create/Note Architecture.py
+++ Note/create/Note Architecture.py
@@ -9,39 +9,6 @@ import pickle
 import time
 
 
-def write_data(data,path):
-    output_file=open(path,'wb')
-    pickle.dump(data,output_file)
-    output_file.close()
-    return
-
-
-def read_data(path,dtype=np.float32):
-    input_file=open(path,'rb')
-    data=pickle.load(input_file)
-    return np.array(data,dtype=dtype)
-
-
-def write_data_csv(data,path,dtype=None,index=False,header=False):
-    if dtype==None:
-        data=pd.DataFrame(data)
-        data.to_csv(path,index=index,header=header)
-    else:
-        data=np.array(data,dtype=dtype)
-        data=pd.DataFrame(data)
-        data.to_csv(path,index=index,header=header)
-    return
-        
-
-def read_data_csv(path,dtype=None,header=None):
-    if dtype==None:
-        data=pd.read_csv(path,header=header)
-        return np.array(data)
-    else:
-        data=pd.read_csv(path,header=header)
-        return np.array(data,dtype=dtype)
-
-
 class unnamed:
     def __init__():
         with tf.name_scope('data'):     
