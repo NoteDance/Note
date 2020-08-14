75c147b6b6cc2aa65ecee816523009eeb4c1a079
7NoteDancing <63648431+7NoteDancing@users.noreply.github.com>
2020-08-14 21:05:48 +0800

    Update M_reluGRU.py

--- Note/nn/RNN/M_reluGRU.py
+++ Note/nn/RNN/M_reluGRU.py
@@ -6,39 +6,6 @@ import pickle
 import time
 
 
-def write_data(data,path):
-    output_file=open(path,'wb')
-    pickle.dump(data,output_file)
-    output_file.close()
-    return
-
-
-def read_data(path,dtype=np.float32):
-    input_file=open(path,'rb')
-    data=pickle.load(input_file)
-    return np.array(data,dtype=dtype)
-
-
-def write_data_csv(data,path,dtype=None,index=False,header=False):
-    if dtype==None:
-        data=pd.DataFrame(data)
-        data.to_csv(path,index=index,header=header)
-    else:
-        data=np.array(data,dtype=dtype)
-        data=pd.DataFrame(data)
-        data.to_csv(path,index=index,header=header)
-    return
-        
-
-def read_data_csv(path,dtype=None,header=None):
-    if dtype==None:
-        data=pd.read_csv(path,header=header)
-        return np.array(data)
-    else:
-        data=pd.read_csv(path,header=header)
-        return np.array(data,dtype=dtype)
-
-
 class m_relugru:
     def __init__(self,train_data=None,train_labels=None,test_data=None,test_labels=None):
         self.graph=tf.Graph()
