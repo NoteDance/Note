c337902d95c9235903badc10b05e642c39793008
7NoteDancing <63648431+7NoteDancing@users.noreply.github.com>
2020-08-30 12:37:01 +0800

    Update Note Architecture.py

--- Note/create/Note Architecture.py
+++ Note/create/Note Architecture.py
@@ -239,10 +239,7 @@ class unnamed:
                 self.time=int(t2-t1)
             else:
                 self.time=int(t2-t1)+1
-            if continue_train!=True:
-                self.total_time=self.time
-            else:
-                self.total_time+=self.time
+            self.total_time+=self.time
             print()
             print('last loss:{0:.6f}'.format(self.train_loss))
             if len(self.labels_shape)==2:
