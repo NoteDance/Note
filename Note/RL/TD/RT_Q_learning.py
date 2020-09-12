c15b1ffaa91ea9cff4a0c80545df4f3347c01901
7NoteDancing <63648431+7NoteDancing@users.noreply.github.com>
2020-09-12 11:16:13 +0800

    Update RT_Q_learning.py

--- Note/RL/TD/RT_Q_learning.py
+++ Note/RL/TD/RT_Q_learning.py
@@ -4,11 +4,12 @@ import time
 
 
 class RT_Q_learning:
-    def __init__(self,q,state,action,search_space,epsilon,alpha,discount,dst,episode_step=None,save_episode=True):
+    def __init__(self,q,state,action,action_name,search_space,epsilon,alpha,discount,dst,episode_step=None,save_episode=True):
         self.q=q
         self.episode=[]
         self.state=state
         self.action=action
+        self.action_name=action_name
         self.search_space=search_space
         self.epsilon=epsilon
         self.alpha=alpha
@@ -18,36 +19,36 @@ class RT_Q_learning:
         self.save_episode=save_episode
 
 
-    def epsilon_greedy_policy(self,q,state,action):
-        action_prob=np.ones(len(action),dtype=np.float32)
-        action_prob=action_prob*self.epsilon/len(action)
-        best_action=np.argmax(q[state])
-        action_prob[best_action]+=1-self.epsilon
+    def epsilon_greedy_policy(self,q,s,action):
+        action_prob=action[s]
+        action_prob=action_prob*self.epsilon/np.sum(action[s])
+        best_a=np.argmax(q[s])
+        action_prob[best_a]+=1-self.epsilon
         return action_prob
     
     
-    def td(self,q,state,action,next_state,reward):
-        q[state][action]=q[state][action]+self.alpha*(reward+self.discount*np.max(q[next_state])-q[state][action])
-        return q,next_state
+    def td(self,q,s,a,next_s,reward):
+        q[s][a]=q[s][a]+self.alpha*(reward+self.discount*np.max(q[next_s])-q[s][a])
+        return q,next_s
     
     
-    def RT_update_q(self,q,state):
+    def RT_update_q(self,q,s):
         a=0
         delta=0
         while True:
             t1=time.time()
             a+=1
-            action_prob=self.epsilon_greedy_policy(q,state,self.action)
-            action=np.random.choice(np.arange(action_prob.shape[0]),p=action_prob)
-            next_state,reward,end=self.search_space[self.state[state]][self.action[action]]
-            temp=q[state][action]
-            delta+=np.abs(q[state][action]-temp)
-            q,next_state=self.td(q,reward,state,next_state,action)
-            state=next_state
+            action_prob=self.epsilon_greedy_policy(q,s,self.action)
+            a=np.random.choice(np.arange(action_prob.shape[0]),p=action_prob)
+            next_s,reward,end=self.search_space[self.state[s]][self.action[a]]
+            temp=q[s][a]
+            delta+=np.abs(q[s][a]-temp)
+            q,next_s=self.td(q,s,a,next_s,reward)
+            s=next_s
             self.dst[0]=delta/self.dst[1]
             self.dst[1]+=1
             if self.save_episode==True and a<=self.episode_step:
-                self.episode.append([self.state[state],self.action[action],reward])
+                self.episode.append([self.state[s],self.action[a],reward])
             t2=time.time()
             _time=t2-t1
             self.dst[2]+=_time
@@ -55,9 +56,9 @@ class RT_Q_learning:
     
     
     def learn(self):
-        if len(self.state_list)>self.q.shape[0] or len(self.action)>self.q.shape[1]:
-            q=self.q*tf.ones([len(self.state),len(self.action)],dtype=tf.float32)[:self.q.shape[0],:self.q.shape[1]]
+        if len(self.state)>self.q.shape[0] or len(self.action_name)>self.q.shape[1]:
+            q=self.q*tf.ones([len(self.state),len(self.action_name)],dtype=tf.float32)[:self.q.shape[0],:self.q.shape[1]]
             self.q=q.numpy()
-        state=np.random.choice(np.arange(len(self.state)),p=np.ones(len(self.state))*1/len(self.state))
-        self.q=self.RT_update_q(self.q,state)
+        s=np.random.choice(np.arange(len(self.state)),p=np.ones(len(self.state))*1/len(self.state))
+        self.q=self.RT_update_q(self.q,s)
         return
