57ae9d9e29858bc645c11ff015b9b590d057b8ab
7NoteDancing <63648431+7NoteDancing@users.noreply.github.com>
2020-09-24 12:18:21 +0800

    Update RT_Sarsa.py

--- Note/RL/TD/RT_Sarsa.py
+++ Note/RL/TD/RT_Sarsa.py
@@ -4,11 +4,10 @@ import time
 
 
 class RT_Sarsa:
-    def __init__(self,q,state_name,action,action_name,search_space,epsilon,alpha,discount,dst,episode_step=None,save_episode=True):
+    def __init__(self,q,state_name,action_name,search_space,epsilon,alpha,discount,dst,episode_step=None,save_episode=True):
         self.q=q
         self.episode=[]
         self.state_name=state_name
-        self.action=action
         self.action_name=action_name
         self.search_space=search_space
         self.epsilon=epsilon
@@ -19,28 +18,28 @@ class RT_Sarsa:
         self.save_episode=save_episode
 
 
-    def epsilon_greedy_policy(self,q,s,action):
-        action_prob=action[s]
-        action_prob=action_prob*self.epsilon/np.sum(action[s])
+    def epsilon_greedy_policy(self,q,s,action_p):
+        action_prob=action_p[s]
+        action_prob=action_prob*self.epsilon/np.sum(action_p[s])
         best_a=np.argmax(q[s])
         action_prob[best_a]+=1-self.epsilon
         return action_prob
     
     
-    def td(self,q,s,a,next_s,reward):
-        action_prob=self.epsilon_greedy_policy(q,next_s,self.action)
+    def td(self,q,s,a,next_s,reward,action_p):
+        action_prob=self.epsilon_greedy_policy(q,next_s,action_p)
         next_a=np.random.choice(np.arange(action_prob.shape[0]),p=action_prob)
         q[s][a]=q[s][a]+self.alpha*(reward+self.discount*q[next_s][next_a]-q[s][a])
         return q,next_s
     
     
-    def RT_update_q(self,q,s):
+    def RT_update_q(self,q,s,action_p):
         a=0
         delta=0
         while True:
             t1=time.time()
             a+=1
-            action_prob=self.epsilon_greedy_policy(q,s,self.action)
+            action_prob=self.epsilon_greedy_policy(q,s,action_p)
             a=np.random.choice(np.arange(action_prob.shape[0]),p=action_prob)
             next_s,reward,end=self.search_space[self.state_name[s]][self.action_name[a]]
             temp=q[s][a]
@@ -58,9 +57,10 @@ class RT_Sarsa:
     
     
     def learn(self):
+        action_prob=np.ones(len(self.action_name),dtype=np.int8)
         if len(self.state_name)>self.q.shape[0] or len(self.action_name)>self.q.shape[1]:
             q=self.q*tf.ones([len(self.state_name),len(self.action_name)],dtype=self.q.dtype)[:self.q.shape[0],:self.q.shape[1]]
             self.q=q.numpy()
         s=np.random.choice(np.arange(len(self.state_name)),p=np.ones(len(self.state_name))*1/len(self.state_name))
-        self.q=self.RT_update_q(self.q,s)
+        self.q=self.RT_update_q(self.q,s,action_prob)
         return
