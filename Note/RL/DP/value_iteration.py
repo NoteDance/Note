c31951cd694f21020d3735f3f90cbf33762b2da9
7NoteDancing <63648431+7NoteDancing@users.noreply.github.com>
2020-10-30 23:45:40 +0800

    Update value_iteration.py

--- Note/RL/DP/value_iteration.py
+++ Note/RL/DP/value_iteration.py
@@ -9,6 +9,8 @@ class value_iteration:
         self.state_name=state_name
         self.action_name=action_name
         self.prs=prs
+        self.state_len=len(self.state_name)
+        self.action_len=len(self.action_name)
         self.discount=discount
         self.theta=theta
         self.delta=0
@@ -17,27 +19,30 @@ class value_iteration:
         self.total_iteration_sum=0
         self.time=0
         self.total_time=0
-        
+    
+    
+    def init(self):
+        self.V=np.zeros(len(self.state_name),dtype=np.float16)
+        self.A=np.zeros(len(self.action_name),dtype=np.float16)
+        return
     
     def learn(self,iteration=None,path=None,one=True):
         if iteration==None:
             iteration=int(len(self.state_name)*3)
         self.delta=0
-        V=np.zeros(len(self.state_name),dtype=np.float16)
-        A=np.zeros(len(self.action_name),dtype=np.float16)
         for i in range(iteration):
             t1=time.time()
             delta=0
             for s in range(len(self.state_name)):
                 for a in range(len(self.action_name)):
                     for prob,r,next_s,done in self.prs[self.state_name[s]][self.action_name[a]]:
-                        A[a]+=prob*(r+self.discount*V[next_s])
+                        self.A[a]+=prob*(r+self.discount*self.V[next_s])
                         if done and next_s!=self.end_flag and self.end_flag!=None:
-                            A[a]=float('-inf')
+                            self.A[a]=float('-inf')
                             break
-                best_action_value=max(A)
-                delta=max(delta,np.abs(best_action_value-V[s]))
-                V[s]=best_action_value
+                best_action_value=max(self.A)
+                delta=max(delta,np.abs(best_action_value-self.V[s]))
+                self.V[s]=best_action_value
             if iteration%10!=0:
                 temp=iteration-iteration%10
                 temp=int(temp/10)
@@ -69,8 +74,8 @@ class value_iteration:
             for s in range(len(self.state_name)):
                 for a in range(len(self.action_name)):
                     for prob,r,next_s in self.prs[self.state_name[s]][self.action_name[a]]:
-                        A[a]+=prob*(r+self.discount*V[next_s])
-                best_a=np.argmax(A)
+                        self.A[a]+=prob*(r+self.discount*self.V[next_s])
+                best_a=np.argmax(self.A)
                 self.policy[self.state_name[s]][best_a]=1
         return
     
@@ -80,6 +85,10 @@ class value_iteration:
             output_file=open(path+'.dat','wb')
         else:
             output_file=open(path+'-{0}.dat'.format(i+1),'wb')
+        pickle.dump(self.state_len,output_file)
+        pickle.dump(self.action_len,output_file)
+        pickle.dump(self.V,output_file)
+        pickle.dump(self.A,output_file)
         pickle.dump(self.discount,output_file)
         pickle.dump(self.theta,output_file)
         pickle.dump(self.delta,output_file)
@@ -92,6 +101,12 @@ class value_iteration:
     
     def restore(self,path):
         input_file=open(path,'rb')
+        self.state_len=pickle.load(input_file)
+        self.action_len=pickle.load(input_file)
+        if self.state_len==len(self.state_name):
+            self.V=pickle.load(input_file)
+        if self.action_len==len(self.action_name):
+            self.A=pickle.load(input_file)
         self.discount=pickle.load(input_file)
         self.theta=pickle.load(input_file)
         self.delta=pickle.load(input_file)
