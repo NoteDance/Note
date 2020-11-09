63e67323dd2dcfc60bbccc1b38033a0a1907ad28
7NoteDancing <63648431+7NoteDancing@users.noreply.github.com>
2020-11-09 12:04:55 +0800

    Add files via upload

---
+++ Note/RL/TD/Q_learning.py
@@ -0,0 +1,171 @@
+import numpy as np
+import pickle
+import time
+
+
+class Q_learning:
+    def __init__(self,q,state_name,action_name,search_space,epsilon=None,alpha=None,discount=None,theta=None,episode_step=None,save_episode=True):
+        self.q=q
+        self.episode=[]
+        self.state_name=state_name
+        self.action_name=action_name
+        self.search_space=search_space
+        self.state_len=len(self.state_name)
+        self.action_len=len(self.action_name)
+        self.epsilon=epsilon
+        self.alpha=alpha
+        self.discount=discount
+        self.theta=theta
+        self.episode_step=episode_step
+        self.save_episode=save_episode
+        self.delta=0
+        self.episode_num=0
+        self.epi_num=0
+        self.total_episode=0
+        self.time=0
+        self.total_time=0
+        
+        
+    def init(self,dtype=np.int32):
+        t3=time.time()
+        if len(self.state_name)>self.state_len:
+            self.state=np.concatenate(self.state,np.arange(len(self.state_name)-self.state_len,dtype=dtype)+len(self.state_len))
+            self.state_one=np.concatenate(self.state_one,np.ones(len(self.state_name)-self.state_len,dtype=dtype))
+            self.state_prob=self.state_one/len(self.state_name)
+            self.action=np.concatenate(self.action,np.arange(len(self.action_name)-self.action_len,dtype=dtype)+len(self.action_len))
+            self.action_prob=np.concatenate(self.action_prob,np.ones(len(self.action_name)-self.action_len,dtype=dtype))
+        else:
+            self.state=np.arange(len(self.state_name),dtype=dtype)
+            self.state_one=np.ones(len(self.state_name),dtype=dtype)
+            self.state_prob=self.state_one/len(self.state_name)
+            self.action=np.arange(len(self.action_name),dtype=dtype)
+            self.action_prob=np.ones(len(self.action_name),dtype=dtype)
+        if len(self.state_name)>self.q.shape[0] or len(self.action_name)>self.q.shape[1]:
+            self.q=np.concatenate([self.q,np.zeros([len(self.state_name),len(self.action_name)-self.action_len],dtype=self.q.dtype)],axis=1)
+            self.q=np.concatenate([self.q,np.zeros([len(self.state_name)-self.state_len,len(self.action_name)],dtype=self.q.dtype)])
+            self.q=self.q.numpy()
+        t4=time.time()
+        self.time+=t4-t3
+        return
+
+
+    def epsilon_greedy_policy(self,q,s,action_p):
+        action_prob=action_p
+        action_prob=action_prob*self.epsilon/np.sum(action_p)
+        best_a=np.argmax(q[s])
+        action_prob[best_a]+=1-self.epsilon
+        return action_prob
+    
+    
+    def td(self,q,episode):
+        for s,a,next_s,r in episode:
+            q[s][a]=q[s][a]+self.alpha*(r+self.discount*np.max(q[next_s])-q[s][a])
+        return q
+    
+    
+    def _episode(self,q,s,action,action_p):
+        a=0
+        episode=[]
+        _episode=[]
+        if self.episode_step==None:
+            while True:
+                action_prob=self.epsilon_greedy_policy(q,s,action_p)
+                a=np.random.choice(action,p=action_prob)
+                next_s,r,end=self.search_space[self.state_name[s]][self.action_name[a]]
+                temp=q[s][a]
+                self.delta+=np.abs(q[s][a]-temp)
+                if end:
+                    self.delta+=self.delta/a
+                    if self.save_episode==True:
+                        episode.append([self.state_name[s],self.action_name[a],r,end])
+                    _episode.append([s,a,next_s,r])
+                    break
+                if self.save_episode==True:
+                    episode.append([self.state_name[s],self.action_name[a],r])
+                _episode.append([s,a,next_s,r])
+                s=next_s
+                a+=1
+        else:
+            for _ in range(self.episode_step):
+                action_prob=self.epsilon_greedy_policy(q,s,action_p)
+                a=np.random.choice(action,p=action_prob)
+                next_s,r,end=self.search_space[self.state_name[s]][self.action_name[a]]
+                temp=q[s][a]
+                self.delta+=np.abs(q[s][a]-temp)
+                if end:
+                    self.delta+=self.delta/a
+                    if self.save_episode==True:
+                        episode.append([self.state_name[s],self.action_name[a],r,end])
+                    _episode.append([s,a,next_s,r])
+                    break
+                if self.save_episode==True:
+                    episode.append([self.state_name[s],self.action_name[a],r])
+                _episode.append([s,a,next_s,r])
+                s=next_s
+                a+=1
+        if self.save_episode==True:
+            self.episode.append(episode)
+        return _episode
+    
+    
+    def episode(self):
+        s=np.random.choice(self.state,p=self.state_prob)
+        return self._episode(self.q,s,self.action,self.action_prob)
+    
+    
+    def update_q(self,q,episode):
+        return self.td(q,episode)
+    
+    
+    def learn(self,episode,i):
+        self.delta=0
+        self.q=self.update_q(self.q,episode)
+        self.delta=self.delta/(i+1)
+        return
+    
+    
+    def save(self,path,i=None,one=True):
+        if one==True:
+            output_file=open(path+'.dat','wb')
+        else:
+            output_file=open(path+'-{0}.dat'.format(i+1),'wb')
+        pickle.dump(self.state_len,output_file)
+        pickle.dump(self.action_len,output_file)
+        pickle.dump(self.state,output_file)
+        pickle.dump(self.state_prob,output_file)
+        pickle.dump(self.action,output_file)
+        pickle.dump(self.action_prob,output_file)
+        pickle.dump(self.epsilon,output_file)
+        pickle.dump(self.alpha,output_file)
+        pickle.dump(self.discount,output_file)
+        pickle.dump(self.theta,output_file)
+        pickle.dump(self.episode_step,output_file)
+        pickle.dump(self.save_episode,output_file)
+        pickle.dump(self.delta,output_file)
+        pickle.dump(self.total_episode,output_file)
+        pickle.dump(self.total_time,output_file)
+        output_file.close()
+        return
+    
+    
+    def restore(self,path):
+        input_file=open(path,'rb')
+        self.state_len=pickle.load(input_file)
+        self.action_len=pickle.load(input_file)
+        if self.state_len==len(self.state_name):
+            self.state=pickle.load(input_file)
+            self.state_prob=pickle.load(input_file)
+        if self.action_len==len(self.action_name):
+            self.action=pickle.load(input_file)
+            self.action_prob=pickle.load(input_file)
+        self.epsilon=pickle.load(input_file)
+        self.alpha=pickle.load(input_file)
+        self.discount=pickle.load(input_file)
+        self.theta=pickle.load(input_file)
+        self.episode_step=pickle.load(input_file)
+        self.save_episode=pickle.load(input_file)
+        self.delta=pickle.load(input_file)
+        self.total_episode=pickle.load(input_file)
+        self.total_time=self.time
+        input_file.close()
+        return
