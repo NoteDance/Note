49f51ef75956ae3bff36e9dc0f93f519f8207f1d
7NoteDancing <63648431+7NoteDancing@users.noreply.github.com>
2021-01-01 04:20:17 +0000

    Update CNN.py

--- Note/nn/CNN/CNN.py
+++ Note/nn/CNN/CNN.py
@@ -517,13 +517,13 @@ class CNN:
                         self.test_accuracy_list.append(self.test_accuracy)
                 self.epoch+=1
                 if epoch%10!=0:
-                    temp=epoch-epoch%10
-                    temp=int(temp/10)
+                    d=epoch-epoch%10
+                    d=int(d/10)
                 else:
-                    temp=epoch/10
-                if temp==0:
-                    temp=1
-                if i%temp==0:
+                    d=epoch/10
+                if d==0:
+                    d=1
+                if i%d==0:
                     if continue_train==True:
                         print('epoch:{0}   loss:{1:.6f}'.format(self.total_epoch+i+1,self.train_loss))
                     else:
