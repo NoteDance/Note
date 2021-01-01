f6053a5225ead1a8e9b5419cbe3193119fa9f315
7NoteDancing <63648431+7NoteDancing@users.noreply.github.com>
2021-01-01 02:56:17 +0000

    Update kernel.py

--- Note/create/kernel.py
+++ Note/create/kernel.py
@@ -310,13 +310,13 @@ class Note:
             self.epoch+=1
             self.total_epoch+=1
             if epoch%10!=0:
-                temp=epoch-epoch%10
-                temp=int(temp/10)
+                d=epoch-epoch%10
+                d=int(d/10)
             else:
-                temp=epoch/10
-            if temp==0:
-                temp=1
-            if i%temp==0:
+                d=epoch/10
+            if d==0:
+                d=1
+            if i%d==0:
                 if self.total_epoch==0:
                     print('epoch:{0}   loss:{1:.6f}'.format(i,self.train_loss))
                 else:
