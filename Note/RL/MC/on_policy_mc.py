6eac687a35e9e5b3c175030908a40ce007b28700
7NoteDancing <63648431+7NoteDancing@users.noreply.github.com>
2021-01-08 14:50:50 +0000

    Update on_policy_mc.py

--- Note/RL/MC/on_policy_mc.py
+++ Note/RL/MC/on_policy_mc.py
@@ -121,9 +121,16 @@ class on_policy_mc:
     
     def save(self,path,i=None,one=True):
         if one==True:
-            output_file=open(path+'.dat','wb')
+            output_file=open(path+'save.dat','wb')
+            path=path+'save.dat'
+            index=path.rfind('\\')
+            episode_file=open(path.replace(path[index+1:],'episode.dat'),'wb')
         else:
-            output_file=open(path+'-{0}.dat'.format(i+1),'wb')
+            output_file=open(path+'save-{0}.dat'.format(i+1),'wb')
+            path=path+'save-{0}.dat'.format(i+1)
+            index=path.rfind('\\')
+            episode_file=open(path.replace(path[index+1:],'episode-{0}.dat'.format(i+1)),'wb')
+        pickle.dump(self.episode,episode_file)
         pickle.dump(self.r_sum,output_file)
         pickle.dump(self.r_count,output_file)
         pickle.dump(self.action_len,output_file)
@@ -141,8 +148,10 @@ class on_policy_mc:
         return
     
     
-    def restore(self,path):
-        input_file=open(path,'rb')
+    def restore(self,s_path,e_path):
+        input_file=open(s_path,'rb')
+        episode_file=open(e_path,'rb')
+        self.episode=pickle.load(episode_file)
         self.r_sum=pickle.load(input_file)
         self.r_count=pickle.load(input_file)
         self.action_len=pickle.load(input_file)
