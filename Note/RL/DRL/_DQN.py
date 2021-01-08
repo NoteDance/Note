9188d27c76c6750f9fc6bc834d81c551798f172d
7NoteDancing <63648431+7NoteDancing@users.noreply.github.com>
2021-01-08 14:51:56 +0000

    Update _DQN.py

--- Note/RL/DRL/_DQN.py
+++ Note/RL/DRL/_DQN.py
@@ -278,9 +278,16 @@ class DQN:
     
     def save(self,path,i=None,one=True):
         if one==True:
-            output_file=open(path+'.dat','wb')
+            output_file=open(path+'save.dat','wb')
+            path=path+'save.dat'
+            index=path.rfind('\\')
+            episode_file=open(path.replace(path[index+1:],'episode.dat'),'wb')
         else:
-            output_file=open(path+'-{0}.dat'.format(i+1),'wb')
+            output_file=open(path+'save-{0}.dat'.format(i+1),'wb')
+            path=path+'save-{0}.dat'.format(i+1)
+            index=path.rfind('\\')
+            episode_file=open(path.replace(path[index+1:],'episode-{0}.dat'.format(i+1)),'wb')
+        pickle.dump(self.episode,episode_file)
         pickle.dump(self.state_pool,output_file)
         pickle.dump(self.action_pool,output_file)
         pickle.dump(self.next_state_pool,output_file)
@@ -306,8 +313,10 @@ class DQN:
         return
     
     
-    def restore(self,path):
-        input_file=open(path,'rb')
+    def restore(self,s_path,e_path):
+        input_file=open(s_path,'rb')
+        episode_file=open(e_path,'rb')
+        self.episode=pickle.load(episode_file)
         self.state_pool=pickle.load(input_file)
         self.action_pool=pickle.load(input_file)
         self.next_state_pool=pickle.load(input_file)
