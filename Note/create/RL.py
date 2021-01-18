5af894c15349ffb485ed99b15205c3f5a008fe27
7NoteDancing <63648431+7NoteDancing@users.noreply.github.com>
2021-01-18 02:53:04 +0000

    Update RL.py

--- Note/create/RL.py
+++ Note/create/RL.py
@@ -10,14 +10,20 @@ def epsilon_greedy_policy(action,action_p,epsilon):
     return action_prob
 
 
-def pool(state_pool,action_pool,next_state_pool,reward_pool,state,action,next_state,reward,pool_size):
-    state_pool=tf.concatenate(state_pool,tf.expand_dims(state,axis=0))
-    action_pool=tf.concatenate(action_pool,tf.expand_dims(action,axis=0))
-    next_state_pool=tf.concatenate(next_state_pool,tf.expand_dims(next_state,axis=0))
-    reward_pool=tf.concatenate(reward_pool,tf.expand_dims(reward,axis=0))
-    if len(state_pool)>pool_size:
-        state_pool=state_pool[1:]
-        action_pool=action_pool[1:]
-        next_state_pool=next_state_pool[1:]
-        reward_pool=reward_pool[1:]
-    return state_pool,action_pool,next_state_pool,reward_pool
+def pool(state,action,next_state,reward,pool_size,state_pool=None,action_pool=None,next_state_pool=None,reward_pool=None):
+   if state_pool==None:
+       state_pool=tf.expand_dims(state,axis=0)
+       action_pool=tf.expand_dims(action,axis=0)
+       next_state_pool=tf.expand_dims(next_state,axis=0)
+       reward_pool=tf.expand_dims(reward,axis=0)
+   else:
+       state_pool=tf.concatenate(state_pool,tf.expand_dims(state,axis=0))
+       action_pool=tf.concatenate(action_pool,tf.expand_dims(action,axis=0))
+       next_state_pool=tf.concatenate(next_state_pool,tf.expand_dims(next_state,axis=0))
+       reward_pool=tf.concatenate(reward_pool,tf.expand_dims(reward,axis=0))
+   if len(state_pool)>pool_size:
+       state_pool=state_pool[1:]
+       action_pool=action_pool[1:]
+       next_state_pool=next_state_pool[1:]
+       reward_pool=reward_pool[1:]
+   return state_pool,action_pool,next_state_pool,reward_pool
