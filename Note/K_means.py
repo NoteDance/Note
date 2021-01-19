51d484a28215e57ce1061e651c07db165a53c765
7NoteDancing <63648431+7NoteDancing@users.noreply.github.com>
2021-01-19 04:17:13 +0000

    Update K_means.py

--- Note/K_means.py
+++ Note/K_means.py
@@ -1,6 +1,5 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import pandas as pd
 import pickle
 from sklearn.decomposition import PCA
 import time
@@ -17,26 +16,6 @@ def read_data(path,dtype=np.float32):
     input_file=open(path+'.dat','rb')
     data=pickle.load(input_file)
     return np.array(data,dtype=dtype)
-
-
-def write_data_csv(data,path,dtype=None,index=False,header=False):
-    if dtype==None:
-        data=pd.DataFrame(data)
-        data.to_csv(path,index=index,header=header)
-    else:
-        data=np.array(data,dtype=dtype)
-        data=pd.DataFrame(data)
-        data.to_csv(path,index=index,header=header)
-    return
-        
-
-def read_data_csv(path,dtype=None,header=None):
-    if dtype==None:
-        data=pd.read_csv(path,header=header)
-        return np.array(data)
-    else:
-        data=pd.read_csv(path,header=header)
-        return np.array(data,dtype=dtype)
 
 
 class K_means:
