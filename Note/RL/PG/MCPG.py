3cf2f728692a222644b20d7ed0d48bb3d6d5d938
7NoteDancing <63648431+7NoteDancing@users.noreply.github.com>
2021-01-19 03:48:07 +0000

    Update MCPG.py

--- Note/RL/PG/MCPG.py
+++ Note/RL/PG/MCPG.py
@@ -99,6 +99,12 @@ class MCPG:
         return
     
     
+    def save_p(self,path):
+        output_file=open(path+'.dat','wb')
+        pickle.dump(self.net_p,output_file)
+        return
+    
+    
     def save(self,path,i=None,one=True):
         if one==True:
             output_file=open(path+'\save.dat','wb')
