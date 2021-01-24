984a170a6f660537d92269ddadf898cff6a8f2b0
7NoteDancing <63648431+7NoteDancing@users.noreply.github.com>
2021-01-24 02:25:21 +0000

    Update ART2.py

--- Note/nn/ART2.py
+++ Note/nn/ART2.py
@@ -182,6 +182,12 @@ class ART2:
         return
     
     
+    def save_p(self,path):
+        parameter_file=open(path+'.dat','wb')
+        pickle.dump([self.W,self.T],parameter_file)
+        return
+    
+    
     def save(self,path):
         output_file=open(path+'\save.dat','wb')
         path=path+'\save.dat'
