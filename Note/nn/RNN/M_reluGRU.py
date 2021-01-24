209d1877d4dc4f7730e08dfb22ea68d6270189ce
7NoteDancing <63648431+7NoteDancing@users.noreply.github.com>
2021-01-24 02:53:57 +0000

    Update M_reluGRU.py

--- Note/nn/RNN/M_reluGRU.py
+++ Note/nn/RNN/M_reluGRU.py
@@ -635,6 +635,8 @@ class M_reluGRU:
                         train_writer.add_summary(train_summary,i)
                 t2=time.time()
                 self.time+=(t2-t1)
+            if model_path!=None:
+                self.save(model_path)
             self.time=self.time-int(self.time)
             if self.time<0.5:
                 self.time=int(self.time)
@@ -945,8 +947,14 @@ class M_reluGRU:
             total_params=(self.data_shape[2]*self.hidden+self.hidden*self.hidden+self.hidden)*4+self.hidden*self.labels_shape[1]+self.labels_shape[1]
         print('total params:{0}'.format(total_params))
         return
-
-
+    
+    
+    def save_p(self,path):
+        parameter_file=open(path+'.dat','wb')
+        pickle.dump([self.embedding_w,self.embedding_b,self.last_ug_weight_x,self.last_ug_weight_h,self.last_cltm_weight_x,self.last_cltm_weight_h,self.last_weight_o,self.last_ug_bias,self.last_cltm_bias,self.last_bias_o],parameter_file)
+        return
+    
+    
     def save(self,model_path,i=None,one=True):
         if one==True:
             output_file=open(model_path+'.dat','wb')
