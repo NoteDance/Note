4c42c39ff0d43021e075652b40153ced407576d3
7NoteDancing <63648431+7NoteDancing@users.noreply.github.com>
2021-01-24 02:54:40 +0000

    Update FNN.py

--- Note/nn/FNN/FNN.py
+++ Note/nn/FNN/FNN.py
@@ -411,6 +411,8 @@ class FNN:
                         train_writer.add_summary(train_summary,i)
                 t2=time.time()
                 self.time+=(t2-t1)
+            if model_path!=None:
+                self.save(model_path)
             self.time=self.time-int(self.time)
             if self.time<0.5:
                 self.time=int(self.time)
@@ -714,6 +716,12 @@ class FNN:
                             print()
             print('total params:{0}'.format(total_params))
             return
+    
+    
+    def save_p(self,path):
+        parameter_file=open(path+'.dat','wb')
+        pickle.dump([self.last_weight,self.last_bias],parameter_file)
+        return
 
 
     def save(self,model_path,i=None,one=True):
