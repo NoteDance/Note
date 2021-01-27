a6c8d7c4410caf03a4a3876919a9fed85ec1510d
7NoteDancing <63648431+7NoteDancing@users.noreply.github.com>
2021-01-27 10:38:32 +0000

    Update _Double_DQN.py

--- Note/RL/DRL/_Double_DQN.py
+++ Note/RL/DRL/_Double_DQN.py
@@ -38,7 +38,7 @@ class Double_DQN:
         self.total_time=0
     
     
-    def init(self,dtype=np.int32):
+    def init_a(self,dtype=np.int32):
         self.t3=time.time()
         if len(self.action_name)>self.action_len:
             self.action=np.concatenate((self.action,np.arange(len(self.action_name)-self.action_len,dtype=dtype)+self.action_len))
@@ -48,6 +48,42 @@ class Double_DQN:
             self.action_one=np.ones(len(self.action_name),dtype=dtype)
         self.index=np.arange(self.batch,dtype=np.int8)
         self.t4=time.time()
+        return
+    
+    
+    def init(self,epsilon=None,discount=None,episode_step=None,pool_size=None,batch=None,update_step=None,optimizer=None,lr=None,value_p=None,target_p=None,flag=None):
+        if epsilon!=None:
+            self.epsilon=epsilon
+        if discount!=None:
+            self.discount=discount
+        if episode_step!=None:
+            self.episode_step=episode_step
+        if pool_size!=None:
+            self.pool_size=pool_size
+        if batch!=None:
+            self.batch=batch
+            self.index=np.arange(self.batch,dtype=np.int8)
+        if update_step!=None:
+            self.update_step=update_step
+        if optimizer!=None:
+            self.optimizer=optimizer
+        if lr!=None:
+            self.lr=lr
+        if value_p!=None:
+            self.value_p=value_p
+            self.target_p=target_p
+        if flag==None:
+            self.episode=[]
+            self.state_pool=None
+            self.action_pool=None
+            self.next_state_pool=None
+            self.reward_pool=None
+            self.loss_list=[]
+            self.a=0
+            self.episode_num=0
+            self.total_episode=0
+            self.time=0
+            self.total_time=0
         return
     
     
@@ -61,7 +97,7 @@ class Double_DQN:
     
     def update_parameter(self):
         for i in range(len(self.value_p)):
-            self.target_p[i]=self.value_p[i]
+            self.target_p[i]=self.value_p[i].copy()
         return
     
     
@@ -328,6 +364,7 @@ class Double_DQN:
         pickle.dump(self.save_episode,output_file)
         pickle.dump(self.loss_list,output_file)
         pickle.dump(self.opt_flag,output_file)
+        pickle.dump(self.episode_num,output_file)
         pickle.dump(self.a,output_file)
         pickle.dump(self.total_episode,output_file)
         pickle.dump(self.total_time,output_file)
@@ -358,6 +395,7 @@ class Double_DQN:
         self.save_episode=pickle.load(input_file)
         self.loss_list=pickle.load(input_file)
         self.opt_flag=pickle.load(input_file)
+        self.episode_num=pickle.load(input_file)
         self.a=pickle.load(input_file)
         self.total_episode=pickle.load(input_file)
         self.total_time=self.time
