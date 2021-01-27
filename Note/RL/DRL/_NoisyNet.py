a80dda67e4c5d90288af71d4445b6abbb1ad66d9
7NoteDancing <63648431+7NoteDancing@users.noreply.github.com>
2021-01-27 13:18:24 +0000

    Update _NoisyNet.py

--- Note/RL/DRL/_NoisyNet.py
+++ Note/RL/DRL/_NoisyNet.py
@@ -32,8 +32,9 @@ class NoisyNet:
         self.save_episode=save_episode
         self.loss_list=[]
         self.opt_flag==False
-        self.episode_num=0
         self.a=0
+        self.epi_num=0
+        self.episode_num=0
         self.total_episode=0
         self.time=0
         self.total_time=0
@@ -79,6 +80,7 @@ class NoisyNet:
             self.reward_pool=None
             self.loss_list=[]
             self.a=0
+            self.epi_num=0
             self.episode_num=0
             self.total_episode=0
             self.time=0
@@ -331,7 +333,7 @@ class NoisyNet:
                 print('episode num:{0}   loss:{1:.6f}'.format(i+1,loss))
                 if path!=None and i%episode_num*2==0:
                     self.save(path,i,one)
-            self.episode_num+=1
+            self.epi_num+=1
             self.total_episode+=1
             if self.save_episode==True:
                 self.episode.append(episode)
@@ -383,6 +385,7 @@ class NoisyNet:
             path=path+'\save-{0}.dat'.format(i+1)
             index=path.rfind('\\')
             episode_file=open(path.replace(path[index+1:],'episode-{0}.dat'.format(i+1)),'wb')
+        self.episode_num=self.epi_num
         pickle.dump(self.episode,episode_file)
         pickle.dump(self.state_pool,output_file)
         pickle.dump(self.action_pool,output_file)
@@ -403,8 +406,8 @@ class NoisyNet:
         pickle.dump(self.save_episode,output_file)
         pickle.dump(self.loss_list,output_file)
         pickle.dump(self.opt_flag,output_file)
-        pickle.dump(self.episode_num,output_file)
         pickle.dump(self.a,output_file)
+        pickle.dump(self.episode_num,output_file)
         pickle.dump(self.total_episode,output_file)
         pickle.dump(self.total_time,output_file)
         output_file.close()
@@ -434,8 +437,8 @@ class NoisyNet:
         self.save_episode=pickle.load(input_file)
         self.loss_list=pickle.load(input_file)
         self.opt_flag=pickle.load(input_file)
-        self.episode_num=pickle.load(input_file)
         self.a=pickle.load(input_file)
+        self.episode_num=pickle.load(input_file)
         self.total_episode=pickle.load(input_file)
         self.total_time=self.time
         input_file.close()
