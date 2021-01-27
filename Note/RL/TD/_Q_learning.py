e12b2312d73db4867c71e2cd6be1106c39cf1176
7NoteDancing <63648431+7NoteDancing@users.noreply.github.com>
2021-01-27 13:30:54 +0000

    Update _Q_learning.py

--- Note/RL/TD/_Q_learning.py
+++ Note/RL/TD/_Q_learning.py
@@ -18,6 +18,7 @@ class Q_learning:
         self.episode_step=episode_step
         self.save_episode=save_episode
         self.delta=0
+        self.epi_num=0
         self.episode_num=0
         self.total_episode=0
         self.time=0
@@ -53,6 +54,7 @@ class Q_learning:
         if flag==None:
             self.episode=[]
             self.delta=0
+            self.epi_num=0
             self.episode_num=0
             self.total_episode=0
             self.time=0
@@ -133,7 +135,7 @@ class Q_learning:
                 print('episode num:{0}   delta:{1:.6f}'.format(i+1,self.delta))
                 if path!=None and i%episode_num*2==0:
                     self.save(path,i,one)
-            self.episode_num+=1
+            self.epi_num+=1
             self.total_episode+=1
             t2=time.time()
             self.time+=(t2-t1)
@@ -175,6 +177,7 @@ class Q_learning:
             path=path+'\save-{0}.dat'.format(i+1)
             index=path.rfind('\\')
             episode_file=open(path.replace(path[index+1:],'episode-{0}.dat'.format(i+1)),'wb')
+        self.episode_num=self.epi_num
         pickle.dump(self.episode,episode_file)
         pickle.dump(self.action_len,output_file)
         pickle.dump(self.action,output_file)
