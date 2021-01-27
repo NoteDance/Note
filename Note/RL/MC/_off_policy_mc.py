05875fd499ebf885c4db4c0ae9ef4e3ffcaa92a4
7NoteDancing <63648431+7NoteDancing@users.noreply.github.com>
2021-01-27 03:49:36 +0000

    Update _off_policy_mc.py

--- Note/RL/MC/_off_policy_mc.py
+++ Note/RL/MC/_off_policy_mc.py
@@ -191,9 +191,8 @@ class off_policy_mc:
         episode_file=open(e_path,'rb')
         self.episode=pickle.load(episode_file)
         self.action_len=pickle.load(input_file)
-        if self.action_len==len(self.action_name):
-            self.action=pickle.load(input_file)
-            self.action_onerob=pickle.load(input_file)
+        self.action=pickle.load(input_file)
+        self.action_onerob=pickle.load(input_file)
         self.epsilon=pickle.load(input_file)
         self.discount=pickle.load(input_file)
         self.theta=pickle.load(input_file)
