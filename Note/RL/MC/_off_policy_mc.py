5fc884710d5ccba8fafa45237b5edddeaae5fb31
7NoteDancing <63648431+7NoteDancing@users.noreply.github.com>
2021-01-28 03:08:58 +0000

    Update _off_policy_mc.py

--- Note/RL/MC/_off_policy_mc.py
+++ Note/RL/MC/_off_policy_mc.py
@@ -25,7 +25,7 @@ class off_policy_mc:
         self.total_time=0
         
         
-    def init_a(self,dtype=np.int32):
+    def init(self,dtype=np.int32):
         self.t3=time.time()
         if len(self.action_name)>self.action_len:
             self.action=np.concatenate((self.action,np.arange(len(self.action_name)-self.action_len,dtype=dtype)+self.action_len))
@@ -45,7 +45,7 @@ class off_policy_mc:
         return
     
     
-    def init(self,epsilon=None,discount=None,theta=None,episode_step=None,flag=None):
+    def set_up(self,epsilon=None,discount=None,theta=None,episode_step=None,init=True):
         if epsilon!=None:
             self.epsilon=epsilon
         if discount!=None:
@@ -54,7 +54,7 @@ class off_policy_mc:
             self.theta=theta
         if episode_step!=None:
             self.episode_step=episode_step
-        if flag==None:
+        if init==True:
             self.r_sum=dict()
             self.r_count=dict()
             self.episode=[]
