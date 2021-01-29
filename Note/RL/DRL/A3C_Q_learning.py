698bd1ca6e9b8d1c4c52ced55abf23281dd9305f
7NoteDancing <63648431+7NoteDancing@users.noreply.github.com>
2021-01-29 13:42:17 +0000

    Update and rename A3C_Q_learning_1.py to A3C_Q_learning.py

--- Note/RL/DRL/A3C_Q_learning.py
+++ Note/RL/DRL/A3C_Q_learning.py
@@ -4,7 +4,7 @@ import pickle
 import time
 
 
-class A3C_Q_learning_1:
+class A3C_Q_learning:
     def __init__(self,value_net,value_p,target_p,state,state_name,action_name,exploration_space,Tmax=None,It=None,Ia=None,alpha=None,lr=None,epsilon=None,discount=None,episode_step=None,save_episode=True):
         self.value_net=value_net
         self.value_p=value_p
