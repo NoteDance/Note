b4cd1ba7cdb40b2a7a3ba5324c94463586a7f5b0
7NoteDancing <63648431+7NoteDancing@users.noreply.github.com>
2021-02-01 02:07:54 +0000

    Update Q_learning.py

--- Note/RL/TD/Q_learning.py
+++ Note/RL/TD/Q_learning.py
@@ -199,6 +199,6 @@ class Q_learning:
         self.delta=pickle.load(input_file)
         self.episode_num=pickle.load(input_file)
         self.total_episode=pickle.load(input_file)
-        self.total_time=self.time
+        self.total_time=pickle.load(input_file)
         input_file.close()
         return
