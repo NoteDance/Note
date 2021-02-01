31b7a6dd49043dfb03bff8f5469b1db2e54857ee
7NoteDancing <63648431+7NoteDancing@users.noreply.github.com>
2021-02-01 02:06:12 +0000

    Update _on_policy_mc.py

--- Note/RL/MC/_on_policy_mc.py
+++ Note/RL/MC/_on_policy_mc.py
@@ -228,6 +228,6 @@ class on_policy_mc:
         self.state_one=pickle.load(input_file)
         self.episode_num=pickle.load(input_file)
         self.total_episode=pickle.load(input_file)
-        self.total_time=self.time
+        self.total_time=pickle.load(input_file)
         input_file.close()
         return
