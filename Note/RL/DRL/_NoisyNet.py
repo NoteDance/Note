5a26109f0b174e0b15d6753eee871b05b098207f
7NoteDancing <63648431+7NoteDancing@users.noreply.github.com>
2021-02-01 02:04:06 +0000

    Update _NoisyNet.py

--- Note/RL/DRL/_NoisyNet.py
+++ Note/RL/DRL/_NoisyNet.py
@@ -387,6 +387,6 @@ class NoisyNet:
         self.a=pickle.load(input_file)
         self.episode_num=pickle.load(input_file)
         self.total_episode=pickle.load(input_file)
-        self.total_time=self.time
+        self.total_time=pickle.load(input_file)
         input_file.close()
         return
