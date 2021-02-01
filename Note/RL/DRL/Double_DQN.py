96d6f0de5c82aa3ca57da04c580979eb6802ae16
7NoteDancing <63648431+7NoteDancing@users.noreply.github.com>
2021-02-01 02:01:44 +0000

    Update Double_DQN.py

--- Note/RL/DRL/Double_DQN.py
+++ Note/RL/DRL/Double_DQN.py
@@ -324,6 +324,6 @@ class Double_DQN:
         self.opt_flag=pickle.load(input_file)
         self.episode_num=pickle.load(input_file)
         self.total_episode=pickle.load(input_file)
-        self.total_time=self.time
+        self.total_time=pickle.load(input_file)
         input_file.close()
         return
