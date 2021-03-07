a3a956cd5a7be54c812a4adc8608a507bce3502a
7NoteDancing <63648431+7NoteDancing@users.noreply.github.com>
2021-03-08 01:42:48 +0800

    Update Dueling_DQN.py

--- Note/RL/DRL/Dueling_DQN.py
+++ Note/RL/DRL/Dueling_DQN.py
@@ -15,7 +15,6 @@ class Dueling_DQN:
         self.next_state_pool=[]
         self.reward_pool=[]
         self.episode=[]
-        self.episode_list=[]
         self.state=state
         self.state_name=state_name
         self.action_name=action_name
@@ -30,12 +29,12 @@ class Dueling_DQN:
         self.optimizer=optimizer
         self.lr=lr
         self.t=0
-        self.flish_list=[]
         self.t_counter=0
         self.one_list=[]
         self.index_list=[]
         self.use_flag=[]
         self.p=None
+        self.flish_list=[]
         self.pool_net=pool_net
         self.save_episode=save_episode
         self.loss=[]
@@ -293,14 +292,7 @@ class Dueling_DQN:
         self.next_state_pool[i]=tf.expand_dims(self.next_state_pool[i][0],axis=0)
         self.reward_pool[i]=tf.expand_dims(self.reward_pool[i][0],axis=0)
         return
-    
-    
-    def merge_episode(self):
-        for i in range(self.episode_list):
-            self.episode.extend(self.episode_list[i])
-            self.episode_list[i].clear()
-        return
-    
+        
     
     def train_visual(self):
         print()
@@ -325,12 +317,14 @@ class Dueling_DQN:
         return
     
     
-    def save(self,path,i):
+    def save(self,path):
         output_file=open(path+'\save.dat','wb')
         path=path+'\save.dat'
+        index=path.rfind('\\')
+        episode_file=open(path.replace(path[index+1:],'episode.dat'),'wb')
         self.episode_num=self.epi_num
         self.thread=self.t
-        pickle.dump(self.episode_list[i],output_file)
+        pickle.dump(self.episode,episode_file)
         pickle.dump(self.state_pool,output_file)
         pickle.dump(self.action_pool,output_file)
         pickle.dump(self.next_state_pool,output_file)
@@ -364,11 +358,10 @@ class Dueling_DQN:
         return
     
     
-    def restore(self,s_path,e_path,i):
+    def restore(self,s_path,e_path):
         input_file=open(s_path,'rb')
         episode_file=open(e_path,'rb')
         self.episode=pickle.load(episode_file)
-        self.episode_list[i]=pickle.load(input_file)
         self.state_pool=pickle.load(input_file)
         self.action_pool=pickle.load(input_file)
         self.next_state_pool=pickle.load(input_file)
