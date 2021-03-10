efedf994ae3ee0f40942b6e6d4bca1410279be9e
7NoteDancing <63648431+7NoteDancing@users.noreply.github.com>
2021-03-10 12:57:28 +0800

    Update _Sarsa.py

--- Note/RL/TD/_Sarsa.py
+++ Note/RL/TD/_Sarsa.py
@@ -160,12 +160,14 @@ class Sarsa:
     def save_policy(self,path):
         policy_file=open(path+'.dat','wb')
         pickle.dump(self.q,policy_file)
+        policy_file.close()
         return
     
     
     def save_e(self,path):
         episode_file=open(path+'.dat','wb')
         pickle.dump(self.episode,episode_file)
+        episode_file.close()
         return
     
     
@@ -197,6 +199,7 @@ class Sarsa:
         pickle.dump(self.total_episode,output_file)
         pickle.dump(self.total_time,output_file)
         output_file.close()
+        episode_file.close()
         return
     
     
@@ -219,4 +222,5 @@ class Sarsa:
         self.total_episode=pickle.load(input_file)
         self.total_time=pickle.load(input_file)
         input_file.close()
+        episode_file.close()
         return
