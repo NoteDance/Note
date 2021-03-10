ca72555500e40676b1b38387d03f39a534e3f806
7NoteDancing <63648431+7NoteDancing@users.noreply.github.com>
2021-03-10 20:45:03 +0800

    Update MCPG.py

--- Note/RL/PG/MCPG.py
+++ Note/RL/PG/MCPG.py
@@ -5,7 +5,7 @@ import time
 
 
 class MCPG:
-    def __init__(self,policy_net,net_p,state,state_name,action_name,exploration_space,epsilon=None,discount=None,reward_min=None,episode_step=None,optimizer=None,save_episode=True):
+    def __init__(self,policy_net,net_p,state,state_name,action_name,exploration_space,epsilon=None,discount=None,reward_min=None,episode_step=None,optimizer=None,lr=None,save_episode=True):
         self.policy_net=policy_net
         self.net_p=net_p
         self.episode=[]
@@ -20,6 +20,8 @@ class MCPG:
         self.reward_min=reward_min
         self.episode_step=episode_step
         self.optimizer=optimizer
+        self.lr=lr
+        if(lr!=None):self.optimizer.lr=lr
         self.save_episode=save_episode
         self.loss=0
         self.opt_flag=False
@@ -41,7 +43,7 @@ class MCPG:
         return
     
     
-    def set_up(self,net_p=None,epsilon=None,discount=None,reward_min=None,episode_step=None,optimizer=None,init=True):
+    def set_up(self,net_p=None,epsilon=None,discount=None,reward_min=None,episode_step=None,optimizer=None,lr=None,init=True):
         if net_p!=None:
             self.net_p=net_p
         if epsilon!=None:
@@ -54,6 +56,9 @@ class MCPG:
             self.episode_step=episode_step
         if optimizer!=None:
             self.optimizer=optimizer
+        if lr!=None:
+            self.lr=lr
+            self.optimizer.lr=lr
         if init==True:
             self.episode=[]
             self.reward_list=[]
@@ -161,6 +166,7 @@ class MCPG:
         pickle.dump(self.reward_min,output_file)
         pickle.dump(self.episode_step,output_file)
         pickle.dump(self.optimizer,output_file)
+        pickle.dump(self.lr,output_file)
         pickle.dump(self.save_episode,output_file)
         pickle.dump(self.opt_flag,output_file)
         pickle.dump(self.state_one,output_file)
@@ -184,6 +190,7 @@ class MCPG:
         self.reward_min=pickle.load(input_file)
         self.episode_step=pickle.load(input_file)
         self.optimizer=pickle.load(input_file)
+        self.lr=pickle.load(input_file)
         self.save_episode=pickle.load(input_file)
         self.opt_flag=pickle.load(input_file)
         self.state_one=pickle.load(input_file)
