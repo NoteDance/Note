c2844f8f2a491614c9bb96a74090c567d2feff9e
7NoteDancing <63648431+7NoteDancing@users.noreply.github.com>
2021-03-10 12:26:53 +0800

    Update NoisyNet.py

--- Note/RL/DRL/NoisyNet.py
+++ Note/RL/DRL/NoisyNet.py
@@ -6,7 +6,7 @@ import time
 
 
 class NoisyNet:
-    def __init__(self,value_net,value_p,target_p,state,state_name,action_name,exploration_space,DUELING=None,discount=None,episode_step=None,pool_size=None,batch=None,update_step=None,optimizer=None,lr=None,pool_net=True,save_episode=True):
+    def __init__(self,value_net,value_p,target_p,state,state_name,action_name,exploration_space,DUELING=None,discount=None,episode_step=None,pool_size=None,batch=None,update_step=None,optimizer=None,pool_net=True,save_episode=True):
         self.value_net=value_net
         self.value_p=value_p
         self.target_p=target_p
@@ -28,7 +28,6 @@ class NoisyNet:
         self.batch=batch
         self.update_step=update_step
         self.optimizer=optimizer
-        self.lr=lr
         self.t=0
         self.t_counter=0
         self.one_list=[]
@@ -61,7 +60,7 @@ class NoisyNet:
         return
     
     
-    def set_up(self,value_p=None,target_p=None,epsilon=None,discount=None,episode_step=None,pool_size=None,batch=None,update_step=None,optimizer=None,lr=None,init=True):
+    def set_up(self,value_p=None,target_p=None,epsilon=None,discount=None,episode_step=None,pool_size=None,batch=None,update_step=None,optimizer=None,init=True):
         if value_p!=None:
             self.value_p=value_p
             self.target_p=target_p
@@ -80,8 +79,6 @@ class NoisyNet:
             self.update_step=update_step
         if optimizer!=None:
             self.optimizer=optimizer
-        if lr!=None:
-            self.lr=lr
         if init==True:
             self.t=0
             self.t_counter=0
@@ -371,7 +368,6 @@ class NoisyNet:
         pickle.dump(self.batch,output_file)
         pickle.dump(self.update_step,output_file)
         pickle.dump(self.optimizer,output_file)
-        pickle.dump(self.lr,output_file)
         pickle.dump(self.thread,output_file)
         pickle.dump(self.t_counter,output_file)
         pickle.dump(self.one_list,output_file)
@@ -386,6 +382,7 @@ class NoisyNet:
         pickle.dump(self.total_episode,output_file)
         pickle.dump(self.total_time,output_file)
         output_file.close()
+        episode_file.close()
         return
     
     
@@ -407,7 +404,6 @@ class NoisyNet:
         self.batch=pickle.load(input_file)
         self.update_step=pickle.load(input_file)
         self.optimizer=pickle.load(input_file)
-        self.lr=pickle.load(input_file)
         self.thread=pickle.load(input_file)
         self.t_counter=pickle.load(input_file)
         self.one_list=pickle.load(input_file)
@@ -422,4 +418,5 @@ class NoisyNet:
         self.total_episode=pickle.load(input_file)
         self.total_time=pickle.load(input_file)
         input_file.close()
+        episode_file.close()
         return
