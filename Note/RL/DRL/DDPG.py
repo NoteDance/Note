2e6feff2a5e943f8b9b1aef498c42062ca1c519a
7NoteDancing <63648431+7NoteDancing@users.noreply.github.com>
2021-03-10 20:54:12 +0800

    Update DDPG.py

--- Note/RL/DRL/DDPG.py
+++ Note/RL/DRL/DDPG.py
@@ -6,7 +6,7 @@ import time
 
 
 class DDPG:
-    def __init__(self,value_net,actor_net,value_target_p,value_p,actor_target_p,actor_p,state,state_name,action_name,exploration_space,discount=None,episode_step=None,pool_size=None,batch=None,optimizer=None,tau=0.001,pool_net=True,save_episode=True):
+    def __init__(self,value_net,actor_net,value_target_p,value_p,actor_target_p,actor_p,state,state_name,action_name,exploration_space,discount=None,episode_step=None,pool_size=None,batch=None,optimizer=None,lr=None.tau=0.001,pool_net=True,save_episode=True):
         self.value_net=value_net
         self.actor_net=actor_net
         self.value_p=value_p
@@ -27,6 +27,8 @@ class DDPG:
         self.pool_size=pool_size
         self.batch=batch
         self.optimizer=optimizer
+        self.lr=lr
+        if(lr!=None):self.optimizer.lr=lr
         self.tau=tau
         self.t=0
         self.t_counter=0
@@ -47,7 +49,7 @@ class DDPG:
         self.total_time=0
     
     
-    def set_up(self,value_p=None,value_target_p=None,actor_p=None,actor_target_p=None,discount=None,episode_step=None,pool_size=None,batch=None,optimizer=None,tau=0.001,init=True):
+    def set_up(self,value_p=None,value_target_p=None,actor_p=None,actor_target_p=None,discount=None,episode_step=None,pool_size=None,batch=None,optimizer=None,lr=None,tau=0.001,init=True):
         if value_p!=None:
             self.value_p=value_p
             self.value_target_p=value_target_p
@@ -64,6 +66,9 @@ class DDPG:
             self.index=np.arange(self.batch,dtype=np.int8)
         if optimizer!=None:
             self.optimizer=optimizer
+        if lr!=None:
+            self.lr=lr
+            self.optimizer.lr=lr
         if tau!=None:
             self.tau=tau
         if init==True:
@@ -337,6 +342,7 @@ class DDPG:
         pickle.dump(self.pool_size,output_file)
         pickle.dump(self.batch,output_file)
         pickle.dump(self.optimizer,output_file)
+        pickle.dump(self.lr,output_file)
         pickle.dump(self.thread,output_file)
         pickle.dump(self.t_counter,output_file)
         pickle.dump(self.one_list,output_file)
@@ -369,6 +375,7 @@ class DDPG:
         self.pool_size=pickle.load(input_file)
         self.batch=pickle.load(input_file)
         self.optimizer=pickle.load(input_file)
+        self.lr=pickle.load(input_file)
         self.thread=pickle.load(input_file)
         self.t_counter=pickle.load(input_file)
         self.one_list=pickle.load(input_file)
