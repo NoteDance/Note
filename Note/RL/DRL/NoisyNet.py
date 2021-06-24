658b05f6b207ac75d2394fd1c8acf796b0cddde7
7NoteDancing <63648431+7NoteDancing@users.noreply.github.com>
2021-06-25 00:04:22 +0800

    Update NoisyNet.py

--- Note/RL/DRL/NoisyNet.py
+++ Note/RL/DRL/NoisyNet.py
@@ -192,9 +192,9 @@ class NoisyNet:
             self.reward_pool[i]=self.reward_pool[i][1:]
         if end:
             if self.save_episode==True:
-                episode=[self.state_name[s],self.action_name[a],r,end]
+                episode=[self.state_name[s],self.action_name[a],self.state_name[next_s],r,end]
         elif self.save_episode==True:
-            episode=[self.state_name[s],self.self.action_name[a],r]
+            episode=[self.state_name[s],self.self.action_name[a],self.state_name[next_s],r]
         self.epi_num+=1
         return next_s,end,episode
     
@@ -361,10 +361,12 @@ class NoisyNet:
         output_file=open(path+'\save.dat','wb')
         path=path+'\save.dat'
         index=path.rfind('\\')
-        episode_file=open(path.replace(path[index+1:],'episode.dat'),'wb')
+        if self.save_episode==True:
+            episode_file=open(path.replace(path[index+1:],'episode.dat'),'wb')
+            pickle.dump(self.episode,episode_file)
+            episode_file.close()
         self.episode_num=self.epi_num
         self.thread=self.t
-        pickle.dump(self.episode,episode_file)
         pickle.dump(self.state_pool,output_file)
         pickle.dump(self.action_pool,output_file)
         pickle.dump(self.next_state_pool,output_file)
@@ -395,14 +397,15 @@ class NoisyNet:
         pickle.dump(self.total_episode,output_file)
         pickle.dump(self.total_time,output_file)
         output_file.close()
-        episode_file.close()
         return
     
     
-    def restore(self,s_path,e_path):
+    def restore(self,s_path,e_path=None):
         input_file=open(s_path,'rb')
-        episode_file=open(e_path,'rb')
-        self.episode=pickle.load(episode_file)
+        if self.save_episode==True:
+            episode_file=open(e_path,'rb')
+            self.episode=pickle.load(episode_file)
+            episode_file.close()
         self.state_pool=pickle.load(input_file)
         self.action_pool=pickle.load(input_file)
         self.next_state_pool=pickle.load(input_file)
@@ -433,5 +436,4 @@ class NoisyNet:
         self.total_episode=pickle.load(input_file)
         self.total_time=pickle.load(input_file)
         input_file.close()
-        episode_file.close()
         return
