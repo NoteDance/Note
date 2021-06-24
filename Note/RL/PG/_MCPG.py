e8ed6d1e2b40b3b83f0fc31fbd4730e6abfc1189
7NoteDancing <63648431+7NoteDancing@users.noreply.github.com>
2021-06-25 00:02:32 +0800

    Update _MCPG.py

--- Note/RL/PG/_MCPG.py
+++ Note/RL/PG/_MCPG.py
@@ -87,10 +87,10 @@ class MCPG:
                 if end:
                     self.reward_list.append(G)
                     if self.save_episode==True:
-                        episode.append([self.state_name[s],self.action_name[a],r,end])
+                        episode.append([self.state_name[s],self.action_name[a],self.state_name[next_s],r,end])
                     break
                 if self.save_episode==True:
-                    episode.append([self.state_name[s],self.action_name[a],r])
+                    episode.append([self.state_name[s],self.action_name[a],self.state_name[next_s],r])
                 s=next_s
         else:
             output=self.policy_net(self.state[s])
@@ -103,10 +103,10 @@ class MCPG:
                 if end:
                     self.reward_list.append(G)
                     if self.save_episode==True:
-                        episode.append([self.state_name[s],self.action_name[a],r,end])
+                        episode.append([self.state_name[s],self.action_name[a],self.state_name[next_s],r,end])
                     break
                 if self.save_episode==True:
-                    episode.append([self.state_name[s],self.action_name[a],r])
+                    episode.append([self.state_name[s],self.action_name[a],self.state_name[next_s],r])
                 s=next_s
             self.reward_list.append(G)
         if self.save_episode==True:
@@ -178,14 +178,19 @@ class MCPG:
             output_file=open(path+'\save.dat','wb')
             path=path+'\save.dat'
             index=path.rfind('\\')
-            episode_file=open(path.replace(path[index+1:],'episode.dat'),'wb')
+            if self.save_episode==True:
+                episode_file=open(path.replace(path[index+1:],'episode.dat'),'wb')
+                pickle.dump(self.episode,episode_file)
+                episode_file.close()
         else:
             output_file=open(path+'\save-{0}.dat'.format(i+1),'wb')
             path=path+'\save-{0}.dat'.format(i+1)
             index=path.rfind('\\')
-            episode_file=open(path.replace(path[index+1:],'episode-{0}.dat'.format(i+1)),'wb')
+            if self.save_episode==True:
+                episode_file=open(path.replace(path[index+1:],'episode-{0}.dat'.format(i+1)),'wb')
+                pickle.dump(self.episode,episode_file)
+                episode_file.close()
         self.episode_num=self.epi_num
-        pickle.dump(self.episode,episode_file)
         pickle.dump(self.action_len,output_file)
         pickle.dump(self.action,output_file)
         pickle.dump(self.reward_list,output_file)
@@ -202,14 +207,15 @@ class MCPG:
         pickle.dump(self.total_episode,output_file)
         pickle.dump(self.total_time,output_file)
         output_file.close()
-        episode_file.close()
         return
     
     
-    def restore(self,s_path,e_path):
+    def restore(self,s_path,e_path=None):
         input_file=open(s_path,'rb')
-        episode_file=open(e_path,'rb')
-        self.episode=pickle.load(episode_file)
+        if self.save_episode==True:
+            episode_file=open(e_path,'rb')
+            self.episode=pickle.load(episode_file)
+            episode_file.close()
         self.action_len=pickle.load(input_file)
         self.action=pickle.load(input_file)
         self.reward_list=pickle.load(input_file)
@@ -226,5 +232,4 @@ class MCPG:
         self.total_episode=pickle.load(input_file)
         self.total_time=pickle.load(input_file)
         input_file.close()
-        episode_file.close()
         return
