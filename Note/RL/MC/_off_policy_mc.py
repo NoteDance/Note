d872c4f94924982beea2c1e380cfbcc9ec3142b8
7NoteDancing <63648431+7NoteDancing@users.noreply.github.com>
2021-06-25 00:02:01 +0800

    Update _off_policy_mc.py

--- Note/RL/MC/_off_policy_mc.py
+++ Note/RL/MC/_off_policy_mc.py
@@ -86,10 +86,10 @@ class off_policy_mc:
                 episode.append([s,a,r])
                 if end:
                     if self.save_episode==True:
-                        _episode.append([self.state_name[s],self.action_name[a],r,end])
+                        _episode.append([self.state_name[s],self.action_name[a],self.state_name[next_s],r,end])
                     break
                 if self.save_episode==True:
-                    _episode.append([self.state_name[s],self.action_name[a],r])
+                    _episode.append([self.state_name[s],self.action_name[a],self.state_name[next_s],r])
                 s=next_s
         else:
             for _ in range(self.episode_step):
@@ -99,10 +99,10 @@ class off_policy_mc:
                 episode.append([s,a,r])
                 if end:
                     if self.save_episode==True:
-                        _episode.append([self.state_name[s],self.action_name[a],r,end])
+                        _episode.append([self.state_name[s],self.action_name[a],self.state_name[next_s],r,end])
                     break
                 if self.save_episode==True:
-                    _episode.append([self.state_name[s],self.action_name[a],r])
+                    _episode.append([self.state_name[s],self.action_name[a],self.state_name[next_s],r])
                 s=next_s
         if self.save_episode==True:
             self.episode.append(_episode)
@@ -188,14 +188,19 @@ class off_policy_mc:
             output_file=open(path+'\save.dat','wb')
             path=path+'\save.dat'
             index=path.rfind('\\')
-            episode_file=open(path.replace(path[index+1:],'episode.dat'),'wb')
+            if self.save_episode==True:
+                episode_file=open(path.replace(path[index+1:],'episode.dat'),'wb')
+                pickle.dump(self.episode,episode_file)
+                episode_file.close()
         else:
             output_file=open(path+'\save-{0}.dat'.format(i+1),'wb')
             path=path+'\save-{0}.dat'.format(i+1)
             index=path.rfind('\\')
-            episode_file=open(path.replace(path[index+1:],'episode-{0}.dat'.format(i+1)),'wb')
+            if self.save_episode==True:
+                episode_file=open(path.replace(path[index+1:],'episode-{0}.dat'.format(i+1)),'wb')
+                pickle.dump(self.episode,episode_file)
+                episode_file.close()
         self.episode_num=self.epi_num
-        pickle.dump(self.episode,episode_file)
         pickle.dump(self.action_len,output_file)
         pickle.dump(self.action,output_file)
         pickle.dump(self.action_prob,output_file)
@@ -210,14 +215,15 @@ class off_policy_mc:
         pickle.dump(self.total_episode,output_file)
         pickle.dump(self.total_time,output_file)
         output_file.close()
-        episode_file.close()
         return
     
     
-    def restore(self,s_path,e_path):
+    def restore(self,s_path,e_path=None):
         input_file=open(s_path,'rb')
-        episode_file=open(e_path,'rb')
-        self.episode=pickle.load(episode_file)
+        if self.save_episode==True:
+            episode_file=open(e_path,'rb')
+            self.episode=pickle.load(episode_file)
+            episode_file.close()
         self.action_len=pickle.load(input_file)
         self.action=pickle.load(input_file)
         self.action_prob=pickle.load(input_file)
@@ -232,5 +238,4 @@ class off_policy_mc:
         self.total_episode=pickle.load(input_file)
         self.total_time=pickle.load(input_file)
         input_file.close()
-        episode_file.close()
         return
