240d847795ccf96e5a4cec6aeabd07e61f03be94
7NoteDancing <63648431+7NoteDancing@users.noreply.github.com>
2021-06-27 03:01:55 +0800

    Update kernel.py

--- Note/create/RL/kernel.py
+++ Note/create/RL/kernel.py
@@ -35,8 +35,7 @@ class kernel:
         self.beta=None
         self.lr=optimizer.lr
         self.end_loss=None
-        self.t=0
-        self.t_counter=0
+        self.thread=0
         self.one_list=[]
         self.index_list=[]
         self.use_flag=[]
@@ -101,8 +100,7 @@ class kernel:
         if end_loss!=None:
             self.end_loss=end_loss
         if init==True:
-            self.t=0
-            self.t_counter=0
+            self.thread=0
             self.one_list=[]
             self.index_list=[]
             self.use_flag=[]
@@ -358,12 +356,12 @@ class kernel:
     
     
     def learn(self,episode_num,epsilon,i):
-        self.t+=1
-        self.t_counter+=1
-        self.one_list.append(1)
-        self.index_list.append(i)
-        self.use_flag.append(False)
-        self.p=np.array(self.one_list,dtype=np.float16)/self.t_counter
+        if i not in self.finish_list:
+            self.thread+=1
+            self.one_list.append(1)
+            self.index_list.append(i)
+            self.use_flag.append(False)
+        self.p=np.array(self.one_list,dtype=np.float16)/self.thread
         self.a.append(0)
         self.TD.append(0)
         self.loss.append(0)
@@ -408,9 +406,9 @@ class kernel:
                 if self.save_episode==True:
                     self.episode.append(episode)
         self.finish_list.append(i)
-        self.t_counter-=1
+        self.thread-=1
         self.one_list[i]=0
-        self.p=np.array(self.one_list,dtype=np.float16)/self.t_counter
+        self.p=np.array(self.one_list,dtype=np.float16)/self.thread
         self.state_pool[i]=tf.expand_dims(self.state_pool[i][0],axis=0)
         self.action_pool[i]=tf.expand_dims(self.action_pool[i][0],axis=0)
         self.next_state_pool[i]=tf.expand_dims(self.next_state_pool[i][0],axis=0)
@@ -452,7 +450,6 @@ class kernel:
             episode_file=open(path.replace(path[index+1:],'episode.dat'),'wb')
             pickle.dump(self.episode,episode_file)
         self.episode_num=self.epi_num
-        self.thread=self.t
         pickle.dump(self.param,parameter_file)
         pickle.dump(self.state_pool,output_file)
         pickle.dump(self.action_pool,output_file)
@@ -477,10 +474,8 @@ class kernel:
         pickle.dump(self.lr,output_file)
         pickle.dump(self.end_loss,output_file)
         pickle.dump(self.thread,output_file)
-        pickle.dump(self.t_counter,output_file)
         pickle.dump(self.one_list,output_file)
         pickle.dump(self.index_list,output_file)
-        pickle.dump(self.p,output_file)
         pickle.dump(self.finish_list,output_file)
         pickle.dump(self.pool_net,output_file)
         pickle.dump(self.save_episode,output_file)
@@ -526,10 +521,8 @@ class kernel:
         self.lr=pickle.load(input_file)
         self.end_loss=pickle.load(input_file)
         self.thread=pickle.load(input_file)
-        self.t_counter=pickle.load(input_file)
         self.one_list=pickle.load(input_file)
         self.index_list=pickle.load(input_file)
-        self.p=pickle.load(input_file)
         self.finish_list=pickle.load(input_file)
         self.pool_net=pickle.load(input_file)
         self.save_episode=pickle.load(input_file)
