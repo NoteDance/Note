f00e9e17af3bcdf4f2f6080dcfcb0afc0ccdc53f
7NoteDancing <63648431+7NoteDancing@users.noreply.github.com>
2022-01-25 19:01:53 +0800

    Update _Q_learning.py

--- Note/RL/TD/_Q_learning.py
+++ Note/RL/TD/_Q_learning.py
@@ -26,7 +26,6 @@ class Q_learning:
     
     
     def init(self,dtype=np.int32):
-        self.t3=time.time()
         if len(self.action_name)>self.action_len:
             self.action=np.concatenate((self.action,np.arange(len(self.action_name)-self.action_len,dtype=dtype)+self.action_len))
             self.action_prob=np.concatenate((self.action_prob,np.ones(len(self.action_name)-self.action_len,dtype=dtype)))
@@ -36,7 +35,6 @@ class Q_learning:
             self.q=np.concatenate((self.q,np.zeros([len(self.state_name),len(self.action_name)-self.action_len],dtype=self.q.dtype)),axis=1)
             self.q=np.concatenate((self.q,np.zeros([len(self.state_name)-self.state_len,len(self.action_name)],dtype=self.q.dtype)))
             self.q=self.q.numpy()
-        self.t4=time.time()
         return
     
     
@@ -144,9 +142,9 @@ class Q_learning:
         if path!=None:
             self.save(path)
         if self.time<0.5:
-            self.time=int(self.time+(self.t4-self.t3))
+            self.time=int(self.time)
         else:
-            self.time=int(self.time+(self.t4-self.t3))+1
+            self.time=int(self.time)+1
         self.total_time+=self.time
         print()
         print('last delta:{0:.6f}'.format(self.delta))
