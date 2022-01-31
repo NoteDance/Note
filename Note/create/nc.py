4279637548bcea3602a21d3413c704cdc84e2cbc
7NoteDancing <63648431+7NoteDancing@users.noreply.github.com>
2022-01-31 21:44:34 +0800

    Update nc.py

--- Note/create/nc.py
+++ Note/create/nc.py
@@ -2,23 +2,51 @@ class compiler:
     def __init__(self,filename):
         self.filename=filename
         self.init={'z':'tf.zeros(','n':'tf.random.normal(','u':'tf.random.uniform(','o':'tf.ones('}
+        self.operator=['.*','.^','.|','.||','./','.=']
         self.oj1=['','','','']
         self.oj2=['','']
         self.oj3=''
+        self.oj4=['','','']
+        self.oj5=''
+        self.oj6=['','']
         self.line=''
     
     
-    def tf_function(self,oj1=None,oj2=None,oj3=None,init=None):
+    def tf_function(self,oj1=None,oj2=None,oj3=None,oj4=None,oj5=None,oj6=None,init=None):
         if oj1!=None:
             return self.init[init]+oj1[0]+oj1[1]+oj1[2]+oj1[3]+')'
         if oj2!=None:
             return 'tf.matmul('+oj2[0]+','+oj2[1]+')'
         elif oj3!=None:
             return 'tf.reverse('+oj3+')'
+        elif oj4!=None:
+            if oj4[2]=='.|':
+                return 'tf.concat('+'['+oj4[0]+','+oj4[1]+']'+','+'0'+')'
+            else:
+                return 'tf.concat('+'['+oj4[0]+','+oj4[1]+']'+','+'1'+')'
+        elif oj5!=None:
+            return 'tf.math.sqrt('+oj5+')'
+        elif oj6!=None:
+            return 'state_ops.assign('+oj6[0]+','+oj6[1]+')'
+    
+    
+    def concat(self,string_list):
+        line=''
+        for i in range(len(string_list)):
+            if i!=len(string_list)-1:
+                if i==0:
+                    line+=self.line[string_list[i][0]-1:]+self.line.replace(self.line[string_list[i][0]:],string_list[i][2])+self.line[string_list[i][1]:string_list[i+1][0]]
+                else:
+                    line+=string_list[i][2]+self.line[string_list[i][1]:string_list[i+1][0]]
+            else:
+                line+=string_list[i][2]+self.line[string_list[i][1]:]
+        self.line=line
+        return
     
     
     def getchar(self,line):
         self.line=line
+        string_list=[]
         for i in range(len(line)):
             if self.line[i]=='.' and self.line[i+1]==' ':
                 if '=' in self.line:
@@ -53,17 +81,35 @@ class compiler:
                 if '.*' in self.line[index1:index3+1]:
                     self.oj1[0]=self.line[index1+1:index2]
                     self.oj2[1]=self.line[index2+2:index3]
-                    string1=self.line[:index1]
-                    string2=self.line[index3+1:]
-                    string=self.tf_function(self,oj2=self.oj2)
-                    self.line=string1+string+string2
+                    string=self.tf_function(oj2=self.oj2)
+                    string_list.append([index1,index3,string])
                 if '.^' in self.line[index1:index3+1]:
                     self.oj2=self.line[index1+1:index2]
-                    string1=self.line[:index1]
-                    string2=self.line[index3+1:]
-                    string=self.tf_function(self,oj3=self.oj3)
-                    self.line=string1+string+string2
+                    string=self.tf_function(oj3=self.oj3)
+                    string_list.append([index1,index3,string])
+                if '.|' in self.line[index1:index3+1]:
+                    self.oj4[0]=self.line[index1+1:index2]
+                    self.oj4[1]=self.line[index2+2:index3]
+                    self.oj4[2]='.|'
+                    string=self.tf_function(oj4=self.oj4)
+                    string_list.append([index1,index3,string])
+                if '.||' in self.line[index1:index3+1]:
+                    self.oj4[0]=self.line[index1+1:index2]
+                    self.oj4[1]=self.line[index2+3:index3]
+                    self.oj4[2]='.||'
+                    string=self.tf_function(oj4=self.oj4)
+                    string_list.append([index1,index3,string])
+                if './' in self.line[index1:index3+1]:
+                    self.oj5=self.line[index1+1:index2]
+                    string=self.tf_function(oj5=self.oj5)
+                    string_list.append([index1,index3,string])
+                if '.=' in self.line[index1:index3+1]:
+                    self.oj6[0]=self.line[index1+1:index2]
+                    self.oj6[1]=self.line[index2+2:index3]
+                    string=self.tf_function(oj6=self.oj6)
+                    string_list.append([index1,index3,string])
                 index1=None
+        self.concat(string_list)
         return
     
     
@@ -78,7 +124,7 @@ class compiler:
         with open(self.filename) as infile:
             while 1:
                 line=infile.readline()
-                if ('. ' in line and "'" not in line or '"' not in line) or ("'" not in line and '.*'  in line or '.^' in line):
+                if ('. ' in line and "'" not in line or '"' not in line) or ("'" not in line and True in [operator in line for operator in self.operator]):
                     self.readlines(line)
                     outfile.write(self.line)
                     self.line=''
