a2700a82d0b99d7d0f48dc45fbf9c4f9e8ae8a90
7NoteDancing <63648431+7NoteDancing@users.noreply.github.com>
2022-02-26 22:55:41 +0800

    Update kernel.py

--- Note/create/kernel.py
+++ Note/create/kernel.py
@@ -7,8 +7,11 @@ import time
 
 class kernel:
     def __init__(self,nn=None):
-        self.nn=nn
-        self.param=nn.param
+        if nn!=None:
+            self.nn=nn
+            self.param=nn.param
+            self.acc_flag1=nn.acc_flag1
+            self.acc_flag2=nn.acc_flag2
         self.thread=None
         self.ol=None
         self.batch=None
@@ -22,8 +25,6 @@ class kernel:
         self.eflag=None
         self.bflag=None
         self.optf=None
-        self.acc_flag1=nn.acc_flag1
-        self.acc_flag2=nn.acc_flag2
         self.flag=None
         self.train_loss=None
         self.train_acc=None
@@ -755,7 +756,6 @@ class kernel:
             index=path.rfind('\\')
             parameter_file=open(path.replace(path[index+1:],'parameter-{0}.dat'.format(i+1)),'wb')
         pickle.dump(self.param,parameter_file)
-        self.nn.param=None
         pickle.dump(self.nn,output_file)
         pickle.dump(self.ol,output_file)
         pickle.dump(self.batch,output_file)
@@ -770,7 +770,6 @@ class kernel:
         pickle.dump(self.optf,output_file)
         pickle.dump(self.acc_flag1,output_file)
         pickle.dump(self.acc_flag2,output_file)
-        pickle.dump(self.shape0,output_file)
         pickle.dump(self.p,output_file)
         pickle.dump(self.s,output_file)
         pickle.dump(self.flag,output_file)
@@ -815,7 +814,6 @@ class kernel:
         self.optf=pickle.load(input_file)
         self.acc_flag1=pickle.load(input_file)
         self.acc_flag2=pickle.load(input_file)
-        self.shape0=pickle.load(input_file)
         self.p=pickle.load(input_file)
         self.s=pickle.load(input_file)
         self.flag=pickle.load(input_file)
