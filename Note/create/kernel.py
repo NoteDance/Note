e5ea0213d6bf0c2dba86656bba827d889966e7b5
7NoteDancing <63648431+7NoteDancing@users.noreply.github.com>
2022-02-27 22:58:04 +0800

    Update kernel.py

--- Note/create/kernel.py
+++ Note/create/kernel.py
@@ -588,8 +588,8 @@ class kernel:
             path=path+'\save-{0}.dat'.format(i+1)
             index=path.rfind('\\')
             parameter_file=open(path.replace(path[index+1:],'parameter-{0}.dat'.format(i+1)),'wb')
-        pickle.dump(self.param,parameter_file)
         self.nn.param=None
+        pickle.dump(self.param,parameter_file)
         pickle.dump(self.nn,output_file)
         pickle.dump(self.ol,output_file)
         pickle.dump(self.batch,output_file)
