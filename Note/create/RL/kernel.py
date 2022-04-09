de6fe8cccd7bbb5492cab82331fedbee99e924e9
7NoteDancing <63648431+7NoteDancing@users.noreply.github.com>
2022-04-09 14:47:47 +0800

    Update kernel.py

--- Note/create/RL/kernel.py
+++ Note/create/RL/kernel.py
@@ -267,17 +267,11 @@ class kernel:
             self.action_pool[i]=self.action_pool[i][1:]
             self.next_state_pool[i]=self.next_state_pool[i][1:]
             self.reward_pool[i]=self.reward_pool[i][1:]
-        if end:
-            if self.save_episode==True:
-                if self.state_name==None and self.action_name==None:
-                    episode=[s,a,next_s,r]
-                elif self.action_name==None:
-                    episode=[self.state_name[s],a,self.state_name[next_s]]
-                else:
-                    episode=[self.state_name[s],self.action_name[a],self.state_name[next_s],r]
-        elif self.save_episode==True:
+        if self.save_episode==True:
             if self.state_name==None and self.action_name==None:
                 episode=[s,a,next_s,r]
+            elif self.state_name==None:
+                episode=[s,self.action_name[a],next_s,r]
             elif self.action_name==None:
                 episode=[self.state_name[s],a,self.state_name[next_s],r]
             else:
@@ -333,23 +327,27 @@ class kernel:
                     else:
                         next_s,r,end=self.nn.transition(self.state_name[s],a)
             if end:
-                if self.state_name!=None and self.action_name!=None:
-                    episode.append([self.state_name[s],self.action_name[a],self.state_name[next_s]])
-                elif self.state_name!=None:
-                    episode.append([self.state_name[s],a,self.state_name[next_s]])
+                if self.state_name==None and self.action_name==None:
+                    episode.append([s,a,next_s,r])
+                elif self.state_name==None:
+                    episode.append(s,self.action_name[a],next_s,r)
+                elif self.action_name==None:
+                    episode.append([self.state_name[s],a,self.state_name[next_s],r])
                 else:
-                    episode.append([s,a,next_s])
+                    episode.append([self.state_name[s],self.action_name[a],self.state_name[next_s],r])
                 episode.append('end')
                 break
             elif self.stop==True:
                 break
             else:
-                if self.state_name!=None and self.action_name!=None:
-                    episode.append([self.state_name[s],self.action_name[a],self.state_name[next_s]])
-                elif self.state_name!=None:
-                    episode.append([self.state_name[s],a,self.state_name[next_s]])
+                if self.state_name==None and self.action_name==None:
+                    episode.append([s,a,next_s,r])
+                elif self.state_name==None:
+                    episode.append(s,self.action_name[a],next_s,r)
+                elif self.action_name==None:
+                    episode.append([self.state_name[s],a,self.state_name[next_s],r])
                 else:
-                    episode.append([s,a,next_s])
+                    episode.append([self.state_name[s],self.action_name[a],self.state_name[next_s],r])
         return episode
     
     
@@ -818,7 +816,7 @@ class kernel:
                 for _ in range(self.episode_step):
                     if self.stop[i]==True:
                         break
-                    next_s,end,episode=self._explore(s,self.epsilon[i],i)
+                    next_s,end,episode,index=self._explore(s,self.epsilon[i],i)
                     s=next_s
                     if self.state_pool[i]!=None and self.action_pool[i]!=None and self.next_state_pool[i]!=None and self.reward_pool[i]!=None:
                         if self.PO==1:
