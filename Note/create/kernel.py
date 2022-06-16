1cb6ddf9d5297c6955bb670d06a271790e5b1365
7NoteDancing <63648431+7NoteDancing@users.noreply.github.com>
2022-06-16 13:22:05 +0800

    Update kernel.py

--- Note/create/kernel.py
+++ Note/create/kernel.py
@@ -775,7 +775,7 @@ class kernel:
         return
     
     
-    def train(self,batch=None,epoch=None,test_batch=None,nn_path=None,one=True,p=None,s=None):
+    def train(self,batch=None,epoch=None,test_batch=None,file_path=None,one=True,p=None,s=None):
         self.batch=batch
         self.epoch=0
         t1=None
@@ -851,8 +851,8 @@ class kernel:
                                 print('epoch:{0}   loss:{1:.6f}'.format(self.total_epoch+i+1,self.train_loss))
                             else:
                                 print('epoch:{0}   loss:{1:.6f},test loss:{2:.6f}'.format(self.total_epoch+i+1,self.train_loss,self.test_loss))
-                    if nn_path!=None and i%s==0:
-                        self.save(nn_path,self.total_epoch,one)
+                    if file_path!=None and i%s==0:
+                        self.save(file_path,self.total_epoch,one)
                 t2=time.time()
                 if self.thread==None:
                     self.time+=(t2-t1)
@@ -911,8 +911,8 @@ class kernel:
                                 print('epoch:{0}   loss:{1:.6f}'.format(self.total_epoch+i+1,self.train_loss))
                             else:
                                 print('epoch:{0}   loss:{1:.6f},test loss:{2:.6f}'.format(self.total_epoch+i+1,self.train_loss,self.test_loss))
-                    if nn_path!=None and i%s==0:
-                        self.save(nn_path,self.total_epoch,one)
+                    if file_path!=None and i%s==0:
+                        self.save(file_path,self.total_epoch,one)
                 if self.thread==None:
                     try:
                         self.nn.ec+=1
@@ -946,14 +946,14 @@ class kernel:
                 train_loss=self.nn.loss(output,data[1])
                 loss=train_loss.numpy()
                 self.nn.train_loss=loss.astype(np.float32)
-                if nn_path!=None:
-                    self.save(nn_path)
+                if file_path!=None:
+                    self.save(file_path)
                 try:
                     self.nn.ec+=1
                 except AttributeError:
                     pass
-        if nn_path!=None:
-            self.save(nn_path)
+        if file_path!=None:
+            self.save(file_path)
         if self.thread==None:
             self.time=self.time-int(self.time)
             if self.time<0.5:
