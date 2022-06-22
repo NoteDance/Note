2921bca89a7936b910d2c7fbb590a03a7f42c356
7NoteDancing <63648431+7NoteDancing@users.noreply.github.com>
2022-06-22 23:59:56 +0800

    Update kernel.py

--- Note/create/kernel.py
+++ Note/create/kernel.py
@@ -389,8 +389,8 @@ class kernel:
                     pass
         if file_path!=None:
             self.save(file_path)
-        self.time=self.time-int(self.time)
-        if self.time<0.5:
+        self._time=self.time-int(self.time)
+        if self._time<0.5:
             self.time=int(self.time)
         else:
             self.time=int(self.time)+1
