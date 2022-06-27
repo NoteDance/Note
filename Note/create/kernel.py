37bd9db91d5e2017c9cd1d4b280620371efb4515
7NoteDancing <63648431+7NoteDancing@users.noreply.github.com>
2022-06-27 08:08:32 +0800

    Update kernel.py

--- Note/create/kernel.py
+++ Note/create/kernel.py
@@ -1246,10 +1246,10 @@ class kernel:
             index=path.rfind('\\')
             parameter_file=open(path.replace(path[index+1:],'parameter.dat'),'wb')
         else:
-            output_file=open(path+'\save-{0}.dat'.format(i+1),'wb')
+            output_file=open(path+'\save-{0}.dat'.format(i),'wb')
             path=path+'\save-{0}.dat'.format(i+1)
             index=path.rfind('\\')
-            parameter_file=open(path.replace(path[index+1:],'parameter-{0}.dat'.format(i+1)),'wb')
+            parameter_file=open(path.replace(path[index+1:],'parameter-{0}.dat'.format(i)),'wb')
             self.file_list.append(['save-{0}.dat','parameter-{0}.dat'])
             if len(self.file_list)>self.s+1:
                 os.remove(self.file_list[0][0])
