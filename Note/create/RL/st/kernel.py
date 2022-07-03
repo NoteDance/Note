4f881be4dc98a11dc1ab836ba3644f99a6aee436
7NoteDancing <63648431+7NoteDancing@users.noreply.github.com>
2022-07-03 09:36:27 +0800

    Update kernel.py

--- Note/create/RL/st/kernel.py
+++ Note/create/RL/st/kernel.py
@@ -434,15 +434,15 @@ class kernel:
                         self.reward_pool=tf.expand_dims(r,axis=0)
                 else:
                     if self.state==None:
-                        self.state_pool=tf.concat([self.state_pool,tf.expand_dims(s,axis=0)])
-                        self.action_pool=tf.concat([self.action_pool,tf.expand_dims(a,axis=0)])
-                        self.next_state_pool=tf.concat([self.next_state_pool,tf.expand_dims(next_s,axis=0)])
-                        self.reward_pool=tf.concat([self.reward_pool,tf.expand_dims(r,axis=0)])
+                        self.state_pool=tf.concat([self.state_pool,tf.expand_dims(s,axis=0)],0)
+                        self.action_pool=tf.concat([self.action_pool,tf.expand_dims(a,axis=0)],0)
+                        self.next_state_pool=tf.concat([self.next_state_pool,tf.expand_dims(next_s,axis=0)],0)
+                        self.reward_pool=tf.concat([self.reward_pool,tf.expand_dims(r,axis=0)],0)
                     else:
-                        self.state_pool=tf.concat([self.state_pool,tf.expand_dims(self.state[self.state_name[s]],axis=0)])
-                        self.action_pool=tf.concat([self.action_pool,tf.expand_dims(a,axis=0)])
-                        self.next_state_pool=tf.concat([self.next_state_pool,tf.expand_dims(self.state[self.state_name[next_s]],axis=0)])
-                        self.reward_pool=tf.concat([self.reward_pool,tf.expand_dims(r,axis=0)])
+                        self.state_pool=tf.concat([self.state_pool,tf.expand_dims(self.state[self.state_name[s]],axis=0)],0)
+                        self.action_pool=tf.concat([self.action_pool,tf.expand_dims(a,axis=0)],0)
+                        self.next_state_pool=tf.concat([self.next_state_pool,tf.expand_dims(self.state[self.state_name[next_s]],axis=0)],0)
+                        self.reward_pool=tf.concat([self.reward_pool,tf.expand_dims(r,axis=0)],0)
                 if len(self.state_pool)>self.pool_size:
                     self.state_pool=self.state_pool[1:]
                     self.action_pool=self.action_pool[1:]
@@ -525,10 +525,10 @@ class kernel:
                     self.next_state_pool=tf.expand_dims(self.state[self.state_name[next_s]],axis=0)
                     self.reward_pool=tf.expand_dims(r,axis=0)
                 else:
-                    self.state_pool=tf.concat([self.state_pool,tf.expand_dims(self.state[self.state_name[s]],axis=0)])
-                    self.action_pool=tf.concat([self.action_pool,tf.expand_dims(a,axis=0)])
-                    self.next_state_pool=tf.concat([self.next_state_pool,tf.expand_dims(self.state[self.state_name[next_s]],axis=0)])
-                    self.reward_pool=tf.concat([self.reward_pool,tf.expand_dims(r,axis=0)])
+                    self.state_pool=tf.concat([self.state_pool,tf.expand_dims(self.state[self.state_name[s]],axis=0)],0)
+                    self.action_pool=tf.concat([self.action_pool,tf.expand_dims(a,axis=0)],0)
+                    self.next_state_pool=tf.concat([self.next_state_pool,tf.expand_dims(self.state[self.state_name[next_s]],axis=0)],0)
+                    self.reward_pool=tf.concat([self.reward_pool,tf.expand_dims(r,axis=0)],0)
                 if len(self.state_pool)>self.pool_size:
                     self.state_pool=self.state_pool[1:]
                     self.action_pool=self.action_pool[1:]
