d56e4adf5a4c2b7094c93205ac182154831cab0d
7NoteDancing <63648431+7NoteDancing@users.noreply.github.com>
2022-07-03 09:37:59 +0800

    Update kernel.py

--- Note/create/kernel.py
+++ Note/create/kernel.py
@@ -188,14 +188,14 @@ class kernel:
                 index2=batch-(self.shape0-batches*batch)
                 if type(self.train_data)==list:
                     for i in range(len(self.train_data)):
-                        data_batch[i]=tf.concat([self.train_data[i][index1:],self.train_data[i][:index2]])
+                        data_batch[i]=tf.concat([self.train_data[i][index1:],self.train_data[i][:index2]],0)
                 else:
-                    data_batch=tf.concat([self.train_data[index1:],self.train_data[:index2]])
+                    data_batch=tf.concat([self.train_data[index1:],self.train_data[:index2]],0)
                 if type(self.train_labels)==list:
                     for i in range(len(self.train_data)):
-                        labels_batch[i]=tf.concat([self.train_labels[i][index1:],self.train_labels[i][:index2]])
+                        labels_batch[i]=tf.concat([self.train_labels[i][index1:],self.train_labels[i][:index2]],0)
                 else:
-                    labels_batch=tf.concat([self.train_labels[index1:],self.train_labels[:index2]])
+                    labels_batch=tf.concat([self.train_labels[index1:],self.train_labels[:index2]],0)
                 with tf.GradientTape() as tape:
                     output=self.nn.fp(data_batch)
                     batch_loss=self.nn.loss(output,labels_batch)
@@ -453,26 +453,14 @@ class kernel:
                 index2=batch-(shape0-batches*batch)
                 if type(test_data)==list:
                     for i in range(len(test_data)):
-                        if type(test_data)==np.ndarray:
-                            data_batch[i]=np.concatenate(test_data[i][index1:],test_data[i][:index2])
-                        else:
-                            data_batch[i]=tf.concat(test_data[i][index1:],test_data[i][:index2])
+                        data_batch[i]=tf.concat(test_data[i][index1:],test_data[i][:index2],0)
                 else:
-                    if type(test_data)==np.ndarray:
-                        data_batch=np.concatenate(test_data[index1:],test_data[:index2])
-                    else:
-                        data_batch=tf.concat(test_data[index1:],test_data[:index2])
+                    data_batch=tf.concat(test_data[index1:],test_data[:index2],0)
                 if type(self.test_labels)==list:
                     for i in range(len(test_labels)):
-                        if type(test_labels)==np.ndarray:
-                            labels_batch[i]=np.concatenate(test_labels[i][index1:],test_labels[i][:index2])
-                        else:
-                            labels_batch[i]=tf.concat(test_labels[i][index1:],test_labels[i][:index2])
+                        labels_batch[i]=tf.concat(test_labels[i][index1:],test_labels[i][:index2],0)
                 else:
-                    if type(test_labels)==np.ndarray:
-                        labels_batch=np.concatenate(test_labels[index1:],test_labels[:index2])
-                    else:
-                        labels_batch=tf.concat(test_labels[index1:],test_labels[:index2])
+                    labels_batch=tf.concat(test_labels[index1:],test_labels[:index2],0)
                 output=self.nn.fp(data_batch)
                 batch_loss=self.nn.loss(output,labels_batch)
                 total_loss+=batch_loss
