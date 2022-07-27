f0fd70846222bbdc6fd3bda581066c96be49e7a9
7NoteDancing <63648431+7NoteDancing@users.noreply.github.com>
2022-07-27 13:47:31 +0800

    Update kernel.py

--- Note/create/RL/st/kernel.py
+++ Note/create/RL/st/kernel.py
@@ -750,7 +750,9 @@ class kernel:
         self.episode_num=self.epi_num
         pickle.dump(self.nn.param,parameter_file)
         self.nn.param=None
+        self.nn.opt=None
         pickle.dump(self.nn,output_file)
+        pickle.dump(self.opt.get_config(),output_file)
         pickle.dump(self.ol,output_file)
         pickle.dump(self.state_pool,output_file)
         pickle.dump(self.action_pool,output_file)
@@ -794,7 +796,7 @@ class kernel:
             self.nn.km=1
         except AttributeError:
             pass
-        self.opt=self.nn.opt
+        self.config=pickle.load(input_file)
         self.ol=pickle.load(input_file)
         self.state_pool=pickle.load(input_file)
         self.action_pool=pickle.load(input_file)
