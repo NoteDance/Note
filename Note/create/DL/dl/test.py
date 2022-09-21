8a7437c2488b1344a734f529e732ca978b4033f1
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2022-09-21 18:26:58 +0800

    Add files via upload

---
+++ Note/create/DL/dl/test.py
@@ -0,0 +1,112 @@
+import numpy as np
+
+
+def test(nn,test_data,test_labels,platform,batch=None,acc_flag='%'):
+    if type(test_data)==list:
+        data_batch=[x for x in range(len(test_data))]
+    if type(test_labels)==list:
+        labels_batch=[x for x in range(len(test_labels))]
+    if batch!=None:
+        total_loss=0
+        total_acc=0
+        if type(test_data)==list:
+            batches=int((test_data[0].shape[0]-test_data[0].shape[0]%batch)/batch)
+            shape0=test_data[0].shape[0]
+        else:
+            batches=int((test_data.shape[0]-test_data.shape[0]%batch)/batch)
+            shape0=test_data.shape[0]
+        for j in range(batches):
+            index1=j*batch
+            index2=(j+1)*batch
+            if type(test_data)==list:
+                for i in range(len(test_data)):
+                    data_batch[i]=test_data[i][index1:index2]
+            else:
+                data_batch=test_data[index1:index2]
+            if type(test_labels)==list:
+                for i in range(len(test_labels)):
+                    labels_batch[i]=test_labels[i][index1:index2]
+            else:
+                labels_batch=test_labels[index1:index2]
+            output=nn.fp(data_batch)
+            batch_loss=nn.loss(output,labels_batch)
+            total_loss+=batch_loss
+            try:
+                if nn.accuracy!=None:
+                    pass
+                batch_acc=nn.accuracy(output,labels_batch)
+                total_acc+=batch_acc
+            except AttributeError:
+                pass
+        if shape0%batch!=0:
+            batches+=1
+            index1=batches*batch
+            index2=batch-(shape0-batches*batch)
+            try:
+                if type(test_data)==list:
+                    for i in range(len(test_data)):
+                        data_batch[i]=platform.concat([test_data[i][index1:],test_data[i][:index2]],0)
+                else:
+                    data_batch=platform.concat([test_data[index1:],test_data[:index2]],0)
+                if type(test_labels)==list:
+                    for i in range(len(test_labels)):
+                        labels_batch[i]=platform.concat([test_labels[i][index1:],test_labels[i][:index2]],0)
+                else:
+                    labels_batch=platform.concat([test_labels[index1:],test_labels[:index2]],0)
+            except:
+                if type(test_data)==list:
+                    for i in range(len(test_data)):
+                        data_batch[i]=platform.concat([test_data[i][index1:],test_data[i][:index2]],0)
+                else:
+                    data_batch=platform.concat([test_data[index1:],test_data[:index2]],0)
+                if type(test_labels)==list:
+                    for i in range(len(test_labels)):
+                        labels_batch[i]=platform.concat([test_labels[i][index1:],test_labels[i][:index2]],0)
+                else:
+                    labels_batch=platform.concat([test_labels[index1:],test_labels[:index2]],0)
+            output=nn.fp(data_batch)
+            batch_loss=nn.loss(output,labels_batch)
+            total_loss+=batch_loss
+            try:
+                if nn.accuracy!=None:
+                    pass
+                batch_acc=nn.accuracy(output,labels_batch)
+                total_acc+=batch_acc
+            except AttributeError:
+                pass
+        test_loss=total_loss.numpy()/batches
+        test_loss=test_loss
+        test_loss=test_loss.astype(np.float32)
+        try:
+            if nn.accuracy!=None:
+                pass
+            test_acc=total_acc.numpy()/batches
+            test_acc=test_acc
+            test_acc=test_acc.astype(np.float32)
+        except AttributeError:
+            pass
+    else:
+        output=nn.fp(test_data)
+        test_loss=nn.loss(output,test_labels)
+        test_loss=test_loss.numpy().astype(np.float32)
+        try:
+            if nn.accuracy!=None:
+                pass
+            test_acc=nn.accuracy(output,test_labels)
+            test_acc=test_acc.numpy().astype(np.float32)
+        except AttributeError:
+            pass
+    print('test loss:{0:.6f}'.format(test_loss))
+    try:
+        if nn.accuracy!=None:
+            pass
+        if acc_flag=='%':
+            print('accuracy:{0:.1f}'.format(test_acc*100))
+        else:
+            print('accuracy:{0:.6f}'.format(test_acc))
+        if acc_flag=='%':
+            return test_loss,test_acc*100
+        else:
+            return test_loss,test_acc
+    except AttributeError:
+        return test_loss
