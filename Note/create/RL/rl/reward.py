a99627a34945d1fa46ca3d0211fb7c2fcd419e5e
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2022-09-22 14:27:24 +0800

    Update reward.py

--- Note/create/RL/rl/reward.py
+++ Note/create/RL/rl/reward.py
@@ -2,40 +2,57 @@ import torch
 import numpy as np
 
 
-def reward(nn,env,max_step=None,device='cuda'):
-    r=0
-    state=env.reset()
-    if max_step!=None:
-        for i in range(max_step):
-            state=torch.tensor(np.expand_dims(state,0),dtype=torch.float)
-            try:
-                if nn.nn!=None:
-                    pass
-                action_prob=nn.nn(state.to(device))
-                action=np.argmax(action_prob).numpy()
-            except AttributeError:
-                action=nn.actor(state.to(device))
-                action=np.squeeze(action).numpy()
-            state,reward,done,_=env.step(action)
-            state=state
-            r+=reward
-            if done:
-                break
-        return r
-    else:
-        while True:
-            state=torch.tensor(np.expand_dims(state,0),dtype=torch.float)
-            try:
-                if nn.nn!=None:
-                    pass
-                action_prob=nn.nn(state.to(device))
-                action=np.argmax(action_prob).numpy()
-            except AttributeError:
-                action=nn.actor(state.to(device))
-                action=np.squeeze(action).numpy()
-            state,reward,done,_=env.step(action)
-            state=state
-            r+=reward
-            if done:
-                break
-        return r
+class reward:
+    def __init__(self,nn,env,device='cuda'):
+        self.nn=nn
+        self.env=env
+        self.end_flag=False
+        self.device=device
+        
+    
+    def reward(self,max_step=None):
+        r=0
+        state=self.env.reset()
+        if max_step!=None:
+            for i in range(max_step):
+                if self.end_flag==True:
+                    break
+                state=torch.tensor(np.expand_dims(state,0),dtype=torch.float).to(self.device)
+                try:
+                    if self.nn.nn!=None:
+                        pass
+                    try:
+                        if self.nn.action!=None:
+                            pass
+                        action=self.nn.action(state)
+                    except AttributeError:
+                        action_prob=self.nn.nn(state)
+                        action=np.argmax(action_prob).numpy()
+                except AttributeError:
+                    action=self.nn.actor(state)
+                    action=np.squeeze(action).numpy()
+                state,reward,done,_=self.env.step(action)
+                state=state
+                r+=reward
+                if done:
+                    break
+            return r
+        else:
+            while True:
+                if self.end_flag==True:
+                    break
+                state=torch.tensor(np.expand_dims(state,0),dtype=torch.float).to(self.device)
+                try:
+                    if self.nn.nn!=None:
+                        pass
+                    action_prob=self.nn.nn(state)
+                    action=np.argmax(action_prob).numpy()
+                except AttributeError:
+                    action=self.nn.actor(state)
+                    action=np.squeeze(action).numpy()
+                state,reward,done,_=self.env.step(action)
+                state=state
+                r+=reward
+                if done:
+                    break
+            return r
