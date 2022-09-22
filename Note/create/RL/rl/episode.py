3c5eb7807fa59e4a3f9890d17e38955a6dc0bc80
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2022-09-22 14:28:00 +0800

    Add files via upload

---
+++ Note/create/RL/rl/episode.py
@@ -0,0 +1,44 @@
+import torch
+import numpy as np
+
+
+class episode:
+    def __init__(self,nn,env,device='cuda'):
+        self.nn=nn
+        self.env=env
+        self.end_flag=False
+        self.device=device
+    
+    
+    def get_episode(self):
+        s=self.env.reset()
+        episode=[]
+        while True:
+            s=torch.tensor(s,dtype=torch.float).to(self.device)
+            try:
+                if self.nn.nn!=None:
+                    pass
+                try:
+                    if self.nn.action!=None:
+                        pass
+                    s=np.expand_dims(s,axis=0)
+                    a=self.nn.action(s)
+                except AttributeError:
+                    s=np.expand_dims(s,axis=0)
+                    a=np.argmax(self.nn.nn(s)).numpy()
+                next_s,r,done=self.env(a)
+            except AttributeError:
+                s=np.expand_dims(s,axis=0)
+                a=self.nn.actor(s).numpy()
+                a=np.squeeze(a)
+                next_s,r,done=self.nn.env(a)
+            if self.end_flag==True:
+                break
+            elif done:
+                episode.append([s,a,next_s,r])
+                episode.append('done')
+                break
+            else:
+                episode.append([s,a,next_s,r])
+            s=next_s
+        return episode
