4db4df57f83bd5590eec7ecc5b236a2b0f663d5c
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2022-10-22 09:54:36 +0800

    Update attenuate.py

--- Note/create/RL/rl/attenuate.py
+++ Note/create/RL/rl/attenuate.py
@@ -1,9 +1,9 @@
-def attenuate(attenuate,nn,t):
+def attenuate(attenuate,nn,oc):
     if type(nn)==list:
         for i in range(len(nn)):
             for param in nn[i].parameters():
-                param.grad=attenuate(t)*param
+                param.grad=attenuate(oc)*param.grad
     else:
         for param in nn.parameters():
-            param.grad=attenuate(t)*param
+            param.grad=attenuate(oc)*param.grad
     return
