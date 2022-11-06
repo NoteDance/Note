0a850c7127d22abe623323f52d042dcacb174db8
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2022-11-06 09:32:19 +0800

    Update and rename attenuate.py to assign_a.py

--- Note/create/RL/rl/attenuate.py
+++
@@ -1,9 +0,0 @@
-def attenuate(attenuate,nn,oc):
-    if type(nn)==list:
-        for i in range(len(nn)):
-            for param in nn[i].parameters():
-                param.grad=attenuate(oc)*param.grad
-    else:
-        for param in nn.parameters():
-            param.grad=attenuate(oc)*param.grad
-    return
