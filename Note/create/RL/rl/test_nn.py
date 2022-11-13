e32f21543c06d12120541a80492ab87fc9a1c249
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2022-11-13 21:59:17 +0800

    Add files via upload

---
+++ Note/create/RL/rl/test_nn.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+
+def test(nn,epsilon=None,action_num=None):
+    s=nn.env(initial=True)
+    try:
+        if nn.nn!=None:
+            s=np.expand_dims(s,axis=0)
+            try:
+                if nn.action!=None:
+                    a=nn.action(s)
+                    try:
+                        if nn.discriminator!=None:
+                            s=np.squeeze(s)
+                    except AttributeError:
+                        pass
+            except AttributeError:
+                a=np.random.choice(action_num)
+            next_s,r,done=nn.env(a)
+    except AttributeError:
+        s=np.expand_dims(s,axis=0)
+        a=(nn.actor(s)+nn.noise()).numpy()
+        next_s,r,done=nn.env(a)
+    try:
+        if nn.pr!=None:
+            nn.pr.TD=np.append(nn.pr.TD,nn.initial_TD)
+    except AttributeError: 
+        pass
+    loss=nn.loss(s,a,next_s,r,done)
+    nn.opt(loss)
+    print('No error')
+    return
