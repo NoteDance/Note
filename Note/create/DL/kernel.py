7b4621c176ff7439e33e7ee8d739bd178467e9f7
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2022-11-22 19:27:21 +0800

    Update kernel.py

--- Note/create/DL/kernel.py
+++ Note/create/DL/kernel.py
@@ -1223,7 +1223,10 @@ class kernel:
                 self.nn.oopt=None
                 pickle.dump(self.nn,output_file)
                 self.nn.oopt=opt
-        pickle.dump(opt.get_config(),output_file)
+        try:
+            pickle.dump(opt.get_config(),output_file)
+        except:
+            pickle.dump(None,output_file)
         pickle.dump(self.ol,output_file)
         pickle.dump(self.batch,output_file)
         pickle.dump(self.end_loss,output_file)
