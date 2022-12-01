44f788769bf5c95a438adaf8317602ed1b375ff0
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2022-12-01 14:06:42 +0800

    Update kernel.py

--- Note/create/RL/nspn/kernel.py
+++ Note/create/RL/nspn/kernel.py
@@ -26,7 +26,6 @@ class kernel:
         self.state=state
         self.state_name=state_name
         self.action_name=action_name
-        self.action_num=None
         self.epsilon=None
         self.episode_step=None
         self.pool_size=None
@@ -41,6 +40,7 @@ class kernel:
         self.save_epi=None
         self.train_counter=0
         self.end_loss=None
+        self.max_episode_num=None
         self.save_episode=save_episode
         self.loss=None
         self.loss_list=[]
@@ -50,19 +50,10 @@ class kernel:
         self.total_time=0
     
     
-    def action_vec(self,action_num=None):
+    def action_vec(self):
         if self.action_name!=None:
-            self.action_num=len(self.action_name)
-        if self.action_name!=None and len(self.action_name)>self.action_num:
-            if self.epsilon!=None:
-                self.action_one=np.concatenate((self.action_one,np.ones(len(self.action_name)-self.action_num,dtype=np.int8)))
-        elif self.action_name!=None:
             if self.epsilon!=None:
                 self.action_one=np.ones(len(self.action_name),dtype=np.int8)
-        if action_num>self.action_num:
-            if self.epsilon!=None:
-                self.action_one=np.concatenate((self.action_one,np.ones(action_num-self.action_num,dtype=np.int8)))
-                self.action_num=action_num
         else:
             if self.epsilon!=None:
                 self.action_one=np.ones(self.action_num,dtype=np.int8)
@@ -86,7 +77,7 @@ class kernel:
             self.criterion=criterion
         if end_loss!=None:
             self.end_loss=end_loss
-        self.action_vec(self.action_num)
+        self.action_vec()
         if init==True:
             self.episode=[]
             self.state_pool=None
@@ -568,6 +559,8 @@ class kernel:
                     if done:
                         episode.append('done')
                     self.episode.append(episode)
+                    if self.max_episode_num!=None and len(self.episode)>=self.max_episode_num:
+                        self.save_episode=False
                 try:
                     self.nn.ec+=1
                 except AttributeError:
@@ -635,6 +628,8 @@ class kernel:
                     if done:
                         episode.append('done')
                     self.episode.append(episode)
+                    if self.max_episode_num!=None and len(self.episode)>=self.max_episode_num:
+                        self.save_episode=False
                 try:
                     self.nn.ec+=1
                 except AttributeError:
@@ -786,14 +781,6 @@ class kernel:
                 del self.file_list[0]
         pickle.dump(self.nn,output_file)
         pickle.dump(self.ol,output_file)
-        pickle.dump(self.state_pool,output_file)
-        pickle.dump(self.action_pool,output_file)
-        pickle.dump(self.next_state_pool,output_file)
-        pickle.dump(self.reward_pool,output_file)
-        pickle.dump(self.done_pool,output_file)
-        pickle.dump(self.action_num,output_file)
-        pickle.dump(self.action,output_file)
-        pickle.dump(self.action_one,output_file)
         pickle.dump(self.epsilon,output_file)
         pickle.dump(self.episode_step,output_file)
         pickle.dump(self.pool_size,output_file)
@@ -801,6 +788,7 @@ class kernel:
         pickle.dump(self.update_step,output_file)
         pickle.dump(self.train_counter,output_file)
         pickle.dump(self.end_loss,output_file)
+        pickle.dump(self.max_episode_num,output_file)
         pickle.dump(self.save_episode,output_file)
         pickle.dump(self.reward_list,output_file)
         pickle.dump(self.loss,output_file)
@@ -824,14 +812,6 @@ class kernel:
         except AttributeError:
             pass
         self.ol=pickle.load(input_file)
-        self.state_pool=pickle.load(input_file)
-        self.action_pool=pickle.load(input_file)
-        self.next_state_pool=pickle.load(input_file)
-        self.reward_pool=pickle.load(input_file)
-        self.done_pool=pickle.load(input_file)
-        self.action_num=pickle.load(input_file)
-        self.action=pickle.load(input_file)
-        self.action_one=pickle.load(input_file)
         self.epsilon=pickle.load(input_file)
         self.episode_step=pickle.load(input_file)
         self.pool_size=pickle.load(input_file)
@@ -839,6 +819,7 @@ class kernel:
         self.update_step=pickle.load(input_file)
         self.train_counter=pickle.load(input_file)
         self.end_loss=pickle.load(input_file)
+        self.max_episode_num=pickle.load(input_file)
         self.save_episode=pickle.load(input_file)
         self.reward_list=pickle.load(input_file)
         self.loss=pickle.load(input_file)
