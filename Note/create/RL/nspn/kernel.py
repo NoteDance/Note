0c1d431f024f20e298807da0be352d90ac1b9714
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2022-12-04 23:39:42 +0800

    Update kernel.py

--- Note/create/RL/nspn/kernel.py
+++ Note/create/RL/nspn/kernel.py
@@ -177,7 +177,7 @@ class kernel:
     
     
     def pool(self,s,a,next_s,r,done):
-        if type(self.state_pool)!=np.ndarray:
+        if type(self.state_pool)!=np.ndarray and self.state_pool==None:
             self.state_pool=s
             if type(a)==int:
                 a=np.array(a,np.int64)
