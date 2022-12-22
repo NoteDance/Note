714dee72a32ef7a52e908e438ad54643be2234d4
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2022-12-22 18:39:35 +0800

    Update assign.py

--- Note/create/RL/rl/assign.py
+++ Note/create/RL/rl/assign.py
@@ -1,4 +1,13 @@
-def assign(nn,grad):
+from tensorflow.python.ops import state_ops
+
+
+def assign(param,new_param):
+    for i in range(len(param)):
+        state_ops.assign(param[i],new_param[i])
+    return
+
+
+def assign_pt(nn,grad):
     i=0
     for param in nn.parameters():
         param.grad=grad[i]
