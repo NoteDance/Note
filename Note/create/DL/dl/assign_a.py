7bebb421cf572ee82869d4b0da5a7e356d853099
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2022-12-23 13:46:38 +0800

    Delete assign_a.py

--- Note/create/DL/dl/assign_a.py
+++
@@ -1,8 +0,0 @@
-def assign(nn,ac,grad=None):
-    if grad==None:
-        for param in nn.parameters():
-            param.grad=ac*param.grad
-    else:
-        for param in nn.parameters():
-            param.grad=ac*grad
-    return
