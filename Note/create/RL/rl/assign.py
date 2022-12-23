4dacab4e91f25c93b5168d6e9418270289491193
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2022-12-23 13:47:50 +0800

    Update assign.py

--- Note/create/RL/rl/assign.py
+++ Note/create/RL/rl/assign.py
@@ -5,11 +5,3 @@ def assign(param,new_param):
     for i in range(len(param)):
         state_ops.assign(param[i],new_param[i])
     return
-
-
-def assign_pt(nn,grad):
-    i=0
-    for param in nn.parameters():
-        param.grad=grad[i]
-        i+=1
-    return
