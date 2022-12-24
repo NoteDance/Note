128c5f4231d0ce2ffa6e9be4df2dfb72ba7ae025
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2022-12-24 21:21:12 +0800

    Update kernel.py

--- Note/create/DL/kernel.py
+++ Note/create/DL/kernel.py
@@ -1576,7 +1576,6 @@ class kernel:
             pickle.dump(self.test_loss_list,output_file)
             pickle.dump(self.test_acc_list,output_file)
         pickle.dump(self.total_epoch,output_file)
-        pickle.dump(self.total_epoch,output_file)
         pickle.dump(self.total_time,output_file)
         output_file.close()
         try:
