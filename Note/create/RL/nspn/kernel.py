6745efd0c4783f2071dc5f593eb7a97e54b18915
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2022-12-25 22:45:40 +0800

    Update kernel.py

--- Note/create/RL/nspn/kernel.py
+++ Note/create/RL/nspn/kernel.py
@@ -745,72 +745,33 @@ class kernel:
     def save(self,i=None,one=True):
         if one==True:
             output_file=open('save.dat','wb')
-            try:
-                if len(self.nn.model.weights)==self.nn.param:
-                    pass
-                else:
-                    parameter_file=open('param.dat','wb')
-            except AttributeError:
-                try:
-                    if self.platform.DType!=None:
-                        parameter_file=open('param.dat','wb')
-                except AttributeError:
-                    pass
             if self.save_episode==True:
                 episode_file=open('episode.dat','wb')
                 pickle.dump(self.episode,episode_file)
                 episode_file.close()
         else:
             output_file=open('save-{0}.dat'.format(i),'wb')
-            try:
-                if len(self.nn.model.weights)==self.nn.param:
-                    self.file_list.append(['save-{0}.dat'])
-                    if len(self.file_list)>self.s+1:
-                        os.remove(self.file_list[0][0])
-                        del self.file_list[0]
-                else:
-                    parameter_file=open('param-{0}.dat'.format(i),'wb')
-                    self.file_list.append(['save-{0}.dat','param-{0}.dat'])
-                    if len(self.file_list)>self.s+1:
-                        os.remove(self.file_list[0][0])
-                        os.remove(self.file_list[0][1])
-                        del self.file_list[0]
-            except AttributeError:
-                try:
-                    if self.platform.DType!=None:
-                        parameter_file=open('param-{0}.dat'.format(i),'wb')
-                        self.file_list.append(['save-{0}.dat','param-{0}.dat'])
-                        if len(self.file_list)>self.s+1:
-                            os.remove(self.file_list[0][0])
-                            os.remove(self.file_list[0][1])
-                            del self.file_list[0]
-                except AttributeError:
-                    self.file_list.append(['save-{0}.dat'])
-                    if len(self.file_list)>self.s+1:
-                        os.remove(self.file_list[0][0])
-                        del self.file_list[0]
+            self.file_list.append(['save-{0}.dat'])
+            if len(self.file_list)>self.s+1:
+                os.remove(self.file_list[0][0])
+                del self.file_list[0]
             if self.save_episode==True:
                 episode_file=open('episode-{0}.dat'.format(i),'wb')
                 pickle.dump(self.episode,episode_file)
                 episode_file.close()
             if self.save_episode==True:
-                self.file_list.append(['save-{0}.dat','param-{0}.dat','episode-{0}.dat'])
+                self.file_list.append(['save-{0}.dat','episode-{0}.dat'])
+                if len(self.file_list)>self.s+1:
+                    os.remove(self.file_list[0][0])
+                    os.remove(self.file_list[0][1])
+                    del self.file_list[0]
             else:
-                self.file_list.append(['save-{0}.dat','param-{0}.dat'])
-            if len(self.file_list)>self.s+1:
-                os.remove(self.file_list[0][0])
-                os.remove(self.file_list[0][1])
-                os.remove(self.file_list[0][2])
-                del self.file_list[0]
+                self.file_list.append(['save-{0}.dat'])
+                if len(self.file_list)>self.s+1:
+                    os.remove(self.file_list[0][0])
+                    del self.file_list[0]
         try:
             if self.platform.DType!=None:
-                try:
-                    if len(self.nn.model.weights)!=self.nn.param:
-                        pickle.dump(self.nn.param[:-len(self.nn.model.weights)],parameter_file)
-                except AttributeError:
-                    pickle.dump(self.nn.param,parameter_file)
-                if self.training_flag==False:
-                    self.nn.param=None
                 try:
                     pickle.dump(self.nn,output_file)
                 except AttributeError:
