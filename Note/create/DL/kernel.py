bddd1c424052f13ea6372a1973e064e46aad53f3
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2023-01-15 22:25:56 +0800

    Update kernel.py

--- Note/create/DL/kernel.py
+++ Note/create/DL/kernel.py
@@ -102,10 +102,6 @@ class kernel:
             except AttributeError:
                 pass
             try:
-                self.nn.ec=np.zeros(self.thread,dtype=np.float32)
-            except AttributeError:
-                pass
-            try:
                 self.nn.bc=np.zeros(self.thread,dtype=np.float32)
             except AttributeError:
                 pass
@@ -130,10 +126,6 @@ class kernel:
         if self.thread!=None:
             self.thread_num=np.arange(self.thread)
             self.thread_num=list(self.thread_num)
-            try:
-                self.nn.ec=np.zeros(self.thread,dtype=np.float32)
-            except AttributeError:
-                pass
             try:
                 self.nn.bc=np.zeros(self.thread,dtype=np.float32)
             except AttributeError:
@@ -184,10 +176,6 @@ class kernel:
                 self.opt_counter=np.concatenate((self.opt_counter,np.zeros(thread,dtype=np.float32)))
             else:
                 self.opt_counter=np.zeros(self.thread,dtype=np.float32)
-        except AttributeError:
-            pass
-        try:
-            self.nn.ec=np.concatenate((self.nn.ec,np.zeros(thread,dtype=np.float32)))
         except AttributeError:
             pass
         try:
@@ -974,16 +962,10 @@ class kernel:
                         return
                 if self.stop_flag==True:
                     return
-                if self.thread==None:
-                    try:
-                        self.nn.ec+=1
-                    except AttributeError:
-                        pass
-                else:
-                    try:
-                        self.nn.ec[t]+=1
-                    except AttributeError:
-                        pass
+                try:
+                    self.nn.ec+=1
+                except AttributeError:
+                    pass
                 if self.thread==None:
                     self.total_epoch+=1
                 if self.thread==None:
@@ -1083,16 +1065,10 @@ class kernel:
                 if self.stop_flag==True:
                     return
                 i+=1
-                if self.thread==None:
-                    try:
-                        self.nn.ec+=1
-                    except AttributeError:
-                        pass
-                else:
-                    try:
-                        self.nn.ec[t]+=1
-                    except AttributeError:
-                        pass
+                try:
+                    self.nn.ec+=1
+                except AttributeError:
+                    pass
                 if self.thread==None:
                     self.total_epoch+=1
                 if self.thread==None:
