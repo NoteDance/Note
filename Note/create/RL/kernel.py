28606b8aabb53565eb5187fc9596af4f3597b560
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2023-01-15 22:32:15 +0800

    Update kernel.py

--- Note/create/RL/kernel.py
+++ Note/create/RL/kernel.py
@@ -581,7 +581,7 @@ class kernel:
             self.loss[t]=self.loss[t]/batches
         self.sc[t]+=1
         try:
-            self.nn.ec[t]+=1
+            self.nn.ec+=1
         except AttributeError:
             pass
         return
@@ -623,10 +623,6 @@ class kernel:
             epsilon=self.epsilon[t]
         except:
             epsilon=None
-        try:
-            self.nn.ec.append(0)
-        except AttributeError:
-            pass
         try:
             self.nn.bc.append(0)
         except AttributeError:
