89db1d532dc3403b2a892180a30a8cb0a0211f49
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2023-01-22 00:26:20 +0800

    Update kernel.py

--- Note/create/RL/nspn/kernel.py
+++ Note/create/RL/nspn/kernel.py
@@ -812,7 +812,10 @@ class kernel:
         except AttributeError:
             pass
         opt_serialized=pickle.load(input_file)
-        self.nn.opt=self.platform.keras.optimizers.deserialize(opt_serialized)
+        try:
+            self.nn.opt=self.platform.keras.optimizers.deserialize(opt_serialized)
+        except TypeError:
+            pass
         self.epsilon=pickle.load(input_file)
         self.episode_step=pickle.load(input_file)
         self.pool_size=pickle.load(input_file)
