2c3446d216cbb0347cf9b7c21f7e60c5ea200ae4
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2023-01-22 17:22:36 +0800

    Update kernel.py

--- Note/create/DL/kernel.py
+++ Note/create/DL/kernel.py
@@ -1396,7 +1396,7 @@ class kernel:
         try:
             self.nn.opt=self.platform.keras.optimizers.deserialize(opt_serialized)
         except:
-            self.nn.opt=self.nn.deserialize(opt_serialized)
+            self.nn.deserialize(opt_serialized)
         else:
             pass
         self.ol=pickle.load(input_file)
