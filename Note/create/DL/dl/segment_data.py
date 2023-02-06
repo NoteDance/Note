d3f41e4b504788febb202f447a0c9896c0e58bf7
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2023-02-06 18:22:09 +0800

    Update segment_data.py

--- Note/create/DL/dl/segment_data.py
+++ Note/create/DL/dl/segment_data.py
@@ -1,12 +1,12 @@
 import numpy as np
 
 
-def segment_data(data,labels,thread):
-    if len(data)!=thread:
+def segment_data(data,labels,pt_count):
+    if len(data)!=pt_count:
         data_=None
         labels_=None
-        segments=int((len(data)-len(data)%thread)/thread)
-        for i in range(thread):
+        segments=int((len(data)-len(data)%pt_count)/pt_count)
+        for i in range(pt_count):
             index1=i*segments
             index2=(i+1)*segments
             if i==0:
@@ -15,10 +15,10 @@ def segment_data(data,labels,thread):
             else:
                 data_=np.concatenate((data_,np.expand_dims(data[index1:index2],axis=0)))
                 labels_=np.concatenate((labels_,np.expand_dims(labels[index1:index2],axis=0)))
-        if len(data)%thread!=0:
+        if len(data)%pt_count!=0:
             segments+=1
-            index1=segments*thread
-            index2=thread-(len(data)-segments*thread)
+            index1=segments*pt_count
+            index2=pt_count-(len(data)-segments*pt_count)
             data_=np.concatenate((data_,np.expand_dims(data[index1:index2],axis=0)))
             labels_=np.concatenate((labels_,np.expand_dims(labels[index1:index2],axis=0)))
         return data_,labels_
