ce781ee2efb695d6c31f4d73198941e7d5efe1af
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2023-02-12 19:44:48 +0800

    Update kernel.py

--- Note/create/RL/kernel.py
+++ Note/create/RL/kernel.py
@@ -529,7 +529,7 @@ class kernel:
     
     
     def end(self):
-        if self.end_loss!=None and self.loss_list[-1]<=self.end_loss:
+        if len(self.loss_list)!=0 and self.end_loss!=None and self.loss_list[-1]<=self.end_loss:
             return True
     
     
@@ -1331,7 +1331,7 @@ class kernel:
             self.save_flag=True
             self.stop_flag=True
             return True
-        else:
+        elif self.end_loss==None:
             self.stop_flag=True
             return True
         return False
