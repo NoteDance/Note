3031adfdfb54848d00cac9affe3ae85bda0cf8ce
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2023-02-16 14:51:51 +0800

    Update test_nn.py

--- Note/create/RL/rl/test_nn.py
+++ Note/create/RL/rl/test_nn.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def test(nn,action_num=None):
+def test(nn,platform,action_num=None):
     s=nn.env(initial=True)
     try:
         if nn.nn!=None:
@@ -29,6 +29,29 @@ def test(nn,action_num=None):
     a=np.array(a)
     a=np.expand_dims(a,axis=0)
     next_s=np.expand_dims(next_s,axis=0)
-    nn.loss(s,a,next_s,r,done)
+    try:
+        if platform.DType!=None: 
+            with platform.GradientTape(persistent=True) as tape:
+                loss=nn.loss(s,a,next_s,r,done)
+            try:
+                gradient=nn.gradient(tape,loss)
+                try:
+                    nn.opt.apply_gradients(zip(gradient,nn.param))
+                except AttributeError:
+                    nn.opt(gradient)
+            except AttributeError:
+                try:
+                    if nn.nn!=None:
+                        gradient=tape.gradient(loss,nn.param)
+                        nn.opt.apply_gradients(zip(gradient,nn.param))
+                except AttributeError:
+                        actor_gradient=tape.gradient(loss[0],nn.param[0])
+                        critic_gradient=tape.gradient(loss[1],nn.param[1])
+                        nn.opt.apply_gradients(zip(actor_gradient,nn.param[0]))
+                        nn.opt.apply_gradients(zip(critic_gradient,nn.param[1]))
+    except AttributeError:
+        loss=nn.loss(s,a,next_s,r,done)
+        nn.backward(loss)
+        nn.opt()
     print('No error')
     return
