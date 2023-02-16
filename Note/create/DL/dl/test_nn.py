2226e8752e1e4f142db019e4def3779ae0830c36
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2023-02-16 13:58:33 +0800

    Update test_nn.py

--- Note/create/DL/dl/test_nn.py
+++ Note/create/DL/dl/test_nn.py
@@ -1,44 +1,13 @@
-def test(nn,platform,data,labels):
+def test(nn,data,labels):
     try:
-        if platform.DType!=None:
-            try:
-                if nn.GradientTape!=None:
-                    tape,output,loss=nn.GradientTape(data,labels)
-            except AttributeError:
-                with platform.GradientTape(persistent=True) as tape:
-                    try:
-                        output=nn.fp(data)
-                        loss=nn.loss(output,labels)
-                    except TypeError:
-                        output,loss=nn.fp(data,labels)
-            try:
-                if nn.accuracy!=None:
-                    nn.accuracy(output,labels)
-            except AttributeError:
-                pass
-            try:
-                gradient=nn.gradient(tape,loss)
-            except AttributeError:
-                gradient=tape.gradient(loss,nn.param)
-            try:
-                nn.opt.apply_gradients(zip(gradient,nn.param))
-            except AttributeError:
-                nn.opt(gradient)
-            print('No error')
-            return
-    except AttributeError:
         output=nn.fp(data)
-        loss=nn.loss(output,labels)
-        try:
-            if nn.accuracy!=None:
-                nn.accuracy(output,labels)
-        except AttributeError:
-            pass
-        try:
-            nn.opt.zero_grad()
-            loss.backward()
-            nn.opt.step()
-        except:
-            nn.opt(loss)
-        print('No error')
-        return
+        nn.loss(output,labels)
+    except TypeError:
+        output,loss=nn.fp(data,labels)
+    try:
+        if nn.accuracy!=None:
+            nn.accuracy(output,labels)
+    except AttributeError:
+        pass
+    print('No error')
+    return
