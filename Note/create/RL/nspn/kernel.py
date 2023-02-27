7d0fb27efc79497a11fe0089446460b04d4d173f
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2023-02-27 14:38:33 +0800

    Update kernel.py

--- Note/create/RL/nspn/kernel.py
+++ Note/create/RL/nspn/kernel.py
@@ -102,6 +102,73 @@ class kernel:
         return action_prob
     
     
+    def get_reward(self,max_step=None,seed=None):
+        reward=0
+        if seed==None:
+            s=self.genv.reset()
+        else:
+            s=self.genv.reset(seed=seed)
+        if max_step!=None:
+            for i in range(max_step):
+                if self.end_flag==True:
+                    break
+                s=np.expand_dims(s,0)
+                s=torch.tensor(s,dtype=torch.float).to(self.nn.device_d)
+                try:
+                    if self.nn.nn!=None:
+                        pass
+                    try:
+                        if self.nn.action!=None:
+                            pass
+                        a=self.nn.action(s).detach().numpy()
+                    except AttributeError:
+                        action_prob=self.nn.nn(s).detach().numpy()
+                        a=np.argmax(action_prob)
+                except AttributeError:
+                    a=self.nn.actor(s).detach().numpy()
+                    a=np.squeeze(a)
+                next_s,r,done,_=self.genv.step(a)
+                s=next_s
+                reward+=r
+                try:
+                    if self.nn.stop!=None:
+                        pass
+                    if self.nn.stop(torch.tensor(next_s,dtype=torch.float).to(self.nn.device_d)):
+                        break
+                except AttributeError:
+                    pass
+                if done:
+                    break
+            return reward
+        else:
+            while True:
+                if self.end_flag==True:
+                    break
+                s=np.expand_dims(s,0)
+                s=torch.tensor(s,dtype=torch.float).to(self.nn.device_d)
+                try:
+                    if self.nn.nn!=None:
+                        pass
+                    action_prob=self.nn.nn(s).detach().numpy()
+                    a=np.argmax(action_prob)
+                except AttributeError:
+                    a=self.nn.actor(s).detach().numpy()
+                    a=np.squeeze(a)
+                next_s,r,done,_=self.genv.step(a)
+                s=next_s
+                reward+=r
+                try:
+                    if self.nn.stop!=None:
+                        pass
+                    if self.nn.stop(torch.tensor(next_s,dtype=torch.float).to(self.nn.device_d)):
+                        break
+                except AttributeError:
+                    pass
+                if done:
+                    break
+            return reward
+    
+    
     def get_episode(self,max_step=None,seed=None):
         counter=0
         episode=[]
