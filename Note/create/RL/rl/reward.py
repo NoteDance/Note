81ebfb7caa52d7181d1ff9ce02343568f07329ee
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2023-03-06 22:15:44 +0800

    Update reward.py

--- Note/create/RL/rl/reward.py
+++ Note/create/RL/rl/reward.py
@@ -1,11 +1,11 @@
-import torch
 import numpy as np
 
 
 class reward:
-    def __init__(self,agent,env):
+    def __init__(self,agent,env,platform):
         self.agent=agent
         self.env=env
+        self.platform=platform
         self.end_flag=False
         
     
@@ -20,27 +20,45 @@ class reward:
                 if self.end_flag==True:
                     break
                 s=np.expand_dims(s,0)
-                s=torch.tensor(s,dtype=torch.float).to(self.agent.device_d)
                 try:
                     if self.agent.nn!=None:
-                        pass
+                        try:
+                           if self.platform.DType!=None: 
+                               s=np.expand_dims(s,axis=0)
+                               a=np.argmax(self.agent.nn.fp(s))
+                        except AttributeError:
+                            s=np.expand_dims(s,axis=0)
+                            s=self.platform.tensor(s,dtype=self.platform.float).to(self.agent.device)
+                            a=self.agent.nn(s).detach().numpy().argmax()
+                except AttributeError:
                     try:
                         if self.agent.action!=None:
-                            pass
-                        a=self.agent.action(s).detach().numpy()
+                            try:
+                               if self.platform.DType!=None: 
+                                   s=np.expand_dims(s,axis=0)
+                                   a=self.agent.action(s).numpy()
+                            except AttributeError:
+                                s=np.expand_dims(s,axis=0)
+                                s=self.platform.tensor(s,dtype=self.platform.float).to(self.agent.device)
+                                a=self.agent.action(s).detach().numpy()
                     except AttributeError:
-                        action_prob=self.agent.nn(s).detach().numpy()
-                        a=np.argmax(action_prob)
-                except AttributeError:
-                    a=self.agent.actor(s).detach().numpy()
-                    a=np.squeeze(a)
+                        try:
+                            if self.platform.DType!=None: 
+                                s=np.expand_dims(s,axis=0)
+                                a=self.agent.actor.fp(s).numpy()
+                                a=np.squeeze(a)
+                        except AttributeError:
+                            s=np.expand_dims(s,axis=0)
+                            s=self.platform.tensor(s,dtype=self.platform.float).to(self.agent.device)
+                            a=self.agent.actor(s).detach().numpy()
+                            a=np.squeeze(a)
                 next_s,r,done,_=self.env.step(a)
                 s=next_s
                 reward+=r
                 try:
                     if self.nn.stop!=None:
                         pass
-                    if self.nn.stop(torch.tensor(next_s,dtype=torch.float).to(self.agent.device_d)):
+                    if self.nn.stop(next_s):
                         break
                 except AttributeError:
                     pass
@@ -52,22 +70,45 @@ class reward:
                 if self.end_flag==True:
                     break
                 s=np.expand_dims(s,0)
-                s=torch.tensor(s,dtype=torch.float).to(self.agent.device_d)
                 try:
                     if self.agent.nn!=None:
-                        pass
-                    action_prob=self.agent.nn(s).detach().numpy()
-                    a=np.argmax(action_prob)
+                        try:
+                           if self.platform.DType!=None: 
+                               s=np.expand_dims(s,axis=0)
+                               a=np.argmax(self.agent.nn.fp(s))
+                        except AttributeError:
+                            s=np.expand_dims(s,axis=0)
+                            s=self.platform.tensor(s,dtype=self.platform.float).to(self.agent.device)
+                            a=self.agent.nn(s).detach().numpy().argmax()
                 except AttributeError:
-                    a=self.agent.actor(s).detach().numpy()
-                    a=np.squeeze(a)
+                    try:
+                        if self.agent.action!=None:
+                            try:
+                               if self.platform.DType!=None: 
+                                   s=np.expand_dims(s,axis=0)
+                                   a=self.agent.action(s).numpy()
+                            except AttributeError:
+                                s=np.expand_dims(s,axis=0)
+                                s=self.platform.tensor(s,dtype=self.platform.float).to(self.agent.device)
+                                a=self.agent.action(s).detach().numpy()
+                    except AttributeError:
+                        try:
+                            if self.platform.DType!=None: 
+                                s=np.expand_dims(s,axis=0)
+                                a=self.agent.actor.fp(s).numpy()
+                                a=np.squeeze(a)
+                        except AttributeError:
+                            s=np.expand_dims(s,axis=0)
+                            s=self.platform.tensor(s,dtype=self.platform.float).to(self.agent.device)
+                            a=self.agent.actor(s).detach().numpy()
+                            a=np.squeeze(a)
                 next_s,r,done,_=self.env.step(a)
                 s=next_s
                 reward+=r
                 try:
                     if self.nn.stop!=None:
                         pass
-                    if self.nn.stop(torch.tensor(next_s,dtype=torch.float).to(self.agent.device_d)):
+                    if self.nn.stop(next_s):
                         break
                 except AttributeError:
                     pass
