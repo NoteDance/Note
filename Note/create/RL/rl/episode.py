94b5ddf004ca47c804ae3a4f636fb985131eb44f
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2023-03-06 21:45:40 +0800

    Update episode.py

--- Note/create/RL/rl/episode.py
+++ Note/create/RL/rl/episode.py
@@ -2,9 +2,10 @@ import numpy as np
 
 
 class episode:
-    def __init__(self,agent,env):
+    def __init__(self,agent,env,platform):
         self.agent=agent
         self.env=env
+        self.platform=platform
         self.end_flag=False
     
     
@@ -17,21 +18,37 @@ class episode:
         while True:
             try:
                 if self.agent.nn!=None:
-                    pass
+                    try:
+                       if self.platform.DType!=None: 
+                           s=np.expand_dims(s,axis=0)
+                           a=np.argmax(self.agent.nn.fp(s))
+                    except AttributeError:
+                        s=np.expand_dims(s,axis=0)
+                        s=self.platform.tensor(s,dtype=self.platform.float).to(self.agent.device)
+                        a=self.agent.nn(s).detach().numpy().argmax()
+            except AttributeError:
                 try:
                     if self.agent.action!=None:
-                        pass
-                    s=np.expand_dims(s,axis=0)
-                    a=self.agent.action(s).numpy()
+                        try:
+                           if self.platform.DType!=None: 
+                               s=np.expand_dims(s,axis=0)
+                               a=self.agent.action(s).numpy()
+                        except AttributeError:
+                            s=np.expand_dims(s,axis=0)
+                            s=self.platform.tensor(s,dtype=self.platform.float).to(self.agent.device)
+                            a=self.agent.action(s).detach().numpy()
                 except AttributeError:
-                    s=np.expand_dims(s,axis=0)
-                    a=np.argmax(self.agent.nn.fp(s))
-                next_s,r,done,_=self.env.step(a)
-            except AttributeError:
-                s=np.expand_dims(s,axis=0)
-                a=self.agent.actor.fp(s).numpy()
-                a=np.squeeze(a)
-                next_s,r,done,_=self.env.step(a)
+                    try:
+                        if self.platform.DType!=None: 
+                            s=np.expand_dims(s,axis=0)
+                            a=self.agent.actor.fp(s).numpy()
+                            a=np.squeeze(a)
+                    except AttributeError:
+                        s=np.expand_dims(s,axis=0)
+                        s=self.platform.tensor(s,dtype=self.platform.float).to(self.agent.device)
+                        a=self.agent.actor(s).detach().numpy()
+                        a=np.squeeze(a)
+            next_s,r,done,_=self.env.step(a)
             try:
                 if self.nn.stop!=None:
                     pass
