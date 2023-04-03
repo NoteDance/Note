dfac114c7386b8842abb0eaba103bb8154a8a395
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2023-04-03 14:15:01 +0800

    Update kernel.py

--- Note/create/DL/kernel.py
+++ Note/create/DL/kernel.py
@@ -993,7 +993,7 @@ class kernel:
             if self.nn.accuracy!=None:
                 return test_loss,test_acc
         except AttributeError:
-            return test_loss,None
+            return test_loss
     
     
     def suspend_func(self,t=None):
