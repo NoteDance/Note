c0d98faf17ca0969451f8632099f35f5643bd32f
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2023-05-09 23:56:07 +0800

    Update rnn.py

--- Note/nn/layer/rnn.py
+++ Note/nn/layer/rnn.py
@@ -5,33 +5,33 @@ class rnn:
     def __init__(self,weight_shape,timestep,weight_func='uniform',bias_func='zero',activation=None,dtype='float64',return_sequence=False,use_bias=True):
         if type(weight_func)==list:
             if weight_func[0]=='normal':
-               self.weight1=tf.Variable(tf.random.normal(shape=weight_shape,mean=weight_func[1],stddev=weight_func[2],dtype=dtype))
-               self.weight2=tf.Variable(tf.random.normal(shape=[weight_shape[1],weight_shape[1]],mean=weight_func[1],stddev=weight_func[2],dtype=dtype))
+               self.weight_i=tf.Variable(tf.random.normal(shape=weight_shape,mean=weight_func[1],stddev=weight_func[2],dtype=dtype))
+               self.weight_s=tf.Variable(tf.random.normal(shape=[weight_shape[1],weight_shape[1]],mean=weight_func[1],stddev=weight_func[2],dtype=dtype))
             elif weight_func[0]=='uniform':
-                self.weight1=tf.Variable(tf.random.uniform(shape=weight_shape,minval=weight_func[1],maxval=weight_func[2],dtype=dtype))
-                self.weight2=tf.Variable(tf.random.uniform(shape=[weight_shape[1],weight_shape[1]],minval=weight_func[1],maxval=weight_func[2],dtype=dtype))
+                self.weight_i=tf.Variable(tf.random.uniform(shape=weight_shape,minval=weight_func[1],maxval=weight_func[2],dtype=dtype))
+                self.weight_s=tf.Variable(tf.random.uniform(shape=[weight_shape[1],weight_shape[1]],minval=weight_func[1],maxval=weight_func[2],dtype=dtype))
         else:
             if weight_func=='normal':
                 if dtype!=None:
-                    self.weight1=tf.Variable(tf.random.normal(shape=weight_shape,dtype=dtype))
-                    self.weight2=tf.Variable(tf.random.normal(shape=[weight_shape[1],weight_shape[1]],dtype=dtype))
+                    self.weight_i=tf.Variable(tf.random.normal(shape=weight_shape,dtype=dtype))
+                    self.weight_s=tf.Variable(tf.random.normal(shape=[weight_shape[1],weight_shape[1]],dtype=dtype))
                 else:
-                    self.weight1=tf.Variable(tf.random.normal(shape=weight_shape))
-                    self.weight2=tf.Variable(tf.random.normal(shape=[weight_shape[1],weight_shape[1]]))
+                    self.weight_i=tf.Variable(tf.random.normal(shape=weight_shape))
+                    self.weight_s=tf.Variable(tf.random.normal(shape=[weight_shape[1],weight_shape[1]]))
             elif weight_func=='uniform':
                 if dtype!=None:
-                    self.weight1=tf.Variable(tf.random.uniform(shape=weight_shape,dtype=dtype))
-                    self.weight2=tf.Variable(tf.random.uniform(shape=[weight_shape[1],weight_shape[1]],dtype=dtype))
+                    self.weight_i=tf.Variable(tf.random.uniform(shape=weight_shape,dtype=dtype))
+                    self.weight_s=tf.Variable(tf.random.uniform(shape=[weight_shape[1],weight_shape[1]],dtype=dtype))
                 else:
-                    self.weight1=tf.Variable(tf.random.uniform(shape=weight_shape))
-                    self.weight2=tf.Variable(tf.random.uniform(shape=[weight_shape[1],weight_shape[1]]))
+                    self.weight_i=tf.Variable(tf.random.uniform(shape=weight_shape))
+                    self.weight_s=tf.Variable(tf.random.uniform(shape=[weight_shape[1],weight_shape[1]]))
             elif weight_func=='zero':
                 if dtype!=None:
-                    self.weight1=tf.Variable(tf.zeros(shape=weight_shape,dtype=dtype))
-                    self.weight2=tf.Variable(tf.zeros(shape=[weight_shape[1],weight_shape[1]],dtype=dtype))
+                    self.weight_i=tf.Variable(tf.zeros(shape=weight_shape,dtype=dtype))
+                    self.weight_s=tf.Variable(tf.zeros(shape=[weight_shape[1],weight_shape[1]],dtype=dtype))
                 else:
-                    self.weight1=tf.Variable(tf.zeros(shape=weight_shape))
-                    self.weight2=tf.Variable(tf.zeros(shape=[weight_shape[1],weight_shape[1]]))
+                    self.weight_i=tf.Variable(tf.zeros(shape=weight_shape))
+                    self.weight_s=tf.Variable(tf.zeros(shape=[weight_shape[1],weight_shape[1]]))
         if use_bias==True and type(bias_func)==list:
             if bias_func[0]=='normal':
                self.bias=tf.Variable(tf.random.normal(shape=[weight_shape[1]],mean=bias_func[1],stddev=bias_func[2],dtype=dtype))
@@ -59,13 +59,17 @@ class rnn:
         self.activation=activation
         self.return_sequence=return_sequence
         self.use_bias=use_bias
+        if use_bias==True:
+            self.weight_list=[self.weight_i,self.weight_s,self.bias]
+        else:
+            self.weight_list=[self.weight_i,self.weight_s]
     
     
     def output(self,data):
         if self.use_bias==True:
             if self.activation=='relu':
                 for i in range(self.timestep):
-                    output=tf.nn.relu(tf.matmul(data[:][:,i],self.weight1)+tf.matmul(self.state,self.weight2)+self.bias)
+                    output=tf.nn.relu(tf.matmul(data[:][:,i],self.weight_i)+tf.matmul(self.state,self.weight_s)+self.bias)
                     self.output_list.append(output)
                     self.state=output
                 if self.return_sequence==True:
@@ -74,7 +78,7 @@ class rnn:
                     return tf.stack(self.output_list,axis=1)
             elif self.activation=='sigmoid':
                 for i in range(self.timestep):
-                    output=tf.nn.sigmoid(tf.matmul(data[:][:,i],self.weight1)+tf.matmul(self.state,self.weight2)+self.bias)
+                    output=tf.nn.sigmoid(tf.matmul(data[:][:,i],self.weight_i)+tf.matmul(self.state,self.weight_s)+self.bias)
                     self.output_list.append(output)
                     self.state=output
                 if self.return_sequence==True:
@@ -83,7 +87,7 @@ class rnn:
                     return tf.stack(self.output_list,axis=1)
             elif self.activation=='tanh':
                 for i in range(self.timestep):
-                    output=tf.nn.tanh(tf.matmul(data[:][:,i],self.weight1)+tf.matmul(self.state,self.weight2)+self.bias)
+                    output=tf.nn.tanh(tf.matmul(data[:][:,i],self.weight_i)+tf.matmul(self.state,self.weight_s)+self.bias)
                     self.output_list.append(output)
                     self.state=output
                 if self.return_sequence==True:
@@ -93,7 +97,7 @@ class rnn:
         else:
             if self.activation=='relu':
                 for i in range(self.timestep):
-                    output=tf.nn.relu(tf.matmul(data[:][:,i],self.weight1)+tf.matmul(self.state,self.weight2))
+                    output=tf.nn.relu(tf.matmul(data[:][:,i],self.weight_i)+tf.matmul(self.state,self.weight_s))
                     self.output_list.append(output)
                     self.state=output
                 if self.return_sequence==True:
@@ -102,7 +106,7 @@ class rnn:
                     return tf.stack(self.output_list,axis=1)
             elif self.activation=='sigmoid':
                 for i in range(self.timestep):
-                    output=tf.nn.sigmoid(tf.matmul(data[:][:,i],self.weight1)+tf.matmul(self.state,self.weight2))
+                    output=tf.nn.sigmoid(tf.matmul(data[:][:,i],self.weight_i)+tf.matmul(self.state,self.weight_s))
                     self.output_list.append(output)
                     self.state=output
                 if self.return_sequence==True:
@@ -111,7 +115,7 @@ class rnn:
                     return tf.stack(self.output_list,axis=1)
             elif self.activation=='tanh':
                 for i in range(self.timestep):
-                    output=tf.nn.tanh(tf.matmul(data[:][:,i],self.weight1)+tf.matmul(self.state,self.weight2))
+                    output=tf.nn.tanh(tf.matmul(data[:][:,i],self.weight_i)+tf.matmul(self.state,self.weight_s))
                     self.output_list.append(output)
                     self.state=output
                 if self.return_sequence==True:
