bf327c6df07fbab72a174ddec26a3642f46b2b2a
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2023-05-10 15:01:38 +0800

    Update and rename rnn.py to RNN.py

--- Note/nn/layer/RNN.py
+++ Note/nn/layer/RNN.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 
 
-class rnn:
+class RNN:
     def __init__(self,weight_shape,timestep,weight_func='uniform',bias_func='zero',activation=None,dtype='float64',return_sequence=False,use_bias=True):
         if type(weight_func)==list:
             if weight_func[0]=='normal':
@@ -20,11 +20,11 @@ class rnn:
                     self.weight_s=tf.Variable(tf.random.normal(shape=[weight_shape[1],weight_shape[1]]))
             elif weight_func=='uniform':
                 if dtype!=None:
-                    self.weight_i=tf.Variable(tf.random.uniform(shape=weight_shape,dtype=dtype))
-                    self.weight_s=tf.Variable(tf.random.uniform(shape=[weight_shape[1],weight_shape[1]],dtype=dtype))
+                    self.weight_i=tf.Variable(tf.random.uniform(shape=weight_shape,maxval=0.01,dtype=dtype))
+                    self.weight_s=tf.Variable(tf.random.uniform(shape=[weight_shape[1],weight_shape[1]],maxval=0.01,dtype=dtype))
                 else:
-                    self.weight_i=tf.Variable(tf.random.uniform(shape=weight_shape))
-                    self.weight_s=tf.Variable(tf.random.uniform(shape=[weight_shape[1],weight_shape[1]]))
+                    self.weight_i=tf.Variable(tf.random.uniform(shape=weight_shape,maxval=0.01))
+                    self.weight_s=tf.Variable(tf.random.uniform(shape=[weight_shape[1],weight_shape[1]],maxval=0.01))
             elif weight_func=='zero':
                 if dtype!=None:
                     self.weight_i=tf.Variable(tf.zeros(shape=weight_shape,dtype=dtype))
@@ -73,27 +73,27 @@ class rnn:
                     self.output_list.append(output)
                     self.state=output
                 if self.return_sequence==True:
-                    return self.state
-                else:
                     return tf.stack(self.output_list,axis=1)
+                else:
+                    return self.state
             elif self.activation=='sigmoid':
                 for i in range(self.timestep):
                     output=tf.nn.sigmoid(tf.matmul(data[:][:,i],self.weight_i)+tf.matmul(self.state,self.weight_s)+self.bias)
                     self.output_list.append(output)
                     self.state=output
                 if self.return_sequence==True:
-                    return self.state
-                else:
                     return tf.stack(self.output_list,axis=1)
+                else:
+                    return self.state
             elif self.activation=='tanh':
                 for i in range(self.timestep):
                     output=tf.nn.tanh(tf.matmul(data[:][:,i],self.weight_i)+tf.matmul(self.state,self.weight_s)+self.bias)
                     self.output_list.append(output)
                     self.state=output
                 if self.return_sequence==True:
-                    return self.state
-                else:
                     return tf.stack(self.output_list,axis=1)
+                else:
+                    return self.state
         else:
             if self.activation=='relu':
                 for i in range(self.timestep):
@@ -101,24 +101,24 @@ class rnn:
                     self.output_list.append(output)
                     self.state=output
                 if self.return_sequence==True:
-                    return self.state
-                else:
                     return tf.stack(self.output_list,axis=1)
+                else:
+                    return self.state
             elif self.activation=='sigmoid':
                 for i in range(self.timestep):
                     output=tf.nn.sigmoid(tf.matmul(data[:][:,i],self.weight_i)+tf.matmul(self.state,self.weight_s))
                     self.output_list.append(output)
                     self.state=output
                 if self.return_sequence==True:
-                    return self.state
-                else:
                     return tf.stack(self.output_list,axis=1)
+                else:
+                    return self.state
             elif self.activation=='tanh':
                 for i in range(self.timestep):
                     output=tf.nn.tanh(tf.matmul(data[:][:,i],self.weight_i)+tf.matmul(self.state,self.weight_s))
                     self.output_list.append(output)
                     self.state=output
                 if self.return_sequence==True:
-                    return self.state
-                else:
                     return tf.stack(self.output_list,axis=1)
+                else:
+                    return self.state
