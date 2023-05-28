4fb2f7eb1dc23b36e0679c72f1947700ccb9a560
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2023-05-28 23:33:16 +0800

    Update optimizer.py

--- Note/DL/process/optimizer.py
+++ Note/DL/process/optimizer.py
@@ -90,3 +90,35 @@ class AdaDelta:
             state_ops.assign(parameter[i],parameter[i]-self.g[i])
             self.x[i]=self.rho*self.x[i]+(1-self.rho)*self.g[i]**2
         return parameter
+    
+    
+class Adam:
+    def __init__(self,lr=0.001,beta1=0.9,beta2=0.999,epsilon=1e-07):
+        self.lr=lr
+        self.beta1=beta1
+        self.beta2=beta2
+        self.epsilon=epsilon
+        self.v=[]
+        self.s=[]
+        self.v_=[]
+        self.s_=[]
+        self.g=[]
+        self.flag=0
+    
+    
+    def opt(self,gradient,parameter,t):
+        if self.flag==0:
+            self.v=[0 for x in range(len(gradient))]
+            self.s=[0 for x in range(len(gradient))]
+            self.v_=[x for x in range(len(gradient))]
+            self.s_=[x for x in range(len(gradient))]
+            self.g=[x for x in range(len(gradient))]
+            self.flag+=1
+        for i in range(len(gradient)):
+            self.v[i]=self.beta1*self.v[i]+(1-self.beta1)*gradient[i]
+            self.s[i]=self.beta2*self.s[i]+(1-self.beta2)*gradient[i]**2
+            self.v_[i]=self.v[i]/(1-self.beta1**(t+1))
+            self.s_[i]=self.s[i]/(1-self.beta2**(t+1))
+            self.g[i]=self.lr*self.v_[i]/(tf.sqrt(self.s_[i])+self.epsilon)
+            state_ops.assign(parameter[i],parameter[i]-self.g[i])
+        return  parameter
