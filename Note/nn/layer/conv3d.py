092fbd3b45000020372d3214d30bbc7586a8e6da
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2023-05-31 14:32:28 +0800

    Update conv3d.py

--- Note/nn/layer/conv3d.py
+++ Note/nn/layer/conv3d.py
@@ -1,39 +1,14 @@
 import tensorflow as tf
+import Note.nn.activation as a
+import Note.nn.initializer as i
 
 
 class conv3d:
-    def __init__(self,data,weight_shape,weight_func='normal',activation=None,dtype='float64'):
-        if type(weight_func)==list:
-            if weight_func[0]=='normal':
-               self.weight=tf.Variable(tf.random.normal(shape=weight_shape,mean=weight_func[1],stddev=weight_func[2],dtype=dtype))
-            elif weight_func[0]=='uniform':
-                self.weight=tf.Variable(tf.random.uniform(shape=weight_shape,minval=weight_func[1],maxval=weight_func[2],dtype=dtype))
-        else:
-            if weight_func=='normal':
-                if dtype!=None:
-                    self.weight=tf.Variable(tf.random.normal(shape=weight_shape,dtype=dtype))
-                else:
-                    self.weight=tf.Variable(tf.random.normal(shape=weight_shape))
-            elif weight_func=='uniform':
-                if dtype!=None:
-                    self.weight=tf.Variable(tf.random.uniform(shape=weight_shape,maxval=0.01,dtype=dtype))
-                else:
-                    self.weight=tf.Variable(tf.random.uniform(shape=weight_shape,maxval=0.01))
-            elif weight_func=='zero':
-                if dtype!=None:
-                    self.weight=tf.Variable(tf.zeros(shape=weight_shape,dtype=dtype))
-                else:
-                    self.weight=tf.Variable(tf.zeros(shape=weight_shape))
+    def __init__(self,data,weight_shape,weight_initializer='normal',activation=None,dtype='float64'):
+        self.weight=i.initializer(weight_shape,weight_initializer,dtype)
         self.activation=activation
         self.weight_list=[self.weight]
     
     
     def output(self,data,strides,padding='VALID',data_format='NHWC',dilations=None):
-        if self.activation=='relu':
-            return tf.nn.relu(tf.nn.conv3d(data,self.weight,strides=strides,padding=padding,data_format=data_format,dilations=dilations))
-        elif self.activation=='sigmoid':
-            return tf.nn.sigmoid(tf.nn.conv3d(data,self.weight,strides=strides,padding=padding,data_format=data_format,dilations=dilations))
-        elif self.activation=='tanh':
-            return tf.nn.tanh(tf.nn.conv3d(data,self.weight,strides=strides,padding=padding,data_format=data_format,dilations=dilations))
-        else:
-            return tf.nn.conv3d(data,self.weight,strides=strides,padding=padding,data_format=data_format,dilations=dilations)
+        return a.activation_conv(data,self.weight,self.activation,strides,padding,data_format,dilations,tf.nn.conv3d)
