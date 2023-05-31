efcc61bd0a46ce4a5de9c191233ac082e121e855
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2023-05-31 14:32:58 +0800

    Update GRU.py

--- Note/nn/layer/GRU.py
+++ Note/nn/layer/GRU.py
@@ -1,106 +1,19 @@
 import tensorflow as tf
+import Note.nn.initializer as i
 
 
 class GRU:
-    def __init__(self,weight_shape,timestep,weight_func='uniform',bias_func='zero',dtype='float64',return_sequence=False,use_bias=True):
-        if type(weight_func)==list:
-            if weight_func[0]=='normal':
-               self.weight_r1=tf.Variable(tf.random.normal(shape=weight_shape,mean=weight_func[1],stddev=weight_func[2],dtype=dtype))
-               self.weight_r2=tf.Variable(tf.random.normal(shape=[weight_shape[1],weight_shape[1]],mean=weight_func[1],stddev=weight_func[2],dtype=dtype))
-               self.weight_z1=tf.Variable(tf.random.normal(shape=weight_shape,mean=weight_func[1],stddev=weight_func[2],dtype=dtype))
-               self.weight_z2=tf.Variable(tf.random.normal(shape=[weight_shape[1],weight_shape[1]],mean=weight_func[1],stddev=weight_func[2],dtype=dtype))
-               self.weight_h1=tf.Variable(tf.random.normal(shape=weight_shape,mean=weight_func[1],stddev=weight_func[2],dtype=dtype))
-               self.weight_h2=tf.Variable(tf.random.normal(shape=[weight_shape[1],weight_shape[1]],mean=weight_func[1],stddev=weight_func[2],dtype=dtype))
-            elif weight_func[0]=='uniform':
-                self.weight_r1=tf.Variable(tf.random.uniform(shape=weight_shape,minval=weight_func[1],maxval=weight_func[2],dtype=dtype))
-                self.weight_r2=tf.Variable(tf.random.uniform(shape=[weight_shape[1],weight_shape[1]],minval=weight_func[1],maxval=weight_func[2],dtype=dtype))
-                self.weight_z1=tf.Variable(tf.random.uniform(shape=weight_shape,minval=weight_func[1],maxval=weight_func[2],dtype=dtype))
-                self.weight_z2=tf.Variable(tf.random.uniform(shape=[weight_shape[1],weight_shape[1]],minval=weight_func[1],maxval=weight_func[2],dtype=dtype))
-                self.weight_h1=tf.Variable(tf.random.uniform(shape=weight_shape,minval=weight_func[1],maxval=weight_func[2],dtype=dtype))
-                self.weight_h2=tf.Variable(tf.random.uniform(shape=[weight_shape[1],weight_shape[1]],minval=weight_func[1],maxval=weight_func[2],dtype=dtype))
-        else:
-            if weight_func=='normal':
-                if dtype!=None:
-                    self.weight_r1=tf.Variable(tf.random.normal(shape=weight_shape,dtype=dtype))
-                    self.weight_r2=tf.Variable(tf.random.normal(shape=[weight_shape[1],weight_shape[1]],dtype=dtype))
-                    self.weight_z1=tf.Variable(tf.random.normal(shape=weight_shape,dtype=dtype))
-                    self.weight_z2=tf.Variable(tf.random.normal(shape=[weight_shape[1],weight_shape[1]],dtype=dtype))
-                    self.weight_h1=tf.Variable(tf.random.normal(shape=weight_shape,dtype=dtype))
-                    self.weight_h2=tf.Variable(tf.random.normal(shape=[weight_shape[1],weight_shape[1]],dtype=dtype))
-                else:
-                    self.weight_r1=tf.Variable(tf.random.normal(shape=weight_shape))
-                    self.weight_r2=tf.Variable(tf.random.normal(shape=[weight_shape[1],weight_shape[1]]))
-                    self.weight_z1=tf.Variable(tf.random.normal(shape=weight_shape))
-                    self.weight_z2=tf.Variable(tf.random.normal(shape=[weight_shape[1],weight_shape[1]]))
-                    self.weight_h1=tf.Variable(tf.random.normal(shape=weight_shape))
-                    self.weight_h2=tf.Variable(tf.random.normal(shape=[weight_shape[1],weight_shape[1]]))
-            elif weight_func=='uniform':
-                if dtype!=None:
-                    self.weight_r1=tf.Variable(tf.random.uniform(shape=weight_shape,maxval=0.01,dtype=dtype))
-                    self.weight_r2=tf.Variable(tf.random.uniform(shape=[weight_shape[1],weight_shape[1]],maxval=0.01,dtype=dtype))
-                    self.weight_z1=tf.Variable(tf.random.uniform(shape=weight_shape,dtype=dtype))
-                    self.weight_z2=tf.Variable(tf.random.uniform(shape=[weight_shape[1],weight_shape[1]],maxval=0.01,dtype=dtype))
-                    self.weight_h1=tf.Variable(tf.random.uniform(shape=weight_shape,dtype=dtype))
-                    self.weight_h2=tf.Variable(tf.random.uniform(shape=[weight_shape[1],weight_shape[1]],maxval=0.01,dtype=dtype))
-                else:
-                    self.weight_r1=tf.Variable(tf.random.uniform(shape=weight_shape))
-                    self.weight_r2=tf.Variable(tf.random.uniform(shape=[weight_shape[1],weight_shape[1]],maxval=0.01))
-                    self.weight_z1=tf.Variable(tf.random.uniform(shape=weight_shape))
-                    self.weight_z2=tf.Variable(tf.random.uniform(shape=[weight_shape[1],weight_shape[1]],maxval=0.01))
-                    self.weight_h1=tf.Variable(tf.random.uniform(shape=weight_shape))
-                    self.weight_h2=tf.Variable(tf.random.uniform(shape=[weight_shape[1],weight_shape[1]],maxval=0.01))
-            elif weight_func=='zero':
-                if dtype!=None:
-                    self.weight_r1=tf.Variable(tf.zeros(shape=weight_shape,dtype=dtype))
-                    self.weight_r2=tf.Variable(tf.zeros(shape=[weight_shape[1],weight_shape[1]],dtype=dtype))
-                    self.weight_z1=tf.Variable(tf.zeros(shape=weight_shape,dtype=dtype))
-                    self.weight_z2=tf.Variable(tf.zeros(shape=[weight_shape[1],weight_shape[1]],dtype=dtype))
-                    self.weight_h1=tf.Variable(tf.zeros(shape=weight_shape,dtype=dtype))
-                    self.weight_h2=tf.Variable(tf.zeros(shape=[weight_shape[1],weight_shape[1]],dtype=dtype))
-                else:
-                    self.weight_r1=tf.Variable(tf.zeros(shape=weight_shape))
-                    self.weight_r2=tf.Variable(tf.zeros(shape=[weight_shape[1],weight_shape[1]]))
-                    self.weight_z1=tf.Variable(tf.zeros(shape=weight_shape))
-                    self.weight_z2=tf.Variable(tf.zeros(shape=[weight_shape[1],weight_shape[1]]))
-                    self.weight_h1=tf.Variable(tf.zeros(shape=weight_shape))
-                    self.weight_h2=tf.Variable(tf.zeros(shape=[weight_shape[1],weight_shape[1]]))
-        if use_bias==True and type(bias_func)==list:
-            if bias_func[0]=='normal':
-               self.bias_r=tf.Variable(tf.random.normal(shape=[weight_shape[1]],mean=bias_func[1],stddev=bias_func[2],dtype=dtype))
-               self.bias_z=tf.Variable(tf.random.normal(shape=[weight_shape[1]],mean=bias_func[1],stddev=bias_func[2],dtype=dtype))
-               self.bias_h=tf.Variable(tf.random.normal(shape=[weight_shape[1]],mean=bias_func[1],stddev=bias_func[2],dtype=dtype))
-            elif bias_func[0]=='uniform':
-                self.bias_r=tf.Variable(tf.random.normal(shape=[weight_shape[1]],minval=bias_func[1],maxval=bias_func[2],dtype=dtype))
-                self.bias_z=tf.Variable(tf.random.normal(shape=[weight_shape[1]],minval=bias_func[1],maxval=bias_func[2],dtype=dtype))
-                self.bias_h=tf.Variable(tf.random.normal(shape=[weight_shape[1]],minval=bias_func[1],maxval=bias_func[2],dtype=dtype))
-        elif use_bias==True:
-            if bias_func=='normal':
-                if dtype!=None:
-                    self.bias_r=tf.Variable(tf.random.normal(shape=[weight_shape[1]],dtype=dtype))
-                    self.bias_z=tf.Variable(tf.random.normal(shape=[weight_shape[1]],dtype=dtype))
-                    self.bias_h=tf.Variable(tf.random.normal(shape=[weight_shape[1]],dtype=dtype))
-                else:
-                    self.bias_r=tf.Variable(tf.random.normal(shape=[weight_shape[1]]))
-                    self.bias_z=tf.Variable(tf.random.normal(shape=[weight_shape[1]]))
-                    self.bias_h=tf.Variable(tf.random.normal(shape=[weight_shape[1]]))
-            elif bias_func=='uniform':
-                if dtype!=None:
-                    self.bias_r=tf.Variable(tf.random.uniform(shape=[weight_shape[1]],dtype=dtype))
-                    self.bias_z=tf.Variable(tf.random.uniform(shape=[weight_shape[1]],dtype=dtype))
-                    self.bias_h=tf.Variable(tf.random.uniform(shape=[weight_shape[1]],dtype=dtype))
-                else:
-                    self.bias_r=tf.Variable(tf.random.uniform(shape=[weight_shape[1]]))
-                    self.bias_z=tf.Variable(tf.random.uniform(shape=[weight_shape[1]]))
-                    self.bias_h=tf.Variable(tf.random.uniform(shape=[weight_shape[1]]))
-            elif bias_func=='zero':
-                if dtype!=None:
-                    self.bias_r=tf.Variable(tf.zeros(shape=[weight_shape[1]],dtype=dtype))
-                    self.bias_z=tf.Variable(tf.zeros(shape=[weight_shape[1]],dtype=dtype))
-                    self.bias_h=tf.Variable(tf.zeros(shape=[weight_shape[1]],dtype=dtype))
-                else:
-                    self.bias_r=tf.Variable(tf.zeros(shape=[weight_shape[1]]))
-                    self.bias_z=tf.Variable(tf.zeros(shape=[weight_shape[1]]))
-                    self.bias_h=tf.Variable(tf.zeros(shape=[weight_shape[1]]))
+    def __init__(self,weight_shape,timestep,weight_initializer='uniform',bias_initializer='zero',dtype='float64',return_sequence=False,use_bias=True):
+        self.weight_r1=i.initializer(weight_shape,weight_initializer,dtype)
+        self.weight_r2=i.initializer(weight_shape,weight_initializer,dtype)
+        self.weight_z1=i.initializer(weight_shape,weight_initializer,dtype)
+        self.weight_z2=i.initializer(weight_shape,weight_initializer,dtype)
+        self.weight_h1=i.initializer(weight_shape,weight_initializer,dtype)
+        self.weight_h2=i.initializer(weight_shape,weight_initializer,dtype)
+        if use_bias==True:
+            self.bias_r=i.initializer([weight_shape[1]],bias_initializer,dtype)
+            self.bias_z=i.initializer([weight_shape[1]],bias_initializer,dtype)
+            self.bias_h=i.initializer([weight_shape[1]],bias_initializer,dtype)
         self.output_list=[]
         self.state=tf.zeros(shape=[1,weight_shape[1]],dtype=dtype)
         self.H=tf.zeros(shape=[1,weight_shape[1]],dtype=dtype)
@@ -115,10 +28,10 @@ class GRU:
     
     def output(self,data):
         if self.use_bias==True:
-            for i in range(self.timestep):
-                R=tf.nn.sigmoid(tf.matmul(data[:][:,i],self.weight_r1)+tf.matmul(self.state,self.weight_r2)+self.bias_r)
-                Z=tf.nn.sigmoid(tf.matmul(data[:][:,i],self.weight_z1)+tf.matmul(self.state,self.weight_z2)+self.bias_z)
-                H_=tf.nn.tanh(tf.matmul(data[:][:,i],self.weight_h1)+tf.matmul(R*self.H,self.weight_h2)+self.bias_h)
+            for j in range(self.timestep):
+                R=tf.nn.sigmoid(tf.matmul(data[:][:,j],self.weight_r1)+tf.matmul(self.state,self.weight_r2)+self.bias_r)
+                Z=tf.nn.sigmoid(tf.matmul(data[:][:,j],self.weight_z1)+tf.matmul(self.state,self.weight_z2)+self.bias_z)
+                H_=tf.nn.tanh(tf.matmul(data[:][:,j],self.weight_h1)+tf.matmul(R*self.H,self.weight_h2)+self.bias_h)
                 output=Z*self.H+(1-Z)*H_
                 self.output_list.append(output)
                 self.H=output
@@ -131,10 +44,10 @@ class GRU:
                 self.output_list=[]
                 return output
         else:
-            for i in range(self.timestep):
-                R=tf.nn.sigmoid(tf.matmul(data[:][:,i],self.weight_r1)+tf.matmul(self.state,self.weight_r2))
-                Z=tf.nn.sigmoid(tf.matmul(data[:][:,i],self.weight_z1)+tf.matmul(self.state,self.weight_z2))
-                H_=tf.nn.tanh(tf.matmul(data[:][:,i],self.weight_h1)+tf.matmul(R*self.H,self.weight_h2))
+            for j in range(self.timestep):
+                R=tf.nn.sigmoid(tf.matmul(data[:][:,j],self.weight_r1)+tf.matmul(self.state,self.weight_r2))
+                Z=tf.nn.sigmoid(tf.matmul(data[:][:,j],self.weight_z1)+tf.matmul(self.state,self.weight_z2))
+                H_=tf.nn.tanh(tf.matmul(data[:][:,j],self.weight_h1)+tf.matmul(R*self.H,self.weight_h2))
                 output=Z*self.H+(1-Z)*H_
                 self.output_list.append(output)
                 self.H=output
