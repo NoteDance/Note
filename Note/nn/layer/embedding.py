04b1a63ad0544be1391139ec2aeaea4b8daf1b57
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2023-06-02 13:39:54 +0800

    Update embedding.py

--- Note/nn/layer/embedding.py
+++ Note/nn/layer/embedding.py
@@ -1,15 +1,14 @@
 import tensorflow as tf
+import Note.nn.initializer as i
 
 
 class EmbeddingLayer:
     def __init__(self,vocab_size,embed_size,initializer=None,dtype=tf.float32):
         self.vocab_size=vocab_size
         self.embed_size=embed_size
-        if initializer is None:
-            initializer=tf.random.uniform_initializer(-1.0,1.0)
-        self.embeddings=tf.Variable(initializer([self.vocab_size,self.embed_size]),dtype=dtype)
+        self.embeddings=i.initializer([vocab_size,embed_size],initializer,dtype)
     
     
-    def __call__(self,inputs):
+    def output(self,inputs):
         outputs=tf.nn.embedding_lookup(self.embeddings,inputs)
         return outputs
