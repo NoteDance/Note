28fb75885e1a2c95d285ffeb5098e56a36209576
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2023-06-03 13:59:47 +0800

    Update self_attention.py

--- Note/nn/layer/self_attention.py
+++ Note/nn/layer/self_attention.py
@@ -12,19 +12,19 @@ class self_attention:
     
     
     def output(self,data,a,mask=None):
-        query=tf.einsum('ijk,kl->ijl',data,self.qw)
-        key=tf.einsum('ijk,kl->ijl',data,self.kw)
-        value=tf.einsum('ijk,kl->ijl',data,self.vw)
+        query=tf.matmul(data,self.qw)
+        key=tf.matmul(data,self.kw)
+        value=tf.matmul(data,self.vw)
         query=tf.reshape(query,shape=[query.shape[0],query.shape[1],a,int(data.shape[2])/a])
         key=tf.reshape(key,shape=[key.shape[0], key.shape[1],a,int(data.shape[2])/a])
         value=tf.reshape(value,shape=[value.shape[0],value.shape[1],a,int(data.shape[2])/a])
-        key=tf.transpose(key,perm=[0,2,3,1])
-        value=tf.transpose(value,perm=[0,2,1,3])
+        key=tf.transpose(key,[0,2,1,3])
+        value=tf.transpose(value,[0,2,1,3])
         scores=tf.matmul(query,key)/np.sqrt(int(data.shape[2])/a)
         if mask is not None:
             scores+=mask*-1e9
         attention_weights=tf.nn.softmax(scores,axis=-1)
         output=tf.matmul(attention_weights,value)
-        output=tf.transpose(output,perm=[0,2,1,3])
+        output=tf.transpose(output,[0,2,1,3])
         output=tf.reshape(output,shape=[output.shape[0],output.shape[1],-1])
         return output,attention_weights
