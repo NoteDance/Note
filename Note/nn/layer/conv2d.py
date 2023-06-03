12fd02f5f4333b515ebb099c614a3b856e10ecb8
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2023-06-03 18:14:46 +0800

    Update conv2d.py

--- Note/nn/layer/conv2d.py
+++ Note/nn/layer/conv2d.py
@@ -4,11 +4,20 @@ import Note.nn.initializer as i
 
 
 class conv2d:
-    def __init__(self,weight_shape,weight_initializer='normal',activation=None,dtype='float64'):
+    def __init__(self,weight_shape,weight_initializer='normal',bias_initializer='zero',activation=None,dtype='float64',use_bias=True):
         self.weight=i.initializer(weight_shape,weight_initializer,dtype)
+        if use_bias==True:
+            self.bias=i.initializer([weight_shape[-1]],bias_initializer,dtype)
         self.activation=activation
-        self.weight_list=[self.weight]
+        self.use_bias=use_bias
+        if use_bias==True:
+            self.weight_list=[self.weight,self.bias]
+        else:
+            self.weight_list=[self.weight]
     
     
     def output(self,data,strides,padding='VALID',data_format='NHWC',dilations=None):
-        return a.activation_conv(data,self.weight,self.activation,strides,padding,data_format,dilations,tf.nn.conv2d)
+        if self.use_bias==True:
+            return a.activation_conv(data,self.weight,self.activation,strides,padding,data_format,dilations,tf.nn.conv2d,self.bias)
+        else:
+            return a.activation_conv(data,self.weight,self.activation,strides,padding,data_format,dilations,tf.nn.conv2d)
