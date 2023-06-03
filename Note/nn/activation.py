cfeee2255c834aea439a1920a2a134c69e3980f2
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2023-06-03 18:15:03 +0800

    Update activation.py

--- Note/nn/activation.py
+++ Note/nn/activation.py
@@ -46,24 +46,46 @@ def activation(data,weight,bias=None,activation_func=None,use_bias=True):
             return tf.matmul(data,weight)
 
 
-def activation_conv(data,weight,activation_func,strides,padding,data_format,dilations,conv_func):
-    if activation=='sigmoid':
-        return tf.nn.sigmoid(conv_func(data,weight,strides=strides,padding=padding,data_format=data_format,dilations=dilations))
-    elif activation=='tanh':
-        return tf.nn.tanh(conv_func(data,weight,strides=strides,padding=padding,data_format=data_format,dilations=dilations))
-    elif activation=='relu':
-        return tf.nn.relu(conv_func(data,weight,strides=strides,padding=padding,data_format=data_format,dilations=dilations))
-    elif activation=='elu':
-        return tf.nn.elu(conv_func(data,weight,strides=strides,padding=padding,data_format=data_format,dilations=dilations))
-    elif activation=='softmax':
-        return tf.nn.softmax(conv_func(data,weight,strides=strides,padding=padding,data_format=data_format,dilations=dilations))
-    elif activation_func=='leaky_relu':
-        return tf.nn.leaky_relu(conv_func(data,weight,strides=strides,padding=padding,data_format=data_format,dilations=dilations))
-    elif activation_func=='gelu':
-        return tf.nn.gelu(conv_func(data,weight,strides=strides,padding=padding,data_format=data_format,dilations=dilations))
-    elif type(activation_func)==list and activation_func[0]=='leaky_relu':
-        return tf.nn.leaky_relu(conv_func(data,weight,strides=strides,padding=padding,data_format=data_format,dilations=dilations),activation_func[1])
-    elif type(activation_func)==list and activation_func[0]=='gelu':
-        return tf.nn.gelu(conv_func(data,weight,strides=strides,padding=padding,data_format=data_format,dilations=dilations),activation_func[1])
+def activation_conv(data,weight,activation_func,strides,padding,data_format,dilations,conv_func,bias=None):
+    if bias is None:
+        if activation=='sigmoid':
+            return tf.nn.sigmoid(conv_func(data,weight,strides=strides,padding=padding,data_format=data_format,dilations=dilations))
+        elif activation=='tanh':
+            return tf.nn.tanh(conv_func(data,weight,strides=strides,padding=padding,data_format=data_format,dilations=dilations))
+        elif activation=='relu':
+            return tf.nn.relu(conv_func(data,weight,strides=strides,padding=padding,data_format=data_format,dilations=dilations))
+        elif activation=='elu':
+            return tf.nn.elu(conv_func(data,weight,strides=strides,padding=padding,data_format=data_format,dilations=dilations))
+        elif activation=='softmax':
+            return tf.nn.softmax(conv_func(data,weight,strides=strides,padding=padding,data_format=data_format,dilations=dilations))
+        elif activation_func=='leaky_relu':
+            return tf.nn.leaky_relu(conv_func(data,weight,strides=strides,padding=padding,data_format=data_format,dilations=dilations))
+        elif activation_func=='gelu':
+            return tf.nn.gelu(conv_func(data,weight,strides=strides,padding=padding,data_format=data_format,dilations=dilations))
+        elif type(activation_func)==list and activation_func[0]=='leaky_relu':
+            return tf.nn.leaky_relu(conv_func(data,weight,strides=strides,padding=padding,data_format=data_format,dilations=dilations),activation_func[1])
+        elif type(activation_func)==list and activation_func[0]=='gelu':
+            return tf.nn.gelu(conv_func(data,weight,strides=strides,padding=padding,data_format=data_format,dilations=dilations),activation_func[1])
+        else:
+            return conv_func(data,weight,strides=strides,padding=padding,data_format=data_format,dilations=dilations)
     else:
-        return conv_func(data,weight,strides=strides,padding=padding,data_format=data_format,dilations=dilations)
+        if activation=='sigmoid':
+            return tf.nn.sigmoid(conv_func(data,weight,strides=strides,padding=padding,data_format=data_format,dilations=dilations)+bias)
+        elif activation=='tanh':
+            return tf.nn.tanh(conv_func(data,weight,strides=strides,padding=padding,data_format=data_format,dilations=dilations)+bias)
+        elif activation=='relu':
+            return tf.nn.relu(conv_func(data,weight,strides=strides,padding=padding,data_format=data_format,dilations=dilations)+bias)
+        elif activation=='elu':
+            return tf.nn.elu(conv_func(data,weight,strides=strides,padding=padding,data_format=data_format,dilations=dilations)+bias)
+        elif activation=='softmax':
+            return tf.nn.softmax(conv_func(data,weight,strides=strides,padding=padding,data_format=data_format,dilations=dilations)+bias)
+        elif activation_func=='leaky_relu':
+            return tf.nn.leaky_relu(conv_func(data,weight,strides=strides,padding=padding,data_format=data_format,dilations=dilations)+bias)
+        elif activation_func=='gelu':
+            return tf.nn.gelu(conv_func(data,weight,strides=strides,padding=padding,data_format=data_format,dilations=dilations)+bias)
+        elif type(activation_func)==list and activation_func[0]=='leaky_relu':
+            return tf.nn.leaky_relu(conv_func(data,weight,strides=strides,padding=padding,data_format=data_format,dilations=dilations)+bias,activation_func[1])
+        elif type(activation_func)==list and activation_func[0]=='gelu':
+            return tf.nn.gelu(conv_func(data,weight,strides=strides,padding=padding,data_format=data_format,dilations=dilations)+bias,activation_func[1])
+        else:
+            return conv_func(data,weight,strides=strides,padding=padding,data_format=data_format,dilations=dilations)+bias
