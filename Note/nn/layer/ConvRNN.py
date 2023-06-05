9508aa5599762b0bbaa2310899eab9c21c6b2ee0
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2023-06-05 19:37:01 +0800

    Add files via upload

---
+++ Note/nn/layer/ConvRNN.py
@@ -0,0 +1,26 @@
+import tensorflow as tf
+
+
+class ConvRNN:
+    def __init__(self,conv_layer,rnn_layer):
+        # Receive a convolution layer object and an RNN layer object as parameters
+        self.conv_layer=conv_layer
+        self.rnn_layer=rnn_layer
+        # Merge the weight lists of the two layer objects into one weight list
+        self.weight_list=conv_layer.weight_list+rnn_layer.weight_list
+    
+    
+    def output(self,inputs):
+        # Get the number of timesteps in the input data
+        timestep=tf.shape(inputs)[1]
+        # Create an empty list to store the convolution results for each timestep
+        conv_outputs=[]
+        # Perform convolution operations on the input data for each timestep and add the results to the list
+        for i in range(timestep):
+            conv_output=self.conv_layer.output(inputs[:,i])
+            conv_outputs.append(conv_output)
+        # Convert the list to a tensor with shape [batch_size, timestep, ...]
+        conv_outputs=tf.stack(conv_outputs,axis=1)
+        # Pass the convolution results to the RNN layer and get the final output
+        rnn_output=self.rnn_layer.output(conv_outputs)
+        return rnn_output
