366ec4e1ac401295412800087149cd1371019ecf
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2023-06-05 23:52:38 +0800

    Update embedding.py

--- Note/nn/layer/embedding.py
+++ Note/nn/layer/embedding.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 import Note.nn.initializer as i
 
 
-class EmbeddingLayer:
+class embedding:
     def __init__(self,vocab_size,embed_size,initializer=None,dtype='float32'):
         self.vocab_size=vocab_size
         self.embed_size=embed_size
