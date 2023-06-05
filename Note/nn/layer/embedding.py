d5608acd5afc0c60f4f89802d19714f1a9fc7d58
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2023-06-05 14:20:23 +0800

    Update embedding.py

--- Note/nn/layer/embedding.py
+++ Note/nn/layer/embedding.py
@@ -3,7 +3,7 @@ import Note.nn.initializer as i
 
 
 class EmbeddingLayer:
-    def __init__(self,vocab_size,embed_size,initializer=None,dtype=tf.float32):
+    def __init__(self,vocab_size,embed_size,initializer=None,dtype='float32'):
         self.vocab_size=vocab_size
         self.embed_size=embed_size
         self.embeddings=i.initializer([vocab_size,embed_size],initializer,dtype)
