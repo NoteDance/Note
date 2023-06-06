5ce013076babd279273e47424b9f3234eba2ca77
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2023-06-06 13:37:39 +0800

    Update LMix.py

--- Note/nn/layer/LMix.py
+++ Note/nn/layer/LMix.py
@@ -2,7 +2,8 @@ import tensorflow as tf
 import numpy as np
 
 
-def lmix(x,y,alpha,batch_size):
+def lmix(x,y,alpha):
+    batch_size=x.shape[0]
     lam=np.random.beta(alpha,alpha)
     index=tf.random.shuffle(tf.range(batch_size))
     mask=tf.cast(tf.random.uniform(x.shape)<lam,x.dtype)
