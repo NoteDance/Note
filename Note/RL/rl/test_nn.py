85307b139a19c0406ce6d583e9c4c2341888986a
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2023-06-08 14:49:38 +0800

    Update test_nn.py

--- Note/RL/rl/test_nn.py
+++ Note/RL/rl/test_nn.py
@@ -1,4 +1,5 @@
 import numpy as np
+import traceback
 
 
 def test(nn,platform,action_num=None):
@@ -12,19 +13,23 @@ def test(nn,platform,action_num=None):
                     try:
                         if nn.discriminator!=None:
                             s=np.squeeze(s)
-                    except AttributeError:
+                    except Exception:
+                        print(traceback.format_exc())
                         pass
-            except AttributeError:
+            except Exception:
+                print(traceback.format_exc())
                 a=np.random.choice(action_num)
             next_s,r,done=nn.env(a)
-    except AttributeError:
+    except Exception:
+        print(traceback.format_exc())
         s=np.expand_dims(s,axis=0)
         a=(nn.actor(s)+nn.noise()).numpy()
         next_s,r,done=nn.env(a)
     try:
         if nn.pr!=None:
             nn.pr.TD=np.append(nn.pr.TD,nn.initial_TD)
-    except AttributeError: 
+    except Exception:
+        print(traceback.format_exc())
         pass
     a=np.array(a)
     a=np.expand_dims(a,axis=0)
@@ -37,19 +42,23 @@ def test(nn,platform,action_num=None):
                 gradient=nn.gradient(tape,loss)
                 try:
                     nn.opt.apply_gradients(zip(gradient,nn.param))
-                except AttributeError:
+                except Exception:
+                    print(traceback.format_exc())
                     nn.opt(gradient)
-            except AttributeError:
+            except Exception:
+                print(traceback.format_exc())
                 try:
                     if nn.nn!=None:
                         gradient=tape.gradient(loss,nn.param)
                         nn.opt.apply_gradients(zip(gradient,nn.param))
-                except AttributeError:
-                        actor_gradient=tape.gradient(loss[0],nn.param[0])
-                        critic_gradient=tape.gradient(loss[1],nn.param[1])
-                        nn.opt.apply_gradients(zip(actor_gradient,nn.param[0]))
-                        nn.opt.apply_gradients(zip(critic_gradient,nn.param[1]))
-    except AttributeError:
+                except Exception:
+                    print(traceback.format_exc())
+                    actor_gradient=tape.gradient(loss[0],nn.param[0])
+                    critic_gradient=tape.gradient(loss[1],nn.param[1])
+                    nn.opt.apply_gradients(zip(actor_gradient,nn.param[0]))
+                    nn.opt.apply_gradients(zip(critic_gradient,nn.param[1]))
+    except Exception:
+        print(traceback.format_exc())
         loss=nn.loss(s,a,next_s,r,done)
         nn.backward(loss)
         nn.opt()
