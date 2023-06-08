66465c099d70b1527ffde25871e9a368c0837849
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2023-06-08 20:05:23 +0800

    Update flatten.py

--- Note/nn/layer/flatten.py
+++ Note/nn/layer/flatten.py
@@ -1,9 +1,9 @@
 import tensorflow as tf
 
 
-def flatten(input_tensor):
-    input_shape=tf.shape(input_tensor)
-    batch_size=input_shape[0]
-    num_elements=tf.reduce_prod(input_shape[1:])
-    output_tensor=tf.reshape(input_tensor,[batch_size,num_elements])
-    return output_tensor
+def flatten(data):
+    data_shape=tf.shape(data)
+    batch_size=data_shape[0]
+    num_elements=tf.reduce_prod(data_shape[1:])
+    output=tf.reshape(data,[batch_size,num_elements])
+    return output
