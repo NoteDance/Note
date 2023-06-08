6995290452a3fa5bde60bebe60a38a4819b780b8
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2023-06-08 20:11:15 +0800

    Update capsule.py

--- Note/nn/layer/capsule.py
+++ Note/nn/layer/capsule.py
@@ -21,34 +21,34 @@ class capsule:
         self.weight_list=[self.weight]
     
     
-    def squash(self,inputs):
+    def squash(self,data):
         # define the squash function to normalize the output vectors
-        # inputs: a tensor of shape [batch_size, num_capsules, dim_capsules]
+        # data: a tensor of shape [batch_size, num_capsules, dim_capsules]
         # return: a tensor of shape [batch_size, num_capsules, dim_capsules]
-        norm=tf.norm(inputs,axis=-1,keepdims=True) # compute the norm of each vector
+        norm=tf.norm(data,axis=-1,keepdims=True) # compute the norm of each vector
         norm_squared=norm*norm # compute the squared norm of each vector
-        return (norm_squared/(1+norm_squared))*(inputs/norm) # apply the squash formula
+        return (norm_squared/(1+norm_squared))*(data/norm) # apply the squash formula
     
     
-    def output(self,inputs):
+    def output(self,data):
         # define the output function to compute the output capsules from the input capsules
-        # inputs: a tensor of shape [batch_size, input_num_capsules, input_dim_capsules]
+        # data: a tensor of shape [batch_size, input_num_capsules, input_dim_capsules]
         # return: a tensor of shape [batch_size, num_capsules, dim_capsules]
-        inputs_expand=tf.expand_dims(inputs,2) # expand the inputs to match the weight matrix shape
-        inputs_tiled=tf.tile(inputs_expand,
-                               [1,1,self.num_capsules,1]) # tile the inputs to match the weight matrix shape
-        inputs_hat=tf.scan(lambda ac,x:tf.matmul(self.weight,x), # compute the predicted output vectors by multiplying the inputs and the weight matrix
-                             elems=inputs_tiled,
+        data_expand=tf.expand_dims(data,2) # expand the data to match the weight matrix shape
+        data_tiled=tf.tile(data_expand,
+                               [1,1,self.num_capsules,1]) # tile the data to match the weight matrix shape
+        data_hat=tf.scan(lambda ac,x:tf.matmul(self.weight,x), # compute the predicted output vectors by multiplying the data and the weight matrix
+                             elems=data_tiled,
                              initializer=tf.zeros([
-                                 inputs.shape[0],self.input_num_capsules,
+                                 data.shape[0],self.input_num_capsules,
                                  self.num_capsules,self.dim_capsules
                              ]))
-        b = tf.zeros([inputs.shape[0],self.input_num_capsules,
+        b = tf.zeros([data.shape[0],self.input_num_capsules,
                       self.num_capsules]) # initialize the coupling coefficients to zero
         for i in range(self.routings):
             c=tf.nn.softmax(b,axis=2) # compute the softmax of the coupling coefficients
-            outputs=self.squash(tf.reduce_sum(tf.multiply(c,inputs_hat),axis=1,
+            outputs=self.squash(tf.reduce_sum(tf.multiply(c,data_hat),axis=1,
                                            keepdims=True)) # compute the weighted sum of the predicted output vectors and apply the squash function
             if i!=self.routings-1:
-                b+=tf.reduce_sum(tf.multiply(outputs,inputs_hat),axis=-1) # update the coupling coefficients by adding the agreement between the outputs and the predictions
+                b+=tf.reduce_sum(tf.multiply(outputs,data_hat),axis=-1) # update the coupling coefficients by adding the agreement between the outputs and the predictions
         return tf.squeeze(outputs) # remove the redundant dimension and return the output capsules
