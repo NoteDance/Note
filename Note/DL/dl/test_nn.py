7606c764699c19461a5c5dae28092170b1b2f2cb
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2023-06-08 14:43:58 +0800

    Update test_nn.py

--- Note/DL/dl/test_nn.py
+++ Note/DL/dl/test_nn.py
@@ -1,44 +1,55 @@
+import traceback
+
+
 def test(nn,platform,data,labels):
     try:
         if platform.DType!=None:
             try:
                 if nn.GradientTape!=None:
                     tape,output,loss=nn.GradientTape(data,labels)
-            except AttributeError:
+            except Exception:
+                print(traceback.format_exc())
                 with platform.GradientTape(persistent=True) as tape:
                     try:
                         output=nn.fp(data)
                         loss=nn.loss(output,labels)
-                    except TypeError:
+                    except Exception:
+                        print(traceback.format_exc())
                         output,loss=nn.fp(data,labels)
             try:
                 if nn.accuracy!=None:
                     nn.accuracy(output,labels)
-            except AttributeError:
+            except Exception:
+                print(traceback.format_exc())
                 pass
             try:
                 gradient=nn.gradient(tape,loss)
-            except AttributeError:
+            except Exception:
+                print(traceback.format_exc())
                 gradient=tape.gradient(loss,nn.param)
             try:
                 nn.opt.apply_gradients(zip(gradient,nn.param))
-            except AttributeError:
+            except Exception:
+                print(traceback.format_exc())
                 nn.opt(gradient)
             print('No error')
             return
-    except AttributeError:
+    except Exception:
+        print(traceback.format_exc())
         output=nn.fp(data)
         loss=nn.loss(output,labels)
         try:
             if nn.accuracy!=None:
                 nn.accuracy(output,labels)
-        except AttributeError:
+        except Exception:
+            print(traceback.format_exc())
             pass
         try:
             nn.opt.zero_grad()
             loss.backward()
             nn.opt.step()
-        except:
+        except Exception:
+            print(traceback.format_exc())
             nn.opt(loss)
         print('No error')
         return
