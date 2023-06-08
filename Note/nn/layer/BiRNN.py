7707ee56214a56a5e9962ae3350cbf84beea13ec
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2023-06-08 20:04:38 +0800

    Update BiRNN.py

--- Note/nn/layer/BiRNN.py
+++ Note/nn/layer/BiRNN.py
@@ -8,13 +8,13 @@ class BiRNN:
         self.bw_cells=bw_cells
     
     
-    def output(self,inputs):
-        # Get batch_size from inputs
-        batch_size=tf.shape(inputs)[0]
+    def output(self,data):
+        # Get batch_size from data
+        batch_size=tf.shape(data)[0]
         # Reverse the input data along the time dimension to get the backward input data
-        inputs_bw=tf.reverse(inputs,axis=[1])
+        data_bw=tf.reverse(data,axis=[1])
         # Define a scan function to calculate the output and state of each time step
-        def scan_fn(state,data):
+        def scan_fn(state,data,cell):
             output,state=cell.output(data,state)
             return output,state
         # Use tf.scan function to scan the forward and backward input data and get the forward and backward output data and state list
@@ -25,16 +25,16 @@ class BiRNN:
         for i in range(len(self.fw_cells)):
             cell=self.fw_cells[i]
             if i==0: # The first layer uses the original input data
-                output_fw,state_fw=tf.scan(scan_fn,inputs,initializer=(tf.zeros([batch_size,32]),tf.zeros([batch_size,32])),swap_memory=True)
+                output_fw,state_fw=tf.scan(scan_fn,(data,cell),initializer=(tf.zeros([batch_size,32]),tf.zeros([batch_size,32])),swap_memory=True)
             else: # The later layers use the output data of the previous layer
-                output_fw,state_fw=tf.scan(scan_fn,outputs_fw[-1],initializer=(tf.zeros([batch_size,32]),tf.zeros([batch_size,32])),swap_memory=True)
+                output_fw,state_fw=tf.scan(scan_fn,(outputs_fw[-1],cell),initializer=(tf.zeros([batch_size,32]),tf.zeros([batch_size,32])),swap_memory=True)
             outputs_fw.append(output_fw)
             states_fw.append(state_fw)
             cell=self.bw_cells[i]
             if i==0: # The first layer uses the reversed input data
-                output_bw,state_bw=tf.scan(scan_fn,inputs_bw,initializer=(tf.zeros([batch_size,32]),tf.zeros([batch_size,32])),swap_memory=True)
+                output_bw,state_bw=tf.scan(scan_fn,(data_bw,cell),initializer=(tf.zeros([batch_size,32]),tf.zeros([batch_size,32])),swap_memory=True)
             else: # The later layers use the output data of the previous layer
-                output_bw,state_bw=tf.scan(scan_fn,outputs_bw[-1],initializer=(tf.zeros([batch_size,32]),tf.zeros([batch_size,32])),swap_memory=True)
+                output_bw,state_bw=tf.scan(scan_fn,(outputs_bw[-1],cell),initializer=(tf.zeros([batch_size,32]),tf.zeros([batch_size,32])),swap_memory=True)
             outputs_bw.append(output_bw)
             states_bw.append(state_bw)
         # Concatenate the forward and backward outputs and states to get the bidirectional outputs and states tensor
