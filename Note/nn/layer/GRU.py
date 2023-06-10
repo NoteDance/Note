b048ffa9e6a4a11474bed4e9063d35f2bb5ca7d5
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2023-06-10 21:47:50 +0800

    Update GRU.py

--- Note/nn/layer/GRU.py
+++ Note/nn/layer/GRU.py
@@ -5,11 +5,11 @@ import Note.nn.initializer as i
 class GRU:
     def __init__(self,weight_shape,weight_initializer='Xavier',bias_initializer='zeros',dtype='float32',return_sequence=False,use_bias=True,activation1=tf.nn.sigmoid,activation2=tf.nn.tanh):
         self.weight_r1=i.initializer(weight_shape,weight_initializer,dtype)
-        self.weight_r2=i.initializer(weight_shape,weight_initializer,dtype)
+        self.weight_r2=i.initializer([weight_shape[1],weight_shape[1]],weight_initializer,dtype)
         self.weight_z1=i.initializer(weight_shape,weight_initializer,dtype)
-        self.weight_z2=i.initializer(weight_shape,weight_initializer,dtype)
+        self.weight_z2=i.initializer([weight_shape[1],weight_shape[1]],weight_initializer,dtype)
         self.weight_h1=i.initializer(weight_shape,weight_initializer,dtype)
-        self.weight_h2=i.initializer(weight_shape,weight_initializer,dtype)
+        self.weight_h2=i.initializer([weight_shape[1],weight_shape[1]],weight_initializer,dtype)
         if use_bias==True:
             self.bias_r=i.initializer([weight_shape[1]],bias_initializer,dtype)
             self.bias_z=i.initializer([weight_shape[1]],bias_initializer,dtype)
