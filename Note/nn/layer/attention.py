a58cc34ec51b0e6aac3b69c25319e6cd3a9a98bc
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2023-06-11 18:54:31 +0800

    Update attention.py

--- Note/nn/layer/attention.py
+++ Note/nn/layer/attention.py
@@ -6,7 +6,7 @@ class attention:
     def __init__(self,weight_shape,weight_initializer='Xavier',dtype='float32'):
         self.qw=i.initializer(weight_shape,weight_initializer,dtype)
         self.kw=i.initializer(weight_shape,weight_initializer,dtype)
-        self.sw=i.initializer([weight_shape[0],1],weight_initializer,dtype)
+        self.sw=i.initializer([weight_shape[1],1],weight_initializer,dtype)
         self.weight_list=[self.qw,self.kw,self.sw]
     
     
@@ -15,5 +15,9 @@ class attention:
             score_en_h=tf.matmul(en_h,self.qw)
         score=tf.matmul(tf.nn.tanh(score_en_h+tf.expand_dims(tf.matmul(de_h,self.kw),axis=1)),self.sw)
         attention_weights=tf.nn.softmax(score,axis=1)
-        context_vector=tf.squeeze(tf.matmul(tf.transpose(en_h,[0,2,1]),attention_weights),axis=-1)
+        if len(en_h.shape)==2:
+            context_vector=tf.squeeze(tf.matmul(tf.transpose(en_h,[1,0]),attention_weights),axis=-1)
+        else:
+            context_vector=tf.squeeze(tf.matmul(tf.transpose(en_h,[0,2,1]),attention_weights),axis=-1)
+        attention_weights=tf.squeeze(attention_weights,axis=-1)
         return context_vector,score_en_h,attention_weights
