9fd9b2e070803aa51889f4789ca86d1068ae9d7e
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2023-06-11 14:00:45 +0800

    Update self_attention.py

--- Note/nn/layer/self_attention.py
+++ Note/nn/layer/self_attention.py
@@ -9,22 +9,24 @@ class self_attention:
         self.kw=i.initializer(weight_shape,weight_initializer,dtype)
         self.vw=i.initializer(weight_shape,weight_initializer,dtype)
         self.weight_list=[self.qw,self.kw,self.vw]
-    
-    
+
+
     def output(self,data,a,mask=None):
         query=tf.matmul(data,self.qw)
         key=tf.matmul(data,self.kw)
         value=tf.matmul(data,self.vw)
-        query=tf.reshape(query,shape=[query.shape[0],query.shape[1],a,int(data.shape[2])/a])
-        key=tf.reshape(key,shape=[key.shape[0], key.shape[1],a,int(data.shape[2])/a])
-        value=tf.reshape(value,shape=[value.shape[0],value.shape[1],a,int(data.shape[2])/a])
-        key=tf.transpose(key,[0,2,1,3])
-        value=tf.transpose(value,[0,2,1,3])
+        query=tf.reshape(query,shape=[query.shape[0],query.shape[1],a,int(data.shape[2])//a]) 
+        key=tf.reshape(key,shape=[key.shape[0],key.shape[1],a,int(data.shape[2])//a]) 
+        value=tf.reshape(value,shape=[value.shape[0],value.shape[1],a,int(data.shape[2])//a]) 
+        query=tf.transpose(query,perm=[0,1,3,2])
+        key=tf.transpose(key,perm=[0,1,3,2])
+        key=tf.transpose(key,perm=[0,2,3,1])
+        value=tf.transpose(value,perm=[0,2,1,3])
         scores=tf.matmul(query,key)/np.sqrt(int(data.shape[2])/a)
         if mask is not None:
             scores+=mask*-1e9
         attention_weights=tf.nn.softmax(scores,axis=-1)
         output=tf.matmul(attention_weights,value)
-        output=tf.transpose(output,[0,2,1,3])
+        output=tf.transpose(output,perm=[0,2,1,3])
         output=tf.reshape(output,shape=[output.shape[0],output.shape[1],-1])
         return output,attention_weights
