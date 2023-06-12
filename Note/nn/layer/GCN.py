bae973c1449182b601465351fe0155018000f0ea
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2023-06-12 15:23:52 +0800

    Update GCN.py

--- Note/nn/layer/GCN.py
+++ Note/nn/layer/GCN.py
@@ -17,6 +17,7 @@ class GCN:
         # initialize the weight matrix and the bias vector
         self.weight=i.initializer([input_dim, output_dim],weight_initializer,dtype)
         self.bias=i.initializer([output_dim],bias_initializer,dtype)
+        self.dtype=dtype
         if use_bias==True:
             self.weight_list=[self.weight,self.bias]
         else:
@@ -29,9 +30,9 @@ class GCN:
         # get the adjacency matrix, and normalize it
         adj_matrix=graph["adj_matrix"]
         degree_matrix=tf.reduce_sum(adj_matrix,axis=1)
-        degree_matrix_inv_sqrt=tf.linalg.diag(tf.pow(degree_matrix+1e-10,-0.5))
+        degree_matrix_inv_sqrt=tf.linalg.diag(tf.pow(tf.cast(degree_matrix,dtype=self.dtype)+1e-10,-0.5))
         norm_adj_matrix=tf.matmul(degree_matrix_inv_sqrt,
-                                    tf.matmul(adj_matrix,degree_matrix_inv_sqrt))
+                                    tf.matmul(tf.cast(adj_matrix,dtype=self.dtype),degree_matrix_inv_sqrt))
         # compute the linear transformation and add bias
         linear_output=tf.matmul(data,self.weight)
         linear_output=tf.nn.bias_add(linear_output,self.bias)
