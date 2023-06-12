4f1cef4d1467aaf0262d5640198dea4d10877782
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2023-06-12 23:24:19 +0800

    Update check_nn.py

--- Note/DL/dl/check_nn.py
+++ Note/DL/dl/check_nn.py
@@ -7,49 +7,64 @@ def check(nn,platform,data,labels):
             try:
                 if nn.GradientTape!=None:
                     tape,output,loss=nn.GradientTape(data,labels)
-            except Exception:
-                print(traceback.format_exc())
-                with platform.GradientTape(persistent=True) as tape:
-                    try:
+            except Exception as e:
+                first_exception=e
+                try:
+                    with platform.GradientTape(persistent=True) as tape:
                         output=nn.fp(data)
                         loss=nn.loss(output,labels)
-                    except Exception:
-                        print(traceback.format_exc())
-                        output,loss=nn.fp(data,labels)
+                except Exception as e:
+                    raise e
+                    raise first_exception
             try:
-                if nn.accuracy!=None:
-                    nn.accuracy(output,labels)
-            except Exception:
-                print(traceback.format_exc())
-                pass
+                nn.accuracy(output,labels)
+            except Exception as e:
+                try:
+                  if nn.accuracy!=None:
+                      raise e
+                except Exception: 
+                    pass
             try:
                 gradient=nn.gradient(tape,loss)
-            except Exception:
-                print(traceback.format_exc())
-                gradient=tape.gradient(loss,nn.param)
+            except Exception as e:
+                first_exception=e
+                try:
+                    gradient=tape.gradient(loss,nn.param)
+                except Exception as e:
+                    raise e
+                    raise first_exception
             try:
                 nn.opt.apply_gradients(zip(gradient,nn.param))
-            except Exception:
-                print(traceback.format_exc())
-                nn.opt(gradient)
+            except Exception as e:
+                first_exception=e
+                try:
+                    nn.opt(gradient)
+                except Exception as e:
+                    raise e
+                    raise first_exception
             print('No error')
             return
     except Exception:
-        print(traceback.format_exc())
         output=nn.fp(data)
         loss=nn.loss(output,labels)
         try:
-            if nn.accuracy!=None:
-                nn.accuracy(output,labels)
-        except Exception:
-            print(traceback.format_exc())
-            pass
+            nn.accuracy(output,labels)
+        except Exception as e:
+            try:
+              if nn.accuracy!=None:
+                  raise e
+            except Exception: 
+                pass
         try:
             nn.opt.zero_grad()
             loss.backward()
             nn.opt.step()
-        except Exception:
-            print(traceback.format_exc())
-            nn.opt(loss)
+        except Exception as e:
+            first_exception=e
+            try:
+                nn.opt(loss)
+            except Exception as e:
+                raise e
+                raise first_exception
         print('No error')
         return
