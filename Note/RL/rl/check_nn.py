8c03e960d5d676947ea98e6cc8aec00e66a69259
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2023-06-12 23:54:00 +0800

    Update check_nn.py

--- Note/RL/rl/check_nn.py
+++ Note/RL/rl/check_nn.py
@@ -1,5 +1,4 @@
 import numpy as np
-import traceback
 
 
 def check(nn,platform,action_num=None):
@@ -8,29 +7,35 @@ def check(nn,platform,action_num=None):
         if nn.nn!=None:
             s=np.expand_dims(s,axis=0)
             try:
-                if nn.action!=None:
-                    a=nn.action(s).numpy()
-                    try:
-                        if nn.discriminator!=None:
-                            s=np.squeeze(s)
-                    except Exception:
-                        print(traceback.format_exc())
-                        pass
+                a=nn.action(s).numpy()
+                try:
+                    if nn.discriminator!=None:
+                        s=np.squeeze(s)
+                except Exception:
+                    pass
             except Exception:
-                print(traceback.format_exc())
                 a=np.random.choice(action_num)
             next_s,r,done=nn.env(a)
-    except Exception:
-        print(traceback.format_exc())
-        s=np.expand_dims(s,axis=0)
-        a=(nn.actor(s)+nn.noise()).numpy()
-        next_s,r,done=nn.env(a)
+    except Exception as e:
+        first_exception=e
+        try:
+            if nn.action!=None:
+                raise first_exception
+        except Exception as e:
+            try:
+                s=np.expand_dims(s,axis=0)
+                a=(nn.actor(s)+nn.noise()).numpy()
+                next_s,r,done=nn.env(a)
+            except Exception:
+                raise e
     try:
-        if nn.pr!=None:
-            nn.pr.TD=np.append(nn.pr.TD,nn.initial_TD)
-    except Exception:
-        print(traceback.format_exc())
-        pass
+        nn.pr.TD=np.append(nn.pr.TD,nn.initial_TD)
+    except Exception as e:
+        try:
+            if nn.pr!=None:
+                raise e
+        except Exception:
+            pass
     a=np.array(a)
     a=np.expand_dims(a,axis=0)
     next_s=np.expand_dims(next_s,axis=0)
@@ -43,24 +48,28 @@ def check(nn,platform,action_num=None):
                 try:
                     nn.opt.apply_gradients(zip(gradient,nn.param))
                 except Exception:
-                    print(traceback.format_exc())
                     nn.opt(gradient)
             except Exception:
-                print(traceback.format_exc())
                 try:
                     if nn.nn!=None:
                         gradient=tape.gradient(loss,nn.param)
                         nn.opt.apply_gradients(zip(gradient,nn.param))
                 except Exception:
-                    print(traceback.format_exc())
                     actor_gradient=tape.gradient(loss[0],nn.param[0])
                     critic_gradient=tape.gradient(loss[1],nn.param[1])
                     nn.opt.apply_gradients(zip(actor_gradient,nn.param[0]))
                     nn.opt.apply_gradients(zip(critic_gradient,nn.param[1]))
-    except Exception:
-        print(traceback.format_exc())
-        loss=nn.loss(s,a,next_s,r,done)
-        nn.backward(loss)
-        nn.opt()
+    except Exception as e:
+        first_exception=e
+        try:
+            if platform.DType!=None: 
+                raise first_exception
+        except Exception as e:
+            try:
+                loss=nn.loss(s,a,next_s,r,done)
+                nn.backward(loss)
+                nn.opt()
+            except Exception: 
+                raise e
     print('No error')
     return
