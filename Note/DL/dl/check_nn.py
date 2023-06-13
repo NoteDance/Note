7628c21d6b4d35c9ab9ced6c1cc06694590ae261
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2023-06-13 08:38:53 +0800

    Update check_nn.py

--- Note/DL/dl/check_nn.py
+++ Note/DL/dl/check_nn.py
@@ -2,17 +2,15 @@ def check(nn,platform,data,labels):
     try:
         if platform.DType!=None:
             try:
-                if nn.GradientTape!=None:
-                    tape,output,loss=nn.GradientTape(data,labels)
-            except Exception as e:
-                first_exception=e
                 try:
+                    if nn.GradientTape!=None:
+                        tape,output,loss=nn.GradientTape(data,labels)
+                except Exception:
                     with platform.GradientTape(persistent=True) as tape:
                         output=nn.fp(data)
                         loss=nn.loss(output,labels)
-                except Exception as e:
-                    raise e
-                    raise first_exception
+            except Exception as e:
+                raise e
             try:
                 nn.accuracy(output,labels)
             except Exception as e:
@@ -22,23 +20,19 @@ def check(nn,platform,data,labels):
                 except Exception: 
                     pass
             try:
-                gradient=nn.gradient(tape,loss)
-            except Exception as e:
-                first_exception=e
                 try:
+                    gradient=nn.gradient(tape,loss)
+                except Exception:
                     gradient=tape.gradient(loss,nn.param)
-                except Exception as e:
-                    raise e
-                    raise first_exception
-            try:
-                nn.opt.apply_gradients(zip(gradient,nn.param))
             except Exception as e:
-                first_exception=e
+                raise e
+            try:
                 try:
+                    nn.opt.apply_gradients(zip(gradient,nn.param))
+                except Exception:
                     nn.opt(gradient)
-                except Exception as e:
-                    raise e
-                    raise first_exception
+            except Exception as e:
+                raise e
             print('No error')
             return
     except Exception:
@@ -53,15 +47,13 @@ def check(nn,platform,data,labels):
             except Exception: 
                 pass
         try:
-            nn.opt.zero_grad()
-            loss.backward()
-            nn.opt.step()
-        except Exception as e:
-            first_exception=e
             try:
+                nn.opt.zero_grad()
+                loss.backward()
+                nn.opt.step()
+            except Exception:
                 nn.opt(loss)
-            except Exception as e:
-                raise e
-                raise first_exception
+        except Exception as e:
+            raise e
         print('No error')
         return
