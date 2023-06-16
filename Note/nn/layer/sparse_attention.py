61b2d33994dedf9874ba90f83128190e490aff8a
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2023-06-16 22:02:58 +0800

    Update sparse_attention.py

--- Note/nn/layer/sparse_attention.py
+++ Note/nn/layer/sparse_attention.py
@@ -11,12 +11,15 @@ class sparse_attention:
     
     
     def output(self, data, a, mask=None):
-        query = tf.sparse.sparse_dense_matmul(data, self.qw)
-        key = tf.sparse.sparse_dense_matmul(data, self.kw)
-        value = tf.sparse.sparse_dense_matmul(data, self.vw)
+        query = tf.sparse.sparse_dense_matmul(tf.sparse.reshape(data, shape=[-1,data.shape[2]]), self.qw)
+        key = tf.sparse.sparse_dense_matmul(tf.sparse.reshape(data, shape=[-1,data.shape[2]]), self.kw)
+        value = tf.sparse.sparse_dense_matmul(tf.sparse.reshape(data, shape=[-1,data.shape[2]]), self.vw)
+        query = tf.reshape(query, shape=[data.shape[0], data.shape[1], data.shape[2]])
         query = tf.reshape(query, shape=[query.shape[0], query.shape[1], a, data.shape[2] // a])
+        key = tf.reshape(key, shape=[data.shape[0], data.shape[1], data.shape[2]])
         key = tf.reshape(key, shape=[key.shape[0], key.shape[1], a, data.shape[2] // a])
-        value = tf.reshape(value, shape=[value.shape[0], value.shape[1], a, data.shape[2] // a])
+        value = tf.reshape(query, shape=[data.shape[0], data.shape[1], data.shape[2]])
+        value = tf.reshape(query, shape=[value.shape[0], value.shape[1], a, data.shape[2] // a])
         query = tf.transpose(query, perm=[0, 2, 1, 3])
         key = tf.transpose(key, perm=[0, 2, 1, 3])
         value = tf.transpose(value, perm=[0, 2, 1, 3])
