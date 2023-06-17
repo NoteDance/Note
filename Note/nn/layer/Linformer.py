feadf71d0f3d07c9120ec0ab067e5e347deab10f
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2023-06-17 20:33:23 +0800

    Add files via upload

---
+++ Note/nn/layer/Linformer.py
@@ -0,0 +1,21 @@
+from Note.nn.layer.Linear_attention import Linear_attention
+from Note.nn.layer.dense import dense
+from Note.nn.layer.layer_normalization import layer_normalization
+
+
+# Define a linear transformer layer
+class Linformer:
+  def __init__(self, dim, num_heads, kernel_function='gaussian', kernel_approximation='low_rank'):
+    self.attention_layer = Linear_attention(dim, num_heads, kernel_function, kernel_approximation) # the linear attention layer with multi-head support and kernel approximation
+    self.ffn_layer = dense((dim, dim), activation=None) # the feed-forward layer
+    self.param=[self.attention_layer.param, self.ffn_layer.param]
+    
+  
+  def output(self, data):
+    # x is a tensor of shape (batch_size, seq_len, dim)
+    # return a tensor of shape (batch_size, seq_len, dim)
+    z_out = self.attention_layer.output(data) # (batch_size, seq_len, dim)
+    z_norm = layer_normalization(data + z_out) # (batch_size, seq_len, dim)
+    ffn_out = self.ffn_layer.output(z_norm) # (batch_size, seq_len, dim)
+    ffn_norm = layer_normalization(z_norm + ffn_out) # (batch_size, seq_len, dim)
+    return ffn_norm
