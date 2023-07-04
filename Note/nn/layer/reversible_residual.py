5d307d9a3af44a1555b7d7099ed025fcc9621e64
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2023-07-04 18:00:42 +0800

    Update reversible_residual.py

--- Note/nn/layer/reversible_residual.py
+++ Note/nn/layer/reversible_residual.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 
 def reversible_residual(data, f, g):
         data1, data2 = tf.split(data, 2, axis=-1) # split the input into two halves
-        output1 = data1 + f.output(data2) # compute the first output half
-        output2 = data2 + g.output(output1) # compute the second output half
+        output1 = data1 + f(data2) # compute the first output half
+        output2 = data2 + g(output1) # compute the second output half
         output = tf.concat([output1, output2], axis=-1) # concatenate the output halves
         return output
