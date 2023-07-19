1624c41974a2748d932704e3dd6bada39302b06f
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2023-07-19 13:48:28 +0800

    Update assign_device.py

--- Note/nn/process/assign_device.py
+++ Note/nn/process/assign_device.py
@@ -1,20 +1,7 @@
 import tensorflow as tf
 
 
-def assign_device_with_index(p, device_type): # a function to assign device according to the process index p and the device type
-    devices = tf.config.list_physical_devices(device_type) # get a list of available devices of the given type
-    if devices: # if there are any devices of the given type
-        try:
-            tf.config.set_visible_devices(devices[p], device_type) # set the device with index p as visible
-            device = '/' + device_type + ':' + str(p) # store the device name as an attribute
-        except RuntimeError as e: # catch any runtime error
-            raise e # raise the error message
-    else: # if there are no devices of the given type
-        device = '/CPU:0' # use CPU device as default
-    return device
-
-
-def assign_device_with_modulo(p, device_type): # a function to assign device according to the process index p and the device type
+def assign_device(p, device_type): # a function to assign device according to the process index p and the device type
     devices = tf.config.list_physical_devices(device_type) # get a list of available devices of the given type
     if devices: # if there are any devices of the given type
         try:
