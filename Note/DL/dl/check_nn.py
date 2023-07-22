072e3c83912e32a9c36f449020b30904dde8a04f
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2023-07-22 18:10:58 +0800

    Update check_nn.py

--- Note/DL/dl/check_nn.py
+++ Note/DL/dl/check_nn.py
@@ -3,57 +3,36 @@ def check(nn,platform,data,labels):
         if platform.DType!=None:
             try:
                 try:
-                    if nn.GradientTape!=None:
-                        tape,output,loss=nn.GradientTape(data,labels)
-                except Exception:
                     with platform.GradientTape(persistent=True) as tape:
                         output=nn.fp(data)
                         loss=nn.loss(output,labels)
+                except Exception:
+                    if hasattr(nn,'GradientTape'):
+                        tape,output,loss=nn.GradientTape(data,labels)
             except Exception as e:
                 raise e
-            try:
+            if hasattr(nn,'accuracy'):
                 nn.accuracy(output,labels)
-            except Exception as e:
-                try:
-                  if nn.accuracy!=None:
-                      raise e
-                except Exception: 
-                    pass
-            try:
-                try:
-                    gradient=nn.gradient(tape,loss)
-                except Exception:
-                    gradient=tape.gradient(loss,nn.param)
-            except Exception as e:
-                raise e
-            try:
-                try:
-                    nn.opt.apply_gradients(zip(gradient,nn.param))
-                except Exception:
-                    nn.opt(gradient)
-            except Exception as e:
-                raise e
+            if hasattr(nn,'gradient'):
+                gradient=nn.gradient(tape,loss)
+            else:
+                gradient=tape.gradient(loss,nn.param)
+            if hasattr(nn.opt,'apply_gradients'):
+                nn.opt.apply_gradients(zip(gradient,nn.param))
+            else:
+                nn.opt(gradient)
             print('No error')
             return
     except Exception:
         output=nn.fp(data)
         loss=nn.loss(output,labels)
-        try:
+        if hasattr(nn,'accuracy'):
             nn.accuracy(output,labels)
-        except Exception as e:
-            try:
-              if nn.accuracy!=None:
-                  raise e
-            except Exception: 
-                pass
-        try:
-            try:
-                nn.opt.zero_grad()
-                loss.backward()
-                nn.opt.step()
-            except Exception:
-                nn.opt(loss)
-        except Exception as e:
-            raise e
+        if hasattr(nn.opt,'zero_grad'):
+            nn.opt.zero_grad()
+            loss.backward()
+            nn.opt.step()
+        else:
+            nn.opt(loss)
         print('No error')
         return
