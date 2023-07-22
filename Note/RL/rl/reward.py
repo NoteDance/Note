12cd6676c8d198ff02d5a0d388098c4dc3014b45
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2023-07-22 17:57:06 +0800

    Update reward.py

--- Note/RL/rl/reward.py
+++ Note/RL/rl/reward.py
@@ -19,53 +19,39 @@ class reward:
             for i in range(max_step):
                 if self.end_flag==True:
                     break
-                try:
-                    try:
-                       if self.platform.DType!=None: 
-                           s=np.expand_dims(s,axis=0)
-                           a=np.argmax(self.agent.nn.fp(s))
-                    except Exception:
+                if hasattr(self.agent,'nn'):
+                    if hasattr(self.platform,'DType'):
+                        s=np.expand_dims(s,axis=0)
+                        a=np.argmax(self.agent.nn.fp(s))
+                    else:
                         s=np.expand_dims(s,axis=0)
                         s=self.platform.tensor(s,dtype=self.platform.float).to(self.agent.device)
                         a=self.agent.nn(s).detach().numpy().argmax()
-                except Exception as e:
-                    try:
-                        if self.agent.nn!=None:
-                            raise e
-                    except Exception:
-                        try:
-                            try:
-                                if self.agent.action!=None:
-                                    try:
-                                       if self.platform.DType!=None: 
-                                           s=np.expand_dims(s,axis=0)
-                                           a=self.agent.action(s).numpy()
-                                    except Exception:
-                                        s=np.expand_dims(s,axis=0)
-                                        s=self.platform.tensor(s,dtype=self.platform.float).to(self.agent.device)
-                                        a=self.agent.action(s).detach().numpy()
-                            except Exception:
-                                try:
-                                    if self.platform.DType!=None: 
-                                        s=np.expand_dims(s,axis=0)
-                                        a=self.agent.actor.fp(s).numpy()
-                                        a=np.squeeze(a)
-                                except Exception:
-                                    s=np.expand_dims(s,axis=0)
-                                    s=self.platform.tensor(s,dtype=self.platform.float).to(self.agent.device)
-                                    a=self.agent.actor(s).detach().numpy()
-                                    a=np.squeeze(a)
-                        except Exception as e:
-                            raise e
+                else:
+                    if hasattr(self.agent,'action'):
+                        if hasattr(self.platform,'DType'):
+                            s=np.expand_dims(s,axis=0)
+                            a=self.agent.action(s).numpy()
+                        else:
+                            s=np.expand_dims(s,axis=0)
+                            s=self.platform.tensor(s,dtype=self.platform.float).to(self.agent.device)
+                            a=self.agent.action(s).detach().numpy()
+                    else:
+                        if hasattr(self.platform,'DType'):
+                            s=np.expand_dims(s,axis=0)
+                            a=self.agent.actor.fp(s).numpy()
+                            a=np.squeeze(a)
+                        else:
+                            s=np.expand_dims(s,axis=0)
+                            s=self.platform.tensor(s,dtype=self.platform.float).to(self.agent.device)
+                            a=self.agent.actor(s).detach().numpy()
+                            a=np.squeeze(a)
                 next_s,r,done,_=self.env.step(a)
                 s=next_s
                 reward+=r
-                try:
-                    if self.nn.stop!=None:
-                        if self.nn.stop(next_s):
-                            break
-                except Exception:
-                    pass
+                if hasattr(self.nn,'stop'):
+                    if self.nn.stop(next_s):
+                        break
                 if done:
                     break
             return reward
@@ -73,53 +59,39 @@ class reward:
             while True:
                 if self.end_flag==True:
                     break
-                try:
-                    try:
-                       if self.platform.DType!=None: 
-                           s=np.expand_dims(s,axis=0)
-                           a=np.argmax(self.agent.nn.fp(s))
-                    except Exception:
+                if hasattr(self.agent,'nn'):
+                    if hasattr(self.platform,'DType'):
+                        s=np.expand_dims(s,axis=0)
+                        a=np.argmax(self.agent.nn.fp(s))
+                    else:
                         s=np.expand_dims(s,axis=0)
                         s=self.platform.tensor(s,dtype=self.platform.float).to(self.agent.device)
                         a=self.agent.nn(s).detach().numpy().argmax()
-                except Exception as e:
-                    try:
-                       if self.agent.nn!=None:
-                           raise e
-                    except Exception:
-                        try:
-                            try:
-                                if self.agent.action!=None:
-                                    try:
-                                       if self.platform.DType!=None: 
-                                           s=np.expand_dims(s,axis=0)
-                                           a=self.agent.action(s).numpy()
-                                    except Exception:
-                                        s=np.expand_dims(s,axis=0)
-                                        s=self.platform.tensor(s,dtype=self.platform.float).to(self.agent.device)
-                                        a=self.agent.action(s).detach().numpy()
-                            except Exception:
-                                try:
-                                    if self.platform.DType!=None: 
-                                        s=np.expand_dims(s,axis=0)
-                                        a=self.agent.actor.fp(s).numpy()
-                                        a=np.squeeze(a)
-                                except Exception:
-                                    s=np.expand_dims(s,axis=0)
-                                    s=self.platform.tensor(s,dtype=self.platform.float).to(self.agent.device)
-                                    a=self.agent.actor(s).detach().numpy()
-                                    a=np.squeeze(a)
-                        except Exception as e:
-                            raise e
+                else:
+                    if hasattr(self.agent,'action'):
+                        if hasattr(self.platform,'DType'):
+                            s=np.expand_dims(s,axis=0)
+                            a=self.agent.action(s).numpy()
+                        else:
+                            s=np.expand_dims(s,axis=0)
+                            s=self.platform.tensor(s,dtype=self.platform.float).to(self.agent.device)
+                            a=self.agent.action(s).detach().numpy()
+                    else:
+                        if hasattr(self.platform,'DType'):
+                            s=np.expand_dims(s,axis=0)
+                            a=self.agent.actor.fp(s).numpy()
+                            a=np.squeeze(a)
+                        else:
+                            s=np.expand_dims(s,axis=0)
+                            s=self.platform.tensor(s,dtype=self.platform.float).to(self.agent.device)
+                            a=self.agent.actor(s).detach().numpy()
+                            a=np.squeeze(a)
                 next_s,r,done,_=self.env.step(a)
                 s=next_s
                 reward+=r
-                try:
-                    if self.nn.stop!=None:
-                        if self.nn.stop(next_s):
-                            break
-                except Exception:
-                    pass
+                if hasattr(self.nn,'stop'):
+                    if self.nn.stop(next_s):
+                        break
                 if done:
                     break
             return reward
