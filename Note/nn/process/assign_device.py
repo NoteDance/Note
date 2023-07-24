188b2148835291ad93359572c6b9dc23b8eb572a
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2023-07-24 19:30:05 +0800

    Update assign_device.py

--- Note/nn/process/assign_device.py
+++ Note/nn/process/assign_device.py
@@ -1,5 +1,4 @@
 import tensorflow as tf
-import torch
 
 
 def assign_device(p, device_type): # a function to assign device according to the process index p and the device type
@@ -20,21 +19,4 @@ def assign_tpu(p): # a function to assign TPU device according to the process in
     tf.config.experimental_connect_to_cluster(resolver) # connect to the cluster
     tf.tpu.experimental.initialize_tpu_system(resolver) # initialize the TPU system
     device = '/job:worker/replica:0/task:0/device:TPU:' + str(p) # store the device name as an attribute
-    return device
-
-
-def assign_device_pytorch(p, device_type): # a function to assign device according to the process index p and the device type
-    if device_type == 'GPU': # if the device type is GPU
-        if torch.cuda.is_available(): # if there are any available GPU devices
-            try:
-                torch.cuda.set_device(p % torch.cuda.device_count()) # set the device with index p modulo the number of devices as current
-                device = torch.device('cuda', p % torch.cuda.device_count()) # create a torch.device object with the current device
-            except RuntimeError as e: # catch any runtime error
-                raise e # raise the error message
-        else: # if there are no available GPU devices
-            device = torch.device('cpu') # use CPU device as default
-    elif device_type == 'CPU': # if the device type is CPU
-        device = torch.device('cpu') # use CPU device as default
-    else: # if the device type is neither GPU nor CPU
-        raise ValueError('Invalid device type') # raise a value error
     return device
