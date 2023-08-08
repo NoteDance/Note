e3ede063e935dec6073c85b6afc723d806e0afe3
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2023-08-08 22:38:24 +0800

    Update segment_data.py

--- Note/DL/dl/segment_data.py
+++ Note/DL/dl/segment_data.py
@@ -1,24 +1,13 @@
 import numpy as np
 
 
-def segment_data(data,labels,pt_count):
-    if len(data)!=pt_count:
-        data_=None
-        labels_=None
-        segments=int((len(data)-len(data)%pt_count)/pt_count)
-        for i in range(pt_count):
-            index1=i*segments
-            index2=(i+1)*segments
-            if i==0:
-                data_=np.expand_dims(data[index1:index2],axis=0)
-                labels_=np.expand_dims(labels[index1:index2],axis=0)
-            else:
-                data_=np.concatenate((data_,np.expand_dims(data[index1:index2],axis=0)))
-                labels_=np.concatenate((labels_,np.expand_dims(labels[index1:index2],axis=0)))
-        if len(data)%pt_count!=0:
-            segments+=1
-            index1=segments*pt_count
-            index2=pt_count-(len(data)-segments*pt_count)
-            data_=np.concatenate((data_,np.expand_dims(data[index1:index2],axis=0)))
-            labels_=np.concatenate((labels_,np.expand_dims(labels[index1:index2],axis=0)))
-        return data_,labels_
+def segment_data(data,labels,process):
+    if len(data)!=process:
+        length=len(data)-len(data)%process
+        data=data[:length]
+        labels=labels[:length]
+        data=np.split(data,process)
+        labels=np.split(labels,process)
+        data=np.stack(data,axis=0)
+        labels=np.stack(labels,axis=0)
+        return data,labels
