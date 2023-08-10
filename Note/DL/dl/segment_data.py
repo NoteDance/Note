50cb7f14b10ff75db414e50d14318f90a0c4227f
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2023-08-10 13:44:33 +0800

    Update segment_data.py

--- Note/DL/dl/segment_data.py
+++ Note/DL/dl/segment_data.py
@@ -8,6 +8,4 @@ def segment_data(data,labels,process):
         labels=labels[:length]
         data=np.split(data,process)
         labels=np.split(labels,process)
-        data=np.stack(data,axis=0)
-        labels=np.stack(labels,axis=0)
         return data,labels
