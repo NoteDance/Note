db8125722df6eda5cb1964809cbd96b29656adc5
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2023-08-14 13:46:54 +0800

    Update segment_data.py

--- Note/DL/dl/segment_data.py
+++ Note/DL/dl/segment_data.py
@@ -3,9 +3,6 @@ import numpy as np
 
 def segment_data(data,labels,process):
     if len(data)!=process:
-        length=len(data)-len(data)%process
-        data=data[:length]
-        labels=labels[:length]
-        data=np.split(data,process)
-        labels=np.split(labels,process)
+        data=np.array_split(data,process)
+        labels=np.array_split(labels,process)
         return data,labels
