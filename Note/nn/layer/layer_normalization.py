13f354b213b9b5318290d8d119c663d6b9f668d3
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2023-08-19 18:46:55 +0800

    Update layer_normalization.py

--- Note/nn/layer/layer_normalization.py
+++ Note/nn/layer/layer_normalization.py
@@ -1,8 +1,11 @@
 import tensorflow as tf
 
 
-def layer_normalization(data,epsilon=1e-6):
-    mean,variance=tf.nn.moments(data,axes=[-1],keepdims=True)
-    std=tf.math.sqrt(variance)
-    normalized=(data-mean)/(std+epsilon)
-    return normalized
+def layer_normalization(data,epsilon=1e-6,train_flag=True):
+    if train_flag:
+        mean,variance=tf.nn.moments(data,axes=[-1],keepdims=True)
+        std=tf.math.sqrt(variance)
+        normalized=(data-mean)/(std+epsilon)
+        return normalized
+    else:
+        return data
