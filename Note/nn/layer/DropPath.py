e6ce81baea331cef212f49973ea763c002d6615b
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2023-08-19 13:15:45 +0800

    Update DropPath.py

--- Note/nn/layer/DropPath.py
+++ Note/nn/layer/DropPath.py
@@ -7,17 +7,16 @@ class DropPath:
         self.seed=seed
     
 
-    def output(self,data,train_flag=True):
-        if train_flag:
-            # get the batch size and the number of samples
-            batch_size=tf.shape(data)[0]
-            num_samples=tf.reduce_prod(tf.shape(data)[1:])
-            # create a random mask of shape (batch_size, num_samples)
-            mask=tf.random.uniform([batch_size,num_samples],seed=self.seed)>self.rate
-            # reshape the mask to match the input shape
-            mask=tf.reshape(mask,tf.shape(data))
-            # scale the inputs by the inverse of the keep probability
-            data=data/(1-self.rate)
-            # apply the mask to the inputs
-            output=data*tf.cast(mask,data.dtype)
+    def output(self,data):
+        # get the batch size and the number of samples
+        batch_size=tf.shape(data)[0]
+        num_samples=tf.reduce_prod(tf.shape(data)[1:])
+        # create a random mask of shape (batch_size, num_samples)
+        mask=tf.random.uniform([batch_size,num_samples],seed=self.seed)>self.rate
+        # reshape the mask to match the input shape
+        mask=tf.reshape(mask,tf.shape(data))
+        # scale the inputs by the inverse of the keep probability
+        data=data/(1-self.rate)
+        # apply the mask to the inputs
+        output=data*tf.cast(mask,data.dtype)
         return output
