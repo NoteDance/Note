60ebd68ff3b7a4147a6d28d99c9d619e9f6a5696
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2023-08-20 16:35:53 +0800

    Update MBConv.py

--- Note/nn/layer/MBConv.py
+++ Note/nn/layer/MBConv.py
@@ -3,59 +3,103 @@ from Note.nn.initializer import initializer
 from Note.nn.activation import activation_dict
 
 
+# Define a class named MBConv, which implements a modified depthwise separable convolution (Mobile Inverted Residual Bottleneck)
 class MBConv:
+    # Define the initialization method, which takes several parameters to create the convolution layers
     def __init__(self, input_size, output_size, kernel_size=3, strides=1, expand_ratio=1, repeats=1, se_ratio=0.25, model_number=0, dtype='float32'):
-        self.expanded_size = input_size * expand_ratio # compute the expanded size of the input channels
-        self.output_size = output_size # store the output size of the output channels
-        self.weight_depthwise = initializer([kernel_size, kernel_size, self.expanded_size, 1], 'Xavier', dtype) # create a weight tensor for the depthwise convolution
-        self.weight_project = initializer([1, 1, self.expanded_size, output_size], 'Xavier', dtype) # create a weight tensor for the projection convolution
+        # Calculate the expanded size based on the input size and the expand ratio
+        self.expanded_size = input_size * expand_ratio
+        # Assign the output size to an attribute
+        self.output_size = output_size
+        # Initialize empty lists to store the weight matrices for each layer
+        self.weight_expand = []
+        self.weight_depthwise = []
+        self.weight_project = []
+        self.weight_se_1 = []
+        self.weight_se_2 = []
+        # Calculate the number of channels for the SE module based on the expanded size and the SE ratio
         se_channels = max(1, int(self.expanded_size * se_ratio))
-        self.weight_se_1 = initializer([1, 1, self.expanded_size, se_channels], 'Xavier', dtype) # create a weight tensor for the first squeeze and excitation convolution
-        self.weight_se_2 = initializer([1, 1, se_channels, self.expanded_size], 'Xavier', dtype) # create a weight tensor for the second squeeze and excitation convolution
-        self.strides = strides # store the strides for the depthwise convolution
-        self.expand_ratio = expand_ratio # store the expand ratio for the expansion step
-        self.repeats = repeats # store the number of repeats for the MBConv module
-        self.se_ratio = se_ratio # store the se ratio for the squeeze and excitation module
-        self.model_number = model_number # store the model number for computing the drop connect probability
-        self.dtype=dtype # store the data type for the tensors
-        self.swish = activation_dict['swish'] # get the swish activation function from the activation dictionary
-        self.param = [self.weight_depthwise, self.weight_project, self.weight_se_1, self.weight_se_2] # store all the weight tensors in a list
-
-
+        # Loop through the number of repeats for each MBConv block
+        for i in range(repeats):
+            # If it is the first block, use the input size as the input channels
+            if i==0:
+                self.weight_expand.append(initializer([1, 1, input_size, self.expanded_size], 'Xavier', dtype))
+                self.weight_depthwise.append(initializer([kernel_size, kernel_size, self.expanded_size, 1], 'Xavier', dtype))
+                self.weight_project.append(initializer([1, 1, self.expanded_size, output_size], 'Xavier', dtype))
+            # Otherwise, use the output size as the input channels
+            else:
+                self.weight_expand.append(initializer([1, 1, output_size, self.expanded_size], 'Xavier', dtype))
+                self.weight_depthwise.append(initializer([kernel_size, kernel_size, self.expanded_size, 1], 'Xavier', dtype))
+                self.weight_project.append(initializer([1, 1, self.expanded_size, output_size], 'Xavier', dtype))
+            # Initialize the weight matrices for the SE module
+            self.weight_se_1.append(initializer([1, 1, self.expanded_size, se_channels], 'Xavier', dtype))
+            self.weight_se_2.append(initializer([1, 1, se_channels, self.expanded_size], 'Xavier', dtype))
+        # Assign the other parameters to attributes
+        self.strides = strides
+        self.expand_ratio = expand_ratio
+        self.repeats = repeats
+        self.se_ratio = se_ratio
+        self.model_number = model_number
+        # Get the swish activation function from a dictionary of activation functions
+        self.swish = activation_dict['swish']
+        # Store all the weight matrices in a list of parameters
+        self.param = [self.weight_expand, self.weight_depthwise, self.weight_project, self.weight_se_1, self.weight_se_2]
+    
+    
+    # Define the output method, which takes an input tensor and a training flag to compute the output tensor of the MBConv block
     def output(self, data, train_flag=True):
-        for i in range(self.repeats): # loop over the number of repeats
-            if i == 0: # if it is the first repeat
-                strides_i = self.strides # use the given strides
-                inputs_i = data # use the input data as inputs_i
-            else: # if it is not the first repeat
-                strides_i = 1 # use strides 1
-                inputs_i = x # use x as inputs_i
-            if self.expand_ratio > 1: # if expand ratio is larger than 1
-                weight_expand = initializer([1, 1, inputs_i.shape[-1], self.expanded_size], 'Xavier', self.dtype) # create a weight tensor for the expansion convolution
-                x = tf.nn.conv2d(inputs_i ,weight_expand ,strides=[1 ,1 ,1 ,1] ,padding="SAME") # apply a 1x1 convolution to expand the input channels to expanded size
-                if train_flag: # if it is in training mode
-                    x = tf.nn.batch_normalization(x ,tf.Variable(tf.zeros([self.expanded_size])) ,tf.Variable(tf.ones([self.expanded_size])) ,None ,None ,1e-5) # apply batch normalization to normalize the output
-                x = self.swish(x) # apply swish activation function to increase nonlinearity
-            else: # if expand ratio is not larger than 1
-                x = inputs_i # use inputs_i as x
-            x = tf.nn.depthwise_conv2d(x ,self.weight_depthwise ,strides=[1 ,strides_i ,strides_i ,1] ,padding="SAME") # apply a depthwise convolution to process each channel separately
-            if train_flag: # if it is in training mode
-                x = tf.nn.batch_normalization(x ,tf.Variable(tf.zeros([self.expanded_size])) ,tf.Variable(tf.ones([self.expanded_size])) ,None ,None ,1e-5) # apply batch normalization to normalize the output
-            x = self.swish(x) # apply swish activation function to increase nonlinearity
-            if 0 < self.se_ratio <= 1: # if se ratio is positive and not larger than 1
-                se_tensor = tf.reduce_mean(x, axis=[1, 2]) # compute the global average pooling of each channel
-                se_tensor = tf.reshape(se_tensor, [-1, 1, 1, self.expanded_size]) # reshape it to match the shape of x
-                se_tensor = tf.nn.conv2d(se_tensor, self.weight_se_1, strides=[1, 1, 1, 1], padding="SAME") # use a 1x1 convolution to reduce the channel number to a fraction of the original size, controlled by se ratio
-                se_tensor = self.swish(se_tensor) # use swish activation function to increase nonlinearity
-                se_tensor = tf.nn.conv2d(se_tensor, self.weight_se_2, strides=[1, 1, 1, 1], padding="SAME") # use another 1x1 convolution to restore the channel number to the original size
-                se_tensor = tf.nn.sigmoid(se_tensor) # use sigmoid activation function to get the weight coefficient of each channel, ranging from 0 to 1
-                x = tf.multiply(x, se_tensor) # multiply the output tensor by the squeeze and excitation tensor element-wise
-            x = tf.nn.conv2d(x, self.weight_project, strides=[1, 1, 1, 1], padding="SAME") # apply a 1x1 convolution to project the output channels to the desired size
-            if train_flag: # if it is in training mode
-                x = tf.nn.batch_normalization(x, tf.Variable(tf.zeros([self.output_size])), tf.Variable(tf.ones([self.output_size])), None, None, 1e-5) # apply batch normalization to normalize the output
-            if inputs_i.shape == x.shape: # if the input shape and the output shape are the same
-                if train_flag: # if it is in training mode
-                    rate = 0.2 * (1 - 0.5 * (self.model_number + 1) / 7) # compute the drop connect probability according to the paper formula, where i is the model number (from 0 to 6)
-                    x = tf.nn.dropout(x, rate=rate, noise_shape=(None, 1, 1, 1)) # apply drop connect to randomly drop some residual connections
-                x = tf.add(x, inputs_i) # add the input tensor and the output tensor element-wise to form a residual connection
-        return x # return the output tensor
+        # Loop through the number of repeats for each MBConv block
+        for i in range(self.repeats):
+            # If it is the first block, use the strides parameter as the strides for the depthwise convolution layer
+            if i == 0:
+                strides_i = self.strides
+                inputs_i = data
+            # Otherwise, use 1 as the strides for the depthwise convolution layer
+            else:
+                strides_i = 1
+                inputs_i = x
+            # If the expand ratio is greater than 1, apply an expansion layer to increase the number of channels
+            if self.expand_ratio > 1:
+                x = tf.nn.conv2d(inputs_i ,self.weight_expand[i] ,strides=[1 ,1 ,1 ,1] ,padding="SAME")
+                if train_flag:
+                    x = tf.nn.batch_normalization(x ,tf.Variable(tf.zeros([self.expanded_size])) ,tf.Variable(tf.ones([self.expanded_size])) ,None ,None ,1e-5)
+                x = self.swish(x)
+            # Otherwise, skip the expansion layer and use the input tensor as it is
+            else:
+                x = inputs_i
+            # Apply a depthwise convolution layer to apply spatial filters to each channel
+            x = tf.nn.depthwise_conv2d(x ,self.weight_depthwise[i] ,strides=[1 ,strides_i ,strides_i ,1] ,padding="SAME")
+            if train_flag:
+                x = tf.nn.batch_normalization(x ,tf.Variable(tf.zeros([self.expanded_size])) ,tf.Variable(tf.ones([self.expanded_size])) ,None ,None ,1e-5)
+            x = self.swish(x)
+            # If the SE ratio is positive and less than or equal to 1, apply an SE module to squeeze and excite the channels
+            if 0 < self.se_ratio <= 1:
+                # Compute the global average pooling of the input tensor
+                se_tensor = tf.reduce_mean(x, axis=[1, 2])
+                # Reshape the pooled tensor to match the number of channels
+                se_tensor = tf.reshape(se_tensor, [-1, 1, 1, self.expanded_size])
+                # Apply a pointwise convolution layer to reduce the number of channels
+                se_tensor = tf.nn.conv2d(se_tensor, self.weight_se_1[i], strides=[1, 1, 1, 1], padding="SAME")
+                # Apply the swish activation function
+                se_tensor = self.swish(se_tensor)
+                # Apply another pointwise convolution layer to restore the number of channels
+                se_tensor = tf.nn.conv2d(se_tensor, self.weight_se_2[i], strides=[1, 1, 1, 1], padding="SAME")
+                # Apply the sigmoid activation function
+                se_tensor = tf.nn.sigmoid(se_tensor)
+                # Multiply the input tensor and the SE tensor element-wise
+                x = tf.multiply(x, se_tensor)
+            # Apply a projection layer to reduce the number of channels
+            x = tf.nn.conv2d(x, self.weight_project[i], strides=[1, 1, 1, 1], padding="SAME")
+            if train_flag:
+                x = tf.nn.batch_normalization(x, tf.Variable(tf.zeros([self.output_size])), tf.Variable(tf.ones([self.output_size])), None, None, 1e-5)
+            # If the input tensor and the output tensor have the same shape, add them together to form a residual connection
+            if inputs_i.shape == x.shape:
+                if train_flag:
+                    # Compute the dropout rate based on the model number
+                    rate = 0.2 * (1 - 0.5 * (self.model_number + 1) / 7)
+                    # Apply dropout to the output tensor
+                    x = tf.nn.dropout(x, rate=rate, noise_shape=(None, 1, 1, 1))
+                    # Add the input tensor and the output tensor element-wise to form a residual connection
+                    x = tf.add(x, inputs_i)
+        # Return the output tensor
+        return x
