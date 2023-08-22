ea4f6373ecd787c8c2ca42d221b57bc7ad4ee25e
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2023-08-22 23:07:43 +0800

    Update embedding.py

--- Note/nn/layer/embedding.py
+++ Note/nn/layer/embedding.py
@@ -7,6 +7,7 @@ class embedding:
         self.vocab_size=vocab_size
         self.embed_size=embed_size
         self.embeddings=i.initializer([vocab_size,embed_size],initializer,dtype)
+        self.output_size=embed_size
         self.param=[self.embeddings]
     
     
