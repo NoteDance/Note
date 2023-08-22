ba90137315b2c454506ea2ea76f1a71f85c9aa97
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2023-08-22 23:05:32 +0800

    Update GRU.py

--- Note/nn/layer/GRU.py
+++ Note/nn/layer/GRU.py
@@ -21,6 +21,7 @@ class GRU: # define a class for gated recurrent unit (GRU) layer
         self.use_bias=use_bias # set the use bias flag
         self.activation1=activation1 # set the activation function for gates (usually sigmoid)
         self.activation2=activation2 # set the activation function for candidate hidden state (usually tanh)
+        self.output_size=weight_shape[-1]
         if use_bias==True: # if use bias is True
             self.param=[self.weight_r1,self.weight_z1,self.weight_h1,self.weight_r2,self.weight_z2,self.weight_h2,self.bias_r,self.bias_z,self.bias_h] # store the parameters in a list
         else: # if use bias is False
