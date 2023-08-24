51e45b5795f8396fa151a50aa02a5ae2178c56cf
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2023-08-24 14:21:59 +0800

    Update separable_conv2d.py

--- Note/nn/layer/separable_conv2d.py
+++ Note/nn/layer/separable_conv2d.py
@@ -4,12 +4,16 @@ import Note.nn.initializer as i # import the initializer module from Note.nn pac
 
 
 class separable_conv2d: # define a class for separable convolutional layer
-    def __init__(self,weight_shape,weight_initializer='Xavier',bias_initializer='zeros',activation=None,dtype='float32',use_bias=True): # define the constructor method
+    def __init__(self,weight_shape,strides=[1,1,1,1],padding='VALID',data_format='NHWC',dilations=None,weight_initializer='Xavier',bias_initializer='zeros',activation=None,dtype='float32',use_bias=True): # define the constructor method
         self.weight_D=i.initializer(weight_shape,weight_initializer,dtype) # initialize the weight matrix for depthwise convolution
         self.weight_P=i.initializer([1,1,weight_shape[-1]*weight_shape[-2],weight_shape[-1]],weight_initializer,dtype) # initialize the weight matrix for pointwise convolution
         if use_bias==True: # if use bias is True
             self.bias_D=i.initializer([weight_shape[-1]*weight_shape[-2]],bias_initializer,dtype) # initialize the bias vector for depthwise convolution
             self.bias_P=i.initializer([weight_shape[-1]],bias_initializer,dtype) # initialize the bias vector for pointwise convolution
+        self.strides=strides
+        self.padding=padding
+        self.data_format=data_format
+        self.dilations=dilations
         self.activation=activation # set the activation function
         self.use_bias=use_bias # set the use bias flag
         self.output_size=weight_shape[-1]
@@ -19,12 +23,12 @@ class separable_conv2d: # define a class for separable convolutional layer
             self.param=[self.weight_D,self.weight_P] # store only the weight matrices in a list
     
     
-    def output(self,data,strides,padding='VALID',data_format='NHWC',dilations=None): # define the output method
+    def output(self,data): # define the output method
         if self.use_bias==True: # if use bias is True
-            output=a.activation_conv(data,self.weight_D,self.activation,strides,padding,data_format,dilations,tf.nn.depthwise_conv2d,self.bias_D) # calculate the output of applying activation function to the depthwise convolution of input data and weight matrix, plus bias vector
-            output=a.activation_conv(output,self.weight_P,self.activation,[1,1,1,1],padding,data_format,None,tf.nn.conv2d,self.bias_P) # calculate the output of applying activation function to the pointwise convolution of previous output and weight matrix, plus bias vector
+            output=a.activation_conv(data,self.weight_D,self.activation,self.strides,self.padding,self.data_format,self.dilations,tf.nn.depthwise_conv2d,self.bias_D) # calculate the output of applying activation function to the depthwise convolution of input data and weight matrix, plus bias vector
+            output=a.activation_conv(output,self.weight_P,self.activation,[1,1,1,1],self.padding,self.data_format,None,tf.nn.conv2d,self.bias_P) # calculate the output of applying activation function to the pointwise convolution of previous output and weight matrix, plus bias vector
             return output # return the final output
         else: # if use bias is False
-            output=a.activation_conv(data,self.weight_D,self.activation,strides,padding,data_format,dilations,tf.nn.depthwise_conv2d) # calculate the output of applying activation function to the depthwise convolution of input data and weight matrix
-            output=a.activation_conv(output,self.weight_P,self.activation,[1,1,1,1],padding,data_format,None,tf.nn.conv2d) # calculate the output of applying activation function to the pointwise convolution of previous output and weight matrix
+            output=a.activation_conv(data,self.weight_D,self.activation,self.strides,self.padding,self.data_format,self.dilations,tf.nn.depthwise_conv2d) # calculate the output of applying activation function to the depthwise convolution of input data and weight matrix
+            output=a.activation_conv(output,self.weight_P,self.activation,[1,1,1,1],self.padding,self.data_format,None,tf.nn.conv2d) # calculate the output of applying activation function to the pointwise convolution of previous output and weight matrix
             return output # return the final output
