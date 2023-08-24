4fca6fc4e9f45ff8a4067b15f156bccace3c2cc7
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2023-08-24 14:20:38 +0800

    Update StochasticDepth.py

--- Note/nn/layer/StochasticDepth.py
+++ Note/nn/layer/StochasticDepth.py
@@ -6,10 +6,12 @@ class StochasticDepth:
         self.pL = pL
         self.layer_depth = layer_depth
         self.survival_prob = 1 - pL * (layer_depth / total_depth) # total_depth is a global variable
+        self.train_flag=True
     
     
     def output(self, x, train_flag=True):
-        if train_flag:
+        self.train_flag=train_flag
+        if self.train_flag:
             r = tf.random.uniform([], 0, 1) < self.survival_prob # generate a Bernoulli random number
             if r: # keep the layer
                 return x / self.survival_prob
