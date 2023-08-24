40ea8ea63f9dc11d4ab29d279967adb8728a5918
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2023-08-24 14:21:26 +0800

    Update conv3d.py

--- Note/nn/layer/conv3d.py
+++ Note/nn/layer/conv3d.py
@@ -4,10 +4,14 @@ import Note.nn.initializer as i # import the initializer module from Note.nn pac
 
 
 class conv3d: # define a class for 3D convolutional layer
-    def __init__(self,weight_shape,weight_initializer='Xavier',bias_initializer='zeros',activation=None,dtype='float32',use_bias=True): # define the constructor method
+    def __init__(self,weight_shape,strides=[1,1,1,1,1],padding='VALID',data_format='NHWC',dilations=None,weight_initializer='Xavier',bias_initializer='zeros',activation=None,dtype='float32',use_bias=True): # define the constructor method
         self.weight=i.initializer(weight_shape,weight_initializer,dtype) # initialize the weight tensor
         if use_bias==True: # if use bias is True
             self.bias=i.initializer([weight_shape[-1]],bias_initializer,dtype) # initialize the bias vector
+        self.strides=strides
+        self.padding=padding
+        self.data_format=data_format
+        self.dilations=dilations
         self.activation=activation # set the activation function
         self.use_bias=use_bias # set the use bias flag
         self.output_size=weight_shape[-1]
@@ -17,8 +21,8 @@ class conv3d: # define a class for 3D convolutional layer
             self.param=[self.weight] # store only the weight in a list
     
     
-    def output(self,data,strides,padding='VALID',data_format='NHWC',dilations=None): # define the output method
+    def output(self,data): # define the output method
         if self.use_bias==True: # if use bias is True
-            return a.activation_conv(data,self.weight,self.activation,strides,padding,data_format,dilations,tf.nn.conv3d,self.bias) # return the output of applying activation function to the convolution of data and weight, plus bias
+            return a.activation_conv(data,self.weight,self.activation,self.strides,self.padding,self.data_format,self.dilations,tf.nn.conv3d,self.bias) # return the output of applying activation function to the convolution of data and weight, plus bias
         else: # if use bias is False
-            return a.activation_conv(data,self.weight,self.activation,strides,padding,data_format,dilations,tf.nn.conv3d) # return the output of applying activation function to the convolution of data and weight
+            return a.activation_conv(data,self.weight,self.activation,self.strides,self.padding,self.data_format,self.dilations,tf.nn.conv3d) # return the output of applying activation function to the convolution of data and weight
