4b58391a2fa8f43a33694a6cd1b897205d6d0e09
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2023-08-24 14:20:01 +0800

    Update ViT.py

--- Note/nn/layer/ViT.py
+++ Note/nn/layer/ViT.py
@@ -13,23 +13,25 @@ class ViT:
         self.num_heads=num_heads
         self.activation=activation_dict[activation]
         self.dropout=dropout
+        self.train_flag=True
         self.output_size=dim
         self.param=[self.attn.param,self.mlp1.param,self.mlp2.param]
     
     
     def output(self,data,train_flag=True):
+        self.train_flag=train_flag
         # data: (B, N, dim)
         attn_input=layer_normalization(data,train_flag=train_flag) # (B,N,dim)
         attn_output,_=self.attn.output(attn_input,self.num_heads) # (B,N,dim)
-        if train_flag==True:
+        if self.train_flag==True:
             attn_output=tf.nn.dropout(attn_output,self.dropout) # (B,N,dim)
         residual_input=data+attn_output # (B,N,dim)
         mlp_input=layer_normalization(residual_input,train_flag=train_flag) # (B,N,dim)
         mlp_output=self.activation(self.mlp1.output(mlp_input)) # (B,N,dim)
-        if train_flag==True:
+        if self.train_flag==True:
             mlp_output=tf.nn.dropout(mlp_output,self.dropout) # (B,N,dim)
         mlp_output=self.mlp2.output(mlp_output)
-        if train_flag==True:
+        if self.train_flag==True:
             mlp_output=self.mlp_dropout(mlp_output,self.dropout)
         output=residual_input+mlp_output # (B,N,dim)
         return output
