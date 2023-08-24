7932b742139533325297ccabc336caa762dfcbf1
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2023-08-24 14:22:55 +0800

    Update Linformer.py

--- Note/nn/layer/Linformer.py
+++ Note/nn/layer/Linformer.py
@@ -8,15 +8,17 @@ class Linformer:
   def __init__(self, dim, num_heads, kernel_function='gaussian', kernel_approximation='low_rank'):
     self.attention_layer = Linear_attention(dim, num_heads, kernel_function, kernel_approximation) # the linear attention layer with multi-head support and kernel approximation
     self.ffn_layer = dense((dim, dim), activation=None) # the feed-forward layer
+    self.train_flag=True
     self.output_size=dim
     self.param=[self.attention_layer.param, self.ffn_layer.param]
     
   
   def output(self, data, train_flag=True):
-    # x is a tensor of shape (batch_size, seq_len, dim)
+    # data is a tensor of shape (batch_size, seq_len, dim)
     # return a tensor of shape (batch_size, seq_len, dim)
+    self.train_flag=train_flag
     z_out = self.attention_layer.output(data) # (batch_size, seq_len, dim)
-    z_norm = layer_normalization(data + z_out, train_flag=train_flag) # (batch_size, seq_len, dim)
+    z_norm = layer_normalization(data + z_out, train_flag=self.train_flag) # (batch_size, seq_len, dim)
     ffn_out = self.ffn_layer.output(z_norm) # (batch_size, seq_len, dim)
-    ffn_norm = layer_normalization(z_norm + ffn_out, train_flag=train_flag) # (batch_size, seq_len, dim)
+    ffn_norm = layer_normalization(z_norm + ffn_out, train_flag=self.train_flag) # (batch_size, seq_len, dim)
     return ffn_norm
