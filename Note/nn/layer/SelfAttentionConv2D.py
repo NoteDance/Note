5bba597adf9bc3e992fbfde0a7b3c35215d7851c
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2023-08-24 14:24:30 +0800

    Update SelfAttentionConv2D.py

--- Note/nn/layer/SelfAttentionConv2D.py
+++ Note/nn/layer/SelfAttentionConv2D.py
@@ -4,13 +4,15 @@ from Note.nn.activation import activation_dict
 
 
 class SelfAttentionConv2D:
-    def __init__(self,input_shape,filters,kernel_size,activation=None,kernel_initializer='Xavier',bias_initializer='zeros',dtype='float32',use_bias=True):
+    def __init__(self,input_shape,filters,kernel_size,strides=(1,1),padding='VALID',activation=None,kernel_initializer='Xavier',bias_initializer='zeros',dtype='float32',use_bias=True):
         # filters: the number of output filters
         # kernel_size: the size of the convolution kernel
         # activation: the activation function, default is None
         # use_bias: whether to use bias term, default is True
         # kernel_initializer: the initializer for the kernel weights, default is 'glorot_uniform'
         # bias_initializer: the initializer for the bias term, default is 'zeros'
+        self.strides=strides
+        self.padding=padding
         if activation is not None:
             self.activation=activation_dict[activation]
         else:
@@ -24,12 +26,12 @@ class SelfAttentionConv2D:
             self.param.append(self.bias)
     
     
-    def output(self,data,strides=(1,1),padding='VALID'):
+    def output(self,data):
         # data: the input tensor, shape [batch_size, height, width, channels]
         # strides: the strides of the convolution
         # padding: the padding mode, either 'VALID' or 'SAME'
         # compute the base convolution output
-        conv_output=tf.nn.conv2d(data,self.kernel,strides=strides,padding=padding)
+        conv_output=tf.nn.conv2d(data,self.kernel,strides=self.strides,padding=self.padding)
         if self.use_bias:
             # add the bias term to the convolution output
             conv_output=tf.nn.bias_add(conv_output,self.bias)
@@ -37,9 +39,9 @@ class SelfAttentionConv2D:
             # apply the activation function to the convolution output
             conv_output=self.activation(conv_output)
         # compute the query, key and value tensors for each spatial location
-        query=tf.nn.conv2d(data,self.kernel,strides=strides,padding=padding)
-        key=tf.nn.conv2d(data,self.kernel,strides=strides,padding=padding)
-        value=tf.nn.conv2d(data,self.kernel,strides=strides,padding=padding)
+        query=tf.nn.conv2d(data,self.kernel,strides=self.strides,padding=self.padding)
+        key=tf.nn.conv2d(data,self.kernel,strides=self.strides,padding=self.padding)
+        value=tf.nn.conv2d(data,self.kernel,strides=self.strides,padding=self.padding)
         input_shape=tf.shape(query) # [batch_size, height, width, filters]
         query=tf.reshape(query,[-1,input_shape[1]*input_shape[2],input_shape[-1]])
         key=tf.reshape(key,[-1,input_shape[1]*input_shape[2],input_shape[-1]])
