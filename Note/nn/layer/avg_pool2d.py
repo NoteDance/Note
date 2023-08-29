ee6f0043aff7b2e3436cdd0863b7171e456bc971
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2023-08-29 21:28:20 +0800

    Add files via upload

---
+++ Note/nn/layer/avg_pool2d.py
@@ -0,0 +1,12 @@
+import tensorflow as tf
+
+
+class avg_pool2d:
+    def __init__(self,ksize,strides,padding):
+        self.ksize=ksize
+        self.strides=strides
+        self.padding=padding
+    
+    
+    def output(self,data):
+        return tf.nn.avg_pool2d(data,ksize=self.ksize,strides=self.strides,padding=self.padding)
---
+++ Note/nn/layer/concat.py
@@ -0,0 +1,6 @@
+import tensorflow as tf
+
+
+class concat:
+    def concat(self,data,axis):
+        return tf.concat(data,axis)
