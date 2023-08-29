c46d9c01ea301f22354a01edd4446b4866abcf65
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2023-08-29 21:27:47 +0800

    Update zeropadding2d.py

--- Note/nn/layer/zeropadding2d.py
+++ Note/nn/layer/zeropadding2d.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 
 
 class zeropadding2d:
-    def __init__(self, padding=1):
+    def __init__(self,input_size=None, padding=1):
         if isinstance(padding, int):
             self.padding = tf.constant([[0, 0], [padding, padding], [padding, padding], [0, 0]])
         elif isinstance(padding, list) and len(padding) == 2:
@@ -11,6 +11,13 @@ class zeropadding2d:
             self.padding = tf.constant([[0, 0], [padding[0], padding[1]], [padding[2], padding[3]], [0, 0]])
         else:
             raise ValueError("Invalid padding argument. It should be an int or a list of two or four ints.")
+        self.input_size=input_size
+        if input_size!=None:
+            self.output_size=input_size
+    
+    
+    def build(self):
+        self.output_size=self.input_size
     
     
     def output(self, data):
