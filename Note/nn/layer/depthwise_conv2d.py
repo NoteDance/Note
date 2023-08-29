234fe5da466d10863cc6866b8e65965c909124e8
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2023-08-29 16:25:51 +0800

    Update depthwise_conv2d.py

--- Note/nn/layer/depthwise_conv2d.py
+++ Note/nn/layer/depthwise_conv2d.py
@@ -4,21 +4,39 @@ import Note.nn.initializer as i # import the initializer module from Note.nn pac
 
 
 class depthwise_conv2d: # define a class for depthwise convolutional layer
-    def __init__(self,weight_shape,strides=[1,1,1,1],padding='VALID',data_format='NHWC',dilations=None,weight_initializer='Xavier',bias_initializer='zeros',activation=None,dtype='float32',use_bias=True): # define the constructor method
-        self.weight=i.initializer(weight_shape,weight_initializer,dtype) # initialize the weight tensor
-        if use_bias==True: # if use bias is True
-            self.bias=i.initializer([weight_shape[-1]],bias_initializer,dtype) # initialize the bias vector
+    def __init__(self,depth_multiplier,kernel_size,input_size=None,strides=[1,1,1,1],padding='VALID',weight_initializer='Xavier',bias_initializer='zeros',activation=None,data_format='NHWC',dilations=None,use_bias=True,dtype='float32',): # define the constructor method
+        self.depth_multiplier=depth_multiplier
+        self.kernel_size=kernel_size
+        self.input_size=input_size
         self.strides=strides
         self.padding=padding
+        self.weight_initializer=weight_initializer
+        self.bias_initializer=bias_initializer
+        self.activation=activation # set the activation function
         self.data_format=data_format
         self.dilations=dilations
-        self.activation=activation # set the activation function
         self.use_bias=use_bias # set the use bias flag
-        self.output_size=weight_shape[-1]*weight_shape[-2]
-        if use_bias==True: # if use bias is True
+        self.dtype=dtype
+        if input_size!=None:
+            self.output_size=depth_multiplier*input_size
+            self.weight=i.initializer([kernel_size[0],kernel_size[1],input_size,depth_multiplier],weight_initializer,dtype) # initialize the weight tensor
+            if use_bias==True: # if use bias is True
+                self.bias=i.initializer([depth_multiplier*input_size],bias_initializer,dtype) # initialize the bias vector
+            if use_bias==True: # if use bias is True
+                self.param=[self.weight,self.bias] # store the parameters in a list
+            else: # if use bias is False
+                self.param=[self.weight] # store only the weight in a list
+    
+    
+    def build(self):
+        self.weight=i.initializer([self.kernel_size[0],self.kernel_size[1],self.input_size,self.depth_multiplier],self.weight_initializer,self.dtype) # initialize the weight tensor
+        if self.use_bias==True: # if use bias is True
+            self.bias=i.initializer([self.depth_multiplier*self.input_size],self.bias_initializer,self.dtype) # initialize the bias vector
+        if self.use_bias==True: # if use bias is True
             self.param=[self.weight,self.bias] # store the parameters in a list
         else: # if use bias is False
             self.param=[self.weight] # store only the weight in a list
+        return
     
     
     def output(self,data): # define the output method
