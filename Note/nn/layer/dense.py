0a0e72948a87581f81bb63d53028c467e8bfb828
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2023-08-29 16:26:05 +0800

    Update dense.py

--- Note/nn/layer/dense.py
+++ Note/nn/layer/dense.py
@@ -3,19 +3,37 @@ import Note.nn.initializer as i # import the initializer module from Note.nn pac
 
 
 class dense: # define a class for dense (fully connected) layer
-    def __init__(self,weight_shape,weight_initializer='Xavier',bias_initializer='zeros',activation=None,dtype='float32',use_bias=True): # define the constructor method
-        self.weight=i.initializer(weight_shape,weight_initializer,dtype) # initialize the weight matrix
-        if use_bias==True: # if use bias is True
-            self.bias=i.initializer([weight_shape[1]],bias_initializer,dtype) # initialize the bias vector
-        else: # if use bias is False
-            self.bias=None # set the bias to None
+    def __init__(self,output_size,input_size=None,weight_initializer='Xavier',bias_initializer='zeros',activation=None,use_bias=True,dtype='float32'): # define the constructor method
+        self.input_size=input_size
+        self.weight_initializer=weight_initializer
+        self.bias_initializer=bias_initializer
         self.activation=activation # set the activation function
         self.use_bias=use_bias # set the use bias flag
-        self.output_size=weight_shape[-1]
-        if use_bias==True: # if use bias is True
+        self.dtype=dtype
+        self.output_size=output_size
+        if input_size!=None:
+            self.weight=i.initializer([input_size,output_size],weight_initializer,dtype) # initialize the weight matrix
+            if use_bias==True: # if use bias is True
+                self.bias=i.initializer([output_size],bias_initializer,dtype) # initialize the bias vector
+            else: # if use bias is False
+                self.bias=None # set the bias to None
+            if use_bias==True: # if use bias is True
+                self.param=[self.weight,self.bias] # store the parameters in a list
+            else: # if use bias is False
+                self.param=[self.weight] # store only the weight in a list
+    
+    
+    def build(self):
+        self.weight=i.initializer([self.input_size,self.output_size],self.weight_initializer,self.dtype) # initialize the weight matrix
+        if self.use_bias==True: # if use bias is True
+            self.bias=i.initializer([self.output_size],self.bias_initializer,self.dtype) # initialize the bias vector
+        else: # if use bias is False
+            self.bias=None # set the bias to None
+        if self.use_bias==True: # if use bias is True
             self.param=[self.weight,self.bias] # store the parameters in a list
         else: # if use bias is False
             self.param=[self.weight] # store only the weight in a list
+        return
     
     
     def output(self,data): # define the output method
