c264d76fa90ee1fc11027adc5d0b5801f69a6ee7
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2023-08-29 16:23:38 +0800

    Update identity.py

--- Note/nn/layer/identity.py
+++ Note/nn/layer/identity.py
@@ -1,6 +1,13 @@
 class identity:
-    def __init__(self,input_size):
-        self.output_size=input_size
+    def __init__(self,input_size=None):
+        self.input_size=input_size
+        if input_size!=None:
+            self.output_size=input_size
+    
+    
+    def build(self):
+        self.output_size=self.input_size
+        return
     
     
     def output(self,data):
