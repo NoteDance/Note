28128fcbdbce217aa24f03b18c9b713157933ab4
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2023-08-29 16:23:57 +0800

    Update batch_normalization.py

--- Note/nn/layer/batch_normalization.py
+++ Note/nn/layer/batch_normalization.py
@@ -3,25 +3,51 @@ from Note.nn.initializer import initializer
 
 
 class batch_normalization:
-    def __init__(self, input_size, axes=-1, momentum=0.99, epsilon=0.001, center=True, scale=True, beta_initializer='zeros', gamma_initializer='ones', moving_mean_initializer='zeros', moving_variance_initializer='ones', keepdims=False, dtype='float32'):
+    def __init__(self, input_size=None, axes=-1, momentum=0.99, epsilon=0.001, center=True, scale=True, beta_initializer='zeros', gamma_initializer='ones', moving_mean_initializer='zeros', moving_variance_initializer='ones', keepdims=False, dtype='float32'):
+        self.input_size=input_size
         self.axes=axes
         self.momentum=momentum
         self.epsilon=epsilon
+        self.center=center
+        self.scale=scale
+        self.beta_initializer=beta_initializer
+        self.gamma_initializer=gamma_initializer
         self.keepdims=keepdims
+        self.dtype=dtype
         self.train_flag=True
+        if input_size!=None:
+            self.output_size=input_size
+            self.moving_mean=tf.zeros([input_size],dtype)
+            self.moving_var=tf.ones([input_size],dtype)
+            self.param=[]
+            if center==True:
+                self.beta=initializer([input_size], beta_initializer, dtype)
+                self.param.append(self.beta)
+            else:
+                self.beta=None
+            if scale==True:
+                self.gamma=initializer([input_size], gamma_initializer, dtype)
+                self.param.append(self.gamma)
+            else:
+                self.gamma=None
+    
+    
+    def build(self):
+        self.output_size=self.input_size
+        self.moving_mean=tf.zeros([self.input_size],self.dtype)
+        self.moving_var=tf.ones([self.input_size],self.dtype)
         self.param=[]
-        self.moving_mean=tf.zeros([input_size],dtype)
-        self.moving_var=tf.ones([input_size],dtype)
-        if center==True:
-            self.beta=initializer([input_size], beta_initializer, dtype)
+        if self.center==True:
+            self.beta=initializer([self.input_size], self.beta_initializer, self.dtype)
             self.param.append(self.beta)
         else:
             self.beta=None
-        if scale==True:
-            self.gamma=initializer([input_size], gamma_initializer, dtype)
+        if self.scale==True:
+            self.gamma=initializer([self.input_size], self.gamma_initializer, self.dtype)
             self.param.append(self.gamma)
         else:
             self.gamma=None
+        return
     
     
     def output(self, data, train_flag=True):
