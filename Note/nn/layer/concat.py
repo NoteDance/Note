eaeb5cc594cccf032ec535934e9c23d7d23dc216
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2023-08-30 14:23:44 +0800

    Update concat.py

--- Note/nn/layer/concat.py
+++ Note/nn/layer/concat.py
@@ -2,5 +2,9 @@ import tensorflow as tf
 
 
 class concat:
-    def concat(self,data,axis):
-        return tf.concat(data,axis)
+    def __init__(self,axis=-1):
+        self.axis=axis
+        
+        
+    def concat(self,data1,data2):
+        return tf.concat([data1,data2],axis=self.axis)
