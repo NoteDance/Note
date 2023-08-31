b9f16130fb7b976955b19f872c54460a77f7586b
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2023-08-31 21:53:49 +0800

    Update highway.py

--- Note/nn/layer/highway.py
+++ Note/nn/layer/highway.py
@@ -4,7 +4,7 @@ from Note.nn.activation import activation_dict
 
 
 class highway:
-    def __init__(self,input_dim,output_dim,weight_initializer='Xavier',bias_initializer='zeros',activation='relu',dtype='float32',use_bias=True):
+    def __init__(self,output_size,input_size=None,weight_initializer='Xavier',bias_initializer='zeros',activation='relu',use_bias=True,dtype='float32',):
         # input_dim: the dimension of the input features
         # output_dim: the dimension of the output features
         # weight_initializer: the initializer for the weight matrices
@@ -12,19 +12,35 @@ class highway:
         # activation: the activation function for the transform gate, default is None
         # dtype: the data type of the parameters
         # use_bias: whether to use bias or not
-        self.input_dim=input_dim
-        self.output_dim=output_dim
+        self.input_size=input_size
+        self.weight_initializer=weight_initializer
+        self.bias_initializer=bias_initializer
         if activation is not None:
             self.activation=activation_dict[activation]
         else:
             self.activation=None
+        self.use_bias=use_bias
+        self.dtype=dtype
+        self.output_size=output_size
+        if input_size!=None:
+            # initialize the weight matrices and the bias vectors for the transform and carry gates
+            self.weight_T=i.initializer([input_size,output_size],weight_initializer,dtype)
+            self.bias_T=i.initializer([output_size],bias_initializer,dtype)
+            self.weight_C=i.initializer([input_size,output_size],weight_initializer,dtype)
+            self.bias_C=i.initializer([output_size],bias_initializer,dtype)
+            if use_bias==True:
+                self.param=[self.weight_T,self.bias_T,self.weight_C,self.bias_C]
+            else:
+                self.param=[self.weight_T,self.weight_C]
+    
+    
+    def build(self):
         # initialize the weight matrices and the bias vectors for the transform and carry gates
-        self.weight_T=i.initializer([input_dim,output_dim],weight_initializer,dtype)
-        self.bias_T=i.initializer([output_dim],bias_initializer,dtype)
-        self.weight_C=i.initializer([input_dim,output_dim],weight_initializer,dtype)
-        self.bias_C=i.initializer([output_dim],bias_initializer,dtype)
-        self.output_size=output_dim
-        if use_bias==True:
+        self.weight_T=i.initializer([self.input_size,self.output_size],self.weight_initializer,self.dtype)
+        self.bias_T=i.initializer([self.output_size],self.bias_initializer,self.dtype)
+        self.weight_C=i.initializer([self.input_size,self.output_size],self.weight_initializer,self.dtype)
+        self.bias_C=i.initializer([self.output_size],self.bias_initializer,self.dtype)
+        if self.use_bias==True:
             self.param=[self.weight_T,self.bias_T,self.weight_C,self.bias_C]
         else:
             self.param=[self.weight_T,self.weight_C]
