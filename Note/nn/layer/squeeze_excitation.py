7e7d46541d57acbb88965375a361094002ef3ced
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2023-09-01 19:47:17 +0800

    Update squeeze_excitation.py

--- Note/nn/layer/squeeze_excitation.py
+++ Note/nn/layer/squeeze_excitation.py
@@ -4,30 +4,47 @@ from Note.nn.activation import activation_dict
 
 
 class squeeze_excitation:
-    def __init__(self,input_dim,output_dim,ratio,weight_initializer='Xavier',bias_initializer='zeros',activation='relu',dtype='float32',use_bias=True):
-        # input_dim: the dimension of the input features
-        # output_dim: the dimension of the output features
+    def __init__(self,output_size,ratio,input_size=None,weight_initializer='Xavier',bias_initializer='zeros',activation='relu',use_bias=True,dtype='float32'):
+        # input_size: the dimension size of the input features
+        # output_size: the dimension size of the output features
         # ratio: the reduction ratio for the squeeze operation
         # weight_initializer: the initializer for the weight matrices
         # bias_initializer: the initializer for the bias vectors
         # activation: the activation function for the squeeze operation, default is relu
         # dtype: the data type of the parameters
         # use_bias: whether to use bias or not
-        self.input_dim=input_dim
-        self.output_dim=output_dim
         self.ratio=ratio
+        self.input_size=input_size
+        self.weight_initializer=weight_initializer
+        self.bias_initializer=bias_initializer
         self.activation=activation_dict[activation]
+        self.use_bias=use_bias
+        self.dtype=dtype
+        self.output_size=output_size
+        if input_size!=None:
+            # initialize the weight matrices and the bias vectors for the squeeze and excitation operations
+            self.weight_S=i.initializer([input_size,output_size//ratio],weight_initializer,dtype)
+            self.bias_S=i.initializer([output_size//ratio],bias_initializer,dtype)
+            self.weight_E=i.initializer([output_size//ratio,output_size],weight_initializer,dtype)
+            self.bias_E=i.initializer([output_size],bias_initializer,dtype)
+            if use_bias==True:
+                self.param=[self.weight_S,self.bias_S,self.weight_E,self.bias_E]
+            else:
+                self.param=[self.weight_S,self.weight_E]
+    
+    
+    def build(self):
         # initialize the weight matrices and the bias vectors for the squeeze and excitation operations
-        self.weight_S=i.initializer([input_dim,output_dim//ratio],weight_initializer,dtype)
-        self.bias_S=i.initializer([output_dim//ratio],bias_initializer,dtype)
-        self.weight_E=i.initializer([output_dim//ratio,output_dim],weight_initializer,dtype)
-        self.bias_E=i.initializer([output_dim],bias_initializer,dtype)
-        self.output_size=output_dim
-        if use_bias==True:
+        self.weight_S=i.initializer([self.input_size,self.output_size//self.ratio],self.weight_initializer,self.dtype)
+        self.bias_S=i.initializer([self.output_size//self.ratio],self.bias_initializer,self.dtype)
+        self.weight_E=i.initializer([self.output_size//self.ratio,self.output_size],self.weight_initializer,self.dtype)
+        self.bias_E=i.initializer([self.output_size],self.bias_initializer,self.dtype)
+        if self.use_bias==True:
             self.param=[self.weight_S,self.bias_S,self.weight_E,self.bias_E]
         else:
             self.param=[self.weight_S,self.weight_E]
-    
+        return
+        
     
     def output(self,data):
         # data: a tensor that represents the input features, shape is [N, H, W, C], where N is the number of samples, H is the height, W is the width, and C is the channel number
