887b1db24d3c80b85f9a17c88ba54903c1842c63
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2023-09-01 19:47:53 +0800

    Update RNNCell.py

--- Note/nn/layer/RNNCell.py
+++ Note/nn/layer/RNNCell.py
@@ -4,7 +4,7 @@ from Note.nn.activation import activation_dict # import the activation function 
 
 
 class RNNCell: # define a class for recurrent neural network (RNN) cell
-    def __init__(self,weight_shape,weight_initializer='Xavier',bias_initializer='zeros',activation=None,dtype='float32',use_bias=True): # define the constructor method
+    def __init__(self,weight_shape,weight_initializer='Xavier',bias_initializer='zeros',activation=None,use_bias=True,trainable=True,dtype='float32'): # define the constructor method
         self.weight_i=i.initializer(weight_shape,weight_initializer,dtype) # initialize the weight matrix for input data
         self.weight_s=i.initializer([weight_shape[1],weight_shape[1]],weight_initializer,dtype) # initialize the weight matrix for previous state
         if use_bias==True: # if use bias is True
@@ -12,10 +12,13 @@ class RNNCell: # define a class for recurrent neural network (RNN) cell
         self.activation=activation_dict[activation] # get the activation function from the activation dictionary
         self.use_bias=use_bias # set the use bias flag
         self.output_size=weight_shape[-1]
-        if use_bias==True: # if use bias is True
-            self.param=[self.weight_i,self.weight_s,self.bias] # store the parameters in a list
-        else: # if use bias is False
-            self.param=[self.weight_i,self.weight_s] # store only the weight matrices in a list
+        if trainable==True:
+            if use_bias==True: # if use bias is True
+                self.param=[self.weight_i,self.weight_s,self.bias] # store the parameters in a list
+            else: # if use bias is False
+                self.param=[self.weight_i,self.weight_s] # store only the weight matrices in a list
+        else:
+            self.param=[]
     
     
     def output(self,data,state): # define the output method
