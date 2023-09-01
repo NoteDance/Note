3feb5fdba9a858645b6e4dfe161372d6c5c6147b
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2023-09-01 19:52:42 +0800

    Update embedding.py

--- Note/nn/layer/embedding.py
+++ Note/nn/layer/embedding.py
@@ -3,20 +3,27 @@ import Note.nn.initializer as i
 
 
 class embedding:
-    def __init__(self,embed_size,input_size=None,initializer='Xavier',dtype='float32'):
+    def __init__(self,embed_size,input_size=None,initializer='Xavier',trainable=True,dtype='float32'):
         self.embed_size=embed_size
         self.input_size=input_size
         self.initializer=initializer
+        self.trainable=trainable
         self.dtype=dtype
         self.output_size=embed_size
         if input_size!=None:
             self.embeddings=i.initializer([input_size,embed_size],initializer,dtype)
-            self.param=[self.embeddings]
+            if trainable==True:
+                self.param=[self.embeddings]
+            else:
+                self.param=[]
     
     
     def build(self):
         self.embeddings=i.initializer([self.input_size,self.embed_size],self.initializer,self.dtype)
-        self.param=[self.embeddings]
+        if self.trainable==True:
+            self.param=[self.embeddings]
+        else:
+            self.param=[]
         return
     
     
