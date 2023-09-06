1bfa33f0a8a36da0a87d6d57de0193749cd75012
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2023-09-06 13:58:35 +0800

    Update MobileNetV2.py

--- Note/nn/neuralnetwork/MobileNet/MobileNetV2.py
+++ Note/nn/neuralnetwork/MobileNet/MobileNetV2.py
@@ -170,6 +170,14 @@ class MobileNetV2:
         return loss_value
     
     
+    def GradientTape(self,data,labels,p):
+        with tf.device(assign_device(p,'GPU')):
+            with tf.GradientTape(persistent=True) as tape:
+                output=self.fp(data,p)
+                loss=self.loss(output,labels,p)
+        return tape,output,loss
+    
+    
     def opt(self,gradient,p):
         with tf.device(assign_device(p,'GPU')):
             param=self.optimizer.opt(gradient,self.param,self.bc[0])
