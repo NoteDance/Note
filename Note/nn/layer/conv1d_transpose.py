89085daf9f6aacdef9fe2093e2c30794fae12c47
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2023-09-23 14:17:33 +0800

    Update conv1d_transpose.py

--- Note/nn/layer/conv1d_transpose.py
+++ Note/nn/layer/conv1d_transpose.py
@@ -4,7 +4,7 @@ import Note.nn.initializer as i # import the initializer module from Note.nn pac
 
 
 class conv1d_transpose: # define a class for 1D transposed convolutional layer
-    def __init__(self,filters,kernel_size,input_size=None,output_shape=None,strides=[1],padding='VALID',weight_initializer='Xavier',bias_initializer='zeros',activation=None,data_format='NWC',dilations=None,use_bias=True,trainable=True,dtype='float32'): # define the constructor method
+    def __init__(self,filters,kernel_size,input_size=None,new_steps=None,strides=[1],padding='VALID',weight_initializer='Xavier',bias_initializer='zeros',activation=None,data_format='NWC',dilations=None,use_bias=True,trainable=True,dtype='float32'): # define the constructor method
         self.kernel_size=kernel_size
         self.input_size=input_size
         self.strides=strides
@@ -17,9 +17,9 @@ class conv1d_transpose: # define a class for 1D transposed convolutional layer
         self.use_bias=use_bias # set the use bias flag
         self.trainable=trainable
         self.dtype=dtype
-        self.output_shape=output_shape # set the output shape of the layer
+        self.new_steps=new_steps # set the new steps of the layer
         self.output_size=filters
-        if input_size!=None and output_shape!=None:
+        if input_size!=None:
             self.weight=i.initializer([kernel_size,filters,input_size],weight_initializer,dtype) # initialize the weight tensor
             if use_bias==True: # if use bias is True
                 self.bias=i.initializer([filters],bias_initializer,dtype) # initialize the bias vector
@@ -46,6 +46,6 @@ class conv1d_transpose: # define a class for 1D transposed convolutional layer
     
     def output(self,data): # define the output method
         if self.use_bias==True: # if use bias is True
-            return a.activation_conv_transpose(data,self.weight,self.output_shape,self.activation,self.strides,self.padding,self.data_format,self.dilations,tf.nn.conv1d_transpose,bias=self.bias) # return the output of applying activation function to the transposed convolution of data and weight, plus bias
+            return a.activation_conv_transpose(data,self.weight,[data.shape[0],self.new_steps,self.output_size],self.activation,self.strides,self.padding,self.data_format,self.dilations,tf.nn.conv1d_transpose,bias=self.bias) # return the output of applying activation function to the transposed convolution of data and weight, plus bias
         else: # if use bias is False
-            return a.activation_conv_transpose(data,self.weight,self.output_shape,self.activation,self.strides,self.padding,self.data_format,self.dilations,tf.nn.conv1d_transpose) # return the output of applying activation function to the transposed convolution of data and weight
+            return a.activation_conv_transpose(data,self.weight,[data.shape[0],self.new_steps,self.output_size],self.activation,self.strides,self.padding,self.data_format,self.dilations,tf.nn.conv1d_transpose) # return the output of applying activation function to the transposed convolution of data and weight
