02534ccaf933e034c874bffdf0365ac3f9f40ab2
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2023-09-26 14:14:37 +0800

    Add files via upload

---
+++ Note/nn/layer/dropout.py
@@ -0,0 +1,18 @@
+import tensorflow as tf
+
+
+class dropout:
+    def __init__(self,rate,noise_shape=None,seed=None):
+        self.rate=rate
+        self.noise_shape=noise_shape
+        self.seed=seed
+        self.train_flag=True
+    
+    
+    def output(self,data,train_flag=True):
+        self.train_flag=train_flag
+        if train_flag==True:
+            output=tf.nn.dropout(data,self.rate,noise_shape=self.noise_shape,seed=self.seed)
+        else:
+            output=data
+        return output
---
+++ Note/nn/layer/stochastic_depth.py
@@ -0,0 +1,18 @@
+import tensorflow as tf
+
+
+class stochastic_depth:
+    def __init__(self, drop_path_rate):
+        self.drop_path_rate=drop_path_rate
+        self.train_flag=True
+    
+    
+    def output(self, x, train_flag=True):
+        self.train_flag=train_flag
+        if train_flag:
+            keep_prob = 1 - self.drop_path_rate
+            shape = (tf.shape(x)[0],) + (1,) * (len(tf.shape(x)) - 1)
+            random_tensor = keep_prob + tf.random.uniform(shape, 0, 1, dtype=x.dtype)
+            random_tensor = tf.floor(random_tensor)
+            return (x / keep_prob) * random_tensor
+        return x
