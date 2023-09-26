7cf0c1ad5369ebd7b5fac1eb73209221e55f76f9
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2023-09-26 14:05:06 +0800

    Add files via upload

---
+++ Note/nn/layer/spatial_dropout2d.py
@@ -0,0 +1,40 @@
+import tensorflow as tf
+
+class spatial_dropout2d:
+    """Spatial 2D dropout layer.
+
+    This layer randomly sets 2D feature maps along the last two dimensions to zero with a
+    frequency of `rate` at each step during training time in order to prevent overfitting.
+    Inputs not set to zero are scaled up by 1/(1 - rate) such that the sum over all inputs
+    is unchanged.
+
+    Arguments:
+        rate: Float between 0 and 1. Fraction of the input units to drop.
+        seed: A Python integer to use as random seed.
+
+    Call arguments:
+        data: A 4D tensor.
+        train_flag: A Python boolean indicating whether to apply dropout to the inputs or not. 
+            If True, the layer will randomly set 2D feature maps to zero with a frequency of rate. 
+            If False, the layer will return the inputs unchanged.
+
+    References:
+        - Efficient Object Localization Using Convolutional Networks
+    """
+
+    def __init__(self, rate, seed=7):
+        self.rate = rate
+        self.seed = seed
+        self.train_flag = True
+
+    def output(self, data, train_flag=True):
+        self.train_flag = train_flag
+        def dropped_inputs():
+            # Generate a mask with shape (batch_size, 1, 1, channels)
+            noise_shape = (tf.shape(data)[0], 1, 1, tf.shape(data)[3])
+            mask = tf.random.stateless_binomial(noise_shape, seed=[self.seed, 0], counts=1, probs=(1 - self.rate), 
+                                                output_dtype=data.dtype)
+            # Scale up the input by 1/(1 - rate) and apply the mask
+            return data * mask * (1.0 / (1.0 - self.rate))
+
+        return tf.cond(train_flag, dropped_inputs, lambda: data)
