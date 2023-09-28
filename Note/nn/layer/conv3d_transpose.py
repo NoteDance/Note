1a525c458666fdaa82dbfb114a778ae957251aba
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2023-09-28 15:03:24 +0800

    Update conv3d_transpose.py

--- Note/nn/layer/conv3d_transpose.py
+++ Note/nn/layer/conv3d_transpose.py
@@ -4,7 +4,7 @@ import Note.nn.initializer as i # import the initializer module from Note.nn pac
 from Note.nn.Module import Module
 
 
-class conv3d_transpose: # define a class for 3D transposed convolutional layer
+class conv3d_transpose(Module): # define a class for 3D transposed convolutional layer
     def __init__(self,filters,kernel_size,input_size=None,new_drc=None,strides=[1,1,1],padding='VALID',output_padding=None,weight_initializer='Xavier',bias_initializer='zeros',activation=None,data_format='NDHWC',dilations=None,use_bias=True,trainable=True,dtype='float32'): # define the constructor method
         self.kernel_size=kernel_size
         self.input_size=input_size
