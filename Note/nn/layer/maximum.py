0ba057177012015d4b9b1af0a80b2310d305f86a
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2023-09-30 22:56:53 +0800

    Add files via upload

---
+++ Note/nn/layer/maximum.py
@@ -0,0 +1,18 @@
+import tensorflow as tf
+
+
+class maximum:
+    def __init__(self):
+        self.save_data_count=None
+        
+        
+    def output(self,data):
+        if self.save_data_count!=None:
+            output=data.pop(0)
+            for i in range(1,self.save_data_count):
+                output=tf.maximum(output,data.pop(0))
+        else:
+            output=data[0]
+            for i in range(1,len(data)):
+                output=tf.maximum(output,data[i])
+        return output
