aca05ee9d2fe7c37c0bea74f6c015130165250a6
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2023-09-30 22:57:38 +0800

    Update average.py

--- Note/nn/layer/average.py
+++ Note/nn/layer/average.py
@@ -4,7 +4,13 @@ class average:
         
         
     def output(self,data):
-        output=data.pop(0)
-        for i in range(1,self.save_data_count):
-            output+=data.pop(0)
-        return output/self.save_data_count
+        if self.save_data_count!=None:
+            output=data.pop(0)
+            for i in range(1,self.save_data_count):
+                output+=data.pop(0)
+            return output/self.save_data_count
+        else:
+            output=data[0]
+            for i in range(1,len(data)):
+                output+=data[i]
+            return output/len(data)
