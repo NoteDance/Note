8f216312b024cf676863ecdf2064ee02e79bd9e2
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2023-09-30 22:58:04 +0800

    Update add.py

--- Note/nn/layer/add.py
+++ Note/nn/layer/add.py
@@ -4,7 +4,12 @@ class add:
         
         
     def output(self,data):
-        output=data.pop(0)
-        for i in range(1,self.save_data_count):
-            output+=data.pop(0)
+        if self.save_data_count!=None:
+            output=data.pop(0)
+            for i in range(1,self.save_data_count):
+                output+=data.pop(0)
+        else:
+            output=data[0]
+            for i in range(1,len(data)):
+                output+=data[i]
         return output
