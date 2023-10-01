f3f9e3658ff3987b6e01dafc02f2e12f02986420
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2023-10-01 23:51:08 +0800

    Update ConvNeXt.py

--- Note/nn/neuralnetwork/ConvNeXt.py
+++ Note/nn/neuralnetwork/ConvNeXt.py
@@ -1,6 +1,5 @@
 import tensorflow as tf
 import numpy as np
-from Note.nn.initializer import initializer
 from Note.nn.layer.conv2d import conv2d
 from Note.nn.layer.dense import dense
 from Note.nn.layer.layer_normalization import layer_normalization
@@ -11,15 +10,13 @@ from Note.nn.Module import Module
 
 
 class ConvNeXtBlock:
-    def __init__(self, input_channels, projection_dim, drop_path_rate=0.0, layer_scale_init_value=1e-6, dtype='float32'):
-        self.conv2d=conv2d(projection_dim,[7,7],input_channels//projection_dim,padding='SAME',dtype=dtype)
-        self.layer_normalization=layer_normalization(self.conv2d.output_size,dtype=dtype)
-        self.dense1=dense(4*projection_dim,projection_dim,activation='gelu',dtype=dtype)
-        self.dense2=dense(projection_dim,4*projection_dim,dtype=dtype)
+    def __init__(self,in_channels, projection_dim, drop_path_rate=0.0, layer_scale_init_value=1e-6, dtype='float32'):
+        self.conv2d=conv2d(projection_dim,[7,7],in_channels//projection_dim,padding='SAME',dtype=dtype)
+        self.projection_dim=projection_dim
         self.gamma=tf.Variable(tf.ones([projection_dim],dtype=dtype)*layer_scale_init_value)
         self.drop_path_rate=drop_path_rate
         self.layer_scale_init_value=layer_scale_init_value
-        self.output_size=projection_dim
+        self.dtype=dtype
         
     
     def LayerScale(self,x):
@@ -38,9 +35,9 @@ class ConvNeXtBlock:
     
     def output(self,data,train_flag=True):
         x=self.conv2d.output(data)
-        x=self.layer_normalization.output(x)
-        x=self.dense1.output(x)
-        x=self.dense2.output(x)
+        x=layer_normalization(dtype=self.dtype).output(x)
+        x=dense(4*self.projection_dim,activation='gelu',dtype=self.dtype).output(x)
+        x=dense(self.projection_dim,dtype=self.dtype).output(x)
         if self.layer_scale_init_value is not None:
             x=self.LayerScale(x)
         if self.drop_path_rate:
@@ -51,7 +48,7 @@ class ConvNeXtBlock:
 
 
 class ConvNeXt:
-    def __init__(self,model_type='base',drop_path_rate=0.0,layer_scale_init_value=1e-6,classes=1000,include_top=True,pooling=None):
+    def __init__(self,model_type='base',drop_path_rate=0.0,layer_scale_init_value=1e-6,classes=1000,include_top=True,pooling=None,dtype='float32'):
         self.model_type=model_type
         self.classes=classes
         self.depths=MODEL_CONFIGS[model_type]['depths']
@@ -62,15 +59,17 @@ class ConvNeXt:
         self.pooling=pooling
         self.loss_object=tf.keras.losses.CategoricalCrossentropy()
         self.optimizer=Adam()
-        self.km=0
-    
-    
-    def build(self,dtype='float32'):
         self.bc=tf.Variable(0,dtype=dtype)
+        self.dtype=dtype
+        self.km=0
+        self.param=Module.param
+    
+    
+    def build(self):
         # Stem block.
         layers=Layers()
-        layers.add(conv2d(self.projection_dims[0],[4,4],3,dtype=dtype))
-        layers.add(layer_normalization(dtype=dtype))
+        layers.add(conv2d(self.projection_dims[0],[4,4],3,dtype=self.dtype))
+        layers.add(layer_normalization(dtype=self.dtype))
         
         # Downsampling blocks.
         self.downsample_layers = []
@@ -79,71 +78,72 @@ class ConvNeXt:
         num_downsample_layers = 3
         for i in range(num_downsample_layers):
             layers=Layers()
-            layers.add(layer_normalization(self.projection_dims[i],dtype=dtype))
-            layers.add(conv2d(self.projection_dims[i+1],[2,2],self.projection_dims[i],dtype=dtype))
+            layers.add(layer_normalization(self.projection_dims[i],dtype=self.dtype))
+            layers.add(conv2d(self.projection_dims[i+1],[2,2],self.projection_dims[i],dtype=self.dtype))
             self.downsample_layers.append(layers)
-        
-        # Stochastic depth schedule.
-        # This is referred from the original ConvNeXt codebase:
-        # https://github.com/facebookresearch/ConvNeXt/blob/main/models/convnext.py#L86
-        depth_drop_rates = [
-            float(x) for x in np.linspace(0.0, self.drop_path_rate, sum(self.depths))
-        ]
-        
-        # First apply downsampling blocks and then apply ConvNeXt stages.
-        cur = 0
-        
-        self.blocks=[]
-        self.num_convnext_blocks = 4
-        for i in range(self.num_convnext_blocks):
-            self.blocks.append([])
-            input_channels=self.downsample_layers[i].output_size
-            for j in range(self.depths[i]):
-                block = ConvNeXtBlock(
-                    input_channels=input_channels,
-                    projection_dim=self.projection_dims[i],
-                    drop_path_rate=depth_drop_rates[cur + j],
-                    layer_scale_init_value=self.layer_scale_init_value,
-                    dtype=dtype
-                    )
-                self.blocks[i].append(block)
-                input_channels=block.output_size
-            cur += self.depths[i]
-        self.layer_normalization=layer_normalization(self.blocks[-1][-1].output_size,dtype=dtype)
-        self.fc_weight = initializer([self.blocks[-1][-1].output_size, self.classes], 'Xavier', dtype)
-        self.fc_bias = initializer([self.classes], 'Xavier', dtype)
-        self.param=Module.param
-        self.param.extend([self.fc_weight,self.fc_bias])
         return
     
     
     def fp(self,data,p=None):
         if self.km==1:
             with tf.device(assign_device(p,'GPU')):
-                for i in range(self.num_convnext_blocks):
+                # Stochastic depth schedule.
+                # This is referred from the original ConvNeXt codebase:
+                # https://github.com/facebookresearch/ConvNeXt/blob/main/models/convnext.py#L86
+                depth_drop_rates = [
+                    float(x) for x in np.linspace(0.0, self.drop_path_rate, sum(self.depths))
+                ]
+                
+                # First apply downsampling blocks and then apply ConvNeXt stages.
+                cur = 0
+                
+                num_convnext_blocks = 4
+                for i in range(num_convnext_blocks):
                     data = self.downsample_layers[i].output(data)
                     for j in range(self.depths[i]):
-                        data=self.blocks[i][j].output(data)
+                        data = ConvNeXtBlock(
+                            in_channels=data.shape[-1],
+                            projection_dim=self.projection_dims[i],
+                            drop_path_rate=depth_drop_rates[cur + j],
+                            layer_scale_init_value=self.layer_scale_init_value,
+                            dtype=self.dtype
+                            ).output(data)
+                    cur += self.depths[i]
                 if self.include_top:
                     data = tf.math.reduce_mean(data, axis=[1, 2])
-                    data = self.layer_normalization.output(data)
-                    data = tf.matmul(data, self.fc_weight) + self.fc_bias
-                    data = tf.nn.softmax(data)
+                    data=layer_normalization(dtype=self.dtype).output(data)
+                    data=dense(self.classes,activation='softmax',dtype=self.dtype).output(data)
                 else:
                     if self.pooling=="avg":
                         data = tf.math.reduce_mean(data, axis=[1, 2])
                     elif self.pooling=="max":
                         data = tf.math.reduce_max(data, axis=[1, 2])
         else:
-            for i in range(self.num_convnext_blocks):
-                data = self.downsample_layers[i].output(data,self.km)
+            # Stochastic depth schedule.
+            # This is referred from the original ConvNeXt codebase:
+            # https://github.com/facebookresearch/ConvNeXt/blob/main/models/convnext.py#L86
+            depth_drop_rates = [
+                float(x) for x in np.linspace(0.0, self.drop_path_rate, sum(self.depths))
+            ]
+            
+            # First apply downsampling blocks and then apply ConvNeXt stages.
+            cur = 0
+            
+            num_convnext_blocks = 4
+            for i in range(num_convnext_blocks):
+                data = self.downsample_layers[i].output(data)
                 for j in range(self.depths[i]):
-                    data=self.blocks[i][j].output(data,self.km)
+                    data = ConvNeXtBlock(
+                        in_channels=data.shape[-1],
+                        projection_dim=self.projection_dims[i],
+                        drop_path_rate=depth_drop_rates[cur + j],
+                        layer_scale_init_value=self.layer_scale_init_value,
+                        dtype=self.dtype
+                        ).output(data,self.km)
             if self.include_top:
                 data = tf.math.reduce_mean(data, axis=[1, 2])
-                data = self.layer_normalization.output(data)
-                data = tf.matmul(data, self.fc_weight) + self.fc_bias
-                data = tf.nn.softmax(data)
+                data=layer_normalization(dtype=self.dtype).output(data)
+                data=dense(self.classes,activation='softmax',dtype=self.dtype).output(data)
             else:
                 if self.pooling=="avg":
                     data = tf.math.reduce_mean(data, axis=[1, 2])
