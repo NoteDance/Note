8884765f0ca06a4a875cd15d60532a01bae5a3d2
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2023-10-02 14:41:59 +0800

    Update InceptionResNetV2.py

--- Note/nn/neuralnetwork/Inception/InceptionResNetV2.py
+++ Note/nn/neuralnetwork/Inception/InceptionResNetV2.py
@@ -32,6 +32,9 @@ class conv2d_bn:
                 mean,var=tf.nn.moments(data,axes=3,keepdims=True)
                 self.moving_mean=self.moving_mean*0.99+mean*(1-0.99)
                 self.moving_var=self.moving_var*0.99+var*(1-0.99)
+                data=tf.nn.batch_normalization(data,mean,var,self.beta,None,1e-3)
+        else:
+            if not self.use_bias:
                 data=tf.nn.batch_normalization(data,self.moving_mean,self.moving_var,self.beta,None,1e-3)
         if self.activation is not None:
             output=self.activation(data)
