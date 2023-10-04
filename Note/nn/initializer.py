6a0d4c6ecaf6115ddaf905b6a26fff2e4a24e60a
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2023-10-04 12:17:38 +0800

    Update initializer.py

--- Note/nn/initializer.py
+++ Note/nn/initializer.py
@@ -8,24 +8,24 @@ def initializer(shape,initializer,dtype):
         elif initializer[0]=='uniform':
             param=tf.Variable(tf.random.uniform(shape=shape,minval=initializer[1],maxval=initializer[2],dtype=dtype))
         elif initializer[0]=='VarianceScaling':
-            fan_in = tf.reduce_prod(shape[:-1])
-            fan_out = shape[-1]
-            if initializer[2] == 'fan_in':
-                n = fan_in
-            elif initializer[2] == 'fan_out':
-                n = fan_out
-            elif initializer[2] == 'fan_avg':
-                n = (fan_in + fan_out) / 2.0
+            scale=initializer[1]
+            fan_in,fan_out=compute_fans(shape)
+            if initializer[2]=='fan_in':
+                scale/=max(1.0,fan_in)
+            elif initializer[2]=='fan_out':
+                scale/=max(1.0,fan_out)
+            elif initializer[2]=='fan_avg':
+                scale/=max(1.0,(fan_in+fan_out)/2.0)
             else:
-                raise ValueError('Invalid mode: ' + initializer[2])
-            if initializer[3] == 'truncated_normal':
-                stddev = tf.sqrt(initializer[1] / n)
-                param=tf.Variable(tf.random.truncated_normal(shape=shape, mean=0.0, stddev=stddev, dtype=dtype))
-            elif initializer[3] == 'uniform':
-                limit = tf.sqrt(3.0 * initializer[1] / n)
-                param=tf.Variable(tf.random.uniform(shape=shape, minval=-limit, maxval=limit, dtype=dtype))
+                raise ValueError('Invalid mode:'+initializer[2])
+            if initializer[3]=='truncated_normal':
+                stddev=tf.cast(tf.sqrt(scale)/0.87962566103423978,dtype)
+                param=tf.Variable(tf.random.truncated_normal(shape=shape,mean=0.0,stddev=stddev,dtype=dtype))
+            elif initializer[3]=='uniform':
+                limit=tf.cast(tf.sqrt(3.0*scale),dtype)
+                param=tf.Variable(tf.random.uniform(shape=shape,minval=-limit,maxval=limit,dtype=dtype))
             else:
-                raise ValueError('Invalid distribution: ' + initializer[3])
+                raise ValueError('Invalid distribution:'+initializer[3])
         elif initializer[0]=='truncated_normal':
             param=tf.Variable(tf.random.truncated_normal(shape=shape,mean=0.0,stddev=initializer[1],dtype=dtype))
     else:
@@ -38,33 +38,56 @@ def initializer(shape,initializer,dtype):
         elif initializer=='ones':
             param=tf.Variable(tf.ones(shape=shape,dtype=dtype))
         elif initializer=='Xavier':
-            fan_in=shape[0]
-            fan_out=shape[1] if len(shape)>1 else 1
-            scale=tf.cast(tf.sqrt(2.0/(fan_in+fan_out)),dtype)
-            param=tf.Variable(tf.random.uniform(shape=shape,minval=-scale*tf.cast(tf.sqrt(3.0),dtype),maxval=scale*tf.cast(tf.sqrt(3.0),dtype),dtype=dtype))
+            scale=1.0
+            fan_in,fan_out=compute_fans(shape)
+            scale/=max(1.0,(fan_in+fan_out)/2.0)
+            limit=tf.cast(tf.sqrt(3.0*scale),dtype)
+            param=tf.Variable(tf.random.uniform(shape=shape,minval=-limit,maxval=limit,dtype=dtype))
         elif initializer=='Xavier_normal':
-            fan_in=shape[0]
-            fan_out=shape[1] if len(shape)>1 else 1
-            scale=tf.cast(tf.sqrt(2.0/(fan_in+fan_out)),dtype)
-            param=tf.Variable(tf.random.normal(shape=shape,mean=0.0,stddev=scale,dtype=dtype))
+            scale=1.0
+            fan_in,fan_out=compute_fans(shape)
+            scale/=max(1.0,(fan_in+fan_out)/2.0)
+            stddev=tf.cast(tf.sqrt(scale)/0.87962566103423978,dtype)
+            param=tf.Variable(tf.random.truncated_normal(shape=shape,mean=0.0,stddev=stddev,dtype=dtype))
         elif initializer=='He':
-            fan_in=shape[0]
-            fan_out=shape[1] if len(shape)>1 else 1
-            scale=tf.cast(tf.sqrt(2.0/fan_in),dtype)
-            param=tf.Variable(tf.random.normal(shape=shape,mean=0.0,stddev=scale,dtype=dtype))
+            scale=2.0
+            fan_in,fan_out=compute_fans(shape)
+            scale/=max(1.0,fan_in)
+            stddev=tf.cast(tf.sqrt(scale)/0.87962566103423978,dtype)
+            param=tf.Variable(tf.random.truncated_normal(shape=shape,mean=0.0,stddev=stddev,dtype=dtype))
         elif initializer=='He_uniform':
-            fan_in=shape[0]
-            fan_out=shape[1] if len(shape)>1 else 1
-            scale=tf.cast(tf.sqrt(2.0/fan_in),dtype)
-            param=tf.Variable(tf.random.uniform(shape=shape,minval=-scale*tf.cast(tf.sqrt(3.0),dtype),maxval=scale*tf.cast(tf.sqrt(3.0),dtype),dtype=dtype))
+            scale=2.0
+            fan_in,fan_out=compute_fans(shape)
+            scale/=max(1.0,fan_in)
+            limit=tf.cast(tf.sqrt(3.0*scale),dtype)
+            param=tf.Variable(tf.random.uniform(shape=shape,minval=-limit,maxval=limit,dtype=dtype))
         elif initializer=='Lecun':
-            fan_in=shape[0]
-            fan_out=shape[1] if len(shape)>1 else 1
-            scale=tf.cast(tf.sqrt(3.0/fan_in),dtype)
-            param=tf.Variable(tf.random.normal(shape=shape,mean=0.0,stddev=scale,dtype=dtype))
+            scale=1.0
+            fan_in,fan_out=compute_fans(shape)
+            scale/=max(1.0,fan_in)
+            stddev=tf.cast(tf.sqrt(scale)/0.87962566103423978,dtype)
+            param=tf.Variable(tf.random.truncated_normal(shape=shape,mean=0.0,stddev=stddev,dtype=dtype))
         elif initializer=='Lecun_uniform':
-            fan_in=shape[0]
-            fan_out=shape[1] if len(shape)>1 else 1
-            scale=tf.cast(tf.sqrt(3.0/fan_in),dtype)
-            param=tf.Variable(tf.random.uniform(shape=shape,minval=-scale*tf.cast(tf.sqrt(3.0),dtype),maxval=scale*tf.cast(tf.sqrt(3.0),dtype),dtype=dtype))
+            scale=1.0
+            fan_in,fan_out=compute_fans(shape)
+            scale/=max(1.0,fan_in)
+            limit=tf.cast(tf.sqrt(3.0*scale),dtype)
+            param=tf.Variable(tf.random.uniform(shape=shape,minval=-limit,maxval=limit,dtype=dtype))
     return param
+
+
+def compute_fans(shape):
+    if len(shape)<1:
+        fan_in=fan_out=1
+    elif len(shape)==1:
+        fan_in=fan_out=shape[0]
+    elif len(shape)==2:
+        fan_in=shape[0]
+        fan_out=shape[1]
+    else:
+        receptive_field_size=1
+        for dim in shape[:-2]:
+            receptive_field_size*=dim
+        fan_in=shape[-2]*receptive_field_size
+        fan_out=shape[-1]*receptive_field_size
+    return int(fan_in),int(fan_out)
