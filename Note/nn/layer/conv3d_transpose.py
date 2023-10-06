51efd532ad91d89005a5c2939ace59efff34c376
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2023-10-06 19:21:33 +0800

    Update conv3d_transpose.py

--- Note/nn/layer/conv3d_transpose.py
+++ Note/nn/layer/conv3d_transpose.py
@@ -8,7 +8,7 @@ class conv3d_transpose(Module): # define a class for 3D transposed convolutional
     def __init__(self,filters,kernel_size,input_size=None,new_drc=None,strides=[1,1,1],padding='VALID',output_padding=None,weight_initializer='Xavier',bias_initializer='zeros',activation=None,data_format='NDHWC',dilations=None,use_bias=True,trainable=True,dtype='float32'): # define the constructor method
         self.kernel_size=kernel_size
         self.input_size=input_size
-        self.strides=strides
+        self.strides = (1,) + tuple(strides) + (1,) # add 1 to both ends of strides to match the data format 
         self.padding=padding
         self.output_padding=output_padding
         self.weight_initializer=weight_initializer
@@ -55,7 +55,6 @@ class conv3d_transpose(Module): # define a class for 3D transposed convolutional
             self.input_size=data.shape[-1]
             self.build()
             
-        strides = (1,) + tuple(self.strides) + (1,) # add 1 to both ends of strides to match the data format 
         depth = data.shape[1] # get the number of depth in the input data
         rows = data.shape[2] # get the number of rows in the input data
         cols = data.shape[3] # get the number of columns in the input data
@@ -70,11 +69,11 @@ class conv3d_transpose(Module): # define a class for 3D transposed convolutional
         else: # if output_padding is not None
             output_padding = self.output_padding # use the given output_padding values
         
-        new_depth = ((depth - 1) * strides[1] + self.kernel_size[0] - 2 * padding[0] + output_padding[0]) # calculate the new number of depth for the output using the formula 
-        new_rows = ((rows - 1) * strides[2] + self.kernel_size[1] - 2 * padding[1] + output_padding[1]) # calculate the new number of rows for the output using the formula 
-        new_cols = ((cols - 1) * strides[3] + self.kernel_size[2] - 2 * padding[2] + output_padding[2]) # calculate the new number of columns for the output using the formula 
+        new_depth = ((depth - 1) * self.strides[1] + self.kernel_size[0] - 2 * padding[0] + output_padding[0]) # calculate the new number of depth for the output using the formula 
+        new_rows = ((rows - 1) * self.strides[2] + self.kernel_size[1] - 2 * padding[1] + output_padding[1]) # calculate the new number of rows for the output using the formula 
+        new_cols = ((cols - 1) * self.strides[3] + self.kernel_size[2] - 2 * padding[2] + output_padding[2]) # calculate the new number of columns for the output using the formula 
         
         if self.use_bias==True: # if use bias is True
-            return a.activation_conv_transpose(data,self.weight,[data.shape[0],new_depth,new_rows,new_cols,self.output_size],self.activation,strides,self.padding,self.data_format,self.dilations,tf.nn.conv3d_transpose,bias=self.bias) # return the output of applying activation function to the transposed convolution of data and weight, plus bias, using output_shape as output shape
+            return a.activation_conv_transpose(data,self.weight,[data.shape[0],new_depth,new_rows,new_cols,self.output_size],self.activation,self.strides,self.padding,self.data_format,self.dilations,tf.nn.conv3d_transpose,bias=self.bias) # return the output of applying activation function to the transposed convolution of data and weight, plus bias, using output_shape as output shape
         else: # if use bias is False
-            return a.activation_conv_transpose(data,self.weight,[data.shape[0],new_depth,new_rows,new_cols,self.output_size],self.activation,strides,self.padding,self.data_format,self.dilations,tf.nn.conv3d_transpose) # return the output of applying activation function to the transposed convolution of data and weight, using output_shape as output shape
+            return a.activation_conv_transpose(data,self.weight,[data.shape[0],new_depth,new_rows,new_cols,self.output_size],self.activation,self.strides,self.padding,self.data_format,self.dilations,tf.nn.conv3d_transpose) # return the output of applying activation function to the transposed convolution of data and weight, using output_shape as output shape
