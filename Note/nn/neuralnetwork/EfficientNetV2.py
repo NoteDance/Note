cdca52974d43c1c19594e1dc7876b4f8f20cf2e1
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2023-10-08 14:57:21 +0800

    Update EfficientNetV2.py

--- Note/nn/neuralnetwork/EfficientNetV2.py
+++ Note/nn/neuralnetwork/EfficientNetV2.py
@@ -78,6 +78,7 @@ class EfficientNetV2:
                 classes=1000,
                 classifier_activation="softmax",
                 include_preprocessing=True,
+                device='GPU',
                 dtype='float32'
             ):
         if include_preprocessing:
@@ -121,6 +122,7 @@ class EfficientNetV2:
         self.include_top=include_top
         self.pooling=pooling
         self.classifier_activation=classifier_activation
+        self.device=device
         self.dtype=dtype
         self.normalization=normalization(input_shape,dtype=dtype)
         self.loss_object=tf.keras.losses.CategoricalCrossentropy() # create a categorical crossentropy loss object
@@ -238,7 +240,7 @@ class EfficientNetV2:
     
     def fp(self,data,p):
         if self.km==1:
-            with tf.device(assign_device(p,'GPU')):
+            with tf.device(assign_device(p,self.device)):
                 data=self.rescaling.output(data)
                 data=self.normalization.output(data)
                 x=self.layers1.output(data)
@@ -271,6 +273,45 @@ class EfficientNetV2:
                 else:
                     x=self.global_max_pool2d.output(x)
         return x
+
+
+    def loss(self,output,labels,p):
+        """A method that calculates the loss value between the output tensor and the labels tensor.
+
+        Args:
+            output: tensor, the output data after applying the model.
+            labels: tensor, the true labels of the input data.
+            p: integer, the index of the device to use.
+
+        Returns:
+            loss_value: tensor, the loss value.
+        """
+        with tf.device(assign_device(p,self.device)): # assign the device to use
+            loss_value=self.loss_object(labels,output) # calculate the loss value using categorical crossentropy loss function
+        return loss_value # return the loss value
+    
+    
+    def GradientTape(self,data,labels,p):
+        with tf.device(assign_device(p,self.device)):
+            with tf.GradientTape(persistent=True) as tape:
+                output=self.fp(data,p)
+                loss=self.loss(output,labels,p)
+        return tape,output,loss
+    
+    
+    def opt(self,gradient,p):
+        """A method that updates the model parameters using the optimizer and the gradient.
+
+        Args:
+            gradient: tensor, the gradient of the loss function with respect to the model parameters.
+            p: integer, the index of the device to use.
+
+        Returns:
+            param: list, the updated model parameters.
+        """
+        with tf.device(assign_device(p,self.device)): # assign the device to use
+            param=self.optimizer.opt(gradient,self.param,self.bc[0]) # update the model parameters using Adam optimizer and batch count
+            return param # return the updated model parameters
 
 
 class MBConvBlock:
