fa887fe0b9957660dbb4036254da2b4189b19d99
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2023-10-08 14:42:43 +0800

    Update assign_device_pytorch.py

--- Note/nn/parallel/assign_device_pytorch.py
+++ Note/nn/parallel/assign_device_pytorch.py
@@ -1,8 +1,8 @@
 import torch
 
 
-def assign_device(p, device_type): # a function to assign device according to the process index p and the device type
-    if device_type == 'GPU': # if the device type is GPU
+def assign_device(p, device): # a function to assign device according to the process index p and the device type
+    if device == 'GPU': # if the device type is GPU
         if torch.cuda.is_available(): # if there are any available GPU devices
             try:
                 torch.cuda.set_device(p % torch.cuda.device_count()) # set the device with index p modulo the number of devices as current
@@ -11,7 +11,7 @@ def assign_device(p, device_type): # a function to assign device according to th
                 raise e # raise the error message
         else: # if there are no available GPU devices
             device = torch.device('cpu') # use CPU device as default
-    elif device_type == 'CPU': # if the device type is CPU
+    elif device == 'CPU': # if the device type is CPU
         device = torch.device('cpu') # use CPU device as default
     else: # if the device type is neither GPU nor CPU
         raise ValueError('Invalid device type') # raise a value error
