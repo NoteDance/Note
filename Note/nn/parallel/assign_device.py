d8c3fa0db26c61f7e58322c1abf79c3efeefe8a0
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2023-10-08 14:42:31 +0800

    Update assign_device.py

--- Note/nn/parallel/assign_device.py
+++ Note/nn/parallel/assign_device.py
@@ -1,12 +1,12 @@
 import tensorflow as tf
 
 
-def assign_device(p, device_type): # a function to assign device according to the process index p and the device type
-    devices = tf.config.list_physical_devices(device_type) # get a list of available devices of the given type
+def assign_device(p, device): # a function to assign device according to the process index p and the device type
+    devices = tf.config.list_physical_devices(device) # get a list of available devices of the given type
     if devices: # if there are any devices of the given type
         try:
-            tf.config.set_visible_devices(devices[p % len(devices)], device_type) # set the device with index p modulo the number of devices as visible
-            device = '/' + device_type + ':' + str(p % len(devices)) # store the device name as an attribute
+            tf.config.set_visible_devices(devices[p % len(devices)], device) # set the device with index p modulo the number of devices as visible
+            device = '/' + device + ':' + str(p % len(devices)) # store the device name as an attribute
         except RuntimeError as e: # catch any runtime error
             raise e # raise the error message
     else: # if there are no devices of the given type
