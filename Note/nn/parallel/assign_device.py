a3f3128beb9bf0606d78d29c2be8565cc32896e9
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2023-10-08 14:38:02 +0800

    Update assign_device.py

--- Note/nn/parallel/assign_device.py
+++ Note/nn/parallel/assign_device.py
@@ -12,11 +12,3 @@ def assign_device(p, device_type): # a function to assign device according to th
     else: # if there are no devices of the given type
         device = '/CPU:0' # use CPU device as default
     return device
-
-
-def assign_tpu(p): # a function to assign TPU device according to the process index p
-    resolver = tf.distribute.cluster_resolver.TPUClusterResolver(tpu='') # create a resolver to connect to the TPU cluster
-    tf.config.experimental_connect_to_cluster(resolver) # connect to the cluster
-    tf.tpu.experimental.initialize_tpu_system(resolver) # initialize the TPU system
-    device = '/job:worker/replica:0/task:0/device:TPU:' + str(p) # store the device name as an attribute
-    return device
