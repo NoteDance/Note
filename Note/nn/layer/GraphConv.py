45d0880cc3d7a6b6b176baf48d4502f6a4303232
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2023-10-11 14:28:17 +0800

    Update GraphConv.py

--- Note/nn/layer/GraphConv.py
+++ Note/nn/layer/GraphConv.py
@@ -4,7 +4,7 @@ from Note.nn.activation import activation_dict
 from Note.nn.Module import Module
 
 
-class GraphConv(Module):
+class GraphConv:
     def __init__(self,in_features,out_features,norm='both',activation=None,weight_initializer='Xavier',bias_initializer='zeros',dtype='float32',use_bias=True):
         # initialize the graph convolution layer with some parameters 
         # in_features: the size of the input features
