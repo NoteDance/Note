6401b896bb03db1c748d697f7567954dcbbb9bb2
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2023-10-11 14:32:00 +0800

    Update batch_normalization.py

--- Note/nn/layer/batch_normalization.py
+++ Note/nn/layer/batch_normalization.py
@@ -3,7 +3,7 @@ from Note.nn.initializer import initializer
 from Note.nn.Module import Module
 
 
-class batch_normalization(Module):
+class batch_normalization:
     def __init__(self, input_size=None, axis=-1, momentum=0.99, epsilon=0.001, center=True, scale=True, beta_initializer='zeros', gamma_initializer='ones', moving_mean_initializer='zeros', moving_variance_initializer='ones', keepdims=True, trainable=True, dtype='float32'):
         self.input_size=input_size
         self.axis=axis
