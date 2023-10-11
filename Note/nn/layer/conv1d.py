86e1276de401c2e924d4895220754a6ebcef70c5
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2023-10-11 14:31:27 +0800

    Update conv1d.py

--- Note/nn/layer/conv1d.py
+++ Note/nn/layer/conv1d.py
@@ -4,7 +4,7 @@ import Note.nn.initializer as i # import the initializer module from Note.nn pac
 from Note.nn.Module import Module
 
 
-class conv1d(Module): # define a class for 1D convolutional layer
+class conv1d: # define a class for 1D convolutional layer
     def __init__(self,filters,kernel_size,input_size=None,strides=[1],padding='VALID',weight_initializer='Xavier',bias_initializer='zeros',activation=None,data_format='NWC',dilations=None,use_bias=True,trainable=True,dtype='float32'): # define the constructor method
         if isinstance(kernel_size,int):
             kernel_size=kernel_size
