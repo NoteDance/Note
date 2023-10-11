4e83ba4427378e9944016f8fbc878e455b618aea
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2023-10-11 14:30:37 +0800

    Update conv2d_transpose.py

--- Note/nn/layer/conv2d_transpose.py
+++ Note/nn/layer/conv2d_transpose.py
@@ -4,7 +4,7 @@ import Note.nn.initializer as i # import the initializer module from Note.nn pac
 from Note.nn.Module import Module
 
 
-class conv2d_transpose(Module): # define a class for 2D transposed convolutional layer
+class conv2d_transpose: # define a class for 2D transposed convolutional layer
     def __init__(self,filters,kernel_size,input_size=None,strides=[1,1],padding='VALID',output_padding=None,weight_initializer='Xavier',bias_initializer='zeros',activation=None,data_format='NHWC',dilations=None,use_bias=True,trainable=True,dtype='float32'): # define the constructor method
         if isinstance(kernel_size,int):
             kernel_size=[kernel_size,kernel_size]
