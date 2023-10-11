95e2b2359e5be851ef5c16e6da2d715455de890f
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2023-10-11 14:29:21 +0800

    Update depthwise_conv2d.py

--- Note/nn/layer/depthwise_conv2d.py
+++ Note/nn/layer/depthwise_conv2d.py
@@ -4,7 +4,7 @@ import Note.nn.initializer as i # import the initializer module from Note.nn pac
 from Note.nn.Module import Module
 
 
-class depthwise_conv2d(Module): # define a class for depthwise convolutional layer
+class depthwise_conv2d: # define a class for depthwise convolutional layer
     def __init__(self,kernel_size,depth_multiplier=1,input_size=None,strides=[1,1],padding='VALID',weight_initializer='Xavier',bias_initializer='zeros',activation=None,data_format='NHWC',dilations=None,use_bias=True,trainable=True,dtype='float32',): # define the constructor method
         self.depth_multiplier=depth_multiplier
         self.kernel_size=kernel_size
