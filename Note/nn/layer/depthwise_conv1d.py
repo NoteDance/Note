0a5e25897cdb77e35129290770ca6559c3aa9671
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2023-10-12 17:56:20 +0800

    Update depthwise_conv1d.py

--- Note/nn/layer/depthwise_conv1d.py
+++ Note/nn/layer/depthwise_conv1d.py
@@ -6,7 +6,8 @@ from Note.nn.Module import Module
 
 
 class depthwise_conv1d: # define a class for depthwise convolutional layer
-    def __init__(self,kernel_size,depth_multiplier=1,input_size=None,strides=[1],padding='VALID',weight_initializer='Xavier',bias_initializer='zeros',activation=None,data_format='NHWC',dilations=None,use_bias=True,trainable=True,dtype='float32',): # define the constructor method
+    def __init__(self,kernel_size,depth_multiplier=1,input_size=None,strides=1,padding='VALID',weight_initializer='Xavier',bias_initializer='zeros',activation=None,data_format='NHWC',dilations=None,use_bias=True,trainable=True,dtype='float32',): # define the constructor method
+        strides = (1,) + tuple([strides]) * 2 + (1,)
         self.kernel_size=kernel_size
         self.depth_multiplier=depth_multiplier
         self.input_size=input_size
@@ -54,10 +55,9 @@ class depthwise_conv1d: # define a class for depthwise convolutional layer
         if self.input_size==None:
             self.input_size=data.shape[-1]
             self.build()
-        strides = (1,) + tuple(self.strides) * 2 + (1,)
         data = tf.expand_dims(data, 1)
         depthwise_kernel = tf.expand_dims(self.depthwise_kernel, axis=0)
-        output=a.activation_conv(data,depthwise_kernel,None,strides,self.padding,self.data_format,self.dilations,tf.nn.depthwise_conv2d) # return the output of applying activation function to the depthwise convolution of data and weight
+        output=a.activation_conv(data,depthwise_kernel,None,self.strides,self.padding,self.data_format,self.dilations,tf.nn.depthwise_conv2d) # return the output of applying activation function to the depthwise convolution of data and weight
         if self.use_bias==True: # if use bias is True
             output+=self.bias
         output = tf.squeeze(output, [1]) # remove the extra dimension from output data to make it three-dimensional
