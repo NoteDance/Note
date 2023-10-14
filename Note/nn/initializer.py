aa74911a03e115a1001356aafb83103b30838a88
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2023-10-14 16:09:55 +0800

    Update initializer.py

--- Note/nn/initializer.py
+++ Note/nn/initializer.py
@@ -29,6 +29,14 @@ def initializer(shape,initializer,dtype):
                 raise ValueError('Invalid distribution:'+initializer[3])
         elif initializer[0]=='truncated_normal':
             param=tf.Variable(tf.random.truncated_normal(shape=shape,mean=0.0,stddev=initializer[1],dtype=dtype))
+        elif initializer[0]=='Orthogonal':
+            if len(initializer)==3:
+                orthogonal=Orthogonal(initializer[1],initializer[2])
+            elif len(initializer)==2:
+                orthogonal=Orthogonal(initializer[1])
+            else:
+                orthogonal=Orthogonal()
+            param=tf.Variable(orthogonal(shape,dtype))
     else:
         if initializer=='normal':
             param=tf.Variable(tf.random.normal(shape=shape,dtype=dtype))
@@ -74,6 +82,9 @@ def initializer(shape,initializer,dtype):
             scale/=max(1.0,fan_in)
             limit=tf.cast(tf.sqrt(3.0*scale),dtype)
             param=tf.Variable(tf.random.uniform(shape=shape,minval=-limit,maxval=limit,dtype=dtype))
+        elif initializer=='Orthogonal':
+            orthogonal=Orthogonal()
+            param=tf.Variable(orthogonal(shape,dtype))
     return param
 
 
@@ -104,6 +115,14 @@ def initializer_(shape,initializer,dtype):
                 raise ValueError('Invalid distribution:'+initializer[3])
         elif initializer[0]=='truncated_normal':
             param=tf.Variable(tf.random.truncated_normal(shape=shape,mean=0.0,stddev=initializer[1],dtype=dtype))
+        elif initializer[0]=='Orthogonal':
+            if len(initializer)==3:
+                orthogonal=Orthogonal(initializer[1],initializer[2])
+            elif len(initializer)==2:
+                orthogonal=Orthogonal(initializer[1])
+            else:
+                orthogonal=Orthogonal()
+            param=tf.Variable(orthogonal(shape,dtype))
     else:
         if initializer=='normal':
             param=tf.Variable(tf.random.normal(shape=shape,dtype=dtype))
@@ -149,6 +168,9 @@ def initializer_(shape,initializer,dtype):
             scale/=max(1.0,fan_in)
             limit=tf.cast(tf.sqrt(3.0*scale),dtype)
             param=tf.Variable(tf.random.uniform(shape=shape,minval=-limit,maxval=limit,dtype=dtype))
+        elif initializer=='Orthogonal':
+            orthogonal=Orthogonal()
+            param=tf.Variable(orthogonal(shape,dtype))
     Module.param.append(param)
     return param
 
@@ -168,3 +190,74 @@ def compute_fans(shape):
         fan_in=shape[-2]*receptive_field_size
         fan_out=shape[-1]*receptive_field_size
     return int(fan_in),int(fan_out)
+
+
+class Orthogonal:
+    """Initializer that generates an orthogonal matrix.
+
+    Also available via the shortcut function `tf.keras.initializers.orthogonal`.
+
+    If the shape of the tensor to initialize is two-dimensional, it is
+    initialized with an orthogonal matrix obtained from the QR decomposition of
+    a matrix of random numbers drawn from a normal distribution. If the matrix
+    has fewer rows than columns then the output will have orthogonal rows.
+    Otherwise, the output will have orthogonal columns.
+
+    If the shape of the tensor to initialize is more than two-dimensional,
+    a matrix of shape `(shape[0] * ... * shape[n - 2], shape[n - 1])`
+    is initialized, where `n` is the length of the shape vector.
+    The matrix is subsequently reshaped to give a tensor of the desired shape.
+
+    Args:
+      gain: multiplicative factor to apply to the orthogonal matrix
+      seed: A Python integer. Used to make the behavior of the initializer
+        deterministic. Note that a seeded initializer will produce the same
+        random values across multiple calls.
+
+    References:
+      - [Saxe et al., 2014](https://openreview.net/forum?id=_wzZwKpTDF_9C)
+    """
+
+    def __init__(self, gain=1.0, seed=7):
+        self.gain = gain
+        self.seed = seed
+        self._random_generator = tf.random.Generator.from_seed(seed)
+
+    def __call__(self, shape, dtype=None):
+        """Returns a tensor object initialized to an orthogonal matrix.
+
+        Args:
+          shape: Shape of the tensor.
+          dtype: Optional dtype of the tensor. Only floating point types are
+            supported. If not specified, `tf.keras.backend.floatx()` is used,
+           which default to `float32` unless you configured it otherwise
+           (via `tf.keras.backend.set_floatx(float_dtype)`)
+        """
+        # Check the shape
+        if len(shape) < 2:
+            raise ValueError(
+                "The tensor to initialize must be "
+                "at least two-dimensional. Received: "
+                f"shape={shape} of rank {len(shape)}."
+            )
+        return self._generate_init_val(shape, dtype)
+
+    def _generate_init_val(self, shape, dtype):
+        # Flatten the input shape with the last dimension remaining
+        # its original shape so it works for conv2d
+        num_rows = 1
+        for dim in shape[:-1]:
+            num_rows *= dim
+        num_cols = shape[-1]
+        flat_shape = (max(num_cols, num_rows), min(num_cols, num_rows))
+
+        # Generate a random matrix
+        a = self._random_generator.normal(flat_shape, dtype=dtype)
+        # Compute the qr factorization
+        q, r = tf.linalg.qr(a, full_matrices=False)
+        # Make Q uniform
+        d = tf.linalg.tensor_diag_part(r)
+        q *= tf.sign(d)
+        if num_rows < num_cols:
+            q = tf.linalg.matrix_transpose(q)
+        return self.gain * tf.reshape(q, shape)
