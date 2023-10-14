6dcd16dba5cc12f0c4c8a7cd35b02b814f9ea62d
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2023-10-14 16:24:53 +0800

    Add files via upload

---
+++ Note/nn/layer/PReLU.py
@@ -0,0 +1,65 @@
+import tensorflow as tf
+from Note.nn.initializer import initializer_
+
+
+class PReLU:
+    """Parametric Rectified Linear Unit.
+
+    It follows:
+
+    ```
+        f(x) = alpha * x for x < 0
+        f(x) = x for x >= 0
+    ```
+
+    where `alpha` is a learned array with the same shape as x.
+
+    Input shape:
+        Arbitrary. Use the keyword argument `input_shape`
+        (tuple of integers, does not include the samples axis)
+        when using this layer as the first layer in a model.
+
+    Output shape:
+        Same shape as the input.
+
+    Args:
+        alpha_initializer: Initializer function for the weights.
+        shared_axes: The axes along which to share learnable
+            parameters for the activation function.
+            For example, if the incoming feature maps
+            are from a 2D convolution
+            with output shape `(batch, height, width, channels)`,
+            and you wish to share parameters across space
+            so that each filter only has one set of parameters,
+            set `shared_axes=[1, 2]`.
+    """
+
+    def __init__(
+        self,
+        input_shape,
+        alpha_initializer="zeros",
+        shared_axes=None,
+        dtype='float32'
+    ):
+        self.alpha_initializer = alpha_initializer
+        if shared_axes is None:
+            self.shared_axes = None
+        elif not isinstance(shared_axes, (list, tuple)):
+            self.shared_axes = [shared_axes]
+        else:
+            self.shared_axes = list(shared_axes)
+        param_shape = list(input_shape[1:])
+        if self.shared_axes is not None:
+            for i in self.shared_axes:
+                param_shape[i - 1] = 1
+        self.alpha = initializer_(
+            shape=param_shape,
+            initializer=alpha_initializer,
+            dtype=dtype
+        )
+
+
+    def output(self, data):
+        pos = tf.nn.relu(data)
+        neg = -self.alpha * tf.nn.relu(-data)
+        return pos + neg
