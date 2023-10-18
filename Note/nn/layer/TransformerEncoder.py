0b477a0561acd3d570d50c4642e6ba10fc3d0c9c
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2023-10-18 18:48:32 +0800

    Update TransformerEncoder.py

--- Note/nn/layer/TransformerEncoder.py
+++ Note/nn/layer/TransformerEncoder.py
@@ -1,6 +1,6 @@
 class TransformerEncoder:
-    def __init__(self, encoder_layer, num_layers, norm=None):
-        self.layers = [encoder_layer for _ in range(num_layers)]
+    def __init__(self, encoder_layers, num_layers, norm=None):
+        self.layers = encoder_layers
         self.num_layers = num_layers
         self.norm = norm
 
@@ -9,11 +9,12 @@ class TransformerEncoder:
             self,
             src,
             mask = None,
+            train_flag=True
             ):
         output = src
 
         for mod in self.layers:
-            output = mod.output(output, src_mask=mask)
+            output = mod.output(output, src_mask=mask, train_flag=train_flag)
 
         if self.norm is not None:
             output = self.norm.output(output)
