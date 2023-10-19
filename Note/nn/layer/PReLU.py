9e6b80276c224c9e826088ad7119ed54bb1460d7
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2023-10-19 14:10:09 +0800

    Update PReLU.py

--- Note/nn/layer/PReLU.py
+++ Note/nn/layer/PReLU.py
@@ -64,6 +64,8 @@ class PReLU:
 
 
     def output(self, data):
+        if data.dtype!=self.dtype:
+            data=tf.cast(data,self.dtype)
         if self.input_shape is None:
             self.input_shape=data.shape
             param_shape = list(self.input_shape[1:])
