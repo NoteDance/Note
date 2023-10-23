b6d43ba7570e9779cec996dadfa2b32098f3ef76
NoteDancing <63648431+NoteDancing@users.noreply.github.com>
2023-10-23 14:39:38 +0800

    Update Module.py

--- Note/nn/Module.py
+++ Note/nn/Module.py
@@ -1,2 +1,16 @@
 class Module:
     param=[]
+    ctl_list=[]
+    ctsl_list=[]
+    
+    
+    def convert_to_list():
+        for ctl in Module.ctl_list:
+            ctl()
+        return
+    
+    
+    def convert_to_shared_list(manager):
+        for ctsl in Module.ctsl_list:
+            ctsl(manager)
+        return
