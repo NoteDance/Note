c18e2e4f78703c344dfd96330c5cfe116bce0e40
NoteDance <63648431+NoteDance@users.noreply.github.com>
2023-12-30 22:23:35 +0800

    Add files via upload

---
+++ Note/nn/layer/adaptive_avg_pooling3d.py
@@ -0,0 +1,112 @@
+import tensorflow as tf
+from typing import Union, Iterable
+
+class adaptive_avg_pooling3d:
+    """Parent class for 3D pooling layers with adaptive kernel size.
+
+    This class only exists for code reuse. It will never be an exposed API.
+
+    Args:
+      output_size: An integer or tuple/list of 3 integers specifying (pooled_dim1, pooled_dim2, pooled_dim3).
+        The new size of output channels.
+      data_format: A string,
+        one of `channels_last` (default) or `channels_first`.
+        The ordering of the dimensions in the inputs.
+        `channels_last` corresponds to inputs with shape
+        `(batch, spatial_dim1, spatial_dim2, spatial_dim3, channels)` while `channels_first`
+        corresponds to inputs with shape
+        `(batch, channels, spatial_dim1, spatial_dim2, spatial_dim3)`.
+    """
+
+    def __init__(
+        self,
+        output_size: Union[int, Iterable[int]],
+        data_format='channels_last',
+    ):
+        self.data_format = data_format
+        self.reduce_function = tf.reduce_mean
+        self.output_size = normalize_tuple(output_size, 3, "output_size")
+
+    def output(self, data):
+        h_bins = self.output_size[0]
+        w_bins = self.output_size[1]
+        d_bins = self.output_size[2]
+        if self.data_format == "channels_last":
+            split_cols = tf.split(data, h_bins, axis=1)
+            split_cols = tf.stack(split_cols, axis=1)
+            split_rows = tf.split(split_cols, w_bins, axis=3)
+            split_rows = tf.stack(split_rows, axis=3)
+            split_depth = tf.split(split_rows, d_bins, axis=5)
+            split_depth = tf.stack(split_depth, axis=5)
+            out_vect = self.reduce_function(split_depth, axis=[2, 4, 6])
+        else:
+            split_cols = tf.split(data, h_bins, axis=2)
+            split_cols = tf.stack(split_cols, axis=2)
+            split_rows = tf.split(split_cols, w_bins, axis=4)
+            split_rows = tf.stack(split_rows, axis=4)
+            split_depth = tf.split(split_rows, d_bins, axis=6)
+            split_depth = tf.stack(split_depth, axis=6)
+            out_vect = self.reduce_function(split_depth, axis=[3, 5, 7])
+        return out_vect
+    
+def normalize_tuple(value, n, name):
+    """Transforms an integer or iterable of integers into an integer tuple.
+
+    A copy of tensorflow.python.keras.util.
+
+    Args:
+      value: The value to validate and convert. Could an int, or any iterable
+        of ints.
+      n: The size of the tuple to be returned.
+      name: The name of the argument being validated, e.g. "strides" or
+        "kernel_size". This is only used to format error messages.
+
+    Returns:
+      A tuple of n integers.
+
+    Raises:
+      ValueError: If something else than an int/long or iterable thereof was
+        passed.
+    """
+    if isinstance(value, int):
+        return (value,) * n
+    else:
+        try:
+            value_tuple = tuple(value)
+        except TypeError:
+            raise TypeError(
+                "The `"
+                + name
+                + "` argument must be a tuple of "
+                + str(n)
+                + " integers. Received: "
+                + str(value)
+            )
+        if len(value_tuple) != n:
+            raise ValueError(
+                "The `"
+                + name
+                + "` argument must be a tuple of "
+                + str(n)
+                + " integers. Received: "
+                + str(value)
+            )
+        for single_value in value_tuple:
+            try:
+                int(single_value)
+            except (ValueError, TypeError):
+                raise ValueError(
+                    "The `"
+                    + name
+                    + "` argument must be a tuple of "
+                    + str(n)
+                    + " integers. Received: "
+                    + str(value)
+                    + " "
+                    "including element "
+                    + str(single_value)
+                    + " of type"
+                    + " "
+                    + str(type(single_value))
+                )
+        return value_tuple
---
+++ Note/nn/layer/adaptive_max_pooling1d.py
@@ -0,0 +1,102 @@
+import tensorflow as tf
+from typing import Union, Iterable
+
+class adaptive_max_pooling1d:
+    """Parent class for 1D pooling layers with adaptive kernel size.
+
+    This class only exists for code reuse. It will never be an exposed API.
+
+    Args:
+      output_size: An integer or tuple/list of a single integer, specifying pooled_features.
+        The new size of output channels.
+      data_format: A string,
+        one of `channels_last` (default) or `channels_first`.
+        The ordering of the dimensions in the inputs.
+        `channels_last` corresponds to inputs with shape
+        `(batch, steps, features)` while `channels_first`
+        corresponds to inputs with shape
+        `(batch, features, steps)`.
+    """
+
+    def __init__(
+        self,
+        output_size: Union[int, Iterable[int]],
+        data_format='channels_last',
+    ):
+        self.data_format = data_format
+        self.reduce_function = tf.reduce_max
+        self.output_size = normalize_tuple(output_size, 1, "output_size")
+
+    def output(self, data):
+        bins = self.output_size[0]
+        if self.data_format == "channels_last":
+            splits = tf.split(data, bins, axis=1)
+            splits = tf.stack(splits, axis=1)
+            out_vect = self.reduce_function(splits, axis=2)
+        else:
+            splits = tf.split(data, bins, axis=2)
+            splits = tf.stack(splits, axis=2)
+            out_vect = self.reduce_function(splits, axis=3)
+        return out_vect
+
+def normalize_tuple(value, n, name):
+    """Transforms an integer or iterable of integers into an integer tuple.
+
+    A copy of tensorflow.python.keras.util.
+
+    Args:
+      value: The value to validate and convert. Could an int, or any iterable
+        of ints.
+      n: The size of the tuple to be returned.
+      name: The name of the argument being validated, e.g. "strides" or
+        "kernel_size". This is only used to format error messages.
+
+    Returns:
+      A tuple of n integers.
+
+    Raises:
+      ValueError: If something else than an int/long or iterable thereof was
+        passed.
+    """
+    if isinstance(value, int):
+        return (value,) * n
+    else:
+        try:
+            value_tuple = tuple(value)
+        except TypeError:
+            raise TypeError(
+                "The `"
+                + name
+                + "` argument must be a tuple of "
+                + str(n)
+                + " integers. Received: "
+                + str(value)
+            )
+        if len(value_tuple) != n:
+            raise ValueError(
+                "The `"
+                + name
+                + "` argument must be a tuple of "
+                + str(n)
+                + " integers. Received: "
+                + str(value)
+            )
+        for single_value in value_tuple:
+            try:
+                int(single_value)
+            except (ValueError, TypeError):
+                raise ValueError(
+                    "The `"
+                    + name
+                    + "` argument must be a tuple of "
+                    + str(n)
+                    + " integers. Received: "
+                    + str(value)
+                    + " "
+                    "including element "
+                    + str(single_value)
+                    + " of type"
+                    + " "
+                    + str(type(single_value))
+                )
+        return value_tuple
