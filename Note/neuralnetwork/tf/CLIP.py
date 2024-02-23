02bef57b9ca7617ee177348f6ecb9cd7aa4fd667
NoteDance <63648431+NoteDance@users.noreply.github.com>
2024-02-23 16:03:46 +0800

    Update CLIP.py

--- Note/neuralnetwork/tf/CLIP.py
+++ Note/neuralnetwork/tf/CLIP.py
@@ -23,8 +23,8 @@ class Bottleneck:
         self.bn1 = batch_norm(planes, parallel=False)
         self.relu1 = tf.nn.relu
 
-        self.conv2 = conv2d(planes, 3, planes, use_bias=False)
         self.zeropadding2d = zeropadding2d(padding=1)
+        self.conv2 = conv2d(planes, 3, planes, use_bias=False)
         self.bn2 = batch_norm(planes, parallel=False)
         self.relu2 = tf.nn.relu
 
@@ -62,6 +62,7 @@ class Bottleneck:
         identity = x
 
         out = self.relu1(self.bn1(self.conv1(x), train_flag))
+        out = self.zeropadding2d(out)
         out = self.relu2(self.bn2(self.conv2(out), train_flag))
         out = self.avgpool(out)
         out = self.bn3(self.conv3(out), train_flag)
@@ -135,6 +136,7 @@ class ModifiedResNet:
         self.input_resolution = input_resolution
 
         # the 3-layer stem
+        self.zeropadding2d = zeropadding2d(padding=1)
         self.conv1 = conv2d(width // 2, input_size=3, kernel_size=3, strides=2, use_bias=False)
         self.bn1 = batch_norm(width // 2, parallel=False)
         self.relu1 = tf.nn.relu
@@ -142,7 +144,6 @@ class ModifiedResNet:
         self.bn2 = batch_norm(width // 2, parallel=False)
         self.relu2 = tf.nn.relu
         self.conv3 = conv2d(width, input_size=width // 2, kernel_size=3, use_bias=False)
-        self.zeropadding2d = zeropadding2d(padding=1)
         self.bn3 = batch_norm(width, parallel=False)
         self.relu3 = tf.nn.relu
         self.avgpool = avg_pool2d(2, 2, 'VALID')
@@ -189,14 +190,14 @@ class ModifiedResNet:
 
     def __call__(self, x, train_flag=True):
         def stem(x):
+            x = self.zeropadding2d(x)
             x = self.conv1(x)
-            x = self.zeropadding2d(x)
             x = self.relu1(self.bn1(x, train_flag))
+            x = self.zeropadding2d(x)
             x = self.conv2(x)
-            x = self.zeropadding2d(x)
             x = self.relu2(self.bn2(x, train_flag))
-            x = self.conv3(x)
             x = self.zeropadding2d(x)
+            x = self.conv3(x)
             x = self.relu3(self.bn3(x, train_flag))
             x = self.avgpool(x)
             return x
