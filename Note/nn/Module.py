1de312452cf76bd6394624ab1897c52ba4295683
NoteDance <63648431+NoteDance@users.noreply.github.com>
2024-04-03 00:07:57 +0800

    Update Module.py

--- Note/nn/Module.py
+++ Note/nn/Module.py
@@ -1,7 +1,21 @@
+import tensorflow as tf
+
+
 class Module:
     param=[]
+    param_table=dict()
+    param_table['dense_weight']=[]
+    param_table['dense_bias']=[]
+    param_table['conv2d_weight']=[]
+    param_table['conv2d_bias']=[]
     ctl_list=[]
     ctsl_list=[]
+    
+    
+    def cast_param(key,dtype):
+        for param in Module.param_table[key]:
+            param.assign(tf.cast(param,dtype))
+        return
     
     
     def convert_to_list():
@@ -18,6 +32,11 @@ class Module:
     
     def init():
         Module.param.clear()
+        param_table=dict()
+        param_table['dense_weight']=[]
+        param_table['dense_bias']=[]
+        param_table['conv2d_weight']=[]
+        param_table['conv2d_bias']=[]
         Module.ctl_list.clear()
         Module.ctsl_list.clear()
         return
