8bff826ae470c9e96669ec97b35218585bf2309a
NoteDance <63648431+NoteDance@users.noreply.github.com>
2024-04-03 15:12:26 +0800

    Add files via upload

--- Note/neuralnetwork/tf/DenseNet/DenseNet201.py
+++ Note/neuralnetwork/tf/DenseNet/DenseNet201.py
@@ -15,10 +15,10 @@ from Note.nn.Module import Module
 def DenseLayer(input_channels, growth_rate, dtype='float32'):
         layers=Layers()
         layers.add(identity(input_channels),save_data=True)
-        layers.add(batch_norm_(epsilon=1.001e-5,parallel=False,dtype=dtype))
+        layers.add(batch_norm_(epsilon=1.001e-5,dtype=dtype))
         layers.add(activation_dict['relu'])
         layers.add(conv2d(4*growth_rate,[1,1],strides=1,padding="SAME",use_bias=False,dtype=dtype))
-        layers.add(batch_norm_(epsilon=1.001e-5,parallel=False,dtype=dtype))
+        layers.add(batch_norm_(epsilon=1.001e-5,dtype=dtype))
         layers.add(activation_dict['relu'])
         layers.add(conv2d(growth_rate,[3,3],strides=1,padding="SAME",use_bias=False,dtype=dtype),save_data=True)
         layers.add(concat(),use_data=True)
@@ -35,7 +35,7 @@ def DenseBlock(input_channels, num_layers, growth_rate, dtype='float32'):
 
 def TransitionLayer(input_channels, compression_factor, dtype='float32'):
         layers=Layers()
-        layers.add(batch_norm_(input_channels,parallel=False,dtype=dtype))
+        layers.add(batch_norm_(input_channels,dtype=dtype))
         layers.add(activation_dict['relu'])
         layers.add(conv2d(int(compression_factor * input_channels),[1,1],strides=[1, 1, 1, 1],padding="SAME",use_bias=False,dtype=dtype))
         layers.add(avg_pool2d(ksize=[2, 2],strides=[2, 2],padding="SAME"))
@@ -59,7 +59,7 @@ class DenseNet201:
         self.layers=Layers()
         self.layers.add(zeropadding2d(3,padding=[3, 3]))
         self.layers.add(conv2d(64,[7,7],strides=2,use_bias=False,dtype=self.dtype))
-        self.layers.add(batch_norm_(epsilon=1.001e-5,parallel=False,dtype=self.dtype))
+        self.layers.add(batch_norm_(epsilon=1.001e-5,dtype=self.dtype))
         self.layers.add(activation_dict['relu'])
         self.layers.add(zeropadding2d(padding=[1, 1]))
         self.layers.add(max_pool2d(ksize=[3, 3],strides=[2, 2],padding="VALID"))
@@ -90,7 +90,7 @@ class DenseNet201:
                                  growth_rate=self.growth_rate,
                                  dtype=self.dtype))
         
-        self.layers.add(batch_norm_(epsilon=1.001e-5,parallel=False,dtype=self.dtype))
+        self.layers.add(batch_norm_(epsilon=1.001e-5,dtype=self.dtype))
         
         self.layers.add(activation_dict['relu'])
         
