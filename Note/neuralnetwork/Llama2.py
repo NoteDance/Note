07a4337de0633164acfd690e0e3717e9d38c716d
NoteDance <63648431+NoteDance@users.noreply.github.com>
2024-04-06 19:24:50 +0800

    Update Llama2.py

--- Note/neuralnetwork/Llama2.py
+++ Note/neuralnetwork/Llama2.py
@@ -210,7 +210,7 @@ class Llama2:
         # some useful precompute for the RoPE relative positional embeddings
         self.freqs_cos, self.freqs_sin = precompute_freqs_cis(self.params.dim // self.params.n_heads, self.params.max_seq_len)
         
-        for param in Module.param_table['dense_weight']:
+        for param in Module.param_dict['dense_weight']:
             param.assign(ModelArgs.weight_decay * param)
         
         self.optimizer=AdamW(lr=self.params.lr, beta1=0.9, beta2=0.95)
