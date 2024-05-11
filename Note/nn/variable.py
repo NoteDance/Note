26619dc0485ca33e4587323bdb3311b81b9dc846
NoteDance <63648431+NoteDance@users.noreply.github.com>
2024-05-11 17:47:09 +0800

    Update variable.py

--- Note/nn/variable.py
+++ Note/nn/variable.py
@@ -2,5 +2,6 @@ import tensorflow as tf
 from Note.nn.Module import Module
 
 def variable(initial_value):
-    Module.param.append(tf.Variable(initial_value))
-    return
+    variable=tf.Variable(initial_value)
+    Module.param.append(variable)
+    return variable
