d6ffdbdfaa64b04bf2e00dd895e6d3fab6c75675
NoteDance <63648431+NoteDance@users.noreply.github.com>
2024-05-12 08:31:33 +0800

    Update Layers.py

--- Note/nn/Layers.py
+++ Note/nn/Layers.py
@@ -11,39 +11,58 @@ class Layers:
     
     
     def add(self,layer,save_data=False,use_data=False,axis=None):
-        if save_data==True:
-            self.save_data_count+=1
-        if use_data==True and hasattr(layer,'save_data_count'):
-            layer.save_data_count=self.save_data_count
-        if use_data==True and hasattr(layer,'concat')!=True:
-            self.output_size_list=[]
-        if use_data==True:
-            self.save_data_count=0
-        if hasattr(layer,'build'):
-            if hasattr(layer,'input_size'):
-                if layer.input_size==None and self.output_size!=None:
-                    layer.input_size=self.output_size
-                    layer.build()
-                    self.layer.append(layer)
+        if type(layer)!=list:
+            if save_data==True:
+                self.save_data_count+=1
+            if use_data==True and hasattr(layer,'save_data_count'):
+                layer.save_data_count=self.save_data_count
+            if use_data==True and hasattr(layer,'concat')!=True:
+                self.output_size_list=[]
+            if use_data==True:
+                self.save_data_count=0
+            if hasattr(layer,'build'):
+                if hasattr(layer,'input_size'):
+                    if layer.input_size==None and self.output_size!=None:
+                        layer.input_size=self.output_size
+                        layer.build()
+                        self.layer.append(layer)
+                    else:
+                        self.layer.append(layer)
                 else:
                     self.layer.append(layer)
             else:
                 self.layer.append(layer)
+            if hasattr(layer,'param'):
+                self.param.append(layer.param)
+            if hasattr(layer,'output_size'):
+                self.output_size=layer.output_size
+            if hasattr(layer,'concat'):
+                if layer.axis==-1 or layer.axis==2:
+                    self.output_size=self.output_size_list.pop(0)
+                    for i in range(1,layer.save_data_count):
+                        self.output_size+=self.output_size_list.pop(0)
+            self.save_data_flag.append(save_data)
+            self.use_data_flag.append(use_data)
+            if save_data==True:
+                self.output_size_list.append(self.output_size)
         else:
-            self.layer.append(layer)
-        if hasattr(layer,'param'):
-            self.param.append(layer.param)
-        if hasattr(layer,'output_size'):
-            self.output_size=layer.output_size
-        if hasattr(layer,'concat'):
-            if layer.axis==-1 or layer.axis==2:
-                self.output_size=self.output_size_list.pop(0)
-                for i in range(1,layer.save_data_count):
-                    self.output_size+=self.output_size_list.pop(0)
-        self.save_data_flag.append(save_data)
-        self.use_data_flag.append(use_data)
-        if save_data==True:
-            self.output_size_list.append(self.output_size)
+            for layer in layer:
+                if hasattr(layer,'build'):
+                    if hasattr(layer,'input_size'):
+                        if layer.input_size==None and self.output_size!=None:
+                            layer.input_size=self.output_size
+                            layer.build()
+                            self.layer.append(layer)
+                        else:
+                            self.layer.append(layer)
+                    else:
+                        self.layer.append(layer)
+                else:
+                    self.layer.append(layer)
+                if hasattr(layer,'param'):
+                    self.param.append(layer.param)
+                if hasattr(layer,'output_size'):
+                    self.output_size=layer.output_size
         return
     
     
