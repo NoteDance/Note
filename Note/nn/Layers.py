7a9053e374e9f516abd75f0d4d9650acdc45bf13
NoteDance <63648431+NoteDance@users.noreply.github.com>
2024-05-26 15:55:59 +0800

    Update Layers.py

--- Note/nn/Layers.py
+++ Note/nn/Layers.py
@@ -2,24 +2,11 @@ class Layers:
     def __init__(self):
         self.layer=[]
         self.param=[]
-        self.saved_data=[]
-        self.save_data_flag=[]
-        self.use_data_flag=[]
-        self.save_data_count=0
-        self.output_size_list=[]
         self.train_flag=True
     
     
-    def add(self,layer,save_data=False,use_data=False,axis=None):
+    def add(self,layer):
         if type(layer)!=list:
-            if save_data==True:
-                self.save_data_count+=1
-            if use_data==True and hasattr(layer,'save_data_count'):
-                layer.save_data_count=self.save_data_count
-            if use_data==True and hasattr(layer,'concat')!=True:
-                self.output_size_list=[]
-            if use_data==True:
-                self.save_data_count=0
             if hasattr(layer,'build'):
                 if hasattr(layer,'input_size'):
                     if layer.input_size==None and self.output_size!=None:
@@ -36,15 +23,6 @@ class Layers:
                 self.param.append(layer.param)
             if hasattr(layer,'output_size'):
                 self.output_size=layer.output_size
-            if hasattr(layer,'concat'):
-                if layer.axis==-1 or layer.axis==2:
-                    self.output_size=self.output_size_list.pop(0)
-                    for i in range(1,layer.save_data_count):
-                        self.output_size+=self.output_size_list.pop(0)
-            self.save_data_flag.append(save_data)
-            self.use_data_flag.append(use_data)
-            if save_data==True:
-                self.output_size_list.append(self.output_size)
         else:
             for layer in layer:
                 if hasattr(layer,'build'):
@@ -68,27 +46,8 @@ class Layers:
     
     def __call__(self,data,train_flag=True):
         for i,layer in enumerate(self.layer):
-            if not hasattr(layer,'concat'):
-                if not hasattr(layer,'train_flag'):
-                    if self.use_data_flag[i]==False:
-                        data=layer(data)
-                    else:
-                        if hasattr(layer,'save_data_count'):
-                            data=layer(self.saved_data)
-                        else:
-                            data=layer(data,self.saved_data.pop(0))
-                else:
-                    if self.use_data_flag[i]==False:
-                        data=layer(data,train_flag)
-                    else:
-                        if hasattr(layer,'save_data_count'):
-                            data=layer(self.saved_data,train_flag)
-                        else:
-                            data=layer(data,self.saved_data.pop(0),train_flag)
-                if self.save_data_flag[i]==True:
-                    self.saved_data.append(data)
+            if not hasattr(layer,'train_flag'):
+                data=layer(data)
             else:
-                data=layer.concat(self.saved_data)
-                if self.save_data_flag[i]==True:
-                    self.saved_data.append(data)
+                data=layer(data,train_flag)
         return data
