1a4779f8948ebd8d1340ad53f062900b701fac3a
NoteDance <63648431+NoteDance@users.noreply.github.com>
2024-05-26 15:55:23 +0800

    Update concat.py

--- Note/nn/layer/concat.py
+++ Note/nn/layer/concat.py
@@ -4,11 +4,10 @@ import tensorflow as tf
 class concat:
     def __init__(self,axis=-1):
         self.axis=axis
-        self.save_data_count=None
         
         
-    def concat(self,data):
+    def __call__(self,data):
         output=data.pop(0)
-        for i in range(1,self.save_data_count):
+        for i in range(1,len(data)):
             output=tf.concat([output,data.pop(0)],axis=self.axis)
         return output
