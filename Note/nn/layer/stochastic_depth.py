a19e9caf5bac57a68d5c31ffd17b00c208029011
NoteDance <63648431+NoteDance@users.noreply.github.com>
2024-05-30 14:19:26 +0800

    Update stochastic_depth.py

--- Note/nn/layer/stochastic_depth.py
+++ Note/nn/layer/stochastic_depth.py
@@ -1,14 +1,17 @@
 import tensorflow as tf
+from Note import nn
 
 
 class stochastic_depth:
     def __init__(self, drop_path_rate):
         self.drop_path_rate=drop_path_rate
         self.train_flag=True
+        nn.Model.layer_list.append(self)
     
     
-    def __call__(self, x, train_flag=True):
-        self.train_flag=train_flag
+    def __call__(self, x, train_flag=None):
+        if train_flag==None:
+            train_flag=self.train_flag
         if train_flag:
             keep_prob = 1 - self.drop_path_rate
             shape = (tf.shape(x)[0],) + (1,) * (len(tf.shape(x)) - 1)
