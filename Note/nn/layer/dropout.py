582283c1f5e5bb2f2e81c520c1bace10466b9234
NoteDance <63648431+NoteDance@users.noreply.github.com>
2024-05-30 14:19:09 +0800

    Update dropout.py

--- Note/nn/layer/dropout.py
+++ Note/nn/layer/dropout.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+from Note import nn
 
 
 class dropout:
@@ -7,10 +8,12 @@ class dropout:
         self.noise_shape=noise_shape
         self.seed=seed
         self.train_flag=True
+        nn.Model.layer_list.append(self)
+        
     
-    
-    def __call__(self,data,train_flag=True):
-        self.train_flag=train_flag
+    def __call__(self,data,train_flag=None):
+        if train_flag==None:
+            train_flag=self.train_flag
         if train_flag==True:
             output=tf.nn.dropout(data,self.rate,noise_shape=self.noise_shape,seed=self.seed)
         else:
