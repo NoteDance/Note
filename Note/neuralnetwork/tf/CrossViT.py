3c524a0051710be90cec958c470b0831715fd0fd
NoteDance <63648431+NoteDance@users.noreply.github.com>
2024-06-04 18:46:45 +0800

    Update CrossViT.py

--- Note/neuralnetwork/tf/CrossViT.py
+++ Note/neuralnetwork/tf/CrossViT.py
@@ -126,7 +126,7 @@ class MultiScaleBlock:
             for i in range(depth[d]):
                 tmp.append(
                     nn.Block(dim=dim[d], num_heads=num_heads[d], mlp_ratio=mlp_ratio[d], qkv_bias=qkv_bias, 
-                          drop=drop, attn_drop=attn_drop, drop_path=drop_path[i], norm_layer=norm_layer))
+                          proj_drop=drop, attn_drop=attn_drop, drop_path=drop_path[i], norm_layer=norm_layer))
             if len(tmp) != 0:
                 layers = nn.Layers()
                 layers.add(tmp)
