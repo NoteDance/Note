647998dd2f3e5dcd38d2c4fbd0dd8a325a14f401
NoteDance <63648431+NoteDance@users.noreply.github.com>
2024-06-04 18:47:56 +0800

    Update vision_transformer.py

--- Note/nn/layer/vision_transformer.py
+++ Note/nn/layer/vision_transformer.py
@@ -5,17 +5,6 @@ from typing import Optional
 import collections.abc
 
 
-def _ntuple(n):
-    def parse(x):
-        if isinstance(x, collections.abc.Iterable) and not isinstance(x, str):
-            return tuple(x)
-        return tuple(repeat(x, n))
-    return parse
-
-
-to_2tuple = _ntuple(2)
-
-
 class PatchEmbed:
     """ 2D Image to Patch Embedding
     """
@@ -51,83 +40,95 @@ class PatchEmbed:
         return x
 
 
-class Mlp:
-    """ MLP as used in Vision Transformer, MLP-Mixer and related networks
-    """
-    def __init__(
-            self,
-            in_features,
-            hidden_features=None,
-            out_features=None,
-            act_layer=tf.nn.gelu,
-            norm_layer=None,
-            bias=True,
-            drop=0.,
-            use_conv=False,
-    ):
-        out_features = out_features or in_features
-        hidden_features = hidden_features or in_features
-        bias = to_2tuple(bias)
-        drop_probs = to_2tuple(drop)
-
-        self.fc1 = nn.dense(hidden_features, in_features, use_bias=bias[0])
-        self.act = act_layer
-        self.drop1 = nn.dropout(drop_probs[0])
-        self.fc2 = nn.dense(out_features, hidden_features, use_bias=bias[1])
-        self.drop2 = nn.dropout(drop_probs[1])
-
-    def __call__(self, x):
-        x = self.fc1(x)
-        x = self.act(x, approximate="tanh")
-        x = self.drop1(x)
-        x = self.fc2(x)
-        x = self.drop2(x)
-        return x
-
-
 class Attention:
-    # taken from https://github.com/rwightman/pytorch-image-models/blob/master/timm/models/vision_transformer.py
-    def __init__(self, dim, num_heads=8, qkv_bias=False, qk_scale=None, attn_drop=0., proj_drop=0.):
+    def __init__(self, dim, num_heads=8, qkv_bias=False, qk_norm=False, attn_drop=0., proj_drop=0., norm_layer=nn.layer_norm, use_fused_attn=True):
+        assert dim % num_heads == 0, 'dim should be divisible by num_heads'
         self.num_heads = num_heads
-        head_dim = dim // num_heads
-        self.scale = qk_scale or head_dim ** -0.5
+        self.head_dim = dim // num_heads
+        self.scale = self.head_dim ** -0.5
+        self.fused_attn = use_fused_attn
 
         self.qkv = nn.dense(dim * 3, dim, use_bias=qkv_bias)
+        self.q_norm = norm_layer(self.head_dim) if qk_norm else nn.identity()
+        self.k_norm = norm_layer(self.head_dim) if qk_norm else nn.identity()
         self.attn_drop = nn.dropout(attn_drop)
         self.proj = nn.dense(dim, dim)
         self.proj_drop = nn.dropout(proj_drop)
 
     def __call__(self, x):
         B, N, C = x.shape
-        qkv = tf.transpose(tf.reshape(self.qkv(x), (B, N, 3, self.num_heads, C // self.num_heads)), (2, 0, 3, 1, 4))
+        qkv = tf.transpose(tf.reshape(self.qkv(x), (B, N, 3, self.num_heads, self.head_dim)), (2, 0, 3, 1, 4))
         q, k, v = qkv[0], qkv[1], qkv[2]
+        q, k = self.q_norm(q), self.k_norm(k)
         
-        q = q * self.scale
+        if self.fused_attn:
+            x = nn.scaled_dot_product_attention(
+                q, k, v,
+                dropout_p=self.attn_drop.rate if nn.Model.training else 0.,
+            )
+        else:
+            q = q * self.scale
+            attn = tf.matmul(q, tf.transpose(k, (0, 1, 3, 2)))
+            attn = tf.nn.softmax(attn)
+            attn = self.attn_drop(attn)
+            x = tf.matmul(attn, v)
 
-        attn = tf.matmul(q, tf.transpose(k, (0, 1, 3, 2)))
-        attn = tf.nn.softmax(attn)
-        attn = self.attn_drop(attn)
-        x = tf.reshape(tf.transpose(tf.matmul(attn, v), (0, 2, 1, 3)), (B, N, C))
+        x = tf.reshape(tf.transpose(x, (0, 2, 1, 3)), (B, N, C))
         x = self.proj(x)
         x = self.proj_drop(x)
         return x
+
+
+class LayerScale:
+    def __init__(
+            self,
+            dim: int,
+            init_values: float = 1e-5,
+    ):
+        self.gamma = nn.variable(init_values * tf.ones(dim))
+
+    def __call__(self, x):
+        return x * self.gamma
     
     
 class Block:
-    # taken from https://github.com/rwightman/pytorch-image-models/blob/master/timm/models/vision_transformer.py
-    def __init__(self, dim, num_heads, mlp_ratio=4., qkv_bias=False, qk_scale=None, drop=0., attn_drop=0.,
-                 drop_path=0., act_layer=tf.nn.gelu, norm_layer=nn.layer_norm,Attention_block = Attention,Mlp_block=Mlp
-                 ,init_values=1e-4):
+    def __init__(self, dim, num_heads, mlp_ratio=4., qkv_bias=False, qk_norm=False, proj_drop=0., attn_drop=0., init_values=None,
+                 drop_path=0., act_layer=tf.nn.gelu, norm_layer=nn.layer_norm, mlp_layer=nn.Mlp
+                 ):
         self.norm1 = norm_layer(dim)
-        self.attn = Attention_block(
-            dim, num_heads=num_heads, qkv_bias=qkv_bias, qk_scale=qk_scale, attn_drop=attn_drop, proj_drop=drop)
-        # NOTE: drop path for stochastic depth, we shall see if this is better than dropout here
+        self.attn = Attention(
+            dim,
+            num_heads=num_heads,
+            qkv_bias=qkv_bias,
+            qk_norm=qk_norm,
+            attn_drop=attn_drop,
+            proj_drop=proj_drop,
+            norm_layer=norm_layer,
+        )
+        self.ls1 = LayerScale(dim, init_values=init_values) if init_values else nn.identity()
         self.drop_path = nn.stochastic_depth(drop_path) if drop_path > 0. else nn.identity()
+        
         self.norm2 = norm_layer(dim)
-        mlp_hidden_dim = int(dim * mlp_ratio)
-        self.mlp = Mlp_block(in_features=dim, hidden_features=mlp_hidden_dim, act_layer=act_layer, drop=drop)
+        self.mlp = mlp_layer(
+            in_features=dim,
+            hidden_features=int(dim * mlp_ratio),
+            act_layer=act_layer,
+            drop=proj_drop,
+        )
+        self.ls2 = LayerScale(dim, init_values=init_values) if init_values else nn.identity()
+        self.drop_path2 = nn.stochastic_depth(drop_path) if drop_path > 0. else nn.identity()
 
     def __call__(self, x):
-        x = x + self.drop_path(self.attn(self.norm1(x)))
-        x = x + self.drop_path(self.mlp(self.norm2(x)))
-        return x 
+        x = x + self.drop_path1(self.ls1(self.attn(self.norm1(x))))
+        x = x + self.drop_path2(self.ls2(self.mlp(self.norm2(x))))
+        return x
+
+
+def _ntuple(n):
+    def parse(x):
+        if isinstance(x, collections.abc.Iterable) and not isinstance(x, str):
+            return tuple(x)
+        return tuple(repeat(x, n))
+    return parse
+
+to_2tuple = _ntuple(2)
