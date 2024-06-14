9f5ea6aaf7430e00ee0e1ad91d31d8c028d4db79
NoteDance <63648431+NoteDance@users.noreply.github.com>
2024-06-14 18:38:21 +0800

    Update max_pool2d.py

--- Note/nn/layer/max_pool2d.py
+++ Note/nn/layer/max_pool2d.py
@@ -1,12 +1,20 @@
 import tensorflow as tf
+from Note import nn
 
 
 class max_pool2d:
-    def __init__(self,ksize,strides,padding):
+    def __init__(self,ksize,strides,padding=0):
         self.ksize=ksize
         self.strides=strides
         self.padding=padding
+        if not isinstance(padding,str):
+            self.zeropadding2d=nn.zeropadding2d(padding=padding)
     
     
     def __call__(self,data):
-        return tf.nn.max_pool2d(data,ksize=self.ksize,strides=self.strides,padding=self.padding)
+        if not isinstance(self.padding,str):
+            data=self.zeropadding2d(data)
+            padding='VALID'
+        else:
+            padding=self.padding
+        return tf.nn.max_pool2d(data,ksize=self.ksize,strides=self.strides,padding=padding)
