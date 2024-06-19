498ceebb197e798fee6aa99f27d90b52ec7bb6c3
NoteDance <63648431+NoteDance@users.noreply.github.com>
2024-06-19 13:45:04 +0800

    Add files via upload

--- Note/neuralnetwork/non_parallel/DenseNet/DenseNet201.py
+++ Note/neuralnetwork/non_parallel/DenseNet/DenseNet201.py
@@ -42,7 +42,7 @@ def TransitionLayer(input_channels, compression_factor):
         layers.add(batch_norm_(input_channels,parallel=False))
         layers.add(activation_dict['relu'])
         layers.add(conv2d(int(compression_factor * input_channels),[1,1],strides=[1, 1, 1, 1],padding="SAME",use_bias=False))
-        layers.add(avg_pool2d(ksize=[2, 2],strides=[2, 2],padding="SAME"))
+        layers.add(avg_pool2d(kernel_size=[2, 2],strides=[2, 2],padding="SAME"))
         return layers
 
 
@@ -61,7 +61,7 @@ class DenseNet201(Model):
         self.layers.add(batch_norm_(epsilon=1.001e-5,parallel=False))
         self.layers.add(activation_dict['relu'])
         self.layers.add(zeropadding2d(padding=[1, 1]))
-        self.layers.add(max_pool2d(ksize=[3, 3],strides=[2, 2],padding="VALID"))
+        self.layers.add(max_pool2d(kernel_size=[3, 3],strides=[2, 2],padding="VALID"))
         
         
         self.layers.add(DenseBlock(input_channels=64,num_layers=6,
