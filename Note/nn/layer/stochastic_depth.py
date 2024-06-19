12a7b27ae0d71288dffe0a9943761a31c6db3062
NoteDance <63648431+NoteDance@users.noreply.github.com>
2024-06-19 14:46:24 +0800

    Update stochastic_depth.py

--- Note/nn/layer/stochastic_depth.py
+++ Note/nn/layer/stochastic_depth.py
@@ -7,6 +7,11 @@ class stochastic_depth:
         self.drop_path_rate=drop_path_rate
         self.train_flag=True
         nn.Model.layer_list.append(self)
+        if nn.Model.name_!=None and nn.Model.name_ not in nn.Model.layer_eval:
+            nn.Model.layer_eval[nn.Model.name_]=[]
+            nn.Model.layer_eval[nn.Model.name_].append(self)
+        elif nn.Model.name_!=None:
+            nn.Model.layer_eval[nn.Model.name_].append(self)
     
     
     def __call__(self, x, train_flag=None):
