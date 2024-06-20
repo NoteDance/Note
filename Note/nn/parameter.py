8902f8b1b8e1d2f4b4eb560169d5b00084f97bba
NoteDance <63648431+NoteDance@users.noreply.github.com>
2024-06-21 00:45:56 +0800

    Update parameter.py

--- Note/nn/parameter.py
+++ Note/nn/parameter.py
@@ -6,4 +6,9 @@ def Parameter(data,trainable=True,name=None):
     if name!=None:
         param=tf.Variable(param,name=name)
     Model.param.append(param)
+    if Model.name_!=None and Model.name_ not in Model.layer_param:
+        Model.layer_param[Model.name_]=[]
+        Model.layer_param[Model.name_].append(param)
+    elif Model.name_!=None:
+        Model.layer_param[Model.name_].append(param)
     return param
