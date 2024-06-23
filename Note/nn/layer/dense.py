592012a62a22fbf4a718d93d12d103183ad1a4bd
NoteDance <notedance@outlook.com>
2024-06-23 14:20:01 +0800

    Update dense.py

--- Note/nn/layer/dense.py
+++ Note/nn/layer/dense.py
@@ -14,16 +14,17 @@ class dense: # define a class for dense (fully connected) layer
         self.dtype=dtype
         self.output_size=output_size
         self.name=name
+        self.init_weights=None
+        if len(Model.name_list)>0:
+            Model.name=Model.name_list[-1]
+        if Model.name!=None and Model.name not in Model.layer_dict:
+            Model.layer_dict[Model.name]=[]
+            Model.layer_dict[Model.name].append(self)
+        elif Model.name!=None:
+               Model.layer_dict[Model.name].append(self)
         if input_size!=None:
             self.weight=nn.initializer([input_size,output_size],weight_initializer,dtype,trainable) # initialize the weight matrix
             Model.param_dict['dense_weight'].append(self.weight)
-            if len(Model.name_list)>0:
-                Model.name=Model.name_list[-1]
-            if Model.name!=None and Model.name not in Model.layer_dict:
-                Model.layer_dict[Model.name]=[]
-                Model.layer_dict[Model.name].append(self)
-            elif Model.name!=None:
-                   Model.layer_dict[Model.name].append(self)
             if Model.name_!=None and Model.name_ not in Model.layer_param:
                 Model.layer_param[Model.name_]=[]
                 Model.layer_param[Model.name_].append(self.weight)
@@ -53,38 +54,29 @@ class dense: # define a class for dense (fully connected) layer
     
     def build(self):
         self.weight=nn.initializer([self.input_size,self.output_size],self.weight_initializer,self.dtype,self.trainable) # initialize the weight matrix
+        if self.name!=None:
+            self.weight=tf.Variable(self.weight,trainable=self.trainable,name=self.name)
         Model.param_dict['dense_weight'].append(self.weight)
-        if len(Model.name_list)>0:
-            Model.name=Model.name_list[-1]
-        if Model.name!=None and Model.name not in Model.layer_dict:
-            Model.layer_dict[Model.name]=[]
-            Model.layer_dict[Model.name].append(self)
-        elif Model.name!=None:
-               Model.layer_dict[Model.name].append(self)
         if Model.name_!=None and Model.name_ not in Model.layer_param:
             Model.layer_param[Model.name_]=[]
             Model.layer_param[Model.name_].append(self.weight)
-        elif Model.name!=None:
+        elif Model.name_!=None:
             Model.layer_param[Model.name_].append(self.weight)
         if self.use_bias==True: # if use bias is True
             self.bias=nn.initializer([self.output_size],self.bias_initializer,self.dtype,self.trainable) # initialize the bias vector
+            if self.name!=None:
+                self.bias=tf.Variable(self.bias,trainable=self.trainable,name=self.name)
             Model.param_dict['dense_bias'].append(self.bias)
             if Model.name_!=None and Model.name_ not in Model.layer_param:
                 Model.layer_param[Model.name_]=[]
                 Model.layer_param[Model.name_].append(self.bias)
-            elif Model.name!=None:
+            elif Model.name_!=None:
                 Model.layer_param[Model.name_].append(self.bias)
-        else: # if use bias is False
-            self.bias=None # set the bias to None
-        if self.use_bias==True: # if use bias is True
-            if self.name!=None:
-                self.weight=tf.Variable(self.weight,trainable=self.trainable,name=self.name)
-                self.bias=tf.Variable(self.bias,trainable=self.trainable,name=self.name)
             self.param=[self.weight,self.bias] # store the parameters in a list
         else: # if use bias is False
-            if self.name!=None:
-                self.weight=tf.Variable(self.weight,trainable=self.trainable,name=self.name)
             self.param=[self.weight] # store only the weight in a list
+        if self.init_weights!=None:
+            self.init_weights(self)
         Model.param.extend(self.param)
         return
     
