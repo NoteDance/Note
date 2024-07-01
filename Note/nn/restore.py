679a5dc9946329ae77c375e3c33b092b8a38d9bf
NoteDance <notedance@outlook.com>
2024-07-01 08:14:54 +0800

    Update restore.py

--- Note/nn/restore.py
+++ Note/nn/restore.py
@@ -1,9 +1,17 @@
 import tensorflow as tf
 import pickle
     
+
 def restore(path):
     input_file=open(path,'rb')
     model=pickle.load(input_file)
     optimizer=tf.keras.optimizers.deserialize(pickle.load(input_file))
     input_file.close()
     return model,optimizer
+
+
+def restore_param(path):
+    input_file=open(path,'rb')
+    param=pickle.load(input_file)
+    input_file.close()
+    return param
