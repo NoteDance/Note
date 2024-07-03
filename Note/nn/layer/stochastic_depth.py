aa4a02fab921247baaf8d79a7d89f87aee09306b
NoteDance <notedance@outlook.com>
2024-07-03 10:06:09 +0800

    Update stochastic_depth.py

--- Note/nn/layer/stochastic_depth.py
+++ Note/nn/layer/stochastic_depth.py
@@ -14,10 +14,10 @@ class stochastic_depth:
             nn.Model.layer_eval[nn.Model.name_].append(self)
     
     
-    def __call__(self, x, train_flag=None):
-        if train_flag==None:
-            train_flag=self.train_flag
-        if train_flag:
+    def __call__(self, x, training=None):
+        if training==None:
+            training=self.train_flag
+        if training:
             keep_prob = 1 - self.drop_path_rate
             shape = (tf.shape(x)[0],) + (1,) * (len(tf.shape(x)) - 1)
             random_tensor = keep_prob + tf.random.uniform(shape, 0, 1, dtype=x.dtype)
