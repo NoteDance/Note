7e4a8e17e548c5d0dd6538b9ef6f91b58b75a92e
NoteDance <notedance@outlook.com>
2024-07-07 19:46:15 +0800

    更新 model2.py

--- Note/neuralnetwork/example/model2.py
+++ Note/neuralnetwork/example/model2.py
@@ -4,7 +4,7 @@ class Model(nn.Model):
   def __init__(self):
     super().__init__()
     self.layers = nn.Sequential()
-    self.layers.add(nn.conv2D(32, 3, activation='relu'))
+    self.layers.add(nn.conv2d(32, 3, activation='relu'))
     self.layers.add(nn.max_pool2d())
     self.layers.add(nn.conv2D(64, 3, activation='relu'))
     self.layers.add(nn.max_pool2d())
