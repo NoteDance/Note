a02e1adc096f2e96a8c8dee25637c66281d687bf
NoteDance <notedance@outlook.com>
2024-07-16 20:17:52 +0800

    Update __init__.py

--- Note/nn/__init__.py
+++ Note/nn/__init__.py
@@ -156,6 +156,7 @@ from Note.nn.parameter import Parameter
 from Note.nn.pos_embed import *
 from Note.nn.positional_encoding import positional_encoding
 from Note.nn.restore import *
+from Note.nn.RL import RL
 from Note.nn.scaled_dot_product_attention import scaled_dot_product_attention
 from Note.nn.Sequential import Sequential
 from Note.nn.init import *
