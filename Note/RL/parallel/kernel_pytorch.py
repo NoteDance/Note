c7af769a1f5077eba1487fde41380a7139c3cb7a
NoteDance <notedance@outlook.com>
2024-07-16 16:39:54 +0800

    Update kernel_pytorch.py

--- Note/RL/parallel/kernel_pytorch.py
+++ Note/RL/parallel/kernel_pytorch.py
@@ -470,9 +470,11 @@ class kernel:
     def visualize_reward(self):
         print()
         plt.figure(1)
-        plt.plot(np.arange(len(self.reward_list)),self.reward_list)
+        plt.plot(np.arange(1,self.total_episode+1),self.reward_list)
         plt.xlabel('episode')
         plt.ylabel('reward')
+        plt.xticks(np.arange(1,self.total_episode+1))
+        plt.show()
         print('reward:{0:.6f}'.format(self.reward_list[-1]))
         return
     
@@ -480,10 +482,12 @@ class kernel:
     def visualize_train(self):
         print()
         plt.figure(1)
-        plt.plot(np.arange(len(self.loss_list)),self.loss_list)
+        plt.plot(np.arange(1,self.total_episode+1),self.loss_list)
         plt.title('train loss')
         plt.xlabel('episode')
         plt.ylabel('loss')
+        plt.xticks(np.arange(1,self.total_episode+1))
+        plt.show()
         print('loss:{0:.6f}'.format(self.loss_list[-1]))
         return
     
@@ -491,10 +495,12 @@ class kernel:
     def visualize_reward_loss(self):
         print()
         plt.figure(1)
-        plt.plot(np.arange(len(self.reward_list)),self.reward_list,'r-',label='reward')
-        plt.plot(np.arange(len(self.loss_list)),self.loss_list,'b-',label='train loss')
+        plt.plot(np.arange(1,self.total_episode+1),self.reward_list,'r-',label='reward')
+        plt.plot(np.arange(1,self.total_episode+1),self.loss_list,'b-',label='train loss')
         plt.xlabel('epoch')
         plt.ylabel('reward and loss')
+        plt.xticks(np.arange(1,self.total_epoch+1))
+        plt.show()
         return
     
     
