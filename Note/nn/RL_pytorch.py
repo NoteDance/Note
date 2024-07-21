2dfa8df6bebf5c557c991b07f6c5f1f776a4e067
NoteDance <notedance@outlook.com>
2024-07-21 08:48:15 +0800

    Update RL_pytorch.py

--- Note/nn/RL_pytorch.py
+++ Note/nn/RL_pytorch.py
@@ -142,8 +142,8 @@ class RL:
             r = []
             d = []
             for _ in range(self.batch):
-                step_state = np.random.randint(0, len(self.state_pool)-2)
-                step_goal = np.random.randint(step_state+1, step_state+1+np.argmax(self.done_pool[step_state+1:]))
+                step_state = np.random.randint(0, len(self.state_pool)-1)
+                step_goal = np.random.randint(step_state+1, step_state+np.argmax(self.done_pool[step_state+1:])+2)
                 state = self.state_pool[step_state]
                 next_state = self.next_state_pool[step_state]
                 action = self.action_pool[step_state]
