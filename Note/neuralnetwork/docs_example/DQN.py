afdce84b984dcc328e06debb4504c33744f510d8
NoteDance <notedance@outlook.com>
2024-07-24 06:55:14 +0800

    Update DQN.py

--- Note/neuralnetwork/docs_example/DQN.py
+++ Note/neuralnetwork/docs_example/DQN.py
@@ -32,6 +32,8 @@ class DQN(nn.RL):
         q_value=tf.gather(self.q_net(s),a,axis=1,batch_dims=1)
         next_q_value=tf.reduce_max(self.target_q_net(next_s),axis=1)
         target=tf.cast(r,'float32')+0.98*next_q_value*(1-tf.cast(d,'float32'))
+        if self.pr==True:
+            self.prioritized_replay.update_TD(target)
         return tf.reduce_mean((q_value-target)**2)
     
     def update_param(self):
