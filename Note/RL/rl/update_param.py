87a380771b5e35778587b54e5733b622d53d483a
NoteDance <notedance@outlook.com>
2024-07-24 19:12:58 +0800

    Delete Note/RL/rl/update_param.py

--- Note/RL/rl/update_param.py
+++
@@ -1,9 +0,0 @@
-def update_param(target_net,net,tau=None):
-    if tau==None:
-        target_net.load_state_dict(net.state_dict())
-    else:
-        for target_param,param in zip(target_net[0].parameters(),net[0].parameters()):
-            target_param.data.copy_(target_param.data*(1.0-tau)+param.data*tau)
-        for target_param,param in zip(target_net[1].parameters(),net[1].parameters()):
-            target_param.data.copy_(target_param.data*(1.0-tau)+param.data*tau)
-    return
