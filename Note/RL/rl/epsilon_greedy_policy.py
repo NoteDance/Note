030a073cc1705ca8bc61275681556f742aaa8b86
NoteDance <notedance@outlook.com>
2024-07-24 19:12:49 +0800

    Delete Note/RL/rl/epsilon_greedy_policy.py

--- Note/RL/rl/epsilon_greedy_policy.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-
-def epsilon_greedy_policy(action_num,value,action_one,epsilon):
-    action_prob=action_one
-    action_prob=action_prob*epsilon/len(action_one)
-    best_a=np.argmax(value)
-    action_prob[best_a]+=1-epsilon
-    return np.random.choice(action_num,p=action_prob)
