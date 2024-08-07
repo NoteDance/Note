6009a3a04e979efe324b80b2a544e0b4163357e4
NoteDance <notedance@outlook.com>
2024-08-07 22:44:07 +0800

    Update DDPG_HER.py

--- Note/models/docs_example/RL/note/multiprocessing/DDPG_HER.py
+++ Note/models/docs_example/RL/note/multiprocessing/DDPG_HER.py
@@ -70,8 +70,9 @@ class DDPG(nn.RL):
         self.target_actor=actor(state_dim,hidden_dim,action_dim,action_bound)
         self.target_actor.detach()
         self.target_critic=critic(state_dim,hidden_dim,action_dim)
-        nn.assign_param(self.target_actor.param,self.actor_param.copy())
-        nn.assign_param(self.target_critic.param,self.critic_param.copy())
+        self.target_critic.detach()
+        nn.assign_param(self.target_actor.param,self.actor_param)
+        nn.assign_param(self.target_critic.param,self.critic_param)
         self.param=[self.actor.param,self.critic.param]
         self.sigma=sigma
         self.gamma=gamma
