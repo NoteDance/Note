356c36245cc0ba9a2f9c03a04de85658ad4f4d98
NoteDance <notedance@outlook.com>
2024-08-25 12:20:21 +0800

    Update assign_param.py

--- Note/nn/assign_param.py
+++ Note/nn/assign_param.py
@@ -2,7 +2,7 @@ from tensorflow.python.ops import state_ops
 from tensorflow.python.util import nest
 
 
-def assign_param(self,param1,param2):
+def assign_param(param1,param2):
     parameter_flat1=nest.flatten(param1)
     parameter_flat2=nest.flatten(param2)
     for i in range(len(parameter_flat1)):
