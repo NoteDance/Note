c0638ca7913eb409b8d8dd72ba978ec82f14f95e
NoteDance <notedance@outlook.com>
2024-09-20 18:17:22 +0800

    Update prioritized_replay.py

--- Note/RL/rl/prioritized_replay.py
+++ Note/RL/rl/prioritized_replay.py
@@ -28,6 +28,32 @@ class pr:
         return
 
 
+class pr_:
+    def __init__(self):
+        self.TD=tf.constant(7.)
+        self.index=None
+    
+    
+    def sample(self,state_pool,action_pool,next_state_pool,reward_pool,done_pool,epsilon,alpha,batch):
+        if self.pool_network==True:
+            TD=self.TD[7]
+        else:
+            TD=self.TD
+        p=(TD+epsilon)**alpha/np.sum((TD+epsilon)**alpha)
+        self.index=np.random.choice(np.arange(len(state_pool)),size=[batch],p=p)
+        return state_pool[self.index],action_pool[self.index],next_state_pool[self.index],reward_pool[self.index],done_pool[self.index]
+    
+    
+    def update_TD(self,TD):
+        if self.pool_network==True:
+            for i in range(len(self.index)):
+                self.TD[7][self.index[i]]=TD[i]
+        else:
+            for i in range(len(self.index)):
+                self.TD[self.index[i]]=TD[i]
+        return
+
+
 class pr_mt:
     def __init__(self):
         self.TD=[]
