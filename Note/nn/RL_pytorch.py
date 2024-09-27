e9ab0de3a2b25686ccd72bae18f9b0e9e20baf3d
NoteDance <notedance@outlook.com>
2024-09-27 15:36:16 +0800

    Update RL_pytorch.py

--- Note/nn/RL_pytorch.py
+++ Note/nn/RL_pytorch.py
@@ -418,11 +418,11 @@ class RL_pytorch:
                 if type(self.state_pool_list[p])!=np.ndarray and self.state_pool_list[p]==None:
                     index=p
                 else:
-                    self.inverse_len[p]=1/len(self.state_pool_list[p])
                     inverse_len=torch.tensor(self.inverse_len)
                     total_inverse=torch.sum(inverse_len)
                     prob=inverse_len/total_inverse
                     index=np.random.choice(self.processes,p=prob.numpy())
+                    self.inverse_len[index]=1/len(self.state_pool_list[index])
             else:
                 index=p
             s=np.expand_dims(s,axis=0)
