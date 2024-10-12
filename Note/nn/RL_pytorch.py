602f7a7e1de032a6c08e5f6b0b49a3799a36cd38
NoteDance <notedance@outlook.com>
2024-10-12 09:20:24 +0800

    Update RL_pytorch.py

--- Note/nn/RL_pytorch.py
+++ Note/nn/RL_pytorch.py
@@ -422,7 +422,6 @@ class RL_pytorch:
         self.reward[p]=0
         s=self.env_(initial=True,p=p)
         s=np.array(s)
-        flag=False
         if self.PPO==True:
             self.state_pool_list[p]=None
             self.action_pool_list[p]=None
@@ -460,18 +459,6 @@ class RL_pytorch:
                 self.step_counter.value+=1
                 lock_list[index].release()
             else:
-                if self.PR==True and flag==False:
-                    if self.prioritized_replay.TD is not None:
-                        if index==0:
-                            self.TD_list[index]=self.prioritized_replay.TD[0:len(self.TD_list[index])]
-                        else:
-                            index1=0
-                            index2=0
-                            for i in range(index):
-                                index1+=len(self.TD_list[i])
-                            index2=index1+len(self.TD_list[index])
-                            self.TD_list[index]=self.prioritized_replay.TD[index1-1:index2]
-                        flag=True
                 self.pool(s,a,next_s,r,done,index)
                 if self.PR==True:
                     if len(self.state_pool_list[index])>1:
