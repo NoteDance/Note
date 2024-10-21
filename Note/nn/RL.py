264c6e443a3245acf3aca6c3b881228dc4c3ff35
NoteDance <notedance@outlook.com>
2024-10-21 13:55:16 +0800

    Update RL.py

--- Note/nn/RL.py
+++ Note/nn/RL.py
@@ -56,6 +56,7 @@ class RL:
         self.PR=PR
         self.epsilon=epsilon
         self.initial_TD=initial_TD
+        self.prioritized_replay.TD=initial_TD
         self.alpha=alpha
         return
     
