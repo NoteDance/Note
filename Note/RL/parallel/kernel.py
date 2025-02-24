fda6ebe7b5dd7c6ab23004f528438c611333ccfc
NoteDance <notedance@outlook.com>
2025-02-24 15:29:00 +0800

    Update kernel.py

--- Note/RL/parallel/kernel.py
+++ Note/RL/parallel/kernel.py
@@ -14,6 +14,7 @@ class kernel:
     def __init__(self,nn=None,process=None):
         self.nn=nn
         self.nn.km=1
+        self.state_pool=None
         self.step_counter=None
         self.pool_size=None
         self.episode=None
@@ -34,11 +35,18 @@ class kernel:
     
     
     def init(self,manager):
-        self.state_pool=manager.dict({})
-        self.action_pool=manager.dict({})
-        self.next_state_pool=manager.dict({})
-        self.reward_pool=manager.dict({})
-        self.done_pool=manager.dict({})
+        if self.state_pool==None:
+            self.state_pool=manager.dict()
+            self.action_pool=manager.dict()
+            self.next_state_pool=manager.dict()
+            self.reward_pool=manager.dict()
+            self.done_pool=manager.dict()
+        else:
+            self.state_pool=manager.dict(self.state_pool)
+            self.action_pool=manager.dict(self.action_pool)
+            self.next_state_pool=manager.dict(self.next_state_pool)
+            self.reward_pool=manager.dict(self.reward_pool)
+            self.done_pool=manager.dict(self.done_pool)
         self.reward=Array('f',np.zeros(self.process,dtype='float32'))
         self.loss=np.zeros(self.process,dtype='float32')
         self.loss=Array('f',self.loss)
@@ -49,9 +57,9 @@ class kernel:
         else:
             self.step_counter=Array('i',self.step_counter)
         self.process_counter=Value('i',0)
-        self.finish_list=manager.list([])
-        self.reward_list=manager.list([])
-        self.loss_list=manager.list([])
+        self.finish_list=manager.list()
+        self.reward_list=manager.list()
+        self.loss_list=manager.list()
         self.episode_counter=Value('i',0)
         self.total_episode=Value('i',0)
         self.priority_p=Value('i',0)
@@ -71,7 +79,7 @@ class kernel:
         self.episode_=Value('i',self.total_episode.value)
         self.stop_flag=Value('b',False)
         self.save_flag=Value('b',False)
-        self.path_list=manager.list([])
+        self.path_list=manager.list()
         self.param=manager.dict()
         self.param[7]=self.nn.param
         return
@@ -838,6 +846,11 @@ class kernel:
             self._batch_counter=list(self._batch_counter)
             self.nn.optimizer.convert_to_list()
             pickle.dump(self.nn,output_file)
+            pickle.dump(self.state_pool,output_file)
+            pickle.dump(self.action_pool,output_file)
+            pickle.dump(self.next_state_pool,output_file)
+            pickle.dump(self.reward_pool,output_file)
+            pickle.dump(self.done_pool,output_file)
             pickle.dump(self.policy,output_file)
             pickle.dump(self.noise,output_file)
             pickle.dump(self.pool_size,output_file)
@@ -875,6 +888,11 @@ class kernel:
         self._batch_counter=list(self._batch_counter)
         self.nn.optimizer.convert_to_list()
         pickle.dump(self.nn,output_file)
+        pickle.dump(self.state_pool,output_file)
+        pickle.dump(self.action_pool,output_file)
+        pickle.dump(self.next_state_pool,output_file)
+        pickle.dump(self.reward_pool,output_file)
+        pickle.dump(self.done_pool,output_file)
         pickle.dump(self.policy,output_file)
         pickle.dump(self.noise,output_file)
         pickle.dump(self.pool_size,output_file)
@@ -904,6 +922,11 @@ class kernel:
         self.ec=self.nn.ec
         self.bc=self.nn.bc
         self.param[7]=self.nn.param
+        self.state_pool=pickle.load(input_file)
+        self.action_pool=pickle.load(input_file)
+        self.next_state_pool=pickle.load(input_file)
+        self.reward_pool=pickle.load(input_file)
+        self.done_pool=pickle.load(input_file)
         self.policy=pickle.load(input_file)
         self.noise=pickle.load(input_file)
         self.pool_size=pickle.load(input_file)
