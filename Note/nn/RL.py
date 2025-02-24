67998aef69f4b4bd86e24feece33f95956ab93df
NoteDance <notedance@outlook.com>
2025-02-24 15:36:09 +0800

    Update RL.py

--- Note/nn/RL.py
+++ Note/nn/RL.py
@@ -930,27 +930,11 @@ class RL:
             mp=multiprocessing
             self.mp=mp
             manager=multiprocessing.Manager()
-            state_pool_list=self.state_pool_list
-            action_pool_list=self.action_pool_list
-            next_state_pool_list=self.next_state_pool_list
-            reward_pool_list=self.reward_pool_list
-            done_pool_list=self.done_pool_list
-            self.state_pool_list=manager.list()
-            self.action_pool_list=manager.list()
-            self.next_state_pool_list=manager.list()
-            self.reward_pool_list=manager.list()
-            self.done_pool_list=manager.list()
-            for i in range(processes):
-                self.state_pool_list.append(state_pool_list[i])
-                self.action_pool_list.append(action_pool_list[i])
-                self.next_state_pool_list.append(next_state_pool_list[i])
-                self.reward_pool_list.append(reward_pool_list[i])
-                self.done_pool_list.append(done_pool_list[i])
-            del state_pool_list
-            del action_pool_list
-            del next_state_pool_list
-            del reward_pool_list
-            del done_pool_list
+            self.state_pool_list=manager.list(self.state_pool_list)
+            self.action_pool_list=manager.list(self.state_pool_list)
+            self.next_state_pool_list=manager.list(self.state_pool_list)
+            self.reward_pool_list=manager.list(self.state_pool_list)
+            self.done_pool_list=manager.list(self.state_pool_list)
             self.reward=np.zeros(processes,dtype='float32')
             self.reward=Array('f',self.reward)
             if self.HER!=True:
@@ -1193,27 +1177,11 @@ class RL:
             mp=multiprocessing
             self.mp=mp
             manager=multiprocessing.Manager()
-            state_pool_list=self.state_pool_list
-            action_pool_list=self.action_pool_list
-            next_state_pool_list=self.next_state_pool_list
-            reward_pool_list=self.reward_pool_list
-            done_pool_list=self.done_pool_list
-            self.state_pool_list=manager.list()
-            self.action_pool_list=manager.list()
-            self.next_state_pool_list=manager.list()
-            self.reward_pool_list=manager.list()
-            self.done_pool_list=manager.list()
-            for i in range(processes):
-                self.state_pool_list.append(state_pool_list[i])
-                self.action_pool_list.append(action_pool_list[i])
-                self.next_state_pool_list.append(next_state_pool_list[i])
-                self.reward_pool_list.append(reward_pool_list[i])
-                self.done_pool_list.append(done_pool_list[i])
-            del state_pool_list
-            del action_pool_list
-            del next_state_pool_list
-            del reward_pool_list
-            del done_pool_list
+            self.state_pool_list=manager.list(self.state_pool_list)
+            self.action_pool_list=manager.list(self.state_pool_list)
+            self.next_state_pool_list=manager.list(self.state_pool_list)
+            self.reward_pool_list=manager.list(self.state_pool_list)
+            self.done_pool_list=manager.list(self.state_pool_list)
             self.reward=np.zeros(processes,dtype='float32')
             self.reward=Array('f',self.reward)
             if self.HER!=True:
