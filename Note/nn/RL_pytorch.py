fc22322345dff6e519202e64c623c139530f62d7
NoteDance <notedance@outlook.com>
2025-02-27 17:59:55 +0800

    Update RL_pytorch.py

--- Note/nn/RL_pytorch.py
+++ Note/nn/RL_pytorch.py
@@ -63,7 +63,7 @@ class RL_pytorch:
     
     def pool(self,s,a,next_s,r,done,index=None):
         if self.pool_network==True:
-            if type(self.state_pool_list[index])!=np.ndarray and self.state_pool_list[index]==None:
+            if self.state_pool_list[index] is None:
                 self.state_pool_list[index]=s
                 self.action_pool_list[index]=np.expand_dims(a,axis=0)
                 self.next_state_pool_list[index]=np.expand_dims(next_s,axis=0)
@@ -82,7 +82,7 @@ class RL_pytorch:
                 self.reward_pool_list[index]=self.reward_pool_list[index][1:]
                 self.done_pool_list[index]=self.done_pool_list[index][1:]
         else:
-            if type(self.state_pool)!=np.ndarray and self.state_pool==None:
+            if self.state_pool is None:
                 self.state_pool=s
                 self.action_pool=np.expand_dims(a,axis=0)
                 self.next_state_pool=np.expand_dims(next_s,axis=0)
