b22fb336fb7eae51cdf74b7871c3cbc7c02615b7
NoteDance <notedance@outlook.com>
2025-03-06 19:12:44 +0800

    Update opt_finder.py

--- Note/nn/opt_finder.py
+++ Note/nn/opt_finder.py
@@ -52,16 +52,16 @@ class OptFinder:
         # Restore the weights to the state before model fitting
         nn.assign_param(self.model.param, initial_weights)
 
-    def plot_loss(self, n_skip_beginning=0, n_skip_end=0, x_scale='linear'):
+    def plot_loss(self, x_scale='linear'):
         plt.ylabel("Loss")
-        x_values = list(range(len(self.losses)))[n_skip_beginning: -n_skip_end]
+        x_values = list(range(len(self.losses)))
         plt.xlabel("Optimizer Index")
-        plt.plot(x_values, self.losses[n_skip_beginning: -n_skip_end])
+        plt.plot(x_values, self.losses)
         plt.xscale(x_scale)
         plt.show()
     
-    def plot_loss_change(self, sma=1, n_skip_beginning=0, n_skip_end=0, y_lim=(-0.01, 0.01)):
-        derivatives = self.get_derivatives(sma)[n_skip_beginning: -n_skip_end]
+    def plot_loss_change(self, sma=1, y_lim=(-0.01, 0.01)):
+        derivatives = self.get_derivatives(sma)
         x_values = list(range(len(derivatives)))
         xlabel = "Optimizer Index"
         x_scale = 'linear'
@@ -86,8 +86,11 @@ class OptFinder_rl:
         self.agent = agent
         self.optimizers = optimizers
         self.rewards = []
-        self.normalized_reward = []
+        self.losses = []
+        self.normalized_rewards = []
+        self.mean_loss = []
         self.best_reward = -1e9
+        self.best_loss = 1e9
             
     def on_episode_end(self, episode, logs):
         reward = logs['reward']
@@ -97,16 +100,33 @@ class OptFinder_rl:
         mean_reward = np.mean(recent_rewards)
         std_reward = np.std(recent_rewards) + 1e-8
         normalized_reward = (reward - mean_reward) / std_reward
-        self.normalized_reward.append(normalized_reward)
+        self.normalized_rewards.append(normalized_reward)
         if normalized_reward > self.best_reward:
             self.best_opt = self.model.optimizer
             self.best_reward = normalized_reward
+            
+        self.rewards.clear()
+    
+    def on_episode_end_(self, episode, logs):
+        loss = logs['loss']
+        self.losses.append(loss)
+        
+        mean_loss = np.mean(self.losses)
+        self.mean_losses.append(mean_loss)
+        if mean_loss < self.best_loss:
+            self.best_opt = self.model.optimizer
+            self.best_loss = mean_loss
+        
+        self.losses.clear()
 
-    def find(self, train_loss=None, pool_network=True, processes=None, processes_her=None, processes_pr=None, strategy=None, episodes=1, jit_compile=True):
+    def find(self, train_loss=None, pool_network=True, processes=None, processes_her=None, processes_pr=None, strategy=None, episodes=1, metrics='reward', jit_compile=True):
         # Save weights into a file
         initial_weights = [tf.Variable(param.read_value()) for param in nest.flatten(self.agent.param)]
-
-        callback = nn.LambdaCallback(on_episode_end=lambda episode, logs: self.on_episode_end(episode, logs))
+        
+        if metrics == 'reward':
+            callback = nn.LambdaCallback(on_episode_end=lambda episode, logs: self.on_episode_end(episode, logs))
+        else:
+            callback = nn.LambdaCallback(on_episode_end=lambda episode, logs: self.on_episode_end_(episode, logs))
         
         for opt in self.optimizers:
             self.model.optimizer = opt
@@ -138,9 +158,9 @@ class OptFinder_rl:
         
     def plot_reward(self, x_scale='linear'):
         plt.ylabel("Reward")
-        x_values = list(range(len(self.normalized_reward)))
+        x_values = list(range(len(self.normalized_rewards)))
         plt.xlabel("Optimizer Index")
-        plt.plot(x_values, self.normalized_reward)
+        plt.plot(x_values, self.normalized_rewards)
         plt.xscale(x_scale)
         plt.show()
     
@@ -158,8 +178,36 @@ class OptFinder_rl:
     
     def get_derivatives(self, sma):
         assert sma >= 1
-        n = len(self.normalized_reward)
+        n = len(self.normalized_rewards)
         derivatives = [0] * sma
         for i in range(sma, n):
-            derivatives.append((self.normalized_reward[i] - self.normalized_reward[i - sma]) / sma)
+            derivatives.append((self.normalized_rewards[i] - self.normalized_rewards[i - sma]) / sma)
+        return derivatives
+    
+    def plot_loss(self, x_scale='linear'):
+        plt.ylabel("Loss")
+        x_values = list(range(len(self.mean_losses)))
+        plt.xlabel("Optimizer Index")
+        plt.plot(x_values, self.mean_losses)
+        plt.xscale(x_scale)
+        plt.show()
+    
+    def plot_loss_change(self, sma=1, y_lim=(-0.01, 0.01)):
+        derivatives = self.get_derivatives_(sma)
+        x_values = list(range(len(derivatives)))
+        xlabel = "Optimizer Index"
+        x_scale = 'linear'
+        plt.ylabel("Rate of Loss Change")
+        plt.xlabel(xlabel)
+        plt.plot(x_values, derivatives)
+        plt.xscale(x_scale)
+        plt.ylim(y_lim)
+        plt.show()
+
+    def get_derivatives_(self, sma):
+        assert sma >= 1
+        n = len(self.mean_losses)
+        derivatives = [0] * sma
+        for i in range(sma, n):
+            derivatives.append((self.mean_losses[i] - self.mean_losses[i - sma]) / sma)
         return derivatives
