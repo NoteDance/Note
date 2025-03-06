1dbe65d0c660974695ceafd8d78b921b96f17741
NoteDance <notedance@outlook.com>
2025-03-06 23:46:46 +0800

    Update opt_finder.py

--- Note/nn/opt_finder.py
+++ Note/nn/opt_finder.py
@@ -96,25 +96,29 @@ class OptFinder_rl:
         reward = logs['reward']
         self.rewards.append(reward)
         
-        mean_reward = np.mean(self.rewards)
-        self.mean_rewards.append(mean_reward)
-        if mean_reward > self.best_reward:
-            self.best_opt = self.model.optimizer
-            self.best_reward = mean_reward
+        if episode+1 == self.episode:
+            mean_reward = np.mean(self.rewards)
+            self.mean_rewards.append(mean_reward)
+            if mean_reward > self.best_reward:
+                self.best_opt = self.model.optimizer
+                self.best_reward = mean_reward
     
     def on_episode_end_(self, episode, logs):
         loss = logs['loss']
         self.losses.append(loss)
         
-        mean_loss = np.mean(self.losses)
-        self.mean_losses.append(mean_loss)
-        if mean_loss < self.best_loss:
-            self.best_opt = self.model.optimizer
-            self.best_loss = mean_loss
+        if episode+1 == self.episode:
+            mean_loss = np.mean(self.losses)
+            self.mean_losses.append(mean_loss)
+            if mean_loss < self.best_loss:
+                self.best_opt = self.model.optimizer
+                self.best_loss = mean_loss
 
     def find(self, train_loss=None, pool_network=True, processes=None, processes_her=None, processes_pr=None, strategy=None, episodes=1, metrics='reward', jit_compile=True):
         # Save weights into a file
         initial_weights = [tf.Variable(param.read_value()) for param in nest.flatten(self.agent.param)]
+        
+        self.episodes = episodes
         
         if metrics == 'reward':
             callback = nn.LambdaCallback(on_episode_end=lambda episode, logs: self.on_episode_end(episode, logs))
