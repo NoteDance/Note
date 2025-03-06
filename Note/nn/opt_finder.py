070c729b53fb5c62c362267878dea4371f01a09a
NoteDance <notedance@outlook.com>
2025-03-06 19:32:18 +0800

    Update opt_finder.py

--- Note/nn/opt_finder.py
+++ Note/nn/opt_finder.py
@@ -104,8 +104,6 @@ class OptFinder_rl:
         if normalized_reward > self.best_reward:
             self.best_opt = self.model.optimizer
             self.best_reward = normalized_reward
-            
-        self.rewards.clear()
     
     def on_episode_end_(self, episode, logs):
         loss = logs['loss']
@@ -116,8 +114,6 @@ class OptFinder_rl:
         if mean_loss < self.best_loss:
             self.best_opt = self.model.optimizer
             self.best_loss = mean_loss
-        
-        self.losses.clear()
 
     def find(self, train_loss=None, pool_network=True, processes=None, processes_her=None, processes_pr=None, strategy=None, episodes=1, metrics='reward', jit_compile=True):
         # Save weights into a file
@@ -149,6 +145,11 @@ class OptFinder_rl:
                                processes_pr=processes_her,
                                callbacks=[callback],
                                jit_compile=jit_compile)
+            
+            if metrics == 'reward':
+                self.rewards.clear()
+            else:
+                self.losses.clear()
 
             # Restore the weights to the state before model fitting
             nn.assign_param(self.agent.param, initial_weights)
