7ed91b0f504524cc1d00dbfac6acbbb831941913
NoteDance <notedance@outlook.com>
2025-03-09 07:33:45 +0800

    Update RL.py

--- Note/nn/RL.py
+++ Note/nn/RL.py
@@ -145,6 +145,13 @@ class RL:
                 self.next_state_pool_list[index]=np.concatenate((self.next_state_pool_list[index],np.expand_dims(next_s,axis=0)),0)
                 self.reward_pool_list[index]=np.concatenate((self.reward_pool_list[index],np.expand_dims(r,axis=0)),0)
                 self.done_pool_list[index]=np.concatenate((self.done_pool[7],np.expand_dims(done,axis=0)),0)
+            if self.clearing_freq!=None and self.total_episode+1%self.clearing_freq==0:
+                if self.window_size_<len(self.state_pool_list[index]):
+                    self.state_pool_list[index]=self.state_pool_list[index][self.window_size_:]
+                    self.action_pool_list[index]=self.action_pool_list[index][self.window_size_:]
+                    self.next_state_pool_list[index]=self.next_state_pool_list[index][self.window_size_:]
+                    self.reward_pool_list[index]=self.reward_pool_list[index][self.window_size_:]
+                    self.done_pool_list[index]=self.done_pool_list[index][self.window_size_:]
             if len(self.state_pool_list[index])>math.ceil(self.pool_size/self.processes):
                 if self.window_size==None:
                     self.state_pool_list[index]=self.state_pool_list[index][1:]
@@ -171,6 +178,12 @@ class RL:
                 self.next_state_pool=np.concatenate((self.next_state_pool,np.expand_dims(next_s,axis=0)),0)
                 self.reward_pool=np.concatenate((self.reward_pool,np.expand_dims(r,axis=0)),0)
                 self.done_pool=np.concatenate((self.done_pool,np.expand_dims(done,axis=0)),0)
+            if self.clearing_freq!=None and self.total_episode+1%self.clearing_freq==0:
+                self.state_pool=self.state_pool[self.window_size_:]
+                self.action_pool=self.action_pool[self.window_size_:]
+                self.next_state_pool=self.next_state_pool[self.window_size_:]
+                self.reward_pool=self.reward_pool[self.window_size_:]
+                self.done_pool=self.done_pool[self.window_size_:]
             if len(self.state_pool)>self.pool_size:
                 if self.window_size==None:
                     self.state_pool=self.state_pool[1:]
@@ -916,7 +929,7 @@ class RL:
             s=next_s
     
     
-    def train(self, train_loss, optimizer, episodes=None, jit_compile=True, pool_network=True, processes=None, processes_her=None, processes_pr=None, window_size=None, save_data=True, callbacks=None, shuffle=False, p=None):
+    def train(self, train_loss, optimizer, episodes=None, jit_compile=True, pool_network=True, processes=None, processes_her=None, processes_pr=None, window_size=None, clearing_freq=None, window_size_=None, save_data=True, callbacks=None, shuffle=False, p=None):
         avg_reward=None
         if p==None:
             self.p=9
@@ -940,6 +953,8 @@ class RL:
         self.processes_her=processes_her
         self.processes_pr=processes_pr
         self.window_size=window_size
+        self.clearing_freq=clearing_freq
+        self.window_size_=window_size_
         self.save_data=save_data
         self.shuffle=shuffle
         self.p=p
@@ -1176,7 +1191,7 @@ class RL:
         return
     
     
-    def distributed_training(self, optimizer, strategy, episodes=None, num_episodes=None, jit_compile=True, pool_network=True, processes=None, processes_her=None, processes_pr=None, window_size=None, save_data=True, callbacks=None, shuffle=False, p=None):
+    def distributed_training(self, optimizer, strategy, episodes=None, num_episodes=None, jit_compile=True, pool_network=True, processes=None, processes_her=None, processes_pr=None, window_size=None, clearing_freq=None, window_size_=None, save_data=True, callbacks=None, shuffle=False, p=None):
         avg_reward=None
         if num_episodes!=None:
             episodes=num_episodes
@@ -1203,6 +1218,8 @@ class RL:
         self.processes_her=processes_her
         self.processes_pr=processes_pr
         self.window_size=window_size
+        self.clearing_freq=clearing_freq
+        self.window_size_=window_size_
         self.save_data=save_data
         self.shuffle=shuffle
         self.p=p
