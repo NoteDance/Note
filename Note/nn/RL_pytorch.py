529fd3bef8a95637a4da55d1cd863bd05c2e2d89
NoteDance <notedance@outlook.com>
2025-03-13 08:50:44 +0800

    Update RL_pytorch.py

--- Note/nn/RL_pytorch.py
+++ Note/nn/RL_pytorch.py
@@ -78,7 +78,7 @@ class RL_pytorch:
                 self.reward_pool_list[index]=np.concatenate((self.reward_pool_list[index],np.expand_dims(r,axis=0)),0)
                 self.done_pool_list[index]=np.concatenate((self.done_pool[7],np.expand_dims(done,axis=0)),0)
             if self.clearing_freq!=None:
-                if self.store_counter[index]>=self.clearing_freq[index]:
+                if self.store_counter[index]%self.clearing_freq==0:
                     self.state_pool_list[index]=self.state_pool_list[index][self.window_size_:]
                     self.action_pool_list[index]=self.action_pool_list[index][self.window_size_:]
                     self.next_state_pool_list[index]=self.next_state_pool_list[index][self.window_size_:]
@@ -468,12 +468,12 @@ class RL_pytorch:
             done=np.array(done)
             if self.PR!=True and self.HER!=True:
                 lock_list[index].acquire()
-                if self.clearing_freq_!=None:
+                if self.clearing_freq!=None:
                     self.store_counter[index]+=1
                 self.pool(s,a,next_s,r,done,index)
                 lock_list[index].release()
             else:
-                if self.clearing_freq_!=None:
+                if self.clearing_freq!=None:
                     self.store_counter[index]+=1
                 self.pool(s,a,next_s,r,done,index)
                 if self.PR==True:
@@ -516,15 +516,14 @@ class RL_pytorch:
             mp=multiprocessing
             self.mp=mp
             manager=multiprocessing.Manager()
-            self.clearing_freq_=clearing_freq
             if save_data and len(self.state_pool_list)!=0:
                 self.state_pool_list=manager.list(self.state_pool_list)
                 self.action_pool_list=manager.list(self.state_pool_list)
                 self.next_state_pool_list=manager.list(self.state_pool_list)
                 self.reward_pool_list=manager.list(self.state_pool_list)
                 self.done_pool_list=manager.list(self.state_pool_list)
-                if self.clearing_freq_!=None:
-                    self.clearing_freq=manager.list(self.clearing_freq)
+                if self.clearing_freq!=None:
+                    self.store_counter=manager.list(self.store_counter)
             else:
                 self.state_pool_list=manager.list()
                 self.action_pool_list=manager.list()
@@ -532,8 +531,7 @@ class RL_pytorch:
                 self.reward_pool_list=manager.list()
                 self.done_pool_list=manager.list()
                 self.inverse_len=manager.list([0 for _ in range(processes)])
-                if self.clearing_freq_!=None:
-                    self.clearing_freq=manager.list()
+                if self.clearing_freq!=None:
                     self.store_counter=manager.list()
             if not save_data or len(self.state_pool_list)==0:
                 for _ in range(processes):
@@ -542,8 +540,7 @@ class RL_pytorch:
                     self.next_state_pool_list.append(None)
                     self.reward_pool_list.append(None)
                     self.done_pool_list.append(None)
-                    if self.clearing_freq_!=None:
-                        self.clearing_freq.append(self.clearing_freq_)
+                    if self.clearing_freq!=None:
                         self.store_counter.append(0)
             self.reward=np.zeros(processes,dtype='float32')
             self.reward=Array('f',self.reward)
