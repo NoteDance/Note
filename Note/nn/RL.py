066b9eb8788148c2f9e75d6ef187c3819879fcc7
NoteDance <notedance@outlook.com>
2025-03-21 08:04:53 +0800

    Update RL.py

--- Note/nn/RL.py
+++ Note/nn/RL.py
@@ -968,7 +968,7 @@ class RL:
             mp=multiprocessing
             self.mp=mp
             manager=multiprocessing.Manager()
-            if save_data and len(self.state_pool_list)!=0:
+            if save_data and len(self.state_pool_list)!=0 and self.state_pool_list[0] is not None:
                 self.state_pool_list=manager.list(self.state_pool_list)
                 self.action_pool_list=manager.list(self.state_pool_list)
                 self.next_state_pool_list=manager.list(self.state_pool_list)
@@ -1240,7 +1240,7 @@ class RL:
             mp=multiprocessing
             self.mp=mp
             manager=multiprocessing.Manager()
-            if save_data and len(self.state_pool_list)!=0:
+            if save_data and len(self.state_pool_list)!=0 and self.state_pool_list[0] is not None:
                 self.state_pool_list=manager.list(self.state_pool_list)
                 self.action_pool_list=manager.list(self.state_pool_list)
                 self.next_state_pool_list=manager.list(self.state_pool_list)
