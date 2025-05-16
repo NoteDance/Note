dde019706bc2c506ca544d2a344d29397c5d5e75
NoteDance <notedance@outlook.com>
2025-05-16 21:12:12 +0800

    Update parallel_finder_rl.py

--- Note/nn/parallel_finder_rl.py
+++ Note/nn/parallel_finder_rl.py
@@ -26,11 +26,7 @@ class ParallelFinder_rl:
     def on_episode_end(self, episode, logs, agent=None, lock=None):
         lock.acquire()
         reward = logs['reward']
-        if agent not in self.rewards:
-            self.rewards[agent] = []
-            self.rewards[agent].append(reward)
-        else:
-            self.rewards[agent].append(reward)
+        self.rewards[agent].append(reward)
         
         if episode+1 == self.episode:
             mean_reward = np.mean(self.rewards[agent])
@@ -49,11 +45,7 @@ class ParallelFinder_rl:
     def on_episode_end_(self, episode, logs, agent=None, lock=None):
         lock.acquire()
         loss = logs['loss']
-        if agent not in self.losses:
-            self.losses[agent] = []
-            self.losses[agent].append(loss)
-        else:
-            self.losses[agent].append(loss)
+        self.losses[agent].append(loss)
         
         if episode+1 == self.episode:
             mean_loss = np.mean(self.losses[agent])
