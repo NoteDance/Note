edb9648ec5b9cbc00ed1e61f3477928019a35782
NoteDance <notedance@outlook.com>
2025-06-12 14:55:19 +0800

    Update RL.py

--- Note/nn/RL.py
+++ Note/nn/RL.py
@@ -120,11 +120,12 @@ class RL:
         return self.info
     
     
-    def set(self,policy=None,noise=None,pool_size=None,batch=None,update_batches=None,update_steps=None,trial_count=None,criterion=None,PPO=False,HER=False,MARL=False,PR=False,IRL=False,epsilon=None,initial_TD=7.,alpha=0.7):
+    def set(self,policy=None,noise=None,pool_size=None,batch=None,num_updates=None,update_batches=None,update_steps=None,trial_count=None,criterion=None,PPO=False,HER=False,MARL=False,PR=False,IRL=False,epsilon=None,initial_TD=7.,alpha=0.7):
         self.policy=policy
         self.noise=noise
         self.pool_size=pool_size
         self.batch=batch
+        self.num_updates=num_updates
         self.update_batches=update_batches
         self.update_steps=update_steps
         self.trial_count=trial_count
@@ -958,7 +959,7 @@ class RL:
             s=next_s
     
     
-    def train(self, train_loss, optimizer, episodes=None, jit_compile=True, pool_network=True, processes=None, processes_her=None, processes_pr=None, window_size=None, clearing_freq=None, window_size_=None, random=True, num_updates=None, save_data=True, callbacks=None, p=None):
+    def train(self, train_loss, optimizer, episodes=None, jit_compile=True, pool_network=True, processes=None, processes_her=None, processes_pr=None, window_size=None, clearing_freq=None, window_size_=None, random=True, save_data=True, callbacks=None, p=None):
         avg_reward=None
         if p!=0:
             if p==None:
@@ -986,8 +987,8 @@ class RL:
         self.clearing_freq=clearing_freq
         self.window_size_=window_size_
         self.random=random
-        self.num_updates=num_updates
-        self.pool_size_=num_updates*self.batch
+        if self.num_updates!=None:
+            self.pool_size_=self.num_updates*self.batch
         self.save_data=save_data
         self.p=p
         self.info_flag=0
@@ -1262,7 +1263,7 @@ class RL:
         return
     
     
-    def distributed_training(self, optimizer, strategy, episodes=None, num_episodes=None, jit_compile=True, pool_network=True, processes=None, processes_her=None, processes_pr=None, window_size=None, clearing_freq=None, window_size_=None, random=True, num_updates=None, save_data=True, callbacks=None, p=None):
+    def distributed_training(self, optimizer, strategy, episodes=None, num_episodes=None, jit_compile=True, pool_network=True, processes=None, processes_her=None, processes_pr=None, window_size=None, clearing_freq=None, window_size_=None, random=True, save_data=True, callbacks=None, p=None):
         avg_reward=None
         if num_episodes!=None:
             episodes=num_episodes
@@ -1293,8 +1294,8 @@ class RL:
         self.clearing_freq=clearing_freq
         self.window_size_=window_size_
         self.random=random
-        self.num_updates=num_updates
-        self.pool_size_=num_updates*self.batch
+        if self.num_updates!=None:
+            self.pool_size_=self.num_updates*self.batch
         self.save_data=save_data
         self.p=p
         self.info_flag=1
