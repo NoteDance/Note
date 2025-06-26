ba2e1e983cabc8e30729e6a9c2e7341079ce85fc
NoteDance <notedance@outlook.com>
2025-06-26 15:28:58 +0800

    Update ranger.py

--- Note/nn/optimizer/ranger.py
+++ Note/nn/optimizer/ranger.py
@@ -82,6 +82,10 @@ class Ranger(optimizer.Optimizer):
             self._track_variable(self.slow_buffer[-1])
 
     def update_step(self, gradient, variable, learning_rate):
+        if tf.keras.backend.is_sparse(gradient):
+            raise RuntimeError(
+                'Ranger optimizer does not support sparse gradients')
+            
         if gradient.dtype != tf.float32:
             gradient = tf.cast(gradient, 'float32')
         if variable.dtype != tf.float32:
@@ -89,10 +93,6 @@ class Ranger(optimizer.Optimizer):
         else:
             variable_fp32 = tf.convert_to_tensor(variable)
         lr = tf.cast(learning_rate, variable_fp32.dtype)
-        
-        if tf.keras.backend.is_sparse(gradient):
-            raise RuntimeError(
-                'Ranger optimizer does not support sparse gradients')
         
         # begin computations
         exp_avg = self.exp_avg[self._get_variable_index(variable)]
