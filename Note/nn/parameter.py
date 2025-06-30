c788a55177e080aff375dcb6f66b3b831da01f91
NoteDance <notedance@outlook.com>
2025-06-30 22:51:34 +0800

    Update parameter.py

--- Note/nn/parameter.py
+++ Note/nn/parameter.py
@@ -9,6 +9,6 @@ def Parameter(data,trainable=True,name=None):
     if Model.name!=None and Model.name not in Model.layer_param:
         Model.layer_param[Model.name]=[]
         Model.layer_param[Model.name].append(param)
-    elif Model.name_!=None:
+    elif Model.name!=None:
         Model.layer_param[Model.name].append(param)
     return param
