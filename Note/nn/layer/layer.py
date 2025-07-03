d6a8165d395936b64d9fe5a4467792d4c15a7d2b
NoteDance <notedance@outlook.com>
2025-07-03 17:58:41 +0800

    Add files via upload

---
+++ Note/nn/layer/layer.py
@@ -0,0 +1,45 @@
+class LayerMeta(type):
+    def __call__(cls, *args, **kwargs):
+        # Create the instance
+        instance = super().__call__(*args, **kwargs)
+        # Process any Parameter assignments that occurred during __init__
+        instance._finalize_parameters()
+        return instance
+
+
+class Layer(metaclass=LayerMeta):
+    def __init__(self):
+        # store this layer’s own variables
+        self._own_params = []
+        # store any sub‑layers you attach
+        self._sub_layers = []
+        
+        self._param_assignments = []
+    
+    def _finalize_parameters(self):
+        # Process any parameter assignments that were deferred
+        for param in self._param_assignments:
+            self._own_params.append(param)
+        self._param_assignments.clear()
+    
+    def add_param(self, var):
+        if hasattr(self, '_param_assignments'):
+            self._param_assignments.append(var)
+        else:
+            self._own_params.append(var)
+
+    def __setattr__(self, name, value):
+        # whenever you assign an nn.Layer (or Layer) to an attribute,
+        # register it as a child
+        if isinstance(value, Layer):
+            # add to our children list
+            object.__getattribute__(self, "_sub_layers").append(value)
+        object.__setattr__(self, name, value)
+    
+    @property
+    def param(self):
+        # recursively collect all own and sub‑layers’ parameters
+        out = list(self._own_params)
+        for child in self._sub_layers:
+            out.extend(child.param)
+        return out
