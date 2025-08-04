39ffec5aea8d26907eacd312a42291adcd600594
NoteDance <notedance@outlook.com>
2025-08-04 19:06:03 +0800

    Update prioritized_replay.py

--- Note/RL/rl/prioritized_replay.py
+++ Note/RL/rl/prioritized_replay.py
@@ -10,9 +10,10 @@ class pr:
         self.PPO=False
     
     
-    def sample(self,state_pool,action_pool,next_state_pool,reward_pool,done_pool,epsilon,alpha,batch):
+    def sample(self,state_pool,action_pool,next_state_pool,reward_pool,done_pool,epsilon,lambda_,alpha,batch):
         if self.PPO:
-            prios=(tf.abs(self.ratio-1.0)+epsilon)**alpha
+            scores=self.lambda_*self.TD+(1.0-self.lambda_)*tf.abs(self.ratio-1.0)
+            prios=tf.pow(scores+epsilon,alpha)
             p=prios/tf.reduce_sum(prios)
         else:
             prios=(self.TD+epsilon)**alpha
@@ -21,21 +22,23 @@ class pr:
         return state_pool[self.index],action_pool[self.index],next_state_pool[self.index],reward_pool[self.index],done_pool[self.index]
     
     
-    def update_TD(self,ratio_TD):
+    def update_TD(self,TD,ratio=None):
         if self.PPO:
             if self.pool_network==True:
                 for i in range(len(self.index)):
-                    self.ratio[7][self.index[i]].assign(ratio_TD[i])
+                    self.ratio[7][self.index[i]].assign(ratio[i])
+                    self.TD[7][self.index[i]].assign(tf.abs(TD[i]))
             else:
                 for i in range(len(self.index)):
-                    self.ratio[self.index[i]].assign(ratio_TD[i])
+                    self.ratio[self.index[i]].assign(ratio[i])
+                    self.TD[self.index[i]].assign(tf.abs(TD[i]))
         else:
             if self.pool_network==True:
                 for i in range(len(self.index)):
-                    self.TD[7][self.index[i]].assign(tf.abs(ratio_TD[i]))
+                    self.TD[7][self.index[i]].assign(tf.abs(TD[i]))
             else:
                 for i in range(len(self.index)):
-                    self.TD[self.index[i]].assign(tf.abs(ratio_TD[i]))
+                    self.TD[self.index[i]].assign(tf.abs(TD[i]))
         return
 
 
@@ -43,13 +46,15 @@ class pr_:
     def __init__(self):
         self.ratio=None
         self.TD=None
+        self.lambda_=None
         self.index=None
         self.PPO=False
     
     
     def sample(self,state_pool,action_pool,next_state_pool,reward_pool,done_pool,epsilon,alpha,batch):
         if self.PPO:
-            prios=(self.ratio+epsilon)**alpha
+            scores=self.lambda_*self.TD+(1.0-self.lambda_)*tf.abs(self.ratio-1.0)
+            prios=tf.pow(scores+epsilon,alpha)
             p=prios/np.sum(prios)
         else:
             prios=(self.TD+epsilon)**alpha
@@ -58,21 +63,23 @@ class pr_:
         return state_pool[self.index],action_pool[self.index],next_state_pool[self.index],reward_pool[self.index],done_pool[self.index]
     
     
-    def update_TD(self,ratio_TD):
+    def update_TD(self,TD,ratio=None):
         if self.PPO:
             if self.pool_network==True:
                 for i in range(len(self.index)):
-                    self.ratio[7][self.index[i]].assign(ratio_TD[i])
+                    self.ratio[7][self.index[i]].assign(ratio[i])
+                    self.TD[7][self.index[i]].assign(tf.abs(TD[i]))
             else:
                 for i in range(len(self.index)):
-                    self.ratio[self.index[i]].assign(ratio_TD[i])
+                    self.ratio[self.index[i]].assign(ratio[i])
+                    self.TD[self.index[i]].assign(tf.abs(TD[i]))
         else:
             if self.pool_network==True:
                 for i in range(len(self.index)):
-                    self.TD[7][self.index[i]].assign(tf.abs(ratio_TD[i]))
+                    self.TD[7][self.index[i]].assign(tf.abs(TD[i]))
             else:
                 for i in range(len(self.index)):
-                    self.TD[self.index[i]].assign(tf.abs(ratio_TD[i]))
+                    self.TD[self.index[i]].assign(tf.abs(TD[i]))
         return
 
 
@@ -80,13 +87,15 @@ class pr_mp:
     def __init__(self):
         self.ratio=None
         self.TD=None
+        self.lambda_=None
         self.index=None
         self.PPO=False
     
     
     def sample(self,state_pool,action_pool,next_state_pool,reward_pool,done_pool,epsilon,alpha,batch,p):
         if self.PPO:
-            prios=(self.ratio[p]+epsilon)**alpha
+            scores=self.lambda_*self.TD[p]+(1.0-self.lambda_)*tf.abs(self.ratio[p]-1.0)
+            prios=tf.pow(scores+epsilon,alpha)
             prob=prios/tf.reduce_sum(prios)
         else:
             prios=(self.TD[p]+epsilon)**alpha
@@ -95,13 +104,14 @@ class pr_mp:
         return state_pool[self.index[p]],action_pool[self.index[p]],next_state_pool[self.index[p]],reward_pool[self.index[p]],done_pool[self.index[p]]
     
     
-    def update_TD(self,ratio_TD,p):
+    def update_TD(self,TD,ratio=None,p=None):
         if self.PPO:
             for i in range(len(self.index[p])):
-                self.TD[p][self.index[p][i]].assign(ratio_TD[i])
+                self.ratio[p][self.index[p][i]].assign(ratio[i])
+                self.TD[p][self.index[p][i]].assign(tf.abs(TD[i]))
         else:
             for i in range(len(self.index[p])):
-                self.TD[p][self.index[p][i]].assign(tf.abs(ratio_TD[i]))
+                self.TD[p][self.index[p][i]].assign(tf.abs(TD[i]))
         return
 
 
@@ -109,13 +119,15 @@ class pr_mp_:
     def __init__(self):
         self.ratio=None
         self.TD=None
+        self.lambda_=None
         self.index=None
         self.PPO=False
     
     
     def sample(self,state_pool,action_pool,next_state_pool,reward_pool,done_pool,epsilon,alpha,batch,p):
         if self.PPO:
-            prios=(self.ratio[p]+epsilon)**alpha
+            scores=self.lambda_*self.TD[p]+(1.0-self.lambda_)*tf.abs(self.ratio[p]-1.0)
+            prios=tf.pow(scores+epsilon,alpha)
             prob=prios/np.sum(prios)
         else:
             prios=(self.TD[p]+epsilon)**alpha
@@ -124,11 +136,12 @@ class pr_mp_:
         return state_pool[self.index[p]],action_pool[self.index[p]],next_state_pool[self.index[p]],reward_pool[self.index[p]],done_pool[self.index[p]]
     
     
-    def update_TD(self,ratio_TD,p):
+    def update_TD(self,TD,ratio=None,p=None):
         if self.PPO:
             for i in range(len(self.index[p])):
-                self.TD[p][self.index[p][i]].assign(ratio_TD[i])
+                self.ratio[p][self.index[p][i]].assign(ratio[i])
+                self.TD[p][self.index[p][i]].assign(np.abs(TD[i]))
         else:
             for i in range(len(self.index[p])):
-                self.TD[p][self.index[p][i]].assign(np.abs(ratio_TD[i]))
+                self.TD[p][self.index[p][i]].assign(np.abs(TD[i]))
         return
