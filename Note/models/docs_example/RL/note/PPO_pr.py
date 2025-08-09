544660bffe583a0c706ef94d0359f1b7222f2365
NoteDance <notedance@outlook.com>
2025-08-09 16:54:20 +0800

    Update PPO_pr.py

--- Note/models/docs_example/RL/note/PPO_pr.py
+++ Note/models/docs_example/RL/note/PPO_pr.py
@@ -91,3 +91,41 @@ class PPO(nn.RL):
     def update_param(self):
         nn.assign_param(self.actor_old.param, self.actor.param)
         return
+
+
+class PPO_(nn.RL):
+    def __init__(self,state_dim,hidden_dim,action_dim,clip_eps,alpha):
+        super().__init__()
+        self.actor=actor(state_dim,hidden_dim,action_dim)
+        self.actor_old=actor(state_dim,hidden_dim,action_dim)
+        nn.assign_param(self.actor_old.param,self.actor.param)
+        self.critic=critic(state_dim,hidden_dim)
+        self.clip_eps=clip_eps
+        self.alpha=alpha
+        self.param=[self.actor.param,self.critic.param]
+        self.env=gym.make('CartPole-v0')
+    
+    def action(self,s):
+        return self.actor_old(s)
+    
+    def window_size_fn(self,p):
+        return self.adjust_window_size(p)
+    
+    def __call__(self,s,a,next_s,r,d):
+        a=tf.expand_dims(a,axis=1)
+        action_prob=tf.gather(self.actor(s),a,axis=1,batch_dims=1)
+        action_prob_old=tf.gather(self.actor_old(s),a,axis=1,batch_dims=1)
+        raito=action_prob/action_prob_old
+        value=self.critic(s)
+        value_tar=tf.cast(r,'float32')+0.98*self.critic(next_s)*(1-tf.cast(d,'float32'))
+        TD=value_tar-value
+        sur1=raito*TD
+        sur2=tf.clip_by_value(raito,clip_value_min=1-self.clip_eps,clip_value_max=1+self.clip_eps)*TD
+        clip_loss=-tf.math.minimum(sur1,sur2)
+        entropy=action_prob*tf.math.log(action_prob+1e-8)
+        clip_loss=clip_loss-self.alpha*entropy
+        return tf.reduce_mean(clip_loss)+tf.reduce_mean((TD)**2)
+    
+    def update_param(self):
+        nn.assign_param(self.actor_old.param, self.actor.param)
+        return
