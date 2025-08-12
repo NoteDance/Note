746bcf668beebdbe119de271dafb92e6065aa202
NoteDance <notedance@outlook.com>
2025-08-12 10:46:28 +0800

    Update RL.py

--- Note/nn/RL.py
+++ Note/nn/RL.py
@@ -1091,12 +1091,12 @@ class RL:
             if self.PR==True:
                 if self.PPO:
                     if len(self.state_pool)>1:
-                        self.prioritized_replay.ratio=np.append(self.prioritized_replay.ratio,self.initial_ratio)
+                        self.prioritized_replay.ratio=np.append(self.prioritized_replay.ratio,np.max(self.prioritized_replay.ratio)).astype('float32')
                     if len(self.state_pool)>1:
-                        self.prioritized_replay.TD=np.append(self.prioritized_replay.ratio,self.initial_TD)
+                        self.prioritized_replay.TD=np.append(self.prioritized_replay.ratio,np.max(self.prioritized_replay.TD)).astype('float32')
                 else:
                     if len(self.state_pool)>1:
-                        self.prioritized_replay.TD=np.append(self.prioritized_replay.ratio,self.initial_TD)
+                        self.prioritized_replay.TD=np.append(self.prioritized_replay.ratio,np.max(self.prioritized_replay.TD)).astype('float32')
             if self.MARL==True:
                 r,done=self.reward_done_func_ma(r,done)
             self.reward=r+self.reward
@@ -1257,12 +1257,12 @@ class RL:
                 if self.PR==True:
                     if self.PPO:
                         if len(self.state_pool_list[index])>1:
-                            self.ratio_list[index]=np.append(self.ratio_list[index],self.initial_ratio)
+                            self.ratio_list[index]=np.append(self.ratio_list[index],np.max(self.prioritized_replay.ratio)).astype('float32')
                         if len(self.state_pool_list[index])>1:
-                            self.TD_list[index]=np.append(self.TD_list[index],self.initial_TD)
+                            self.TD_list[index]=np.append(self.TD_list[index],np.max(self.prioritized_replay.TD)).astype('float32')
                     else:
                         if len(self.state_pool_list[index])>1:
-                            self.TD_list[index]=np.append(self.TD_list[index],self.initial_TD)
+                            self.TD_list[index]=np.append(self.TD_list[index],np.max(self.prioritized_replay.TD)).astype('float32')
             if self.MARL==True:
                 r,done=self.reward_done_func_ma(r,done)
             self.reward[p]=r+self.reward[p]
