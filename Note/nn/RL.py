1a8257fdcb7676293990bb83cf7229206b9d67f0
NoteDance <notedance@outlook.com>
2025-08-18 16:33:30 +0800

    Update RL.py

--- Note/nn/RL.py
+++ Note/nn/RL.py
@@ -1042,9 +1042,8 @@ class RL:
                 if len(self.reward_list)>self.trial_count:
                     del self.reward_list[0]
                 return loss
-            if self.num_steps==None:
-                s=next_s
-            elif counter%self.num_steps==0 or (self.num_steps!=None and done):
+            s=next_s
+            if (self.num_steps!=None and counter%self.num_steps==0) or (self.num_steps!=None and done):
                 s=next_s_
     
     
@@ -1205,9 +1204,8 @@ class RL:
             self.reward[p]=r+self.reward[p]
             if (self.num_steps==None and done) or (self.num_steps!=None and done_):
                 return
-            if self.num_steps==None:
-                s=next_s
-            elif counter%self.num_steps==0 or (self.num_steps!=None and done):
+            s=next_s
+            if (self.num_steps!=None and counter%self.num_steps==0) or (self.num_steps!=None and done):
                 s=next_s_
     
     
