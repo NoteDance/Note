320914101647c02e9a23bc71fec6576daea77504
NoteDance <notedance@outlook.com>
2025-08-19 15:59:57 +0800

    Add files via upload

--- Note/models/docs_example/RL/note/PPO.py
+++ Note/models/docs_example/RL/note/PPO.py
@@ -44,12 +44,12 @@ class PPO(nn.RL):
         a=tf.expand_dims(a,axis=1)
         action_prob=tf.gather(self.actor(s),a,axis=1,batch_dims=1)
         action_prob_old=tf.gather(self.actor_old(s),a,axis=1,batch_dims=1)
-        raito=action_prob/action_prob_old
+        ratio=action_prob/action_prob_old
         value=self.critic(s)
         value_tar=tf.cast(r,'float32')+0.98*self.critic(next_s)*(1-tf.cast(d,'float32'))
         TD=value_tar-value
-        sur1=raito*TD
-        sur2=tf.clip_by_value(raito,clip_value_min=1-self.clip_eps,clip_value_max=1+self.clip_eps)*TD
+        sur1=ratio*TD
+        sur2=tf.clip_by_value(ratio,clip_value_min=1-self.clip_eps,clip_value_max=1+self.clip_eps)*TD
         clip_loss=-tf.math.minimum(sur1,sur2)
         entropy=action_prob*tf.math.log(action_prob+1e-8)
         clip_loss=clip_loss-self.alpha*entropy
