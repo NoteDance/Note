c354d4a901d267ba76e49b0a09bb66b5f3253704
NoteDance <notedance@outlook.com>
2025-08-23 15:55:42 +0800

    Add files via upload

--- Note/models/docs_example/RL/note/PPO_pr.py
+++ Note/models/docs_example/RL/note/PPO_pr.py
@@ -64,7 +64,7 @@ class PPO(nn.RL):
         weights = tf.pow(scores + 1e-7, self.alpha)
         p = weights / (tf.reduce_sum(weights))
         ess = 1.0 / (tf.reduce_sum(p * p))
-        features = tf.reshape([ratio_score, td_score, ess], (1,3))
+        features = tf.reshape([ratio_score, td_score, ess, len(self.prioritized_replay.ratio)], (1,4))
         features = (features - tf.reduce_min(features)) / (tf.reduce_max(features) - tf.reduce_min(features) + 1e-8)
         return self.controller(features)
     
@@ -89,7 +89,7 @@ class PPO(nn.RL):
         weights = tf.pow(scores + 1e-7, self.alpha)
         p = weights / (tf.reduce_sum(weights))
         ess = 1.0 / (tf.reduce_sum(p * p))
-        features = tf.reshape([ratio_score, td_score, ess], (1,3))
+        features = tf.reshape([ratio_score, td_score, ess, len(self.prioritized_replay.ratio)], (1,4))
         features = (features - tf.reduce_min(features)) / (tf.reduce_max(features) - tf.reduce_min(features) + 1e-8)
         w = self.controller(features)
         idx = tf.cast(tf.range(len(self.prioritized_replay.ratio), w.dtype))
