735101e10e4c73c9eadb44c34afeadcccdab489a
NoteDance <notedance@outlook.com>
2025-08-27 22:19:33 +0800

    Update DQN_pr.py

--- Note/models/docs_example/RL/note/DQN_pr.py
+++ Note/models/docs_example/RL/note/DQN_pr.py
@@ -18,7 +18,7 @@ class Controller(nn.Model):
     def __init__(self, hidden=32, temp=10.0):
         super().__init__()
         self.fc1 = nn.dense(hidden, 3, activation='relu')
-        self.fc2 = nn.dense(3, 1, activation='sigmoid')
+        self.fc2 = nn.dense(hidden, 1, activation='sigmoid')
         self.max_w = None
         self.temp = temp
 
