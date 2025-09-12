cef6c72a138a2b9b30f77526454417de8e44604f
NoteDance <notedance@outlook.com>
2025-09-12 15:30:35 +0800

    Add files via upload

--- Note/models/docs_example/RL/keras/DQN_pr.py
+++ Note/models/docs_example/RL/keras/DQN_pr.py
@@ -74,6 +74,11 @@ class DQN_(nn.RL):
     def window_size_fn(self):
         return self.adjust_window_size()
     
+    def batch_size_fn(self):
+        if self.step_counter%777:
+            return self.adjust_batch_size()
+        return self.adjust_batch_size()
+    
     def __call__(self,s,a,next_s,r,d):
         a=tf.expand_dims(a,axis=1)
         q_value=tf.gather(self.q_net(s),a,axis=1,batch_dims=1)
@@ -117,6 +122,11 @@ class _DQN(nn.RL):
         features = tf.reshape([td_score, ess, len(self.prioritized_replay.TD)], (1,3))
         features = (features - tf.reduce_min(features)) / (tf.reduce_max(features) - tf.reduce_min(features) + 1e-8)
         return self.controller(features)
+    
+    def batch_size_fn(self):
+        if self.step_counter%777:
+            return self.adjust_batch_size()
+        return self.adjust_batch_size()
     
     def __call__(self,s,a,next_s,r,d):
         a=tf.expand_dims(a,axis=1)
