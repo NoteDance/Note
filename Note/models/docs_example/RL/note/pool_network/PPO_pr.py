4f77f0625e2ad9784c48467a1603b11cf02faf0e
NoteDance <notedance@outlook.com>
2025-09-12 15:27:51 +0800

    Add files via upload

--- Note/models/docs_example/RL/note/pool_network/PPO_pr.py
+++ Note/models/docs_example/RL/note/pool_network/PPO_pr.py
@@ -61,10 +61,10 @@ class PPO(nn.RL):
     def window_size_fn(self,p):
         return self.adjust_window_size(p)
     
-#    def batch_size_fn(self):
-#        if self.batch_counter%777:
-#            return self.adjust_batch_size()
-#        return self.adjust_batch_size()
+    def batch_size_fn(self):
+        if self.batch_counter%777:
+            return self.adjust_batch_size()
+        return self.adjust_batch_size()
     
     def __call__(self,s,a,next_s,r,d):
         a=tf.expand_dims(a,axis=1)
@@ -126,10 +126,10 @@ class PPO_(nn.RL):
         features = (features - tf.reduce_min(features)) / (tf.reduce_max(features) - tf.reduce_min(features) + 1e-8)
         return self.controller(features)
     
-#    def batch_size_fn(self):
-#        if self.batch_counter%777:
-#            return self.adjust_batch_size()
-#        return self.adjust_batch_size()
+    def batch_size_fn(self):
+        if self.batch_counter%777:
+            return self.adjust_batch_size()
+        return self.adjust_batch_size()
     
     def __call__(self,s,a,next_s,r,d):
         a=tf.expand_dims(a,axis=1)
