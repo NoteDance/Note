ae65860363332c4262421dca50705ef4883a7498
NoteDance <notedance@outlook.com>
2025-09-14 16:27:57 +0800

    Update RL_pytorch.py

--- Note/nn/RL_pytorch.py
+++ Note/nn/RL_pytorch.py
@@ -594,7 +594,11 @@ class RL_pytorch:
                                 self.next_state_pool=np.concatenate(self.next_state_pool_list)
                                 self.reward_pool=np.concatenate(self.reward_pool_list)
                                 self.done_pool=np.concatenate(self.done_pool_list)
-                                idx=np.random.choice(self.state_pool.shape[0], size=self.num_updates*self.batch, replace=False)
+                                self.pool_size_=self.num_updates*self.batch
+                                if len(self.state_pool)>=self.pool_size_:
+                                    idx=np.random.choice(self.state_pool.shape[0], size=self.pool_size_, replace=False)
+                                else:
+                                    idx=np.random.choice(self.state_pool.shape[0], size=self.state_pool.shape[0], replace=False)
                                 self.state_pool=self.state_pool[idx]
                                 self.action_pool=self.action_pool[idx]
                                 self.next_state_pool=self.next_state_pool[idx]
@@ -607,7 +611,11 @@ class RL_pytorch:
                                 self.next_state_pool[7]=np.concatenate(self.next_state_pool_list)
                                 self.reward_pool[7]=np.concatenate(self.reward_pool_list)
                                 self.done_pool[7]=np.concatenate(self.done_pool_list)
-                                idx=np.random.choice(self.state_pool.shape[0], size=self.num_updates*self.batch, replace=False)
+                                self.pool_size_=self.num_updates*self.batch
+                                if len(self.state_pool)>=self.pool_size_:
+                                    idx=np.random.choice(self.state_pool[7].shape[0], size=self.pool_size_, replace=False)
+                                else:
+                                    idx=np.random.choice(self.state_pool[7].shape[0], size=self.state_pool[7].shape[0], replace=False)
                                 self.state_pool[7]=self.state_pool[7][idx]
                                 self.action_pool[7]=self.action_pool[7][idx]
                                 self.next_state_pool[7]=self.next_state_pool[7][idx]
