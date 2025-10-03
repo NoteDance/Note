85e33ac754a47b88d1c026113540ab332ca09504
NoteDance <notedance@outlook.com>
2025-10-03 15:20:30 +0800

    Update RL_pytorch.py

--- Note/nn/RL_pytorch.py
+++ Note/nn/RL_pytorch.py
@@ -1246,6 +1246,8 @@ class RL_pytorch:
             for process in process_list:
                 process.join()
             counter+=1
+            if self.state_pool is not None and len(self.state_pool)>=self.batch and counter<self.num_store:
+                    continue
             if self.processes_her==None and self.processes_pr==None:
                 self.state_pool=np.concatenate(self.state_pool_list)
                 self.action_pool=np.concatenate(self.action_pool_list)
