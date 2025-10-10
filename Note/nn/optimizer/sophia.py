0d073d05bcc8d7683fa9d6f1ea311ea14f24b1c0
NoteDance <notedance@outlook.com>
2025-10-10 16:48:15 +0800

    Update sophia.py

--- Note/nn/optimizer/sophia.py
+++ Note/nn/optimizer/sophia.py
@@ -562,8 +562,14 @@ class SophiaH_e(optimizer.Optimizer):
                 
                 if self.trust_ratio:
                     # Layer-wise LR adaptation
-                    w_norm = tf.norm(p, ord=2)
-                    g_norm = tf.norm(update, ord=2)
+                    if self.sn:
+                        reshaped_p = tf.reshape(p, (size // self.subset_size_[self._get_variable_index(p)], self.subset_size_[self._get_variable_index(p)]))
+                        reshaped_update = tf.reshape(update, (size // self.subset_size_[self._get_variable_index(p)], self.subset_size_[self._get_variable_index(p)]))
+                        w_norm = tf.sqrt(tf.reduce_sum(tf.reduce_sum(reshaped_p ** 2, axis=1)))
+                        g_norm = tf.sqrt(tf.reduce_sum(tf.reduce_sum(reshaped_update ** 2, axis=1)))
+                    else:
+                        w_norm = tf.norm(p, ord=2)
+                        g_norm = tf.norm(update, ord=2)
                     trust_ratio = w_norm / g_norm
                     trust_ratio = tf.where(
                         w_norm > 0,
@@ -643,8 +649,14 @@ class SophiaH_e(optimizer.Optimizer):
                     
                     if self.trust_ratio:
                         # Layer-wise LR adaptation
-                        w_norm = tf.norm(p, ord=2)
-                        g_norm = tf.norm(update, ord=2)
+                        if self.sn:
+                            reshaped_p = tf.reshape(p, (size // self.subset_size_[self._get_variable_index(p)], self.subset_size_[self._get_variable_index(p)]))
+                            reshaped_update = tf.reshape(update, (size // self.subset_size_[self._get_variable_index(p)], self.subset_size_[self._get_variable_index(p)]))
+                            w_norm = tf.sqrt(tf.reduce_sum(tf.reduce_sum(reshaped_p ** 2, axis=1)))
+                            g_norm = tf.sqrt(tf.reduce_sum(tf.reduce_sum(reshaped_update ** 2, axis=1)))
+                        else:
+                            w_norm = tf.norm(p, ord=2)
+                            g_norm = tf.norm(update, ord=2)
                         trust_ratio = w_norm / g_norm
                         trust_ratio = tf.where(
                             w_norm > 0,
@@ -1291,8 +1303,14 @@ def _single_tensor_sophiag(params,
             
             if trust_ratio:
                 # Layer-wise LR adaptation
-                w_norm = tf.norm(param, ord=2)
-                g_norm = tf.norm(ratio, ord=2)
+                if sn:
+                    reshaped_p = tf.reshape(param, (size // subset_size_[i], subset_size_[i]))
+                    reshaped_update = tf.reshape(ratio, (size // subset_size_[i], subset_size_[i]))
+                    w_norm = tf.sqrt(tf.reduce_sum(tf.reduce_sum(reshaped_p ** 2, axis=1)))
+                    g_norm = tf.sqrt(tf.reduce_sum(tf.reduce_sum(reshaped_update ** 2, axis=1)))
+                else:
+                    w_norm = tf.norm(param, ord=2)
+                    g_norm = tf.norm(ratio, ord=2)
                 trust_ratio = w_norm / g_norm
                 trust_ratio = tf.where(
                     w_norm > 0,
@@ -1379,8 +1397,14 @@ def _single_tensor_sophiag(params,
                 
                 if trust_ratio:
                     # Layer-wise LR adaptation
-                    w_norm = tf.norm(param, ord=2)
-                    g_norm = tf.norm(ratio, ord=2)
+                    if sn:
+                        reshaped_p = tf.reshape(param, (size // subset_size_[i], subset_size_[i]))
+                        reshaped_update = tf.reshape(ratio, (size // subset_size_[i], subset_size_[i]))
+                        w_norm = tf.sqrt(tf.reduce_sum(tf.reduce_sum(reshaped_p ** 2, axis=1)))
+                        g_norm = tf.sqrt(tf.reduce_sum(tf.reduce_sum(reshaped_update ** 2, axis=1)))
+                    else:
+                        w_norm = tf.norm(param, ord=2)
+                        g_norm = tf.norm(ratio, ord=2)
                     trust_ratio = w_norm / g_norm
                     trust_ratio = tf.where(
                         w_norm > 0,
