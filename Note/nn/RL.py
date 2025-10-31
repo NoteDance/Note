84c6dc2d1a28993c0766433b5a2c8b8ac6495045
NoteDance <notedance@outlook.com>
2025-10-31 16:03:29 +0800

    Add files via upload

--- Note/nn/RL.py
+++ Note/nn/RL.py
@@ -735,9 +735,6 @@ class RL:
     
     
     def initialize_adjusting(self):
-        if hasattr(self, 'original_batch'):
-            self.batch = self.original_batch
-            self.ema_ess = None
         if hasattr(self, 'original_alpha'):
             self.alpha = self.original_alpha
             self.ema_alpha = None
@@ -1872,8 +1869,9 @@ class RL:
         else:
             self.modify_TD()
         counter=0
-        if hasattr(self, 'adjust_flag'):
-            self.initialize_adjusting()
+        if hasattr(self, 'original_batch'):
+            self.batch = self.original_batch
+            self.ema_ess = None
         while True:
             for p in range(self.processes):
                 process=mp.Process(target=self.store_in_parallel,args=(p,lock_list))
@@ -1945,6 +1943,8 @@ class RL:
             else:
                 if len(self.state_pool[7])>=self.batch:
                     break
+        if hasattr(self, 'adjust_flag'):
+            self.initialize_adjusting()
         if self.PR==True:
             if self.PPO:
                 self.prioritized_replay.ratio=np.concat(self.ratio_list, axis=0)
--- Note/nn/RL_pytorch.py
+++ Note/nn/RL_pytorch.py
@@ -596,9 +596,6 @@ class RL_pytorch:
     
     
     def initialize_adjusting(self):
-        if hasattr(self, 'original_batch'):
-            self.batch = self.original_batch
-            self.ema_ess = None
         if hasattr(self, 'original_alpha'):
             self.alpha = self.original_alpha
             self.ema_alpha = None
@@ -1170,8 +1167,9 @@ class RL_pytorch:
         else:
             self.modify_TD()
         counter=0
-        if hasattr(self, 'adjust_flag'):
-            self.initialize_adjusting()
+        if hasattr(self, 'original_batch'):
+            self.batch = self.original_batch
+            self.ema_ess = None
         while True:
             for p in range(self.processes):
                 process=mp.Process(target=self.store_in_parallel,args=(p,lock_list))
@@ -1224,6 +1222,8 @@ class RL_pytorch:
             else:
                 if len(self.state_pool[7])>=self.batch:
                     break
+        if hasattr(self, 'adjust_flag'):
+            self.initialize_adjusting()    
         if self.PR==True:
             if self.PPO:
                 self.prioritized_replay.ratio=np.concat(self.ratio_list, axis=0)
