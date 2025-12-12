692360ceb6a45d0e29cf52fca4956f05c42197e1
NoteDance <notedance@outlook.com>
2025-12-12 23:26:21 +0800

    Update RL.py

--- Note/nn/RL.py
+++ Note/nn/RL.py
@@ -1326,19 +1326,6 @@ class RL:
                         self.TD_list[p]=self.TD_list[p][len(self.state_pool_list[p])-math.ceil(self.pool_size/self.processes):]
                         if self.PPO:
                             self.ratio_list[p]=self.ratio_list[p][len(self.state_pool_list[p])-math.ceil(self.pool_size/self.processes):]
-        
-        
-    def get_trainable_variables(self, optimizer):
-        if self.parallel_store_and_training:
-            if type(optimizer)==list:
-                for i in range(len(optimizer)):
-                    if self.parallel_store_and_training:
-                        if self.share_trainable_variables[7][i] is None and optimizer[i].built==True:
-                            self.share_trainable_variables[7][i] = optimizer[i]._trainable_variables
-            else:
-                if self.parallel_store_and_training:
-                    if self.share_trainable_variables[7] is None and optimizer.built==True:
-                        self.share_trainable_variables[7] = optimizer._trainable_variables    
     
     
     def build_opt(self, optimizer=None):
@@ -1352,9 +1339,8 @@ class RL:
                     else:
                         optimizer.append(self.share_opt_class[7][i]())
                     optimizer.from_config(self.share_opt.config[7][i])
-                    if self.share_trainable_variables[7][i] is not None:
-                        optimizer.build(self.share_trainable_variables[7][i])
-                        optimizer.load_own_variables(self.share_opt_variables[7][i])
+                    optimizer.build(self.param[i])
+                    optimizer.load_own_variables(self.share_opt_variables[7][i])
             else:
                 if self.strategy is not None:
                     with self.strategy.scope():
@@ -1362,17 +1348,16 @@ class RL:
                 else:
                     optimizer = self.share_opt_class[7]()
                 optimizer.from_config(self.share_opt.config[7])
-                if self.share_trainable_variables[7] is not None:
-                    optimizer.build(self.share_trainable_variables[7])
-                    optimizer.load_own_variables(self.share_opt_variables[7])
+                optimizer.build(self.param)
+                optimizer.load_own_variables(self.share_opt_variables[7])
             return optimizer
         else:
             if type(optimizer)==list:
                 for i in range(len(optimizer)):
-                    optimizer.build(self.share_trainable_variables[7][i])
+                    optimizer.build(self.param[i])
                     optimizer.load_own_variables(self.share_opt_variables[7][i])
             else:
-                optimizer.build(self.share_trainable_variables[7])
+                optimizer.build(self.param)
                 optimizer.load_own_variables(self.share_opt_variables[7])
     
     
@@ -1401,7 +1386,6 @@ class RL:
                         return self.train_loss.result().numpy()
                 if self.num_updates!=None and self.batch_counter%self.num_updates==0:
                     break
-                self.get_trainable_variables(optimizer)
                 if self.parallel_store_and_training:
                     if type(optimizer)==list:
                         for i in range(len(optimizer)):
@@ -1810,7 +1794,6 @@ class RL:
                             return (total_loss / num_batches).numpy()
                         if self.num_updates!=None and self.batch_counter%self.num_updates==0:
                             break
-                        self.get_trainable_variables(optimizer)
                         for callback in self.callbacks:
                             if hasattr(callback, 'on_batch_begin'):
                                 callback.on_batch_begin(batch, logs={})
@@ -1890,7 +1873,6 @@ class RL:
                         return self.train_loss.result().numpy() 
                     if self.num_updates!=None and self.batch_counter%self.num_updates==0:
                         break
-                    self.get_trainable_variables(optimizer)
                     for callback in self.callbacks:
                         if hasattr(callback, 'on_batch_begin'):
                             callback.on_batch_begin(batch, logs={})
@@ -2460,12 +2442,10 @@ class RL:
                 self.share_TD=manager.dict()
             if type(self.optimizer)==list:
                 self.share_opt_class[7]=[opt.__class__ for opt in self.optimizer]
-                self.share_trainable_variables[7]=[None for _ in self.optimizer]
                 self.share_opt.config[7]=[opt.get_config() for opt in self.optimizer]
                 self.share_opt_variables[7]=[None for _ in self.optimizer]
             else:
                 self.share_opt_class[7]=self.optimizer.__class__
-                self.share_trainable_variables[7]=None
                 self.share_opt.config[7]=self.optimizer.get_config()
                 self.share_opt_variables[7]=None
             self.num_store=mp.Value('i',self.num_store)
@@ -2768,12 +2748,10 @@ class RL:
                 self.share_TD=manager.dict()
             if type(self.optimizer)==list:
                 self.share_opt_class[7]=[opt.__class__ for opt in self.optimizer]
-                self.share_trainable_variables[7]=[None for _ in self.optimizer]
                 self.share_opt.config[7]=[opt.get_config() for opt in self.optimizer]
                 self.share_opt_variables[7]=[None for _ in self.optimizer]
             else:
                 self.share_opt_class[7]=self.optimizer.__class__
-                self.share_trainable_variables[7]=None
                 self.share_opt.config[7]=self.optimizer.get_config()
                 self.share_opt_variables[7]=None
             self.num_store=mp.Value('i',self.num_store)
