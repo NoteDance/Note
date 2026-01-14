66305badb711135c2f0b907ab0abbe51d0022e2a
NoteDance <notedance@outlook.com>
2026-01-14 16:55:23 +0800

    Add files via upload

---
+++ Note/DL/dl/prioritized_replay.py
@@ -0,0 +1,25 @@
+import tensorflow as tf
+import numpy as np
+
+
+class pr:
+    def __init__(self):
+        self.loss=None
+        self.index=None
+    
+    
+    def sample(self,train_data,train_labels,alpha,batch):
+        prios=(self.loss+1e-7)**alpha
+        p=prios/tf.reduce_sum(prios)
+        self.index=np.random.choice(np.arange(len(train_data)),size=[batch],p=p.numpy(),replace=False)
+        self.batch=batch
+        return train_data[self.index],train_labels[self.index]
+    
+    
+    def update(self,loss=None):
+        if loss is not None:
+            loss=tf.cast(loss,tf.float32)
+            self.loss_[:self.batch].assign(loss)
+        else:
+            self.loss[self.index]=tf.abs(self.loss_[:self.batch])
+        return
