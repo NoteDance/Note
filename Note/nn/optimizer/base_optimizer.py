b7eb28d791ce9fe4e848a400036fc52925dee2c4
NoteDance <notedance@outlook.com>
2026-04-11 18:13:38 +0800

    Update base_optimizer.py

--- Note/nn/optimizer/base_optimizer.py
+++ Note/nn/optimizer/base_optimizer.py
@@ -640,7 +640,7 @@ class BaseOptimizer(KerasSaveable):
             grads[self._get_variable_index(p)] = tf.reshape(g_ortho_scaled, original_shape)
     
     def apply_weight_decay(self, variable, gradient, lr):
-        if self.weight_decouple:
+        if hasattr(self, 'weight_decouple') and self.weight_decouple:
             variable.assign(variable * (1.0 - self.weight_decay * (1.0 if self.fixed_decay else lr)))
         elif self.weight_decay > 0.0:
             gradient += variable * self.weight_decay
