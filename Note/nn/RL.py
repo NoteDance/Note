31c8437977b665abbef645c28e1871937a049d8d
NoteDance <notedance@outlook.com>
2026-04-29 15:04:26 +0800

    Update RL.py

--- Note/nn/RL.py
+++ Note/nn/RL.py
@@ -354,21 +354,12 @@ class RL:
             for p in nest.flatten(self.param):
                 fixed_bytes += p.numpy().nbytes
         
+        param_shm_bytes = 0
+        for p in nest.flatten(self.shared_param):
+            param_shm_bytes += p.numpy().nbytes
+        
         if parallel_dump:
-            param_shm_bytes = 0
-            if type(self.param) == list:
-                for p in nest.flatten(self.param[0]):
-                    param_shm_bytes += p.numpy().nbytes
-                param_shm_bytes += fixed_bytes
-            else:
-                param_shm_bytes = fixed_bytes * 2
-        else:
-            param_shm_bytes = 0
-            if type(self.param) == list:
-                for p in nest.flatten(self.param[0]):
-                    param_shm_bytes += p.numpy().nbytes
-            else:
-                param_shm_bytes = fixed_bytes
+            fixed_bytes = fixed_bytes * 2
         
         if self.PR:
             fixed_bytes += self.pool_size * 4
@@ -386,7 +377,10 @@ class RL:
                 bytes_per_exp += 4
         
         batch = getattr(self, 'batch', 256)
-        var_per_process_bytes = batch * 4 * (2 if self.PPO else 1)
+        if hasattr(self, 'build'):
+            var_per_process_bytes = batch * 4 * (2 if self.PPO else 1) + param_shm_bytes
+        elif hasattr(self, 'build_'):
+            var_per_process_bytes = batch * 4 * (2 if self.PPO else 1)
         per_process_overhead_bytes = int(135 * 1024 * 1024)
         
         best_processes = 1
@@ -2693,7 +2687,7 @@ class RL:
                 train_loss.reset_states()
                 if pool_network==True:
                     if parallel_store_and_training:
-                        if hasattr(self, 'build'):
+                        if hasattr(self, 'build') or hasattr(self, 'build_'):
                             self.shm_metadata = []
                             active_shms = []
                             for param in self.shared_param:
@@ -2783,7 +2777,7 @@ class RL:
                 train_loss.reset_states()
                 if pool_network==True:
                     if parallel_store_and_training:
-                        if hasattr(self, 'build'):
+                        if hasattr(self, 'build') or hasattr(self, 'build_'):
                             self.shm_metadata = []
                             active_shms = []
                             for param in self.shared_param:
@@ -3034,7 +3028,7 @@ class RL:
                             callback.on_episode_begin(i, logs={})
                     if pool_network==True:
                         if parallel_store_and_training:
-                            if hasattr(self, 'build'):
+                            if hasattr(self, 'build') or hasattr(self, 'build_'):
                                 self.shm_metadata = []
                                 active_shms = []
                                 for param in self.shared_param:
@@ -3123,7 +3117,7 @@ class RL:
                             callback.on_episode_begin(i, logs={})
                     if pool_network==True:
                         if parallel_store_and_training:
-                            if hasattr(self, 'build'):
+                            if hasattr(self, 'build') or hasattr(self, 'build_'):
                                 self.shm_metadata = []
                                 active_shms = []
                                 for param in self.shared_param:
@@ -3215,7 +3209,7 @@ class RL:
                             callback.on_episode_begin(i, logs={})
                     if pool_network==True:
                         if parallel_store_and_training:
-                            if hasattr(self, 'build'):
+                            if hasattr(self, 'build') or hasattr(self, 'build_'):
                                 self.shm_metadata = []
                                 active_shms = []
                                 for param in self.shared_param:
@@ -3312,7 +3306,7 @@ class RL:
                             callback.on_episode_begin(i, logs={})
                     if pool_network==True:
                         if parallel_store_and_training:
-                            if hasattr(self, 'build'):
+                            if hasattr(self, 'build') or hasattr(self, 'build_'):
                                 self.shm_metadata = []
                                 active_shms = []
                                 for param in self.shared_param:
@@ -3407,7 +3401,7 @@ class RL:
                             callback.on_episode_begin(i, logs={})
                     if pool_network==True:
                         if parallel_store_and_training:
-                            if hasattr(self, 'build'):
+                            if hasattr(self, 'build') or hasattr(self, 'build_'):
                                 self.shm_metadata = []
                                 active_shms = []
                                 for param in self.shared_param:
@@ -3824,6 +3818,9 @@ class RL:
         output_file=open(path,'wb')
         param=self.param
         self.param=None
+        if hasattr(self, 'build') or hasattr(self, 'build_'):
+            shared_param=self.shared_param
+        self.shared_param=None
         if type(self.optimizer)==list:
             opt_config=[opt.get_config() for opt in self.optimizer]
         else:
@@ -3833,6 +3830,7 @@ class RL:
         self.optimizer=None
         pickle.dump(self,output_file)
         self.param=param
+        self.shared_param=shared_param
         self.optimizer=optimizer
         output_file.close()
         return
