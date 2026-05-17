81eb419ea497422a6bbd76667be6f0534c7ad52b
NoteDance <notedance@outlook.com>
2026-05-17 13:40:12 +0800

    Update replace_with_array.py

--- Note/nn/replace_with_array.py
+++ Note/nn/replace_with_array.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+import torch
 
 
 def replace_with_array(model, shared_params):
@@ -24,3 +25,12 @@ def replace_with_array_k(model: tf.keras.Model, shared_params: list):
             attr_name = var_to_attr.get(id(var))
             if attr_name is not None:
                 object.__setattr__(layer, attr_name, arr)
+
+
+def replace_with_array_p(model: torch.nn.Module, shared_params: list):
+    shared_iter = iter(shared_params)
+    for module in model.modules():
+        for attr_name in list(module._parameters):
+            if module._parameters[attr_name] is not None:
+                arr = next(shared_iter)
+                object.__setattr__(module, attr_name, torch.from_numpy(arr))
