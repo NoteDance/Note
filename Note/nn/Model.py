516362acba258d6588e232fd8090f84ec97f4b64
NoteDance <notedance@outlook.com>
2026-06-09 01:10:29 +0800

    Update Model.py

--- Note/nn/Model.py
+++ Note/nn/Model.py
@@ -417,12 +417,11 @@ class Model:
             gradients = tape.gradient(loss, self.param)
             optimizer.apply_gradients(zip(gradients, self.param))
         else:
-            if type(loss) == list:
-                loss_list = loss
             for i in range(len(optimizer)):
                 if type(loss) == list:
-                    loss = loss_list[i]
-                gradients = tape.gradient(loss, self.param[i])
+                    gradients = tape.gradient(loss[i], self.param[i])
+                else:
+                    gradients = tape.gradient(loss, self.param[i])
                 optimizer[i].apply_gradients(zip(gradients, self.param[i]))
         if hasattr(self, 'soft_update'):
             self.soft_update()
@@ -444,12 +443,11 @@ class Model:
             gradients = tape.gradient(loss, self.param)
             optimizer.apply_gradients(zip(gradients, self.param))
         else:
-            if type(loss) == list:
-                loss_list = loss
             for i in range(len(optimizer)):
                 if type(loss) == list:
-                    loss = loss_list[i]
-                gradients = tape.gradient(loss, self.param[i])
+                    gradients = tape.gradient(loss[i], self.param[i])
+                else:
+                    gradients = tape.gradient(loss, self.param[i])
                 optimizer[i].apply_gradients(zip(gradients, self.param[i]))
         if hasattr(self, 'soft_update'):
             self.soft_update()
@@ -493,12 +491,11 @@ class Model:
             gradients = tape.gradient(loss, self.param)
             optimizer.apply_gradients(zip(gradients, self.param))
         else:
-            if type(loss) == list:
-                loss_list = loss
             for i in range(len(optimizer)):
                 if type(loss) == list:
-                    loss = loss_list[i]
-                gradients = tape.gradient(loss, self.param[i])
+                    gradients = tape.gradient(loss[i], self.param[i])
+                else:
+                    gradients = tape.gradient(loss, self.param[i])
                 optimizer[i].apply_gradients(zip(gradients, self.param[i]))
         if hasattr(self, 'soft_update'):
             self.soft_update()
