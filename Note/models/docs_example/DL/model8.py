5aa49605b12c46f7008cf168dae1732f8530f10b
NoteDance <notedance@outlook.com>
2026-07-05 01:02:12 +0800

    Add files via upload

--- Note/models/docs_example/DL/model8.py
+++ Note/models/docs_example/DL/model8.py
@@ -29,7 +29,7 @@ class Model_new(nn.Model):
 
 
 class Model(nn.Model):
-    def __init__(self, input_dim: int, n_train_samples: int, trained_param, kl_threshold: float):
+    def __init__(self, input_dim: int, n_train_samples: int, trained_param, kl_threshold1: float, kl_threshold2: float):
         self.Model_trained = Model_trained(input_dim, n_train_samples)
         nn.assign_param(self.Model_trained.param, trained_param)
         self.Model_new = Model_new(input_dim, n_train_samples + 1)
@@ -40,7 +40,8 @@ class Model(nn.Model):
         self.param = [self.Model_new.param, [self.Model_new.param[-2][:, -1:], self.Model_new.param[-1][-1:]]]
         self.svd_k = tf.Variable(7)
         self.sv_threshold = 1e-7
-        self.kl_threshold = tf.constant(kl_threshold, dtype=tf.float32)
+        self.kl_threshold1 = tf.constant(kl_threshold1, dtype=tf.float32)
+        self.kl_threshold2 = tf.constant(kl_threshold2, dtype=tf.float32)
 
     # ------------------------------------------------------------------
     def __call__(self, x):
@@ -74,11 +75,15 @@ class Model(nn.Model):
 
         kl_per_sample_detached = tf.stop_gradient(kl_per_sample)
 
-        mask = tf.cast(kl_per_sample_detached > self.kl_threshold, kl_per_sample.dtype)
+        mask = tf.cast(kl_per_sample_detached > self.kl_threshold1, kl_per_sample.dtype)
         gated_kl_per_sample = kl_per_sample_detached + mask * (kl_per_sample - kl_per_sample_detached)  # [B]
 
         kl = tf.reduce_mean(gated_kl_per_sample)
         kl_weight = tf.reduce_mean(kl_per_sample_detached)                           # pure scaling factor, no gradient
+        if kl_weight < self.kl_threshold2:
+            kl_weight = 0
+        else:
+            kl_weight = 1
 
         # ----------------------------------------------------------------
         # Parameter difference norm penalty (gradients flow only through Model_new.param)
--- Note/models/docs_example/DL/model9.py
+++ Note/models/docs_example/DL/model9.py
@@ -42,7 +42,7 @@ class Model_new(nn.Model):
 
 
 class Model(nn.Model):
-    def __init__(self, input_dim: int, n_train_samples: int, trained_param, kl_threshold: float):
+    def __init__(self, input_dim: int, n_train_samples: int, trained_param, kl_threshold1: float, kl_threshold2: float):
         self.Model_trained = Model_trained(input_dim, n_train_samples)
         nn.assign_param(self.Model_trained.param, trained_param)
         self.Model_new = Model_new(input_dim, n_train_samples + 1)
@@ -53,7 +53,8 @@ class Model(nn.Model):
         self.param = [self.Model_new.param, [self.Model_new.param[-2][:, -1:], self.Model_new.param[-1][-1:]]]
         self.svd_k = tf.Variable(7)
         self.sv_threshold = 1e-7
-        self.kl_threshold = tf.constant(kl_threshold, dtype=tf.float32)
+        self.kl_threshold1 = tf.constant(kl_threshold1, dtype=tf.float32)
+        self.kl_threshold2 = tf.constant(kl_threshold2, dtype=tf.float32)
 
     # ------------------------------------------------------------------
     def __call__(self, x):
@@ -92,6 +93,10 @@ class Model(nn.Model):
 
         kl = tf.reduce_mean(gated_kl_per_sample)
         kl_weight = tf.reduce_mean(kl_per_sample_detached)      # pure scaling factor, no gradient
+        if kl_weight < self.kl_threshold2:
+            kl_weight = 0
+        else:
+            kl_weight = 1
 
         # ----------------------------------------------------------------
         # Parameter difference norm penalty (gradients flow only through Model_new.param)
