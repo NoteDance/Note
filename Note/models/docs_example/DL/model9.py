b6e0b4d09dd48714f238c3eb9efcbd6f474dd54a
NoteDance <notedance@outlook.com>
2026-07-04 17:55:23 +0800

    Add files via upload

--- Note/models/docs_example/DL/model9.py
+++ Note/models/docs_example/DL/model9.py
@@ -91,8 +91,7 @@ class Model(nn.Model):
         gated_kl_per_sample = kl_per_sample_detached + mask * (kl_per_sample - kl_per_sample_detached)  # [B]
 
         kl = tf.reduce_mean(gated_kl_per_sample)
-        mask_ratio = tf.reduce_mean(mask)
-        kl_weight = tf.reduce_mean(kl_per_sample_detached) * mask_ratio      # pure scaling factor, no gradient
+        kl_weight = tf.reduce_mean(kl_per_sample_detached)      # pure scaling factor, no gradient
 
         # ----------------------------------------------------------------
         # Parameter difference norm penalty (gradients flow only through Model_new.param)
