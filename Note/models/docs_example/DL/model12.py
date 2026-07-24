f7b73bfd3a7cd4b9680b0ee910af11603c879269
NoteDance <notedance@outlook.com>
2026-07-24 15:59:00 +0800

    Update model12.py

--- Note/models/docs_example/DL/model12.py
+++ Note/models/docs_example/DL/model12.py
@@ -39,6 +39,7 @@ class Model(nn.Model):
         self.Model_new.param[-1][:-1].assign(trained_param[-1])
         self.new_class_batch_size = 64
         self.param = [self.Model_new.param, [self.Model_new.param[-2][:, -1:], self.Model_new.param[-1][-1:]]]
+        self.init_priority = 1.0
         self.svd_k = tf.Variable(7)
         self.sv_threshold = 1e-7
         self.kl_threshold = tf.constant(kl_threshold1, dtype=tf.float32)
