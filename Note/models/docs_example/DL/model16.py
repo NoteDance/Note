e11092a6c8219622f3d8a3d254d12446b69f00ed
NoteDance <notedance@outlook.com>
2026-08-14 18:27:11 +0800

    Update model16.py

--- Note/models/docs_example/DL/model16.py
+++ Note/models/docs_example/DL/model16.py
@@ -147,6 +147,7 @@ class Model(nn.Model):
             dist_param_col = tf.norm(approx_param * approx_param, axis=0)
             dist_copy_col = tf.norm(approx_copy * approx_copy, axis=0)
 
+            # After transforming the original problem into an augmented Lagrangian problem, the method of parameter tuning has changed.
             diff_norm = tf.reduce_mean((u_param_k - u_copy_k + self.z[i] / 2 * self.u[i])**2)
             diff_norm = self.u[i] * diff_norm
             self.update_z_u(diff_norm, i)
